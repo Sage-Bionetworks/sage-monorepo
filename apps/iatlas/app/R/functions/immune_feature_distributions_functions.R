595f000d57761a0264a184d07fa704a3b63392a9
andrewelamb <andrewelamb@gmail.com>
2020-02-05 11:24:31 -0800

    immune features working

--- apps/iatlas/app/R/functions/immune_feature_distributions_functions.R
+++ apps/iatlas/app/R/functions/immune_feature_distributions_functions.R
@@ -3,7 +3,7 @@ build_distplot_tbl <- function(tbl, id, scale_method){
     id %>%
         get_feature_values_from_ids() %>%
         dplyr::inner_join(tbl, by = "sample_id") %>%
-        dplyr::select(-sample_id, sample_name) %>%
+        dplyr::select(group, value) %>%
         scale_db_connection(scale_method) %>%
         dplyr::rename(x = group, y = value)
 }
