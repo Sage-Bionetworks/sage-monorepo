9dc13d320e78ef2c2a8c511d36882c1435873305
andrewelamb <andrewelamb@gmail.com>
2020-02-11 08:59:04 -0800

    datainfo module now working

--- apps/iatlas/app/R/modules/ui/other_modules/data_info_ui.R
+++ apps/iatlas/app/R/modules/ui/other_modules/data_info_ui.R
@@ -1,18 +1,17 @@
 data_info_ui <- function(id) {
     ns <- shiny::NS(id)
-    
+
     shiny::tagList(
         .GlobalEnv$titleBox("iAtlas Explorer — Data Description"),
         .GlobalEnv$textBox(
             width = 12,
-            p("Each row in the table corresponds to a variable for which data are available for exploration in CRI iAtlas. Variables are organized into classes, as displayed in the Variable Class column. In a number of iAtlas modules the Variable Class will appear in drop-down selections for variables.")  
+            shiny::includeMarkdown("markdown/data_info.markdown")
         ),
-        sectionBox(
+        .GlobalEnv$sectionBox(
             title = "PanImmune Readouts",
             .GlobalEnv$messageBox(
                 width = 12,
-                p("Select a row in the table to view more details about variables in the same class."),
-                p("Use the class selector to filter on the Variable class column")
+                shiny::includeMarkdown("markdown/data_info_readouts.markdown")
             ),
             shiny::fluidRow(
                 .GlobalEnv$optionsBox(
@@ -22,14 +21,14 @@ data_info_ui <- function(id) {
                 .GlobalEnv$tableBox(
                     width = 12,
                     shiny::div(
-                        'feature_table' %>% 
-                            ns() %>% 
-                            DT::dataTableOutput() %>% 
+                        'feature_table' %>%
+                            ns() %>%
+                            DT::dataTableOutput() %>%
                             shinycssloaders::withSpinner()
                     )
                 )
             )
         ),
-        shiny::uiOutput(ns("variable_details_section")) 
+        shiny::uiOutput(ns("variable_details_section"))
     )
 }
