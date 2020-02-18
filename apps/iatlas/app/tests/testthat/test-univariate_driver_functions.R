5d233aa0decc06568985b7370d858187ec9163a9
andrewelamb <andrewelamb@gmail.com>
2020-02-18 14:37:18 -0800

    clean up some bugs

--- apps/iatlas/app/tests/testthat/test-univariate_driver_functions.R
+++ apps/iatlas/app/tests/testthat/test-univariate_driver_functions.R
@@ -1,11 +1,13 @@
-test_that("Build Univariate Driver Results Tibble", {
-    result1 <- build_udr_results_tbl("Immune Subtype", 2, 50, 50)
-    expect_named(
-        result1,
-        c("p_value", "fold_change", "log10_p_value", "gene_id", "tag_id",
-          "log10_fold_change", "mutation_code_id", "gene", "mutation_code",
-          "group", "label"
+with_test_db_env({
+    test_that("Build Univariate Driver Results Tibble", {
+        result1 <- build_udr_results_tbl("Immune Subtype", 2, 50, 50)
+        expect_named(
+            result1,
+            c("p_value", "fold_change", "log10_p_value", "gene_id", "tag_id",
+              "log10_fold_change", "mutation_code_id", "gene", "mutation_code",
+              "group", "label"
+            )
         )
-    )
-    # expect_identical(result1, tidyr::drop_na(result1))
+        # expect_identical(result1, tidyr::drop_na(result1))
+    })
 })
