92413ae2fb22698f0162c7864385d1bbe2d527b3
andrewelamb <andrewelamb@gmail.com>
2020-02-24 07:21:16 -0800

    separated driver fucntiosn into spearate files

--- apps/iatlas/app/tests/testthat/test-univariate_driver_functions.R
+++ apps/iatlas/app/tests/testthat/test-univariate_driver_functions.R
@@ -1,6 +1,6 @@
 with_test_db_env({
     test_that("Build Univariate Driver Results Tibble", {
-        result1 <- build_udr_results_tbl("Immune Subtype", 2, 50, 50)
+        result1 <- build_ud_results_tbl("Immune Subtype", 2, 50, 50)
         expect_named(
             result1,
             c("p_value", "fold_change", "log10_p_value", "gene_id", "tag_id",
@@ -10,8 +10,8 @@ with_test_db_env({
         )
     })
 
-    test_that("Build Driver Violin Tibble", {
-        result1 <- build_driver_violin_tbl(1, 1, 1, 1)
+    test_that("Build Univariate Driver Violin Tibble", {
+        result1 <- build_ud_driver_violin_tbl(1, 1, 1, 1)
         expect_named(result1, c("x", "y"))
     })
 })
