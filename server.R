2fadc91efebfe8b80faeb5a12725b0e27b2ab084
andrewelamb <andrewelamb@gmail.com>
2020-02-27 13:54:00 -0800

    cleaned up server.r

--- server.R
+++ server.R
@@ -21,67 +21,33 @@ shiny::shinyServer(function(input, output, session) {
         }
     })
 
-    call_iatlas_module <- function(
-        file,
-        ...,
-        observe_event = T,
-        iatlas_tab = "explorertabs"
-    ){
-        function_string <- stringr::str_remove(basename(file), ".R")
-        module_string   <- stringr::str_remove(basename(file), "_server.R")
-        link_string     <- paste0("link_to_", module_string)
-        source(file, local = T)
-        if (observe_event) {
-            shiny::observeEvent(input[[link_string]], {
-                shinydashboard::updateTabItems(
-                    session,
-                    iatlas_tab,
-                    module_string
-                )
-            })
-        }
-        shiny::callModule(
-            get(function_string),
-            module_string,
-            ...
-        )
-    }
+    source("R/functions/call_iatlas_module.R")
 
     # Non analysis modules -----------------------------------------------------
 
     cohort_obj <- call_iatlas_module(
-        "R/modules/server/other_modules/cohort_selection_server.R"
+        "R/modules/server/other_modules/cohort_selection_server.R",
+        input,
+        session
     )
 
     call_iatlas_module(
         "R/modules/server/other_modules/data_info_server.R",
+        input,
+        session,
         observe_event = F
     )
 
     # Analysis modules --------------------------------------------------------
-    analysis_module_dir   <- "R/modules/server/analysis_modules/"
-    analysis_module_files <- list.files(analysis_module_dir, full.names = T)
-    purrr::walk(analysis_module_files, call_iatlas_module, cohort_obj)
-
+    "R/modules/server/analysis_modules/" %>%
+        list.files(full.names = T) %>%
+        purrr::walk(call_iatlas_module, input, session, cohort_obj)
 
     # Tool modules --------------------------------------------------------
 
-    tool_module_dir   <- "R/modules/server/tool_modules/"
-    tool_module_files <- list.files(tool_module_dir, full.names = T)
-    for (item in tool_module_files) {
-        source(item, local = T)
-    }
-
-    shiny::callModule(
-        immune_subtype_classifier_server,
-        "immune_subtype_classifier"
-    )
-
-
-    shiny::observeEvent(input$link_to_immune_subtype_classifier, {
-        updateNavlistPanel(session, "toolstabs", "Immune Subtype Classifier")
-    })
-
+    "R/modules/server/tool_modules/" %>%
+        list.files(full.names = T) %>%
+        purrr::walk(call_iatlas_module, input, session, tab_id = "toolstabs")
 })
 
 
