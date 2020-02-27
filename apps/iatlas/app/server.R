5b0d69959a005666c479240a26525ed90b8a15c3
andrewelamb <andrewelamb@gmail.com>
2020-02-27 11:59:43 -0800

    simplified calling modules on the sever side

--- apps/iatlas/app/server.R
+++ apps/iatlas/app/server.R
@@ -21,118 +21,50 @@ shiny::shinyServer(function(input, output, session) {
         }
     })
 
-    source(
-        "R/modules/server/other_modules/cohort_selection_server.R",
-        local = T
+    call_iatlas_module <- function(
+        file,
+        ...,
+        observe_event = T,
+        iatlas_tab = "explorertabs"
+    ){
+        function_string <- stringr::str_remove(basename(file), ".R")
+        module_string   <- stringr::str_remove(basename(file), "_server.R")
+        link_string     <- paste0("link_to_", module_string)
+        source(file, local = T)
+        if (observe_event) {
+            shiny::observeEvent(input[[link_string]], {
+                shinydashboard::updateTabItems(
+                    session,
+                    iatlas_tab,
+                    module_string
+                )
+            })
+        }
+        shiny::callModule(
+            get(function_string),
+            module_string,
+            ...
+        )
+    }
+
+    # Non analysis modules -----------------------------------------------------
+
+    cohort_obj <- call_iatlas_module(
+        "R/modules/server/other_modules/cohort_selection_server.R"
     )
 
-    cohort_obj <- shiny::callModule(
-        cohort_selection_server,
-        "cohort_selection"
+    call_iatlas_module(
+        "R/modules/server/other_modules/data_info_server.R",
+        observe_event = F
     )
 
-
-    # analysis modules --------------------------------------------------------
+    # Analysis modules --------------------------------------------------------
     analysis_module_dir   <- "R/modules/server/analysis_modules/"
     analysis_module_files <- list.files(analysis_module_dir, full.names = T)
-    for (item in analysis_module_files) {
-        source(item, local = T)
-    }
-
-    # output$cohort_group_text <- shiny::renderText(
-    #     paste("Selected Group: ", cohort_cons()$group_name)
-    # )
-
-    shiny::callModule(
-        tumor_microenvironment_server,
-        "tumor_microenvironment",
-        cohort_obj
-    )
-
-    shiny::callModule(
-        immune_features_server,
-        "immune_features",
-        cohort_obj
-    )
-
-    shiny::callModule(
-        til_maps_server,
-        "til_maps",
-        cohort_obj
-    )
-
-    shiny::callModule(
-        immunomodulators_server,
-        "immunomodulators",
-        cohort_obj
-    )
-
-    shiny::callModule(
-        clinical_outcomes_server,
-        "clinical_outcomes",
-        cohort_obj
-    )
-
-    shiny::callModule(
-        io_targets_server,
-        "io_targets",
-        cohort_obj
-    )
-
-    shiny::callModule(
-        driver_associations_server,
-        "driver_associations",
-        cohort_obj
-    )
-
-    shiny::observeEvent(input$link_to_tumor_microenvironment, {
-        shinydashboard::updateTabItems(session, "explorertabs", "tumor_microenvironment")
-    })
-
-    shiny::observeEvent(input$link_to_cohort_selection, {
-        shinydashboard::updateTabItems(session, "explorertabs", "cohort_selection")
-    })
-
-    shiny::observeEvent(input$link_to_clinical_outcomes, {
-        shinydashboard::updateTabItems(session, "explorertabs", "clinical_outcomes")
-    })
-
-    shiny::observeEvent(input$link_to_immunomodulators, {
-        shinydashboard::updateTabItems(session, "explorertabs", "immunomodulators")
-    })
-
-    shiny::observeEvent(input$link_to_immune_features, {
-        shinydashboard::updateTabItems(session, "explorertabs", "immune_features")
-    })
-
-    shiny::observeEvent(input$link_to_driver_associations, {
-        shinydashboard::updateTabItems(session, "explorertabs", "driver_associations")
-    })
-
-    shiny::observeEvent(input$link_to_til_maps, {
-        shinydashboard::updateTabItems(session, "explorertabs", "til_maps")
-    })
-
-    shiny::observeEvent(input$link_to_io_targets, {
-        shinydashboard::updateTabItems(session, "explorertabs", "io_targets")
-    })
+    purrr::walk(analysis_module_files, call_iatlas_module, cohort_obj)
 
 
-
-    # other modules --------------------------------------------------------
-
-    other_module_dir   <- "R/modules/server/other_modules/"
-    other_module_files <- list.files(other_module_dir, full.names = T)
-    for (item in other_module_files) {
-        source(item, local = T)
-    }
-
-    shiny::callModule(
-        data_info_server,
-        "data_info"
-    )
-
-    # tool modules --------------------------------------------------------
+    # Tool modules --------------------------------------------------------
 
     tool_module_dir   <- "R/modules/server/tool_modules/"
     tool_module_files <- list.files(tool_module_dir, full.names = T)
@@ -142,7 +74,8 @@ shiny::shinyServer(function(input, output, session) {
 
     shiny::callModule(
         immune_subtype_classifier_server,
-        "immune_subtype_classifier")
+        "immune_subtype_classifier"
+    )
 
 
     shiny::observeEvent(input$link_to_immune_subtype_classifier, {
