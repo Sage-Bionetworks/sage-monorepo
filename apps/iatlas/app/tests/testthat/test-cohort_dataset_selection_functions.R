8f75cd75a04caa94cad76f18b260fa104ef30525
andrewelamb <andrewelamb@gmail.com>
2020-03-12 10:52:34 -0700

    added tests

---
+++ apps/iatlas/app/tests/testthat/test-cohort_dataset_selection_functions.R
@@ -0,0 +1,16 @@
+with_test_db_env({
+    test_that("Create Cohort Module String", {
+        tbl1 <- dplyr::tibble(
+            dataset = c(rep("DS1", 5), rep("DS2", 3)),
+            module = c(paste0("M", 1:5), paste0("M", 1:3))
+        )
+        expect_equal(
+            create_cohort_module_string(tbl1, "DS1"),
+            "Modules available for dataset DS1: M1, M2, M3, M4, M5"
+        )
+        expect_equal(
+            create_cohort_module_string(tbl1, "DS2"),
+            "Modules available for dataset DS2: M1, M2, M3"
+        )
+    })
+})
