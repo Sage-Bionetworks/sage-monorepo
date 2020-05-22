af7524a6e81323175c1d370e3bdaae3060e00772
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-05-22 12:52:34 -0700

    patch/improvement: Open GraphiQL when the container builds.

--- apps/iatlas/api-gitlab/start.sh
+++ apps/iatlas/api-gitlab/start.sh
@@ -21,5 +21,24 @@ else
     docker-compose up -d
 fi
 
+check_status() {
+    status_code=$(curl --write-out %{http_code} --silent --output /dev/null localhost:${PORT}/graphiql)
+    if [[ ${iterator} -lt 35 && ${status_code} -eq 200 || ${status_code} -eq 302 || ${status_code} -eq 400 ]]
+    then
+        >&2 echo -e "${GREEN}GraphiQL is Up at localhost:${PORT}/graphiql${NC}"
+        open http://localhost:${PORT}/graphiql
+    elif [[ ${iterator} -eq 35 ]]
+    then
+        >&2 echo -e "${YELLOW}Did not work :(${NC}"
+    else
+        sleep 1
+        ((iterator++))
+        check_status
+    fi
+}
+
+iterator=0
+check_status
+
 # Open a command line prompt in the container.
 docker exec -ti iatlas-api-dev bash
