d10e207e47e0a4737f24cc9c1a642e0ce55adedd
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-05-26 20:03:39 +0000

    patch/refactor: [#173019143] Moved example query to its own file.

--- apps/iatlas/api-gitlab/flaskr/app.py
+++ apps/iatlas/api-gitlab/flaskr/app.py
@@ -9,27 +9,6 @@ from os import getenv
 app = Flask(__name__)
 app.debug = True
 
-example_query = """
-{
-  allEmployees(sort: [NAME_ASC, ID_ASC]) {
-    edges {
-      node {
-        id
-        name
-        department {
-          id
-          name
-        }
-        role {
-          id
-          name
-        }
-      }
-    }
-  }
-}
-"""
-
 app.add_url_rule(
     '/graphiql',
     view_func=GraphQLView.as_view(
