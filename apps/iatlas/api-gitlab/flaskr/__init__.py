0b66bddb225aa77d5a2ee55059a78ff35aacd81a
Thatcher Hubbard <26883046+thatcherhubbard@users.noreply.github.com>
2020-05-27 20:37:57 +0000

    Working DB connection and single model created for POC

--- apps/iatlas/api-gitlab/flaskr/__init__.py
+++ apps/iatlas/api-gitlab/flaskr/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+from flaskr import db_models
 from flaskr import routes
