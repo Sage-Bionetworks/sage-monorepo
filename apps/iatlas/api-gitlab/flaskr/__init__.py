80726fc570f5aca036ae8f504b2026d8cdeb9f7f
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-05-27 23:57:49 +0000

    patch/refactor: [#173019143] Created default values for db connection. Created stub db_models.

--- apps/iatlas/api-gitlab/flaskr/__init__.py
+++ apps/iatlas/api-gitlab/flaskr/__init__.py
@@ -2,9 +2,21 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 
+POSTGRES = {
+    'user': os.environ['PG_USER'],
+    'pw': os.environ['PG_PW'],
+    'db': os.environ['PG_DATABASE'],
+    'host': os.environ['PG_HOST'],
+    'port': os.environ['PG_PORT']
+}
+DATABASE_URI = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+if 'DATABASE_URI' in os.environ:
+    DATABASE_URI = os.environ['DATABASE_URI']
+
 app = Flask(__name__)
 app.debug = True
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI']
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
