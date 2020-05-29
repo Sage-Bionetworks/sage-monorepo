c198653c471fb66947b962470a33dd915b6225d7
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-05-29 23:43:00 +0000

    patch/refactor: [#173084306] Initial GitLab CI to get test coverage. Updated environment variables to be in line with other deployments (consistency).

--- apps/iatlas/api-gitlab/config.py
+++ apps/iatlas/api-gitlab/config.py
@@ -3,11 +3,11 @@ import os
 
 def get_database_uri():
     POSTGRES = {
-        'user': os.environ['PG_USER'],
-        'pw': os.environ['PG_PW'],
-        'db': os.environ['PG_DATABASE'],
-        'host': os.environ['PG_HOST'],
-        'port': os.environ['PG_PORT']
+        'user': os.environ['POSTGRES_USER'],
+        'pw': os.environ['POSTGRES_PASSWORD'],
+        'db': os.environ['POSTGRES_DB'],
+        'host': os.environ['POSTGRES_HOST'],
+        'port': os.environ['POSTGRES_PORT']
     }
     DATABASE_URI = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
     if 'DATABASE_URI' in os.environ:
