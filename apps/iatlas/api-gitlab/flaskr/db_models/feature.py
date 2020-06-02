3f5f8ebf693f4a8c72414213007492e04aabb9e7
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-06-02 21:17:07 +0000

    patch/improvement: [#173084306] Added feature and method_tag models with tests.

---
+++ apps/iatlas/api-gitlab/flaskr/db_models/feature.py
@@ -0,0 +1,19 @@
+from flaskr import db
+from flaskr.enums import unit_enum
+
+
+class Feature(db.Model):
+    __tablename__ = 'features'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    display = db.Column(db.String, nullable=True)
+    order = db.Column(db.Integer, nullable=True)
+    unit = db.Column(unit_enum, nullable=True)
+
+    class_id = db.Column(db.Integer, db.ForeignKey('class.id'), nullable=False)
+
+    method_tag_id = db.Column(
+        db.Integer, db.ForeignKey('method_tag.id'), nullable=True)
+
+    def __repr__(self):
+        return '<Feature %r>' % self.name
---
+++ apps/iatlas/api-gitlab/flaskr/db_models/method_tag.py
@@ -0,0 +1,10 @@
+from flaskr import db
+
+
+class MethodTag(db.Model):
+    __tablename__ = 'method_tags'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    def __repr__(self):
+        return '<MethodTag %r>' % self.name
