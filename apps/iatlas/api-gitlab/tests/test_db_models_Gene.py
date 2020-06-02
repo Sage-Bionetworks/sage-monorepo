9439a73f77963deffde2809efe1c8d9bdf384c0c
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-06-02 21:34:06 +0000

    patch/test: [#173084306] Better assertion for gene reference value.

--- apps/iatlas/api-gitlab/tests/test_db_models_Gene.py
+++ apps/iatlas/api-gitlab/tests/test_db_models_Gene.py
@@ -1,7 +1,7 @@
 import json
 import os
 import pytest
-from tests import app, client
+from tests import app, client, db
 from flaskr.db_models import Gene
 
 
@@ -11,8 +11,7 @@ def test_Gene(app):
     hgnc = 'ADORA2A'
     description = 'It is a popular target in immuno-oncology due to its function of immunosuppressive effects in tumor microenvironment.'
     io_landscape_name = hgnc
-    references = [
-        'https://www.cancerresearch.org/scientists/immuno-oncology-landscape?2019IOpipelineDB=2017;Target;ADORA2A']
+    reference = 'https://www.cancerresearch.org/scientists/immuno-oncology-landscape?2019IOpipelineDB=2017;Target;ADORA2A'
     node_type_id = 2
     pathway_id = 3
     therapy_type_id = 2
@@ -23,7 +22,7 @@ def test_Gene(app):
     assert result.hgnc == hgnc
     assert result.description == description
     assert result.io_landscape_name == io_landscape_name
-    assert result.references == references
+    assert reference in result.references
     assert result.node_type_id == node_type_id
     assert result.pathway_id == pathway_id
     assert result.therapy_type_id == therapy_type_id
