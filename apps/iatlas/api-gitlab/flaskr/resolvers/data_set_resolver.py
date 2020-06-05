b6b5874dd10b44e7de680d1dd1ed3eeb85a37acc
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-06-05 15:43:37 -0700

    wip: [#173084306] Initial approach for using the SQLAlchemy ORM.

--- apps/iatlas/api-gitlab/flaskr/resolvers/data_set_resolver.py
+++ apps/iatlas/api-gitlab/flaskr/resolvers/data_set_resolver.py
@@ -1,15 +1,53 @@
-from sqlalchemy import func
-from flaskr.db_models import FeatureClass, Sample, SampleToTag
+from sqlalchemy import and_, func, or_
+from flaskr import db
+from flaskr.db_models import FeatureClass, Sample, SampleToTag, Tag, TagToTag
 
 
 def resolve_dataSet(_obj, info, name, group, feature=None):
-    # query = Sample.query.with_entities(Sample.id)
+    # "SELECT sample_id FROM samples_to_tags WHERE tag_id IN (",
+    # "SELECT tag_id FROM tags_to_tags WHERE related_tag_id IN (",
+    # "SELECT id FROM tags WHERE display IN (",
+    # string_values_to_query_list(display),
+    # ")))"
+    sess = db.session
 
-    # query = query.join(SampleToTag, Sample.id == SampleToTag.sample_id)
+    # Get all tag ids or the passed groups.
+    group_tag_query = sess.query(Tag.id).filter(
+        Tag.name.in_(group))
+    # Get all tag ids or the passed names.
+    name_tag_query = sess.query(Tag.id).filter(
+        Tag.name.in_(name))
+    # Get all the tags related to the passed groups.
+    tag_to_group_query = sess.query(TagToTag.tag_id).filter(TagToTag.related_tag_id.in_(
+        group_tag_query.subquery()))
+    # Get all the sample ids associated with the tags.
+    sample_to_tag_sub_query = sess.query(
+        SampleToTag.sample_id).filter(SampleToTag.tag_id.in_(tag_to_group_query.subquery())).subquery()
+    # Limit samples_to_tags to only rows with tags in the passed names.
+    reduced_to_names_query = sess.query(
+        SampleToTag.sample_id).filter(SampleToTag.tag_id.in_(name_tag_query.subquery()))
+    # Limit samples ids to only ids that are associated with names
+    samples_to_names_query = reduced_to_names_query.join(
+        sample_to_tag_sub_query, SampleToTag.sample_id == sample_to_tag_sub_query.c.sample_id)
+    # .filter(or_(SampleToTag.tag_id.in_(name_tag_to_tag_sub_query)))
+
+    query = sess.query(Sample.id)
+    # sample = query.first()
+    results = samples_to_names_query.distinct().all()
+    tcga_samples = samples_to_names_query.distinct().count()
+
+    print("results: ", results)
+
+    # query = query.join(SampleToTag.query.join(
+    #     Tag.query.filter(Tag.name.in_(name)), SampleToTag.tag_id == Tag.id),
+    #     Sample.id == SampleToTag.sample_id)
+
+    # result = query.all()
+
+    # print("result: ", result)
 
     # query = Sample.query.with_entities(Sample.id)
     # query = Sample.query(label('sample_number', func.count(Sample.id)))
-    samples = Sample.query.with_entities(Sample.id).count()
 
     # classes = FeatureClass.query.
 
@@ -19,6 +57,6 @@ def resolve_dataSet(_obj, info, name, group, feature=None):
     return [{
         "sampleGroup": name[0],
         "groupName": group[0],
-        "groupSize": samples,
+        "groupSize": tcga_samples,
         "characteristics": feature[0] if feature is not None else None
     }]
