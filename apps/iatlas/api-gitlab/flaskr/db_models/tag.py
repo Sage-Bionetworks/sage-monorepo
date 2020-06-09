05ab78810d85729cd558d20d484a86c4c2947521
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-06-09 17:47:48 +0000

    patch/improvement: [#173084306] Use the common Base model. Added tag relationships.

--- apps/iatlas/api-gitlab/flaskr/db_models/tag.py
+++ apps/iatlas/api-gitlab/flaskr/db_models/tag.py
@@ -1,13 +1,19 @@
 from flaskr import db
+from . import Base
 
 
-class Tag(db.Model):
+class Tag(Base):
     __tablename__ = 'tags'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     characteristics = db.Column(db.String, nullable=True)
     display = db.Column(db.String, nullable=True)
     color = db.Column(db.String, nullable=True)
+
+    tags = db.relationship(
+        "TagToTag", foreign_keys='TagToTag.related_tag_id', back_populates="related_tag")
+    related_tags = db.relationship(
+        "TagToTag", foreign_keys='TagToTag.tag_id', back_populates="tag")
 
     def __repr__(self):
         return '<Tag %r>' % self.name
