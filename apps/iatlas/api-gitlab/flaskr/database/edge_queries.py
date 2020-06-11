e95b0fe685fff96c69cc011f487b6d77e3e1e5f7
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-06-11 19:41:20 +0000

    patch/improvement: [#173084306] Added relationships to edges.

--- apps/iatlas/api-gitlab/flaskr/database/edge_queries.py
+++ apps/iatlas/api-gitlab/flaskr/database/edge_queries.py
@@ -1,7 +1,9 @@
 from sqlalchemy import orm
 from flaskr import db
 from flaskr.db_models import Edge
+from .database_helpers import build_option_args
 
 
-def return_edge_query():
-    return db.session.query(Edge)
+def return_edge_query(*args):
+    option_args = build_option_args(*args, accepted_args=['node_1', 'node_2'])
+    return db.session.query(Edge).options(*option_args)
--- apps/iatlas/api-gitlab/tests/test_db_models_Edge.py
+++ apps/iatlas/api-gitlab/tests/test_db_models_Edge.py
@@ -3,7 +3,31 @@ from tests import app, NoneType
 from flaskr.database import return_edge_query
 
 
-def test_Edge(app):
+def test_Edge_with_relations(app):
+    app()
+    node_1_id = 42
+    string_representation_list = []
+    separator = ', '
+
+    query = return_edge_query('node_1', 'node_2')
+    results = query.filter_by(node_1_id=node_1_id).all()
+
+    assert isinstance(results, list)
+    for result in results:
+        string_representation = '<Edge %r>' % result.id
+        string_representation_list.append(string_representation)
+        assert result.node_1.id == node_1_id
+        assert result.node_2.id == result.node_2_id
+        assert result.node_1_id == node_1_id
+        assert type(result.node_2_id) is int
+        assert type(result.label) is str or NoneType
+        assert type(result.score) is float or NoneType
+        assert repr(result) == string_representation
+    assert repr(results) == '[' + separator.join(
+        string_representation_list) + ']'
+
+
+def test_Edge_no_relations(app):
     app()
     node_1_id = 42
     string_representation_list = []
@@ -16,10 +40,10 @@ def test_Edge(app):
     for result in results:
         string_representation = '<Edge %r>' % result.id
         string_representation_list.append(string_representation)
+        assert type(result.node_1) is NoneType
+        assert type(result.node_2) is NoneType
         assert result.node_1_id == node_1_id
         assert type(result.node_2_id) is int
         assert type(result.label) is str or NoneType
         assert type(result.score) is float or NoneType
         assert repr(result) == string_representation
-    assert repr(results) == '[' + separator.join(
-        string_representation_list) + ']'
