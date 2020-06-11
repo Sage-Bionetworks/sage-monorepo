e0df9efd96fb64373b9d5c7d257bb92030dc365c
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-06-11 23:45:50 +0000

    patch/improvement: [#173292762] Added relations to Copy Number Result.

---
+++ apps/iatlas/api-gitlab/flaskr/database/copy_number_result_queries.py
@@ -0,0 +1,26 @@
+from sqlalchemy import orm
+from flaskr import db
+from flaskr.db_models import CopyNumberResult
+from .database_helpers import build_option_args, build_query_args
+
+accepted_option_args = ['feature', 'gene', 'tag']
+
+accepted_query_args = ['id',
+                       'direction',
+                       'mean_normal',
+                       'mean_cnv',
+                       'p_value',
+                       'log10_p_value',
+                       't_stat',
+                       'feature_id',
+                       'gene_id',
+                       'tag_id']
+
+
+def return_copy_number_result_query(*args):
+    option_args = build_option_args(*args, accepted_args=accepted_option_args)
+    query_args = build_query_args(*args, accepted_args=accepted_query_args)
+    query = db.session.query(*query_args)
+    if option_args:
+        query = db.session.query(CopyNumberResult).options(*option_args)
+    return query
