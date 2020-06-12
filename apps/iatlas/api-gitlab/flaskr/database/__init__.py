28e70a1ca767325604a09d3841df415ae23d8362
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-06-12 00:28:19 +0000

    patch/improvement: [#173292762] Removed Edge_to_Tag.

--- apps/iatlas/api-gitlab/flaskr/database/__init__.py
+++ apps/iatlas/api-gitlab/flaskr/database/__init__.py
@@ -1,6 +1,5 @@
 from .copy_number_result_queries import *
 from .edge_queries import *
-from .edge_to_tag_queries import *
 from .feature_queries import *
 from .feature_to_sample_queries import *
 from .gene_queries import *
