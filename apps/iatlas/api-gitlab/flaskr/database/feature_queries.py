78cdef2e6670d4a4d1715583c0386a7cc24d295d
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-06-12 00:30:28 +0000

    patch/improvement: [#173292762] Made feature class query have dynamic fields.

--- apps/iatlas/api-gitlab/flaskr/database/feature_queries.py
+++ apps/iatlas/api-gitlab/flaskr/database/feature_queries.py
@@ -1,9 +1,14 @@
 from sqlalchemy import orm
 from flaskr import db
 from flaskr.db_models import FeatureClass, Feature, MethodTag
+from .database_helpers import accepted_simple_table_query_args, build_query_args
 
 
-def return_feature_class_query():
+def return_feature_class_query(*args):
+    query_args = build_query_args(
+        FeatureClass, *args, accepted_args=accepted_simple_table_query_args)
+    if query_args:
+        return db.session.query(*query_args)
     return db.session.query(FeatureClass)
 
 
