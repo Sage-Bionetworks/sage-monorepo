9b8870ad37957abceb5813347ddcf72af5108d7a
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-06-13 00:26:03 +0000

    patch/improvement: [#173292762] Added backref in features to feature_class and method_tag.

--- apps/iatlas/api-gitlab/flaskr/database/feature_queries.py
+++ apps/iatlas/api-gitlab/flaskr/database/feature_queries.py
@@ -3,27 +3,29 @@ from flaskr import db
 from flaskr.db_models import FeatureClass, Feature, MethodTag
 from .database_helpers import general_core_fields, build_general_query
 
-related_fields = [
+feature_related_fields = [
     'copy_number_results', 'feature_class', 'method_tag', 'samples']
 
-core_fields = [
+feature_core_fields = [
     'id', 'name', 'display', 'order', 'unit', 'class_id', 'method_tag_id']
 
 
 def return_feature_class_query(*args):
     return build_general_query(
         FeatureClass, args=args,
+        accepted_option_args=['features'],
         accepted_query_args=general_core_fields)
 
 
 def return_feature_query(*args):
     return build_general_query(
         Feature, args=args,
-        accepted_option_args=related_fields,
-        accepted_query_args=core_fields)
+        accepted_option_args=feature_related_fields,
+        accepted_query_args=feature_core_fields)
 
 
 def return_method_tag_query(*args):
     return build_general_query(
         MethodTag, args=args,
+        accepted_option_args=['features'],
         accepted_query_args=general_core_fields)
--- apps/iatlas/api-gitlab/tests/test_db_models_FeatureClass.py
+++ apps/iatlas/api-gitlab/tests/test_db_models_FeatureClass.py
@@ -3,18 +3,29 @@ from tests import app
 from flaskr.database import return_feature_class_query
 
 
-def test_FeatureClass(app):
+def test_FeatureClass_with_relations(app):
+    app()
+    name = 'Adaptive Receptor - B cell'
+    relationships_to_join = ['features']
+
+    query = return_feature_class_query(*relationships_to_join)
+    result = query.filter_by(name=name).first()
+
+    assert isinstance(result.features, list)
+    # Don't need to iterate through every result.
+    for feature in result.features[0:2]:
+        assert type(feature.name) is str
+    assert type(result.id) is int
+    assert result.name == name
+    assert repr(result) == '<FeatureClass %r>' % name
+
+
+def test_FeatureClass_no_relations(app):
     app()
     name = 'Adaptive Receptor - B cell'
 
     query = return_feature_class_query()
     result = query.filter_by(name=name).first()
 
-    assert result.name == name
-
-    query = return_feature_class_query()
-    result = query.filter_by(name=name).first()
-
     assert type(result.id) is int
     assert result.name == name
-    assert repr(result) == '<FeatureClass %r>' % name
