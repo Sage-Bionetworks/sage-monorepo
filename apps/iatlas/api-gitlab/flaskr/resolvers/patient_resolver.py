0da3cc304d7e7bddd8a6bc23805ad06fdb6f0928
Rene Martinez <rene.martinez@genui.com>
2020-06-18 17:55:18 +0000

    wip: [#173369268] patient

--- apps/iatlas/api-gitlab/flaskr/resolvers/patient_resolver.py
+++ apps/iatlas/api-gitlab/flaskr/resolvers/patient_resolver.py
@@ -14,7 +14,33 @@ valid_patient_node_mapping = {
     'race': 'race'
 }
 
-def resolve_patient(_obj, info, id):
+def resolve_patient(_obj, info, id=None, barcode=None):
+    option_args = build_option_args(
+        info.field_nodes[0].selection_set,
+        valid_patient_node_mapping
+    )
+    query = return_patient_query(*option_args)
+    if id is not None and barcode is not None:
+        return "you can't do this"
+
+    if id is not None:
+        patient = query.filter_by(id=id).first()
+    
+    if barcode is not None:
+        patient = query.filter_by(barcode=barcode).first()
+        
+    return {
+        "id": get_value(patient, 'id'),
+        "age": get_value(patient, 'age'),
+        "barcode": get_value(patient, 'barcode'),
+        "etnicity": get_value(patient, 'etnicity'),
+        "gender": get_value(patient, 'gender'),
+        "height": get_value(patient, 'height'),
+        "weight": get_value(patient, 'weight'),
+        "race": get_value(patient, 'race')
+    }
+
+def resolve_patients(_obj, info, id):
     option_args = build_option_args(
         info.field_nodes[0].selection_set,
         valid_patient_node_mapping
