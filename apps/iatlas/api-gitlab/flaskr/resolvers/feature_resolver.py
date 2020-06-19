90d360c8c40a0c715be6379d6bbbf900dee40180
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-06-19 16:22:15 +0000

    patch/improvement: [#173405763] Adjusted features resolver and tags resolver for new dataset data model.

--- apps/iatlas/api-gitlab/flaskr/resolvers/feature_resolver.py
+++ apps/iatlas/api-gitlab/flaskr/resolvers/feature_resolver.py
@@ -17,18 +17,18 @@ def build_classes_join_condition(features_model, classes_model, feature_classes=
     return classes_join_conditions
 
 
-def build_features_to_samples_join_condition(features_to_samples_model,
-                                             samples_to_tags_model,
-                                             feature=None):
-    features_to_samples_join_conditions = [
+def build_feature_to_sample_join_condition(features_to_samples_model,
+                                           samples_to_tags_model,
+                                           feature=None):
+    feature_to_sample_join_condition = [
         features_to_samples_model.sample_id == samples_to_tags_model.sample_id]
     if type(feature) is not NoneType:
         chosen_feature = orm.aliased(Feature, name='cf')
-        features_to_samples_join_conditions.append(features_to_samples_model.feature_id.in_(
+        feature_to_sample_join_condition.append(features_to_samples_model.feature_id.in_(
             db.session.query(chosen_feature.id).filter(
                 chosen_feature.name.in_(feature))
         ))
-    return features_to_samples_join_conditions
+    return feature_to_sample_join_condition
 
 
 def request_features(_obj, info, dataSet=None, related=None, feature=None, feature_class=None, byClass=False, byTag=True):
@@ -37,6 +37,7 @@ def request_features(_obj, info, dataSet=None, related=None, feature=None, featu
 
     The query may be larger or smaller depending on the requested fields.
     An example of the full query in SQL:
+
     SELECT DISTINCT
         feature_1."name" AS "name",
         feature_1.display AS display,
@@ -44,16 +45,16 @@ def request_features(_obj, info, dataSet=None, related=None, feature=None, featu
         feature_1.unit AS unit,
         class_1.name AS class,
         method_tag_1.name AS method_tag
-    FROM samples_to_tags AS samples_to_tags_1
-    INNER JOIN features_to_samples AS features_to_samples_1 ON features_to_samples_1.sample_id = samples_to_tags_1.sample_id AND features_to_samples_1.feature_id
+    FROM samples_to_tags AS sample_to_tag_1
+    INNER JOIN features_to_samples AS feature_to_sample_1 ON feature_to_sample_1.sample_id = sample_to_tag_1.sample_id AND feature_to_sample_1.feature_id
         IN(SELECT chosen_features.id FROM features AS chosen_features WHERE chosen_features."name" IN('Neutrophils_Aggregate2'))
-    INNER JOIN datasets_to_samples AS datasets_to_samples_1 ON features_to_samples_1.sample_id = datasets_to_samples_1.sample_id AND datasets_to_samples_1.dataset_id
+    INNER JOIN datasets_to_samples AS datasets_to_samples_1 ON feature_to_sample_1.sample_id = datasets_to_samples_1.sample_id AND datasets_to_samples_1.dataset_id
         IN(SELECT dataset_1.id FROM datasets AS dataset_1 WHERE dataset_1."name" IN('TCGA'))
-    INNER JOIN tags_to_tags AS tags_to_tags_1 ON samples_to_tags_1.tag_id = tags_to_tags_1.related_tag_id AND tags_to_tags_1.related_tag_id
+    INNER JOIN tags_to_tags AS tag_to_tag_1 ON sample_to_tag_1.tag_id = tag_to_tag_1.related_tag_id AND tag_to_tag_1.related_tag_id
         IN(SELECT related_tag.id FROM tags AS related_tag WHERE related_tag."name" IN('Immune_Subtype'))
-    INNER JOIN samples_to_tags AS samples_to_tags_2 ON samples_to_tags_2.sample_id = features_to_samples_1.sample_id
-        AND tags_to_tags_1.tag_id = samples_to_tags_2.tag_id
-    JOIN features AS feature_1 ON feature_1.id = features_to_samples_1.feature_id
+    INNER JOIN samples_to_tags AS sample_to_tag_2 ON sample_to_tag_2.sample_id = feature_to_sample_1.sample_id
+        AND tag_to_tag_1.tag_id = sample_to_tag_2.tag_id
+    JOIN features AS feature_1 ON feature_1.id = feature_to_sample_1.feature_id
     JOIN classes AS class_1 ON class_1.id = feature_1.class_id
     JOIN method_tags AS method_tag_1 ON method_tag_1.id = feature_1.method_tag_id
     """
@@ -64,7 +65,7 @@ def request_features(_obj, info, dataSet=None, related=None, feature=None, featu
 
     class_1 = orm.aliased(FeatureClass, name='fc')
     feature_1 = orm.aliased(Feature, name='f')
-    features_to_samples_1 = orm.aliased(FeatureToSample, name='fs1')
+    feature_to_sample_1 = orm.aliased(FeatureToSample, name='fs1')
     method_tag_1 = orm.aliased(MethodTag, name='mt')
     sample_1 = orm.aliased(Sample, name='s')
 
@@ -95,7 +96,7 @@ def request_features(_obj, info, dataSet=None, related=None, feature=None, featu
             select_fields.append(sample_1.name.label('sample'))
         if join_value in option_args:
             select_fields.append(
-                features_to_samples_1.value.label('value'))
+                feature_to_sample_1.value.label('value'))
 
     query = sess.query(*select_fields)
 
@@ -106,42 +107,41 @@ def request_features(_obj, info, dataSet=None, related=None, feature=None, featu
             query = query.filter(feature_1.name.in_(feature))
     else:
         dataset_1 = orm.aliased(Dataset, name='d')
-        dataset_tag = orm.aliased(Tag, name='dt')
         dataset_to_sample_1 = orm.aliased(DatasetToSample, name='ds')
         related_tag = orm.aliased(Tag, name='rt')
-        samples_to_tags_1 = orm.aliased(SampleToTag, name='st1')
-        samples_to_tags_2 = orm.aliased(SampleToTag, name='st2')
-        tags_to_tags_1 = orm.aliased(TagToTag, name='tt')
+        sample_to_tag_1 = orm.aliased(SampleToTag, name='st1')
+        sample_to_tag_2 = orm.aliased(SampleToTag, name='st2')
+        tag_to_tag_1 = orm.aliased(TagToTag, name='tt')
 
-        query = query.select_from(samples_to_tags_1)
+        query = query.select_from(sample_to_tag_1)
 
-        features_to_samples_join_conditions = build_features_to_samples_join_condition(
-            features_to_samples_1, samples_to_tags_1, feature)
+        feature_to_sample_join_condition = build_feature_to_sample_join_condition(
+            feature_to_sample_1, sample_to_tag_1, feature)
 
-        query = query.join(features_to_samples_1, and_(
-            *features_to_samples_join_conditions))
+        query = query.join(feature_to_sample_1, and_(
+            *feature_to_sample_join_condition))
 
         if type(dataSet) is not NoneType:
             query = query.join(dataset_to_sample_1,
-                               and_(dataset_to_sample_1.sample_id == features_to_samples_1.sample_id,
+                               and_(dataset_to_sample_1.sample_id == feature_to_sample_1.sample_id,
                                     dataset_to_sample_1.dataset_id.in_(
                                         sess.query(dataset_1.id).filter(
                                             dataset_1.name.in_(dataSet))
                                     )))
 
         if type(related) is not NoneType:
-            query = query.join(tags_to_tags_1,
-                               and_(samples_to_tags_1.tag_id == tags_to_tags_1.related_tag_id,
-                                    tags_to_tags_1.related_tag_id.in_(
+            query = query.join(tag_to_tag_1,
+                               and_(sample_to_tag_1.tag_id == tag_to_tag_1.related_tag_id,
+                                    tag_to_tag_1.related_tag_id.in_(
                                         sess.query(related_tag.id).filter(
                                             related_tag.name.in_(related)))))
             query = query.join(
-                samples_to_tags_2, and_(
-                    features_to_samples_1.sample_id == samples_to_tags_2.sample_id,
-                    samples_to_tags_2.tag_id == tags_to_tags_1.tag_id))
+                sample_to_tag_2, and_(
+                    feature_to_sample_1.sample_id == sample_to_tag_2.sample_id,
+                    sample_to_tag_2.tag_id == tag_to_tag_1.tag_id))
 
         query = query.join(feature_1, feature_1.id ==
-                           features_to_samples_1.feature_id)
+                           feature_to_sample_1.feature_id)
 
     if 'class' in option_args or type(feature_class) is not NoneType:
         classes_join_condition = build_classes_join_condition(
