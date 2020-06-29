3bd245821a85c9ef21f3bc168f0ec00c88c0d747
andrewelamb <andrewelamb@gmail.com>
2020-06-29 07:57:40 -0700

    immune feature trends partially working with new api

--- R/api_queries.R
+++ R/api_queries.R
@@ -2,13 +2,32 @@ query_cohort_selector <- function(
     dataset = "TCGA",
     group_tag = "Immune_Subtype"
 ){
-    perform_api_query(
+    iatlas.app::perform_api_query(
         "cohort_selection",
         list(
             dataSet = dataset,
             related = group_tag,
             feature = list(),
             featureClass = list()
+        )
+    ) %>%
+        purrr::pluck("data", 1) %>%
+        dplyr::as_tibble()
+}
+
+query_features_by_class <- function(
+    dataset = list(),
+    group_tag = list(),
+    feature = list(),
+    feature_class = list()
+){
+    iatlas.app::perform_api_query(
+        "features_by_class",
+        list(
+            dataSet = dataset,
+            related = group_tag,
+            feature = feature,
+            featureClass = feature_class
         )
     ) %>%
         purrr::pluck("data", 1) %>%
