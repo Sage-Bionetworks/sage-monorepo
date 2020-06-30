a127f04f02a6e72df97affce328a2e7fd3516cd4
andrewelamb <andrewelamb@gmail.com>
2020-06-30 07:51:39 -0700

    immune features distributions now using api

--- tests/testthat/test-immune_feature_distributions_functions.R
+++ tests/testthat/test-immune_feature_distributions_functions.R
@@ -1,23 +1,10 @@
-with_test_db_env({
+with_test_api_env({
   test_that("Build Distribution Plot Tibble", {
-      sample_tbl <- dplyr::tibble(
-          sample_id = c(1:10),
-          group = c(rep("C1", 5), rep("C2", 5))
-      )
-      result1 <- build_ifd_distplot_tbl(sample_tbl, 1, "None")
-      expect_named(result1, c("x", "y"))
-  })
+    sample_tbl <- iatlas.app::query_cohort_selector() %>%
+      dplyr::select("group" = "name", "sample_name" = "samples") %>%
+      tidyr::unnest(cols = c(sample_name))
 
-  test_that("Build Histogram Tibble", {
-      distplot_tbl <- dplyr::tibble(
-          y = c(1:10),
-          x = c(rep("C1", 3), rep("C2", 7))
-      )
-      result1 <- build_ifd_histplot_tbl(distplot_tbl, "C1")
-      expect_named(result1, "x")
-      expect_true(nrow(result1) == 3)
-      result1 <- build_ifd_histplot_tbl(distplot_tbl, "C2")
-      expect_named(result1, "x")
-      expect_true(nrow(result1) == 7)
+    result1 <- build_ifd_distplot_tbl(sample_tbl, "leukocyte_fraction", "None")
+    expect_named(result1, c("x", "y"))
   })
 })
