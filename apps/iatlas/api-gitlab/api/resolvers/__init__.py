5fa7a104633668c21a1fee49526262481557065b
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-07-01 22:50:29 +0000

    patch: [#173617206] All the files didn't get committed.

--- apps/iatlas/api-gitlab/api/resolvers/__init__.py
+++ apps/iatlas/api-gitlab/api/resolvers/__init__.py
@@ -8,6 +8,7 @@ from .features_by_tag_resolver import resolve_features_by_tag
 from .tags_resolver import resolve_tags
 from .test_resolver import resolve_test
 from .mutation_resolver import resolve_mutation, resolve_mutations
+from .mutation_types_resolver import resolve_mutation_types
 from .patient_resolver import resolve_patient, resolve_patients
 from .slide_resolver import resolve_slide, resolve_slides
 from .sample_resolver import resolve_sample, resolve_samples
--- apps/iatlas/api-gitlab/api/schema/__init__.py
+++ apps/iatlas/api-gitlab/api/schema/__init__.py
@@ -3,8 +3,8 @@ import os
 from api.resolvers import (
     resolve_data_sets, resolve_features, resolve_features_by_class, resolve_features_by_tag,
     resolve_gene, resolve_genes, resolve_genes_by_tag, resolve_mutation, resolve_mutations,
-    resolve_patient, resolve_patients, resolve_sample, resolve_samples, resolve_slide,
-    resolve_slides, resolve_tags, resolve_test)
+    resolve_mutation_types, resolve_patient, resolve_patients, resolve_sample, resolve_samples,
+    resolve_slide, resolve_slides, resolve_tags, resolve_test)
 
 schema_dirname, _filename = os.path.split(os.path.abspath(__file__))
 
@@ -51,6 +51,7 @@ gene = ObjectType('Gene')
 genes_by_tag = ObjectType('GenesByTag')
 gene_type = ObjectType('GeneType')
 mutation = ObjectType('Mutation')
+mutation_type = ObjectType('MutationType')
 patient = ObjectType('Patient')
 publication = ObjectType('Publication')
 sample = ObjectType('Sample')
@@ -75,6 +76,7 @@ root.set_field('genes', resolve_genes)
 root.set_field('genesByTag', resolve_genes_by_tag)
 root.set_field('mutation', resolve_mutation)
 root.set_field('mutations', resolve_mutations)
+root.set_field('mutationTypes', resolve_mutation_types)
 root.set_field('patient', resolve_patient)
 root.set_field('patients', resolve_patients)
 root.set_field('sample', resolve_sample)
@@ -89,7 +91,7 @@ schema = make_executable_schema(
     type_defs,
     [root, data_set, feature, features_by_class, features_by_tag,
      feature_value_type, gene, genes_by_tag, gene_type, mutation,
-     patient, publication, sample, simple_data_set, simple_gene,
-     simple_gene_type, simple_publication, simple_sample, simple_tag,
-     slide, tag]
+     mutation_type, patient, publication, sample, simple_data_set,
+     simple_gene, simple_gene_type, simple_publication, simple_sample,
+     simple_tag, slide, tag]
 )
