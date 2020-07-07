e0a461c7ca2e710bff94ea23ffce09cf6c598298
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-07-07 01:04:03 +0000

    patch/doc: [#173369221] Added comments to features queries.

--- apps/iatlas/api-gitlab/api/db_models/tag.py
+++ apps/iatlas/api-gitlab/api/db_models/tag.py
@@ -10,19 +10,19 @@ class Tag(Base):
     display = db.Column(db.String, nullable=True)
     color = db.Column(db.String, nullable=True)
 
-    nodes = db.relationship("Node", lazy='noload', uselist=True,
+    nodes = db.relationship('Node', lazy='noload', uselist=True,
                             secondary='nodes_to_tags')
 
     related_tags = db.relationship(
-        "Tag", foreign_keys='TagToTag.tag_id', lazy='noload',
-        secondary='tags_to_tags', back_populates="tags", uselist=True)
+        'Tag', foreign_keys='TagToTag.tag_id', lazy='noload',
+        secondary='tags_to_tags', back_populates='tags', uselist=True)
 
-    samples = db.relationship("Sample", lazy='noload', uselist=True,
+    samples = db.relationship('Sample', lazy='noload', uselist=True,
                               secondary='samples_to_tags')
 
     tags = db.relationship(
-        "Tag", foreign_keys='TagToTag.related_tag_id', lazy='noload',
-        secondary='tags_to_tags', back_populates="related_tags", uselist=True)
+        'Tag', foreign_keys='TagToTag.related_tag_id', lazy='noload',
+        secondary='tags_to_tags', back_populates='related_tags', uselist=True)
 
     def __repr__(self):
         return '<Tag %r>' % self.name
