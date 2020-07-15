538699b04739c793bde692dfdf84ede6c56e774b
andrewelamb <andrewelamb@gmail.com>
2020-07-15 09:21:05 -0700

    various

--- tests/testthat/test-immune_feature_correlations_functions.R
+++ tests/testthat/test-immune_feature_correlations_functions.R
@@ -1,22 +1,26 @@
 test_that("Build Immune Feature Correlations Value Table", {
-    response_tbl <- dplyr::tibble(sample_id = 1:3, feature_id = 1L, value = 1:3)
-    feature_tbl  <- dplyr::tribble(
-        ~sample_id, ~feature_id, ~value, ~order, ~feature,
-        1,          1,           .1,     2,      "f1",
-        1,          2,           .2,     1,      "f2",
-        1,          3,           .3,     3,      "f3",
-        2,          1,           .4,     2,      "f1",
-        2,          2,           .5,     1,      "f2",
-        2,          3,           .6,     3,      "f3"
+    response_tbl <- dplyr::tibble(
+        sample = c("A", "B", "C"),
+        name   = "F1",
+        value  = 1:3
     )
-    sample_tbl <- dplyr::tibble(sample_id = 1:3, group = rep("C1", 3))
+    feature_tbl  <- dplyr::tribble(
+        ~sample,    ~feature_name,    ~value, ~order, ~feature2,
+        "A",        "F1",        .1,     2,      "f1",
+        "A",        "F2",        .2,     1,      "f2",
+        "A",        "F3",        .3,     3,      "f3",
+        "B",        "F1",        .4,     2,      "f1",
+        "B",        "F2",        .5,     1,      "f2",
+        "B",        "F3",        .6,     3,      "f3"
+    )
+    sample_tbl <- dplyr::tibble(sample = c("A", "B", "C"), group = rep("C1", 3))
     result1 <- build_ifc_value_tbl(response_tbl, feature_tbl, sample_tbl)
     expect_named(
         result1,
         c("response_value",
           "feature_value",
           "feature_name",
-          "sample_id",
+          "sample",
           "order",
           "group"
         )
