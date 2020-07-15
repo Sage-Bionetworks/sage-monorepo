1d5242e7a96d155de77afcc73ccb478e59fb9489
andrewelamb <andrewelamb@gmail.com>
2020-07-15 08:36:40 -0700

    fixed immune feature distributions tests

--- tests/testthat/test-immune_feature_distributions_functions.R
+++ tests/testthat/test-immune_feature_distributions_functions.R
@@ -1,9 +1,7 @@
 with_test_api_env({
   test_that("Build Distribution Plot Tibble", {
     sample_tbl <- iatlas.app::query_cohort_selector() %>%
-      dplyr::select("group" = "name", "sample_name" = "samples") %>%
-      tidyr::unnest(cols = c(sample_name))
-
+      dplyr::select("group" = "name", "sample")
     result1 <- build_ifd_distplot_tbl(sample_tbl, "leukocyte_fraction", "None")
     expect_named(result1, c("x", "y"))
   })
