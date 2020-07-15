9eb7464945a991709a7d8700804dec9ca02c5934
andrewelamb <andrewelamb@gmail.com>
2020-07-15 06:28:40 -0700

    add cellimage network

---
+++ R/modules/server/analysis_modules/cellimage_server.R
@@ -0,0 +1,27 @@
+cellimage_server <- function(
+    input,
+    output,
+    session,
+    cohort_obj
+){
+
+    source_files <- c(
+        "R/modules/server/submodules/cellimage_main_server.R",
+        "R/modules/server/submodules/call_module_server.R"
+    )
+
+    for (file in source_files) {
+        source(file, local = T)
+    }
+
+    shiny::callModule(
+        call_module_server,
+        "cellimage_main",
+        cohort_obj,
+        shiny::reactive(function(cohort_obj) T),
+        cellimage_main_server
+    )
+}
+
+
+
