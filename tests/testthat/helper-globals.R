5794c84da1b0806c9c843787561b25048b24bd89
andrewelamb <andrewelamb@gmail.com>
2020-07-16 14:53:41 -0700

    ocp module passing tests

--- tests/testthat/helper-globals.R
+++ tests/testthat/helper-globals.R
@@ -1,5 +1,5 @@
 with_test_api_env <- function(expr){
-  if (iatlas.app::present(.GlobalEnv$ghql_query_object)){
+  if (present(.GlobalEnv$ghql_query_object)){
     rm(ghql_query_object, envir = .GlobalEnv)
   }
   iatlas.app::create_and_add_all_queries_to_qry_obj(
