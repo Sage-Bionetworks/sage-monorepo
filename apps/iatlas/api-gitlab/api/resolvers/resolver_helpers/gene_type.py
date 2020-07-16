790e12b52879e2c89f2ecfaab32a9b00cd9b08d3
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-07-16 23:22:54 +0000

    patch: [delivered #173839854] Created geneTypes query.

---
+++ apps/iatlas/api-gitlab/api/resolvers/resolver_helpers/gene_type.py
@@ -0,0 +1,62 @@
+from sqlalchemy import and_, orm
+from api import db
+from api.db_models import Gene, GeneType
+from .general_resolvers import build_option_args, get_selection_set
+
+
+def build_gene_type_core_request(selection_set, name=None):
+    """
+    Builds a SQL request with just core gene type fields.
+    """
+    sess = db.session
+
+    gene_type_1 = orm.aliased(GeneType, name='g')
+
+    core_field_mapping = {'display': gene_type_1.display.label('display'),
+                          'name': gene_type_1.name.label('name')}
+
+    core = build_option_args(selection_set, core_field_mapping)
+
+    query = sess.query(*core)
+
+    if name:
+        query = query.filter(gene_type_1.name.in_(name))
+
+    return query
+
+
+def build_gene_type_request(_obj, info, name=None):
+    """
+    Builds a SQL request.
+    """
+    sess = db.session
+
+    selection_set = get_selection_set(info.field_nodes[0].selection_set, False)
+
+    gene_1 = orm.aliased(Gene, name='g')
+    gene_type_1 = orm.aliased(GeneType, name='m')
+
+    related_field_mapping = {'genes': 'genes'}
+
+    relations = build_option_args(selection_set, related_field_mapping)
+    option_args = []
+
+    query = sess.query(gene_type_1)
+
+    if name:
+        query = query.filter(gene_type_1.name.in_(name))
+
+    if 'genes' in relations:
+        query = query.join((gene_1, gene_type_1.genes), isouter=True)
+        option_args.append(orm.contains_eager(
+            gene_type_1.genes.of_type(gene_1)))
+
+    if option_args:
+        return query.options(*option_args)
+
+    return build_gene_type_core_request(selection_set, name)
+
+
+def request_gene_types(_obj, info, name=None):
+    query = build_gene_type_request(_obj, info, name=name)
+    return query.distinct().all()
