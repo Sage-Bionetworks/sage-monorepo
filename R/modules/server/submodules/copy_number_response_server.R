4a6049dab465ef864a106e4b34607e4c29c3e506
andrewelamb <andrewelamb@gmail.com>
2020-07-20 11:17:57 -0700

    copy number module working with api queries

--- R/modules/server/submodules/copy_number_response_server.R
+++ R/modules/server/submodules/copy_number_response_server.R
@@ -8,7 +8,7 @@ copy_number_response_server <- function(
     ns <- session$ns
 
     group_tbl <- shiny::reactive({
-        iatlas.app::get_cnv_group_tbl(
+        iatlas.app::query_tags(
             cohort_obj()$dataset,
             cohort_obj()$group_name
         )
@@ -16,9 +16,9 @@ copy_number_response_server <- function(
 
     output$select_cn_group_ui <- shiny::renderUI({
         shiny::selectInput(
-            inputId  = ns("cn_group_point_filter"),
+            inputId  = ns("group_choices"),
             label    = "Select Group Filter",
-            choices  = iatlas.app::get_cnv_group_list(group_tbl()),
+            choices  = iatlas.app::build_cnv_group_list(group_tbl()),
             selected = "All",
             multiple = T
         )
@@ -28,63 +28,68 @@ copy_number_response_server <- function(
         shiny::selectInput(
             inputId  = ns("response_variable"),
             label    = "Select or Search for Response Variable",
-            choices  = build_cnv_feature_list(),
-            selected = iatlas.app::get_feature_id_from_display(
-                "Leukocyte Fraction"
-            )
+            choices = iatlas.app::create_nested_named_list(
+                cohort_obj()$feature_tbl, values_col = "name"
+            ),
+            selected = "leukocyte_fraction"
         )
     })
 
     gene_tbl  <- shiny::reactive(iatlas.app::build_cnv_gene_tbl())
 
+    gene_set_tbl <- shiny::reactive(iatlas.app::query_gene_types())
+
+    gene_choice_list <- shiny::reactive({
+        shiny::req(gene_set_tbl(), gene_tbl())
+        build_cnv_gene_list(gene_set_tbl(), gene_tbl())
+    })
+
     output$select_cn_gene_ui <- shiny::renderUI({
-        shiny::req(gene_tbl())
         shiny::selectInput(
-            ns("cn_gene_point_filter"),
+            ns("gene_filter_choices"),
             "Select Gene Filter",
-            choices = iatlas.app::get_cnv_gene_list(gene_tbl()),
-            selected = 0,
+            choices = gene_choice_list(),
+            selected = "All",
             multiple = T
         )
     })
 
-    gene_ids <- shiny::reactive({
-        shiny::req(input$cn_gene_point_filter)
-        id_choices <- as.integer(input$cn_gene_point_filter)
-        if (0 %in% id_choices) return(0)
-        gene_ids <- purrr::keep(id_choices, id_choices > 0)
-        if (-1 %in% id_choices) {
-            genesets_gene_ids <- get_cnv_im_ids()
-        } else {
-            genesets_gene_ids <- c()
-        }
-        base::union(gene_ids, genesets_gene_ids)
+    gene_entrez_query <- shiny::reactive({
+        shiny::req(input$gene_filter_choices)
+        iatlas.app::get_cnv_entrez_query_from_filters(
+            input$gene_filter_choices,
+            gene_set_tbl(),
+            gene_tbl()
+        )
     })
 
-    tag_ids <- shiny::reactive({
-        shiny::req(input$cn_group_point_filter)
-        if (0 %in% input$cn_group_point_filter) {
-            shiny::req(group_tbl())
-            ids <- dplyr::pull(group_tbl(), .data$id)
-        } else {
-            ids <- input$cn_group_point_filter
-        }
-        return(ids)
+    groups <- shiny::reactive({
+        shiny::req(input$group_choices)
+        if ("All" %in% input$group_choices) return(group_tbl()$name)
+        else return(input$group_choices)
+    })
+
+    direction_query <- shiny::reactive({
+        shiny::req(input$cn_dir_point_filter)
+        if (input$cn_dir_point_filter == "All") return(list())
+        else return(input$cn_dir_point_filter)
     })
 
     result_tbl <- shiny::reactive({
 
         shiny::req(
-            tag_ids(),
-            gene_ids(),
+            groups(),
+            gene_entrez_query(),
             input$response_variable,
-            input$cn_dir_point_filter
+            direction_query()
         )
-        result_tbl <- build_cnv_result_tbl(
-            tag_ids(),
-            gene_ids(),
-            input$response_variable,
-            input$cn_dir_point_filter
+
+        result_tbl <- iatlas.app::query_copy_number_results(
+            datasets = cohort_obj()$dataset,
+            tags = groups(),
+            genes = gene_entrez_query(),
+            features = input$response_variable,
+            direction = direction_query()
         )
         shiny::validate(need(
             all(!is.null(result_tbl), nrow(result_tbl) > 0),
@@ -103,7 +108,7 @@ copy_number_response_server <- function(
 
     output$cnvPlot <- plotly::renderPlotly({
         shiny::req(result_tbl())
-        create_histogram(
+        iatlas.app::create_histogram(
             dplyr::select(result_tbl(), x = t_stat),
             x_lab = 'T statistics, Positive if normal value higher',
             y_lab = 'Number of tests',
@@ -115,7 +120,7 @@ copy_number_response_server <- function(
     output$cnvtable <- DT::renderDataTable({
         shiny::req(result_tbl())
         DT::datatable(
-            build_cnv_dt_tbl(result_tbl()),
+            iatlas.app::build_cnv_dt_tbl(result_tbl()),
             extensions = 'Buttons', options = list(
                 scrollY = '300px',
                 paging = TRUE,
