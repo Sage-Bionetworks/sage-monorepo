47732f37156e2176bb38b326c131b33021cc4c49
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-07-22 01:29:41 +0000

    wip: [#173911953] Created tests and schema for samplesBymMutationStatus query.

--- apps/iatlas/api-gitlab/api/enums.py
+++ apps/iatlas/api-gitlab/api/enums.py
@@ -2,7 +2,7 @@ from sqlalchemy.dialects.postgresql import ENUM
 
 direction_enum = ENUM('Amp', 'Del', name='direction_enum')
 
-status_enum = ENUM('Wt', 'Mut', name='direction_enum')
+status_enum = ENUM('Wt', 'Mut', name='status_enum')
 
 unit_enum = ENUM('Count', 'Fraction', 'Per Megabase',
                  'Score', 'Year', name='unit_enum')
