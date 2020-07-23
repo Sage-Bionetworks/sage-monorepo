8f77e248023ccc140f782779fa5b2d9f5be88133
Rene Martinez <rene.martinez@genui.com>
2020-07-23 16:18:30 +0000

    minor/feature: [#173932654] patient query modifications

--- apps/iatlas/api-gitlab/tests/queries/test_patients_query.py
+++ apps/iatlas/api-gitlab/tests/queries/test_patients_query.py
@@ -13,6 +13,12 @@ def test_patients_query(client, patient):
             height
             race
             weight
+            slides{
+                name
+            }
+            samples{
+                name
+            }
         }
     }"""
     response = client.post(
@@ -23,6 +29,9 @@ def test_patients_query(client, patient):
     assert isinstance(results, list)
     assert len(results) == 1
     for result in results:
+        slides = result['slides']
+        samples = result['samples']
+
         assert type(result['age']) is int or NoneType
         assert result['barcode'] == patient
         assert type(result['ethnicity']) is str or NoneType
@@ -30,3 +39,10 @@ def test_patients_query(client, patient):
         assert type(result['height']) is int or NoneType
         assert type(result['race']) is str or NoneType
         assert type(result['weight']) is int or NoneType
+        if slides:
+            for slide in slides:
+                assert type(slide['name']) is str
+        if samples:
+            for sample in samples:
+                assert type(sample['name']) is str
+
