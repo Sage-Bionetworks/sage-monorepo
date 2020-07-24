bbbbd687bb3dadf37c5d2ea9299804ce6996d63c
andrewelamb <andrewelamb@gmail.com>
2020-07-24 07:54:44 -0700

    added tests to immunomodulators

--- apps/iatlas/app/R/cohort_object_functions.R
+++ apps/iatlas/app/R/cohort_object_functions.R
@@ -14,11 +14,25 @@ get_cohort_feature_class_list <- function(cohort_obj){
 
 query_feature_values_with_cohort_object <- function(
   cohort_object,
-  feature = list(),
-  class = list()
+  feature = NA,
+  class = NA
 ){
   if (cohort_object$group_type == "tag") related <- cohort_object$group_name
-  else related <- list()
+  else related <- NA
   dataset <- cohort_object$dataset
   query_feature_values(dataset, related, feature, class)
+}
+
+# genes -----------------------------------------------------------------------
+
+query_gene_expression_with_cohort_object <- function(
+  cohort_object,
+  gene_types = NA,
+  entrez_ids = NA
+){
+  query_expression_by_genes(
+    type = gene_types,
+    entrez = entrez_ids,
+    sample = cohort_object$sample_tbl$sample
+  )
 }
--- apps/iatlas/app/R/immunomodulators_functions.R
+++ apps/iatlas/app/R/immunomodulators_functions.R
@@ -22,23 +22,22 @@ get_im_hgnc_from_tbl <- function(tbl, .entrez){
 }
 
 
-#' #' Build Immunomodulators Distributions Plot Tibble
-#' #'
-#' #' @param gene_id An integer in the gene_id column of the genes_types table
-#' #' @param sample_tbl A tibble with columns sample, group
-#' #' @param scale_method A string, that is a scaling method
-#' #' @importFrom magrittr %>%
-#' #' @importFrom dplyr select inner_join
-#' #' @importFrom rlang .data
-#' build_im_distplot_tbl <- function(gene, sample_tbl, scale_method){
-#'     gene %>%
-#'         as.integer() %>%
-#'         build_gene_expression_tbl_by_gene_ids() %>%
-#'         dplyr::inner_join(sample_tbl, by = "sample_id") %>%
-#'         dplyr::select(.data$group, value = .data$rna_seq_expr) %>%
-#'         scale_tbl_value_column(scale_method) %>%
-#'         dplyr::select(x = .data$group, y = .data$value)
-#' }
+#' Build Immunomodulators Distributions Plot Tibble
+#'
+#' @param gene_id An integer in the gene_id column of the genes_types table
+#' @param sample_tbl A tibble with columns sample, group
+#' @param scale_method A string, that is a scaling method
+#' @importFrom magrittr %>%
+#' @importFrom dplyr select inner_join
+#' @importFrom rlang .data
+build_im_distplot_tbl <- function(cohort_object, gene, scale_method){
+    cohort_object %>%
+        query_gene_expression_with_cohort_object(gene) %>%
+        dplyr::inner_join(cohort_object$sample_tbl, by = "sample") %>%
+        dplyr::select(.data$group, "value" = .data$rna_seq_expr) %>%
+        scale_tbl_value_column(scale_method) %>%
+        dplyr::select("x" = .data$group, "y" = .data$value)
+}
 
 #' Build Immunomodulators Datatable Tibble
 #'
