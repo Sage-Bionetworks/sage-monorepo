622cae4127ef0d821cc5767382558919c888ba54
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-07-28 03:37:03 +0000

    patch: [#174029671] Features may now return the min or max values of a feature.

--- apps/iatlas/api-gitlab/api/resolvers/features_by_class_resolver.py
+++ apps/iatlas/api-gitlab/api/resolvers/features_by_class_resolver.py
@@ -1,10 +1,13 @@
 from itertools import groupby
-from .resolver_helpers import build_feature_graphql_response, get_value, request_features
+from .resolver_helpers import build_feature_graphql_response, get_max_min_feature_values, get_value, request_features
 
 
 def resolve_features_by_class(_obj, info, dataSet=None, feature=None, featureClass=None, related=None, sample=None, tag=None):
     feature_results = request_features(_obj, info, data_set=dataSet, feature=feature, feature_class=featureClass,
                                        related=related, sample=sample, tag=tag, by_class=True)
+
+    max_min_dict = get_max_min_feature_values(
+        info, features=feature_results, by_class=True)
 
     class_dict = dict()
     for feature_class, features_list in groupby(feature_results, key=lambda f: get_value(f, 'class')):
@@ -14,7 +17,8 @@ def resolve_features_by_class(_obj, info, dataSet=None, feature=None, featureCla
     return_list = []
     append_to_list = return_list.append
     for feature_class, features_list in class_dict.items():
-        features = list(map(build_feature_graphql_response, features_list))
+        features = list(
+            map(build_feature_graphql_response(max_min_dict), features_list))
         append_to_list({
             'class': feature_class,
             'features': features
--- apps/iatlas/api-gitlab/api/resolvers/features_by_tag_resolver.py
+++ apps/iatlas/api-gitlab/api/resolvers/features_by_tag_resolver.py
@@ -1,10 +1,13 @@
 from itertools import groupby
-from .resolver_helpers import build_feature_graphql_response, get_value, request_features
+from .resolver_helpers import build_feature_graphql_response, get_max_min_feature_values, get_value, request_features
 
 
 def resolve_features_by_tag(_obj, info, dataSet=None, feature=None, featureClass=None, related=None, sample=None, tag=None):
     feature_results = request_features(_obj, info, data_set=dataSet, feature=feature, feature_class=featureClass,
                                        related=related, sample=sample, tag=tag, by_tag=True)
+
+    max_min_dict = get_max_min_feature_values(
+        info, features=feature_results, by_tag=True)
 
     tag_dict = dict()
     for feature_tag, features_list in groupby(feature_results, key=lambda f: f.tag):
@@ -14,7 +17,8 @@ def resolve_features_by_tag(_obj, info, dataSet=None, feature=None, featureClass
     return_list = []
     append_to_list = return_list.append
     for feature_tag, features_list in tag_dict.items():
-        features = list(map(build_feature_graphql_response, features_list))
+        features = list(
+            map(build_feature_graphql_response(max_min_dict), features_list))
         append_to_list({
             'characteristics': get_value(features[0], 'tag_characteristics'),
             'color': get_value(features[0], 'tag_color'),
