86e8f7c6c7ab91a7f124b4e422eef6d47f0f46cf
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-07-30 19:44:44 +0000

    patch/fix: [#174074393] Fixed samples query.

--- apps/iatlas/api-gitlab/api/resolvers/samples_resolver.py
+++ apps/iatlas/api-gitlab/api/resolvers/samples_resolver.py
@@ -2,6 +2,6 @@ from .resolver_helpers import build_sample_graphql_response, request_samples
 
 
 def resolve_samples(_obj, info, name=None, patient=None):
-    samples = request_samples(_obj, info, name=name, patient=patient)
+    samples = request_samples(_obj, info, sample=name, patient=patient)
 
     return map(build_sample_graphql_response, samples)
