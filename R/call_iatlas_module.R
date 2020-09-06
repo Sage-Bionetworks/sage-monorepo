25c1cc71f9c0a4fb1d69c32634acb8bda918a136
andrewelamb <andrewelamb@gmail.com>
2020-09-06 15:39:57 -0700

    temp working

--- R/call_iatlas_module.R
+++ R/call_iatlas_module.R
@@ -3,33 +3,25 @@ call_iatlas_module <- function(
   input,
   session,
   ...,
-  observe_event = T,
   tab_id = "explorertabs"
 ){
-  module_string   <- name
   function_string <- stringr::str_c(name, "_server")
-  link_string     <- paste0("link_to_", module_string)
-  # function_string <- stringr::str_remove(basename(file), ".R")
-  # module_string   <- stringr::str_remove(basename(file), "_server.R")
-  # link_string     <- paste0("link_to_", module_string)
-  if (observe_event) {
-    if (tab_id == "explorertabs") {
-      update_function <- shinydashboard::updateTabItems
-      tab_name        <- module_string
-    } else if (tab_id == "toolstabs") {
-      update_function <- shiny::updateNavlistPanel
-      tab_name        <- module_string %>%
-        stringr::str_replace_all("_", " ") %>%
-        stringr::str_to_title(.)
-    }
-    shiny::observeEvent(input[[link_string]], {
-      update_function(
-        session,
-        tab_id,
-        tab_name
-      )
-    })
+  link_string     <- paste0("link_to_", name)
+  if (tab_id == "explorertabs") {
+    update_function <- shinydashboard::updateTabItems
+    tab_name        <- name
+  } else if (tab_id == "toolstabs") {
+    update_function <- shiny::updateNavlistPanel
+    tab_name        <- name %>%
+      stringr::str_replace_all("_", " ") %>%
+      stringr::str_to_title(.)
   }
-
-  get(function_string)(module_string, ...)
+  shiny::observeEvent(input[[link_string]], {
+    update_function(
+      session,
+      tab_id,
+      tab_name
+    )
+  })
+  get(function_string)(name, ...)
 }
