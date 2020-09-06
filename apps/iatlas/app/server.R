873cb1be6ba2ed4890bf2ecb30a2c4c6a34eef61
andrewelamb <andrewelamb@gmail.com>
2020-09-06 11:03:23 -0700

    moved all server modules to R

--- apps/iatlas/app/server.R
+++ apps/iatlas/app/server.R
@@ -8,49 +8,66 @@ options(shiny.usecairo = FALSE)
 
 library(magrittr)
 
+modules_tbl <- dplyr::tribble(
+  ~path,                                  ~type,
+  "R/cellimage_server.R",                 "analysis",
+  "R/clinical_outcomes_server.R",         "analysis",
+  "R/copy_number_server.R",               "analysis",
+  "R/driver_associations_server.R",       "analysis",
+  "R/extracellular_network_server.R",     "analysis",
+  "R/immune_features_server.R",           "analysis",
+  "R/immunomodulators_server.R",          "analysis",
+  "R/io_targets_server.R",                "analysis",
+  "R/til_maps_server.R",                  "analysis",
+  "R/tumor_microenvironment_server.R",    "analysis",
+  "R/immune_subtype_classifier_server.R", "tool"
+)
+
 ################################################################################
 # Begin Shiny Server definition.
 ################################################################################
 shiny::shinyServer(function(input, output, session) {
 
-    shiny::observe({
-        query <- shiny::parseQueryString(session$clientData$url_search)
-        if (!is.null(query[['module']])) {
-            shinydashboard::updateTabItems(
-                session,
-                "explorertabs",
-                query[['module']]
-            )
-        }
-    })
-
-    # Non analysis modules -----------------------------------------------------
-
-    cohort_obj <- call_iatlas_module(
-        "R/modules/server/other_modules/cohort_selection_server.R",
-        input,
-        session
-    )
-
-    call_iatlas_module(
-        "R/modules/server/other_modules/data_info_server.R",
-        input,
+  shiny::observe({
+    query <- shiny::parseQueryString(session$clientData$url_search)
+    if (!is.null(query[['module']])) {
+      shinydashboard::updateTabItems(
         session,
-        observe_event = F
+        "explorertabs",
+        query[['module']]
+      )
+    }
+  })
+
+  # Non analysis modules -----------------------------------------------------
+
+  cohort_obj <- call_iatlas_module(
+    "R/cohort_selection_server.R",
+    input,
+    session
+  )
+
+  call_iatlas_module(
+    "R/data_info_server.R",
+    input,
+    session,
+    observe_event = F
+  )
+
+  # Analysis modules --------------------------------------------------------
+  modules_tbl %>%
+    dplyr::filter(.data$type == "analysis") %>%
+    dplyr::pull("path") %>%
+    purrr::walk(iatlas.app::call_iatlas_module, input, session, cohort_obj)
+
+  # Tool modules --------------------------------------------------------
+
+  modules_tbl %>%
+    dplyr::filter(.data$type == "tool") %>%
+    dplyr::pull("path") %>%
+    purrr::walk(
+      iatlas.app::call_iatlas_module, input, session, tab_id = "toolstabs"
     )
-
-    # Analysis modules --------------------------------------------------------
-    "R/modules/server/analysis_modules/" %>%
-        list.files(full.names = T) %>%
-        purrr::walk(iatlas.app::call_iatlas_module, input, session, cohort_obj)
-
-    # Tool modules --------------------------------------------------------
-
-    "R/modules/server/tool_modules/" %>%
-        list.files(full.names = T) %>%
-        purrr::walk(
-            iatlas.app::call_iatlas_module, input, session, tab_id = "toolstabs"
-        )
 })
 
 
