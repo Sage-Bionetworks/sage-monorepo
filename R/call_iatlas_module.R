536531d2720080f4e3f4c4327442abbe259224a3
andrewelamb <andrewelamb@gmail.com>
2020-09-06 13:20:12 -0700

    using module config file

--- R/call_iatlas_module.R
+++ R/call_iatlas_module.R
@@ -1,32 +1,35 @@
 call_iatlas_module <- function(
-    file,
-    input,
-    session,
-    ...,
-    observe_event = T,
-    tab_id = "explorertabs"
+  name,
+  input,
+  session,
+  ...,
+  observe_event = T,
+  tab_id = "explorertabs"
 ){
-    function_string <- stringr::str_remove(basename(file), ".R")
-    module_string   <- stringr::str_remove(basename(file), "_server.R")
-    link_string     <- paste0("link_to_", module_string)
-    if (observe_event) {
-        if (tab_id == "explorertabs") {
-            update_function <- shinydashboard::updateTabItems
-            tab_name        <- module_string
-        } else if (tab_id == "toolstabs") {
-            update_function <- shiny::updateNavlistPanel
-            tab_name        <- module_string %>%
-                stringr::str_replace_all("_", " ") %>%
-                stringr::str_to_title(.)
-        }
-        shiny::observeEvent(input[[link_string]], {
-            update_function(
-                session,
-                tab_id,
-                tab_name
-            )
-        })
+  module_string   <- name
+  function_string <- stringr::str_c(name, "_server")
+  link_string     <- paste0("link_to_", module_string)
+  # function_string <- stringr::str_remove(basename(file), ".R")
+  # module_string   <- stringr::str_remove(basename(file), "_server.R")
+  # link_string     <- paste0("link_to_", module_string)
+  if (observe_event) {
+    if (tab_id == "explorertabs") {
+      update_function <- shinydashboard::updateTabItems
+      tab_name        <- module_string
+    } else if (tab_id == "toolstabs") {
+      update_function <- shiny::updateNavlistPanel
+      tab_name        <- module_string %>%
+        stringr::str_replace_all("_", " ") %>%
+        stringr::str_to_title(.)
     }
+    shiny::observeEvent(input[[link_string]], {
+      update_function(
+        session,
+        tab_id,
+        tab_name
+      )
+    })
+  }
 
-    get(function_string)(module_string, ...)
+  get(function_string)(module_string, ...)
 }
