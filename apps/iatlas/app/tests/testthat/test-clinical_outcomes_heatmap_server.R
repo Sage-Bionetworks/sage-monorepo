1adbe861b24522a9fbff22ac76b724b9dbb8be82
andrewelamb <andrewelamb@gmail.com>
2020-09-06 09:50:28 -0700

    added tests to clinical outcomes submodules

---
+++ apps/iatlas/app/tests/testthat/test-clinical_outcomes_heatmap_server.R
@@ -0,0 +1,15 @@
+test_that("module_works", {
+  shiny::testServer(
+    clinical_outcomes_heatmap_server,
+    args = list(
+      "cohort_obj" = shiny::reactiveVal(tcga_immune_subtype_cohort_obj_50)
+    ),
+    {
+      session$setInputs("time_feature_choice" = "OS_time")
+      session$setInputs("class_choice" = "DNA Alteration")
+      expect_type(output$class_selection_ui, "list")
+      expect_type(output$time_feature_selection_ui, "list")
+      expect_type(output$heatmap, "character")
+    }
+  )
+})
