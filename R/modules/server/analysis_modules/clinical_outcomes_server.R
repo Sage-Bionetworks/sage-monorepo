a3728ec60db686d7942ba1f6a7e6e72b48ef2fff
andrewelamb <andrewelamb@gmail.com>
2020-09-06 08:58:51 -0700

    clincal_outcomes modules now working with call_module2

--- R/modules/server/analysis_modules/clinical_outcomes_server.R
+++ R/modules/server/analysis_modules/clinical_outcomes_server.R
@@ -8,25 +8,29 @@ clinical_outcomes_server <- function(
 
       source_files <- c(
         "R/modules/server/submodules/clinical_outcomes_survival_server.R",
-        "R/modules/server/submodules/clinical_outcomes_heatmap_server.R"
+        "R/modules/server/submodules/clinical_outcomes_heatmap_server.R",
+        "R/modules/ui/submodules/clinical_outcomes_survival_ui.R",
+        "R/modules/ui/submodules/clinical_outcomes_heatmap_ui.R"
       )
 
       for (file in source_files) {
         source(file, local = T)
       }
 
-      call_module_server(
+      call_module_server2(
         "clinical_outcomes_survival",
         cohort_obj,
         server_function = clinical_outcomes_survival_server,
+        ui_function = clinical_outcomes_survival_ui,
         test_function = shiny::reactive(show_co_submodules)
       )
 
-      call_module_server(
+      call_module_server2(
         "clinical_outcomes_heatmap",
         cohort_obj,
-        test_function = shiny::reactive(show_co_submodules),
-        server_function = clinical_outcomes_heatmap_server
+        server_function = clinical_outcomes_heatmap_server,
+        ui_function = clinical_outcomes_heatmap_ui,
+        test_function = shiny::reactive(show_co_submodules)
       )
     }
   )
