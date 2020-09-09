94710432490bbdaeb00d67e258b96378eed294d5
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-09-09 23:08:56 +0000

    patch: [#174740513] Changed patients' age to age_at_diagnosis.

--- apps/iatlas/api-gitlab/api/db_models/patient.py
+++ apps/iatlas/api-gitlab/api/db_models/patient.py
@@ -7,7 +7,7 @@ from api.enums import ethnicity_enum, gender_enum, race_enum
 class Patient(Base):
     __tablename__ = 'patients'
     id = db.Column(db.Integer, primary_key=True)
-    age = db.Column(db.Integer, nullable=True)
+    age_at_diagnosis = db.Column(db.Integer, nullable=True)
     barcode = db.Column(db.String, nullable=False)
     ethnicity = db.Column(ethnicity_enum, nullable=True)
     gender = db.Column(gender_enum, nullable=True)
