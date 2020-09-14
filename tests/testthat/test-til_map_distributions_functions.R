c35ae20dddd3ff50db7f820fcdc496e92d7fd160
andrewelamb <andrewelamb@gmail.com>
2020-09-14 08:36:49 -0700

    some updating of tilmap modules

--- tests/testthat/test-til_map_distributions_functions.R
+++ tests/testthat/test-til_map_distributions_functions.R
@@ -1,13 +1,12 @@
-
-cohort_obj1 <- tcga_immune_subtype_cohort_obj_50
-
 test_that("Create Tilmap Named List", {
-    result1 <- create_tm_named_list(cohort_obj1)
+    result1 <- create_tm_named_list(tcga_immune_subtype_cohort_obj_50)
     expect_named(result1)
 })
 
 test_that("Build Tilmap Distplot Tibble", {
-    result1 <- build_tm_distplot_tbl(cohort_obj1, "til_percentage", "None")
+    result1 <- build_tm_distplot_tbl(
+      tcga_immune_subtype_cohort_obj_50, "til_percentage", "None"
+    )
     expect_named(result1, c("x", "y"))
 })
 
