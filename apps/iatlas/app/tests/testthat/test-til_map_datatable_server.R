0efbbca5fb032e8632c905d859a805bd3abd7e3f
andrewelamb <andrewelamb@gmail.com>
2020-09-14 08:36:49 -0700

    some updating of tilmap modules

---
+++ apps/iatlas/app/tests/testthat/test-til_map_datatable_server.R
@@ -0,0 +1,11 @@
+test_that("til_map_datatable_server", {
+  shiny::testServer(
+    til_map_datatable_server,
+    args = list(
+      "cohort_obj" = shiny::reactiveVal(tcga_immune_subtype_cohort_obj_50)
+    ),
+    {
+      expect_true(T)
+    }
+  )
+})
