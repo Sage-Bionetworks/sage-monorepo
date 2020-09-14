dab5c111a9facbfcb0dd5c25e778268b6e673208
andrewelamb <andrewelamb@gmail.com>
2020-09-14 09:58:36 -0700

    added tests

--- apps/iatlas/app/R/immunomodulators_ui.R
+++ apps/iatlas/app/R/immunomodulators_ui.R
@@ -1,24 +1,24 @@
 immunomodulators_ui <- function(id) {
 
-    ns <- shiny::NS(id)
+  ns <- shiny::NS(id)
 
-    shiny::tagList(
-        titleBox("iAtlas Explorer — Immunomodulators"),
-        textBox(
-            width = 12,
-            shiny::p(paste0(
-                "Explore the expression of genes that code for ",
-                "immunomodulating proteins, including checkpoint proteins."
-            )),
-        ),
-        sectionBox(
-            title = "Immunomodulator Distributions",
-            module_ui(ns("distributions"))
-        ),
-        sectionBox(
-            title = "Immunomodulator Annotations",
-            module_ui(ns("datatable")
-                      )
-        )
+  shiny::tagList(
+    titleBox("iAtlas Explorer — Immunomodulators"),
+    textBox(
+      width = 12,
+      shiny::p(paste0(
+        "Explore the expression of genes that code for ",
+        "immunomodulating proteins, including checkpoint proteins."
+      )),
+    ),
+    sectionBox(
+      title = "Immunomodulator Distributions",
+      module_ui(ns("distributions"))
+    ),
+    sectionBox(
+      title = "Immunomodulator Annotations",
+      module_ui(ns("datatable")
+      )
     )
+  )
 }
---
+++ apps/iatlas/app/tests/testthat/test-driver_associations_ui.R
@@ -0,0 +1,3 @@
+test_that("driver_associations_ui", {
+  expect_type(driver_associations_ui("id"), "list")
+})
