fbeacd4bd716e67fec37a30e8ccc9ed0cbb380ae
Geoffrey Roth <geoffroth@gmail.com>
2020-09-24 11:16:07 -0400

    hasNextPage false when paginating backward, hasPreviousPage false when paginating forward

--- apps/iatlas/api-gitlab/api/resolvers/resolver_helpers/cursor_utils.py
+++ apps/iatlas/api-gitlab/api/resolvers/resolver_helpers/cursor_utils.py
@@ -6,17 +6,17 @@ ASC = 'ASC'
 DESC = 'DESC'
 
 def to_cursor_hash(val):
-    return str(base64.urlsafe_b64encode(str(val).encode("utf-8")), "utf-8")
+    return str(base64.b64encode(str(val).encode("utf-8")), "utf-8")
 
 def from_cursor_hash(encoded):
     return str(base64.b64decode(str(encoded)), "utf-8")
 
 def get_cursor(before, after):
     if after != None:
-        return (from_cursor_hash(after), 'ASC')
+        return (from_cursor_hash(after), ASC)
     if before != None:
-        return (from_cursor_hash(before), 'DESC')
-    return (None, 'ASC')
+        return (from_cursor_hash(before), DESC)
+    return (None, ASC)
 
 def get_limit(first, last):
     if first and not math.isnan(first):
