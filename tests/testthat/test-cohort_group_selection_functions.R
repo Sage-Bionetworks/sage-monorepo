726215b2fba88bbda8ab3d81407aea55e134aaf1
andrewelamb <andrewelamb@gmail.com>
2020-10-01 07:30:45 -0700

    fix test

--- tests/testthat/test-cohort_group_selection_functions.R
+++ tests/testthat/test-cohort_group_selection_functions.R
@@ -1,37 +1,9 @@
-
-# tcga_samples_50 = "TCGA" %>%
-#   iatlas.api.client::query_dataset_samples(.) %>%
-#   dplyr::slice(1:50) %>%
-#   dplyr::pull("name")
-#
-# pcawg_samples_50 = "PCAWG" %>%
-#   iatlas.api.client::query_dataset_samples(.) %>%
-#   dplyr::slice(1:50) %>%
-#   dplyr::pull("name")
-
-# filter_obj1 <- list(
-#     "samples" = tcga_samples_50,
-#     "filters" = list(
-#         "feature_filters" = list(),
-#         "group_filters" = list()
-#     )
-# )
-#
-# filter_obj2 <- list(
-#   "samples" = pcawg_samples_50,
-#   "filters" = list(
-#     "feature_filters" = list(),
-#     "group_filters" = list()
-#   )
-# )
-
 tag_group_tbl <- iatlas.api.client::query_dataset_tags("PCAWG")
 custom_group_tbl <- build_custom_group_tbl("PCAWG")
 available_groups_list <- build_cohort_group_list(
   tag_group_tbl, custom_group_tbl
 )
 
-# mutation_tbl <- build_cohort_mutation_tbl()
 feature_bin_tbl <- "PCAWG" %>%
   iatlas.api.client::query_features_by_class() %>%
   dplyr::select("class", "display", "name")
