9e628a70d0f2c0711cf4fe89d8a5ae585923a001
andrewelamb <andrewelamb@gmail.com>
2020-10-06 11:30:32 -0700

    temp commit, starting to use iatlas.module

--- apps/iatlas/app/tests/testthat/test-tumor_microenvironment_cell_proportions_server.R
+++ apps/iatlas/app/tests/testthat/test-tumor_microenvironment_cell_proportions_server.R
@@ -5,8 +5,8 @@ test_that("module_works", {
       "cohort_obj" = shiny::reactiveVal(tcga_immune_subtype_cohort_obj_50)
     ),
     {
-      expect_type(output$barplot, "character")
-      expect_error(output$scatterplot, class = "shiny.silent.error")
+      expect_type(plot_data(), "list")
+      expect_enamed(plot_data(), c("sample", "x", "y", "color"))
     }
   )
 })
