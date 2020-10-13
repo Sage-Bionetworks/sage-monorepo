4fd4906d5d0f737ee76be934d50b6b8917c5b4f3
andrewelamb <andrewelamb@gmail.com>
2020-10-13 07:51:59 -0700

    temp commit

--- R/tumor_microenvironment_cell_proportions_server.R
+++ R/tumor_microenvironment_cell_proportions_server.R
@@ -6,94 +6,37 @@ tumor_microenvironment_cell_proportions_server  <- function(
     id,
     function(input, output, session) {
 
-      plot_data <- shiny::reactive({
-        cohort_obj() %>%
-          build_ocp_value_tbl() %>%
-          dplyr::select(
-            "sample",
-            "x" = "group",
-            "y" = "feature_value",
-            "color" = "feature_display"
-          )
+      plot_data_function <- shiny::reactive({
+        function(.feature_class){
+          group_data <- cohort_obj()$group_tbl %>%
+            dplyr::select("group", "group_description" = "characteristics")
+
+          cohort_obj() %>%
+            query_feature_values_with_cohort_object(
+              feature = c(
+                "leukocyte_fraction", "Stromal_Fraction", "Tumor_fraction"
+              )
+            ) %>%
+            dplyr::inner_join(cohort_obj()$sample_tbl, by = "sample") %>%
+            dplyr::inner_join(group_data, by = "group") %>%
+            dplyr::select(
+              "sample",
+              "group",
+              "feature" = "feature_display",
+              "feature_value",
+              "group_description"
+            )
+        }
       })
 
       iatlas.modules::barplot_server(
-        "tumor_microenvironment_cell_proportions",
-        plot_data,
-        group_data = shiny::reactive(cohort_obj()$group_tbl),
+        "barplot",
+        plot_data_function,
         barplot_xlab  = shiny::reactive("Fraction type by group"),
         barplot_ylab  = shiny::reactive("Fraction mean"),
-        barplot_label = shiny::reactive("Sample"),
+        barplot_label = shiny::reactive("Fraction"),
         drilldown     = shiny::reactive(T)
       )
-
-      # barplot_tbl <- shiny::reactive({
-      #   req(value_tbl())
-      #   build_ocp_barplot_tbl(value_tbl())
-      # })
-      #
-      # output$barplot <- plotly::renderPlotly({
-      #   shiny::req(barplot_tbl())
-      #
-      #   create_barplot(
-      #     barplot_tbl(),
-      #     source_name = "overall_cell_proportions_barplot",
-      #     color_col = "color",
-      #     label_col = "label",
-      #     xlab = "Fraction type by group",
-      #     ylab = "Fraction mean"
-      #   )
-      # })
-      #
-      # barplot_eventdata <- shiny::reactive({
-      #   plotly::event_data("plotly_click", "overall_cell_proportions_barplot")
-      # })
-      #
-      # plotly_server(
-      #   "barplot",
-      #   plot_tbl       = barplot_tbl,
-      #   plot_eventdata = barplot_eventdata,
-      #   group_tbl      = shiny::reactive(cohort_obj()$group_tbl)
-      # )
-      #
-      # barplot_selected_group <- shiny::reactive({
-      #   shiny::req(barplot_eventdata())
-      #   barplot_eventdata()$x[[1]]
-      # })
-      #
-      # scatterplot_tbl <- shiny::reactive({
-      #   shiny::req(value_tbl(), barplot_selected_group())
-      #   build_ocp_scatterplot_tbl(
-      #     value_tbl(),
-      #     barplot_selected_group()
-      #   )
-      # })
-      #
-      # output$scatterplot <- plotly::renderPlotly({
-      #   shiny::validate(shiny::need(barplot_eventdata(), "Click above plot"))
-      #   shiny::req(value_tbl())
-      #
-      #   groups <- dplyr::pull(value_tbl(), group)
-      #   shiny::validate(shiny::need(
-      #     barplot_selected_group() %in% groups,
-      #     "Click above barchart"
-      #   ))
-      #   create_scatterplot(
-      #     scatterplot_tbl(),
-      #
-      #     source_name = "overall_cell_proportions_scatterplot",
-      #     xlab = "Stromal Fraction",
-      #     ylab = "Leukocyte Fraction",
-      #     label_col = "label",
-      #     title = barplot_selected_group(),
-      #     identity_line = TRUE
-      #   )
-      # })
-      #
-      # plotly_server(
-      #   "scatterplot",
-      #   plot_tbl = barplot_tbl
-      # )
     }
   )
 }
--- tests/testthat/test-tumor_microenvironment_type_fractions_server.R
+++ tests/testthat/test-tumor_microenvironment_type_fractions_server.R
@@ -1,12 +1,25 @@
-test_that("module_works", {
+test_that("tumor_microenvironment_type_fractions_server_tcga", {
   shiny::testServer(
     tumor_microenvironment_type_fractions_server,
     args = list(
       "cohort_obj" = shiny::reactiveVal(tcga_immune_subtype_cohort_obj_50)
     ),
     {
-      session$setInputs("fraction_group_choice" = "Immune Cell Proportion - Differentiated Lymphoid and Myeloid Cell Derivative Class")
-      expect_type(output$barplot, "character")
+      expect_type(feature_classes(), "character")
+      expect_type(plot_data_function(), "closure")
+    }
+  )
+})
+
+test_that("tumor_microenvironment_type_fractions_server_pcawg", {
+  shiny::testServer(
+    tumor_microenvironment_type_fractions_server,
+    args = list(
+      "cohort_obj" = shiny::reactiveVal(pcawg_immune_subtype_cohort_obj)
+    ),
+    {
+      expect_type(feature_classes(), "character")
+      expect_type(plot_data_function(), "closure")
     }
   )
 })
