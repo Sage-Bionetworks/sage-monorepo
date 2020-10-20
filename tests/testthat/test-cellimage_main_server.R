69ce013dcf845628969cf2c997e575993a4614bd
andrewelamb <andrewelamb@gmail.com>
2020-10-20 12:49:33 -0700

    some work done, but cellimage network missing from db

--- tests/testthat/test-cellimage_main_server.R
+++ tests/testthat/test-cellimage_main_server.R
@@ -1,10 +1,17 @@
-# test_that("module_works", {
-#   shiny::testServer(
-#     cellimage_main_server,
-#     args = list(
-#       "cohort_obj" = shiny::reactiveVal(tcga_immune_subtype_cohort_obj)
-#     ),
-#     {
-#     }
-#   )
-# })
+test_that("cellimage_main_server", {
+  shiny::testServer(
+    cellimage_main_server,
+    args = list(
+      "cohort_obj" = shiny::reactiveVal(tcga_immune_subtype_cohort_obj)
+    ),
+    {
+      expect_equal(cohort_groups(), c("C1", "C2", "C3", "C4", "C5", "C6"))
+      expect_type(output$select_group1_ui, "list")
+      expect_type(output$select_group2_ui, "list")
+      session$setInputs("group_selected1" = "C1")
+      session$setInputs("group_selected2" = "C1")
+      # print(gene_nodes1())
+      print(feature_nodes1())
+    }
+  )
+})
