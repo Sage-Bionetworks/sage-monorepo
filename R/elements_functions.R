778a38a7a83e161988d307c5087ed094f4472bfb
andrewelamb <andrewelamb@gmail.com>
2020-10-26 09:43:35 -0700

    updated api client

--- R/elements_functions.R
+++ R/elements_functions.R
@@ -1,18 +1,3 @@
-#' #' Build Numeric Filter Tibble
-#' #'
-#' #' @param feature_id An integer in the feature_id column of the
-#' #' features_to_samples table
-#' #' @importFrom magrittr %>%
-#' #' @importFrom dplyr summarise
-#' #' @importFrom rlang .data
-#' build_numeric_filter_tbl <- function(feature_id){
-#'     feature_id %>%
-#'         build_feature_value_tbl_from_ids() %>%
-#'         dplyr::summarise(
-#'             feature_max = max(.data$value),
-#'             feature_min = min(.data$value)
-#'         )
-#' }
 
 #' Build Tag Filter Named List
 #'
@@ -20,6 +5,9 @@
 #' @importFrom magrittr %>%
 #' @importFrom tibble deframe
 build_tag_filter_list <- function(parent_tag_name, dataset){
-  iatlas.api.client::query_tags(dataset, parent_tag_name) %>%
-        dplyr::pull("name")
+  iatlas.api.client::query_tags(
+    datasets = dataset,
+    parent_tags = parent_tag_name
+  ) %>%
+    dplyr::pull("name")
 }
