f82cdb14bdb0987bf9a283b153139c2afb74654a
andrewelamb <andrewelamb@gmail.com>
2020-12-03 09:55:49 -0800

    remove uneeded code and tests

--- R/immune_feature_distributions_functions.R
+++
@@ -1,9 +0,0 @@
-
-build_ifd_distplot_tbl <- function(cohort_object, feature_name, scale_method){
-    cohort_object %>%
-        query_feature_values_with_cohort_object(feature = feature_name) %>%
-        dplyr::inner_join(cohort_object$sample_tbl, by = "sample") %>%
-        dplyr::select("group", "value" = "feature_value") %>%
-        scale_tbl_value_column(scale_method) %>%
-        dplyr::rename(x = .data$group, y = .data$value)
-}
--- tests/testthat/test-immune_feature_distributions_functions.R
+++
@@ -1,8 +0,0 @@
-cohort_obj1 <- pcawg_immune_subtype_cohort_obj
-
-test_that("Build Distribution Plot Tibble", {
-
-  result1 <- build_ifd_distplot_tbl(cohort_obj1, "leukocyte_fraction", "None")
-  expect_named(result1, c("x", "y"))
-})
-
