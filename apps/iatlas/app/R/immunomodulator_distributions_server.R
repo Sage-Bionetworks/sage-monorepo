bcfafdc5cb1622b3e353fbc1a3e3c593277f1c1e
andrewelamb <andrewelamb@gmail.com>
2020-12-03 09:21:32 -0800

    all distribution modules now using iatlas.modules

--- apps/iatlas/app/R/immunomodulator_distributions_server.R
+++ apps/iatlas/app/R/immunomodulator_distributions_server.R
@@ -8,63 +8,44 @@ immunomodulator_distributions_server <- function(
 
       ns <- session$ns
 
-      im_tbl <- shiny::reactive(iatlas.api.client::query_immunomodulators())
-
-      output$gene_choice_ui <- shiny::renderUI({
-        shiny::req(input$gene_group_choice, im_tbl())
-        shiny::selectInput(
-          ns("gene_choice_entrez"),
-          label = "Select or Search Gene",
-          choices = create_im_gene_list(
-            im_tbl(),
-            input$gene_group_choice
+      features <- shiny::reactive({
+        iatlas.api.client::query_immunomodulators() %>%
+          dplyr::select(
+            "feature_name" = "entrez",
+            "feature_display" = "hgnc",
+            "Gene Family" = "gene_family",
+            "Gene Function" = "gene_function",
+            "Immune Checkpoint" = "immune_checkpoint",
+            "Super Category" = "super_category"
           )
-        )
       })
 
-      gene_choice_hgnc <- shiny::reactive({
-        shiny::req(input$gene_choice_entrez, im_tbl())
-        get_im_hgnc_from_tbl(im_tbl(), input$gene_choice_entrez)
+      plot_data_function <- shiny::reactive({
+        function(.feature){
+          group_data <- cohort_obj()$group_tbl %>%
+            dplyr::select("group", "group_description" = "characteristics", "color")
+          cohort_obj() %>%
+            query_gene_expression_with_cohort_object(entrez = as.integer(.feature)) %>%
+            dplyr::inner_join(cohort_obj()$sample_tbl, by = "sample") %>%
+            dplyr::inner_join(group_data, by = "group") %>%
+            dplyr::select(
+              "sample",
+              "group",
+              "feature" = "hgnc",
+              "feature_value" = "rna_seq_expr",
+              "group_description",
+              "color"
+            )
+        }
       })
 
-      gene_plot_label <- shiny::reactive({
-        shiny::req(gene_choice_hgnc(), input$scale_method_choice)
-
-        transform_feature_string(
-          gene_choice_hgnc(),
-          input$scale_method_choice
-        )
-      })
-
-      distplot_tbl <- shiny::reactive({
-        shiny::req(input$gene_choice_entrez, input$scale_method_choice)
-
-        tbl <- build_im_distplot_tbl(
-          cohort_obj(),
-          as.integer(input$gene_choice_entrez),
-          input$scale_method_choice
-        )
-
-        shiny::validate(need(
-          nrow(tbl) > 0,
-          paste0(
-            "Current selected cohort has no expression data for current ",
-            "selected gene."
-          )
-        ))
-
-        return(tbl)
-      })
-
-      distribution_plot_server(
-        "immunomodulators_dist_plot",
-        cohort_obj,
-        distplot_tbl    = distplot_tbl,
-        distplot_type   = shiny::reactive(input$plot_type_choice),
-        distplot_ylab   = gene_plot_label,
-        distplot_title  = gene_choice_hgnc
+      iatlas.modules::distributions_plot_server(
+        "distplot",
+        plot_data_function,
+        features   = features,
+        distplot_xlab = shiny::reactive(cohort_obj()$group_name),
+        drilldown  = shiny::reactive(T)
       )
-
     }
   )
 }
--- apps/iatlas/app/R/io_target_distributions_server.R
+++ apps/iatlas/app/R/io_target_distributions_server.R
@@ -8,63 +8,100 @@ io_target_distributions_server <- function(
 
       ns <- session$ns
 
-      io_target_tbl <- shiny::reactive({
-        iatlas.api.client::query_io_targets()
+      features <- shiny::reactive({
+        iatlas.api.client::query_io_targets() %>%
+          dplyr::select(
+            "feature_name" = "entrez",
+            "feature_display" = "hgnc",
+            "Pathway" = "pathway",
+            "Therapy Type" = "therapy_type"
+          )
       })
 
-      url_gene <- shiny::reactive({
-        query <- shiny::parseQueryString(session$clientData$url_search)
-        get_gene_from_url(query)
+      plot_data_function <- shiny::reactive({
+        function(.feature){
+          group_data <- cohort_obj()$group_tbl %>%
+            dplyr::select("group", "group_description" = "characteristics", "color")
+          cohort_obj() %>%
+            query_gene_expression_with_cohort_object(entrez = as.integer(.feature)) %>%
+            dplyr::inner_join(cohort_obj()$sample_tbl, by = "sample") %>%
+            dplyr::inner_join(group_data, by = "group") %>%
+            dplyr::select(
+              "sample",
+              "group",
+              "feature" = "hgnc",
+              "feature_value" = "rna_seq_expr",
+              "group_description",
+              "color"
+            )
+        }
       })
 
-      output$gene_choice_ui <- shiny::renderUI({
-        shiny::req(input$group_choice, io_target_tbl())
-        shiny::selectInput(
-          ns("gene_choice"),
-          label = "Select or Search Gene",
-          choices = create_io_target_gene_list(
-            io_target_tbl(),
-            input$group_choice
-          ),
-          selected = url_gene()
-        )
-      })
-
-      gene_choice_hgnc <- shiny::reactive({
-        shiny::req(input$gene_choice, io_target_tbl())
-        get_io_hgnc_from_tbl(io_target_tbl(), input$gene_choice)
-      })
-
-      gene_plot_label <- shiny::reactive({
-        shiny::req(gene_choice_hgnc(), input$scale_method_choice)
-
-        transform_feature_string(
-          gene_choice_hgnc(),
-          input$scale_method_choice
-        )
-      })
-
-      distplot_tbl <- shiny::reactive({
-        shiny::req(
-          cohort_obj(),
-          input$gene_choice,
-          input$scale_method_choice
-        )
-        build_io_target_distplot_tbl(
-          cohort_obj(),
-          as.integer(input$gene_choice),
-          input$scale_method_choice
-        )
-      })
-
-      distribution_plot_server(
-        "io_targets_dist_plot",
-        cohort_obj,
-        distplot_tbl    = distplot_tbl,
-        distplot_type   = shiny::reactive(input$plot_type_choice),
-        distplot_ylab   = gene_plot_label,
-        distplot_title  = gene_choice_hgnc
+      iatlas.modules::distributions_plot_server(
+        "distplot",
+        plot_data_function,
+        features   = features,
+        distplot_xlab = shiny::reactive(cohort_obj()$group_name),
+        drilldown  = shiny::reactive(T)
       )
+
+      # io_target_tbl <- shiny::reactive({
+      #   iatlas.api.client::query_io_targets()
+      # })
+      #
+      # url_gene <- shiny::reactive({
+      #   query <- shiny::parseQueryString(session$clientData$url_search)
+      #   get_gene_from_url(query)
+      # })
+      #
+      # output$gene_choice_ui <- shiny::renderUI({
+      #   shiny::req(input$group_choice, io_target_tbl())
+      #   shiny::selectInput(
+      #     ns("gene_choice"),
+      #     label = "Select or Search Gene",
+      #     choices = create_io_target_gene_list(
+      #       io_target_tbl(),
+      #       input$group_choice
+      #     ),
+      #     selected = url_gene()
+      #   )
+      # })
+      #
+      # gene_choice_hgnc <- shiny::reactive({
+      #   shiny::req(input$gene_choice, io_target_tbl())
+      #   get_io_hgnc_from_tbl(io_target_tbl(), input$gene_choice)
+      # })
+      #
+      # gene_plot_label <- shiny::reactive({
+      #   shiny::req(gene_choice_hgnc(), input$scale_method_choice)
+      #
+      #   transform_feature_string(
+      #     gene_choice_hgnc(),
+      #     input$scale_method_choice
+      #   )
+      # })
+      #
+      # distplot_tbl <- shiny::reactive({
+      #   shiny::req(
+      #     cohort_obj(),
+      #     input$gene_choice,
+      #     input$scale_method_choice
+      #   )
+      #   build_io_target_distplot_tbl(
+      #     cohort_obj(),
+      #     as.integer(input$gene_choice),
+      #     input$scale_method_choice
+      #   )
+      # })
+      #
+      # distribution_plot_server(
+      #   "io_targets_dist_plot",
+      #   cohort_obj,
+      #   distplot_tbl    = distplot_tbl,
+      #   distplot_type   = shiny::reactive(input$plot_type_choice),
+      #   distplot_ylab   = gene_plot_label,
+      #   distplot_title  = gene_choice_hgnc
+      # )
     }
   )
 }
