deb2019ed04089833260d9c31010b591ed161813
heimannch <carolina.heimann@gmail.com>
2020-12-21 10:00:16 -0800

    update IGV references

--- R/germline_gwas_ui.R
+++ R/germline_gwas_ui.R
@@ -8,9 +8,9 @@ germline_gwas_ui <- function(id){
     since these were most likely to have significant genetic effects. "),
             shiny::p("The Manhattan plot below represents the -log10 p of the significant and suggestive GWAS hits by chromosomal position across the 33 immune
 traits. Select an Immune Trait of interest to highlight the GWAS hits associated with this trait. You can also select a region of interest to narrow down the visualization."),
-            shiny::p("Manuscript context: Figure 3A is reproduced with the 'See all chromossomes' option.
+            shiny::p("Manuscript context: Figure 3A is reproduced with the 'See all chromosomes' option.
                      Figures 4A can be reproduced by selecting IFN 21978456 in 'Select Immune Features'.
-              To generate Figure 4B, change the range of visualization to 'Select a region', Chromossome 2 and then select the coordinates by zooming in the plot or
+              To generate Figure 4B, change the range of visualization to 'Select a region', Chromosome 2 and then select the coordinates by zooming in the plot or
               by manually updating the start and end of the region of interest. Similar procedures should be followed for
                      Figures 4E, 5B, 5D, S4D, S5A, S5C, S5E.")
         ),
@@ -26,7 +26,7 @@ traits. Select an Immune Trait of interest to highlight the GWAS hits associated
                 shiny::uiOutput(ns("to_exclude"))
               ),
               shiny::sliderInput(ns("yrange"), "Select range of -log10(p-values) to be included", min = 6, max = 30, value = c(6,12), step = 1),
-              shiny::radioButtons(ns("selection"), "Select range of visualization", choices = c("See all chromossomes", "Select a region"), selected = "See all chromossomes")
+              shiny::radioButtons(ns("selection"), "Select range of visualization", choices = c("See all chromosomes", "Select a region"), selected = "See all chromosomes")
            )
          )
         ),
