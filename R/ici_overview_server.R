66787630a5704b6af1530ffe29111af471f71858
heimannch <carolina.heimann@gmail.com>
2021-01-12 10:25:42 -0800

    added ICI Clinical Outcomes

--- R/ici_overview_server.R
+++ R/ici_overview_server.R
@@ -5,16 +5,16 @@ ici_overview_server <- function(
     id,
     function(input, output, session) {
 
-      ici_data <- load_io_data()
+     # ici_data <- load_io_data()
 
       ici_overview_datasets_server(
         "ici_overview_datasets",
-        ioresponse_data = ici_data
+        ioresponse_data
       )
 
       ici_overview_category_server(
         "ici_overview_category",
-        ioresponse_data = ici_data
+        ioresponse_data
       )
 
     }
