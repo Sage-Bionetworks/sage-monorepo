b0addd78c527cd30e99428d1c02c1fc96a9bca7f
Andrew Lamb <andrewelamb@gmail.com>
2021-03-24 09:08:58 -0700

    refactored remaining utils

--- apps/iatlas/app/R/utils.R
+++ apps/iatlas/app/R/utils.R
@@ -1,38 +1,33 @@
 
 # system files ----------------------------------------------------------------
 
+get_system_path_file <- function(
+  prefix, extension, folder, package = "iatlas.app"
+){
+  file_name <- stringr::str_c(prefix, extension)
+  file.path(system.file(folder, package = package), file_name)
+}
+
 get_markdown_path <- function(name, extension = ".markdown"){
-  name %>%
-    stringr::str_c(extension) %>%
-    file.path(system.file("markdown", package = "iatlas.app"), .)
+  get_system_path_file(name, extension, "markdown")
 }
 
 get_example_path <- function(name, extension = ".csv"){
-  name %>%
-    stringr::str_c(extension) %>%
-    file.path(system.file("examples", package = "iatlas.app"), .)
+  get_system_path_file(name, extension, "examples")
 }
 
 get_javascript_path <- function(name, extension = ".js"){
-  name %>%
-    stringr::str_c(extension) %>%
-    file.path(system.file("javascript", package = "iatlas.app"), .)
+  get_system_path_file(name, extension, "javascript")
 }
 
 get_html_path <- function(name, extension = ".html"){
-  name %>%
-    stringr::str_c(extension) %>%
-    file.path(system.file("html_files", package = "iatlas.app"), .)
+  get_system_path_file(name, extension, "html_files")
 }
 
 get_tsv_path <- function(name, extension = ".tsv"){
-  name %>%
-    stringr::str_c(extension) %>%
-    file.path(system.file("tsv", package = "iatlas.app"), .)
+  get_system_path_file(name, extension, "tsv")
 }
 
 get_svg_path <- function(name, extension = ".svg"){
-  name %>%
-    stringr::str_c(extension) %>%
-    file.path(system.file("svg", package = "iatlas.app"), .)
+  get_system_path_file(name, extension, "svg")
 }
