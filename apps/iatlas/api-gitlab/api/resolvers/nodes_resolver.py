ed8e793f1a4e1cf48f999befcf6a0ae6ca737005
Andrew Lamb <andrewelamb@gmail.com>
2021-05-21 11:42:38 -0700

    update nodes to use new paging limits

--- apps/iatlas/api-gitlab/api/resolvers/nodes_resolver.py
+++ apps/iatlas/api-gitlab/api/resolvers/nodes_resolver.py
@@ -1,5 +1,5 @@
 from .resolver_helpers import build_node_graphql_response, build_node_request, feature_request_fields, fetch_nodes_with_tags, get_selection_set, gene_request_fields, get_requested, node_request_fields, simple_data_set_request_fields, simple_tag_request_fields
-from .resolver_helpers.paging_utils import fetch_page, paginate, Paging, paging_fields, process_page
+from .resolver_helpers.paging_utils import fetch_page, paginate, Paging, paging_fields, process_page, create_paging
 from api.telemetry import profile
 
 
@@ -25,11 +25,7 @@ def resolve_nodes(
     if distinct == False:
         requested.add('id')  # Add the id as a cursor if not selecting distinct
 
-    max_results = 12_000
-    paging = paging if paging else Paging.DEFAULT
-    paging.MAX_LIMIT = paging.MAX_LIMIT if paging.MAX_LIMIT < max_results else max_results
-    paging['first'] = paging['first'] if paging['first'] < max_results else max_results
-    paging['last'] = paging['last'] if paging['last'] < max_results else max_results
+    paging = create_paging(paging, 12_000)
 
     query, count_query = build_node_request(
         requested, data_set_requested, feature_requested, gene_requested, data_set=dataSet, distinct=distinct, entrez=entrez, feature=feature, feature_class=featureClass, gene_type=geneType, max_score=maxScore, min_score=minScore, network=network, related=related, paging=paging, tag=tag)
