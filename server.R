5691d46b764ed394cccaa6b41f4d44ae52cbf40a
Andrew Lamb <andrewelamb@gmail.com>
2021-07-02 09:10:29 -0700

    fix ui issues

--- server.R
+++ server.R
@@ -167,7 +167,7 @@ shiny::shinyServer(function(input, output, session) {
   })
 
   module_image_boxes <- shiny::reactive({
-    purrr::pmap(
+    lst <- purrr::pmap(
       list(
         title  = analysis_modules_tbl$display,
         linkId = analysis_modules_tbl$link,
@@ -178,6 +178,28 @@ shiny::shinyServer(function(input, output, session) {
       width = 6,
       linkText = "Open Module"
     )
+
+    row_tbl <- lst %>%
+      dplyr::tibble("item" = .) %>%
+      dplyr::mutate("row" = as.character(ceiling(dplyr::row_number() / 2))) %>%
+      dplyr::group_by(.data$row) %>%
+      dplyr::mutate("n" = as.character(dplyr::row_number())) %>%
+      dplyr::ungroup() %>%
+      tidyr::pivot_wider(names_from = "n", values_from = "item")
+
+    func <- function(i, tbl){
+      item1 <- tbl$`1`[[i]]
+      item2 <- tbl$`2`[[i]]
+      if(is.null(item2)){
+        row_list <- shiny::tagList(item1)
+      } else {
+        row_list <- shiny::tagList(item1, item2)
+      }
+      shiny::fluidRow(row_list)
+    }
+
+    return(purrr::map(1:nrow(row_tbl), func, row_tbl))
+
   })
 
   module_tab_items <- shiny::reactive({
@@ -187,7 +209,7 @@ shiny::shinyServer(function(input, output, session) {
   })
 
   output$dashboard_body <- shiny::renderUI({
-    # shiny::req(readout_info_boxes(), module_image_boxes(), module_tab_items())
+    shiny::req(readout_info_boxes(), module_image_boxes(), module_tab_items())
     tab_item <- list(shinydashboard::tabItem(
       tabName = "dashboard",
       iatlas.modules::titleBox("iAtlas Explorer — Home"),
@@ -205,10 +227,9 @@ shiny::shinyServer(function(input, output, session) {
           width = 12,
           shiny::includeMarkdown("inst/markdown/explore2.markdown")
         ),
-        shiny::fluidRow(module_image_boxes())
+        module_image_boxes()
       )
     ))
-
     do.call(
       shinydashboard::tabItems,
       c(tab_item, module_tab_items())
