524b5f75b56ce36173b0f77445073b2b7362f1d0
Andrew Lamb <andrewelamb@gmail.com>
2021-12-01 07:48:26 -0800

    fix column name error

--- apps/iatlas/app/tests/testthat/test-clinical_outcomes_heatmap_server.R
+++ apps/iatlas/app/tests/testthat/test-clinical_outcomes_heatmap_server.R
@@ -9,9 +9,32 @@ test_that("clinical_outcomes_heatmap_server", {
     {
       session$setInputs("time_feature_choice" = "OS_time")
       session$setInputs("class_choice" = "DNA Alteration")
+      session$setInputs("mock_event_data" = data.frame(
+        "x" = "C1", "y" = "Silent Mutation Rate"
+      ))
+
       expect_type(output$class_selection_ui, "list")
       expect_type(output$time_feature_selection_ui, "list")
+
+      expect_type(heatmap_tbl(), "list")
+      expect_named(
+        heatmap_tbl(),
+        c(
+          'sample',
+          'group',
+          'time',
+          'status',
+          'feature_display',
+          'feature_value',
+          'feature_order'
+        )
+      )
+
       expect_type(output$heatmap, "character")
+      expect_type(heatmap_eventdata(), "list")
+      expect_named(heatmap_eventdata(), c("x", "y"))
+      expect_type(group_data(), "list")
+      expect_named(group_data(), c("group", "description"))
     }
   )
 })
