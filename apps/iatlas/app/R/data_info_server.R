9463641109ef5f40189e608df6bb028f72026512
Andrew Lamb <andrewelamb@gmail.com>
2021-12-02 08:31:29 -0800

    add missing library names

--- apps/iatlas/app/R/data_info_server.R
+++ apps/iatlas/app/R/data_info_server.R
@@ -91,14 +91,14 @@ data_info_server <- function(id){
 
       shiny::observeEvent(input$feature_table_rows_selected, {
         output$variable_details_section <- shiny::renderUI({
-          sectionBox(
+          iatlas.modules::sectionBox(
             title = "Variable Class Details",
             iatlas.modules::messageBox(
               width = 12,
               shiny::p("Here is additional information about the variables in the Variable Class you selected. To the right you can access description of the methods used to obtain the variables.")
             ),
             shiny::fluidRow(
-              tableBox(
+              iatlas.modules::tableBox(
                 width = 9,
                 shiny::tableOutput(ns('variable_class_table'))
               ),
