f79919ad032d3cbd285145bbf50746b21c103263
Andrew Lamb <andrewelamb@gmail.com>
2021-12-07 08:53:12 -0800

    fix cohort object dataset name check

--- apps/iatlas/app/tests/testthat/test-cellimage_server.R
+++ apps/iatlas/app/tests/testthat/test-cellimage_server.R
@@ -1,9 +1,25 @@
-test_that("cellimage_server", {
+test_that("cellimage_server_tcga", {
   shiny::testServer(
     cellimage_server,
     args = list(
       "cohort_obj" = shiny::reactiveVal(
         iatlas.modules2::tcga_immune_subtype_cohort_obj
+      )
+    ),
+    {
+      expect_true(shiny::is.reactive(show_submodule))
+      expect_type(show_submodule(), "closure")
+      expect_true(show_submodule()(cohort_obj()))
+    }
+  )
+})
+
+test_that("cellimage_server_tpcawg", {
+  shiny::testServer(
+    cellimage_server,
+    args = list(
+      "cohort_obj" = shiny::reactiveVal(
+        iatlas.modules2::pcawg_immune_subtype_cohort_obj
       )
     ),
     {
