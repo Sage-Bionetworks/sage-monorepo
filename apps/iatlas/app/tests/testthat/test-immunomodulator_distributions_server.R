6346051edae8bc5fdf27dc3304cea756408505cb
Andrew Lamb <andrewelamb@gmail.com>
2022-02-11 10:59:35 -0800

    temp

--- apps/iatlas/app/tests/testthat/test-immunomodulator_distributions_server.R
+++ apps/iatlas/app/tests/testthat/test-immunomodulator_distributions_server.R
@@ -7,9 +7,16 @@ test_that("immunomodulator_distributions_server", {
       )
     ),
     {
-      expect_type(features(), "list")
+      session$setInputs("disttplot-distplot-mock_event_data" = data.frame(
+        "curveNumber" = c(0,0),
+        "pointNumber" = c(0,0),
+        "x" = "C1",
+        "y" = c(5.1, 2.1),
+        "key" = "PCAWG"
+      ))
+      expect_type(feature_data(), "list")
       expect_named(
-        features(),
+        feature_data(),
         c(
           "feature_name",
           "feature_display",
@@ -19,10 +26,10 @@ test_that("immunomodulator_distributions_server", {
           "Super Category"
         )
       )
-      expect_type(plot_data_function(), "closure")
-      entrez = as.character(features()$feature_name[[1]])
+      expect_type(sample_data_function(), "closure")
+      entrez = as.character(feature_data()$feature_name[[1]])
 
-      plot_data <- plot_data_function()(.feature = entrez)
+      plot_data <- sample_data_function()(.feature = entrez)
       expect_type(plot_data, "list")
       expect_named(
         plot_data,
@@ -33,14 +40,17 @@ test_that("immunomodulator_distributions_server", {
           "feature_display",
           "feature_value",
           "group_description",
-          "group_color"
+          "group_color",
+          "dataset_name"
         )
       )
+      expect_equal(unique(plot_data$dataset_name), "PCAWG")
       expect_true(nrow(plot_data) > 0)
       expect_equal(
         unique(plot_data$feature_display),
-        features()$feature_display[[1]]
+        feature_data()$feature_display[[1]]
       )
+      expect_error(result(), "fff")
     }
   )
 })
