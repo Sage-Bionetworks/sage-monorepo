5b7859e03f203959916c46824cc58382716f5fd2
tschaffter <thomas.schaffter@gmail.com>
2022-02-12 13:35:20 -0800

    Add lib api-angular

--- apps/web-app/src/app/app.component.ts
+++ apps/web-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { Challenge } from '@challenge-registry/api-angular';
 
 @Component({
   selector: 'challenge-registry-root',
---
+++ libs/api-angular/src/index.ts
@@ -0,0 +1,5 @@
+export * from './lib/api/api';
+export * from './lib/model/models';
+export * from './lib/variables';
+export * from './lib/configuration';
+export * from './lib/api.module';
