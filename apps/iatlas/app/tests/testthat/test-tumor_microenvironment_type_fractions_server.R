715bc6b864653e86cc8c63ce7f1a1f592d5fd131
Andrew Lamb <andrewelamb@gmail.com>
2022-02-15 11:56:47 -0800

    updated barplot server

--- apps/iatlas/app/tests/testthat/test-tumor_microenvironment_type_fractions_server.R
+++ apps/iatlas/app/tests/testthat/test-tumor_microenvironment_type_fractions_server.R
@@ -5,35 +5,43 @@ test_that("tumor_microenvironment_type_fractions_server_pcawg", {
     args = list(
       "cohort_obj" = shiny::reactiveVal(
         iatlas.modules2::pcawg_immune_subtype_cohort_obj
-      )
-    ),
-    {
-      session$setInputs(
-        "barplot-feature_class_choice" =
-          "Immune Cell Proportion - Common Lymphoid and Myeloid Cell Derivative Class"
-      )
-      session$setInputs("barplot-mock_event_data" = data.frame(
+      ),
+      "mock_event_data" = shiny::reactive(data.frame(
         "curveNumber" = 1,
         "pointNumber" = 2,
         "x" = "C4",
         "y" = 6.588,
         "key" = "C4"
       ))
-      expect_type(feature_classes(), "character")
-      expect_type(plot_data_function(), "closure")
-      expect_type(
-        plot_data_function()("Immune Cell Proportion - Original"),
-        "list"
+    ),
+    {
+      session$setInputs(
+        "barplot-feature_class_choice" =
+          "Immune Cell Proportion - Common Lymphoid and Myeloid Cell Derivative Class"
       )
 
-      scatterplot_data <- result$scatterplot_data()
-      expect_type(scatterplot_data, "list")
+      expect_type(sample_data_function(), "closure")
+      sample_data <- sample_data_function()("Immune Cell Proportion - Original")
+      expect_true(tibble::is_tibble(sample_data))
+      expect_named(
+        sample_data,
+        c(
+          "sample_name",
+          "group_name",
+          "feature_name",
+          "feature_display",
+          "feature_value"
+        )
+      )
+
+      scatterplot_data <- result()$scatterplot_data
+      expect_true(tibble::is_tibble(scatterplot_data))
       expect_named(scatterplot_data, c("x", "y", "text"))
-      barplot_data <- result$barplot_data()
-      expect_type(barplot_data, "list")
+      barplot_data <- result()$barplot_data
+      expect_true(tibble::is_tibble(barplot_data))
       expect_named(
         barplot_data,
-        c('group_name', 'feature_display', 'text', 'MEAN', 'SE')
+        c('group_display', 'feature_display', 'text', 'MEAN', 'SE')
       )
     }
   )
