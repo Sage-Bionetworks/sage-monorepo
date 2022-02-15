2f9bceae94e89344ba3284414a8d317d7b9c5606
Andrew Lamb <andrewelamb@gmail.com>
2022-02-15 10:33:40 -0800

    fixed distributions plot modules

--- apps/iatlas/app/R/immune_feature_distributions_server.R
+++ apps/iatlas/app/R/immune_feature_distributions_server.R
@@ -1,6 +1,7 @@
 immune_feature_distributions_server <- function(
   id,
-  cohort_obj
+  cohort_obj,
+  mock_event_data = shiny::reactive(NULL)
 ) {
   shiny::moduleServer(
     id,
@@ -8,16 +9,7 @@ immune_feature_distributions_server <- function(
 
       ns <- session$ns
 
-      features <- shiny::reactive({
-        cohort_obj()$feature_tbl %>%
-          dplyr::select(
-            "feature_class" = "class",
-            "feature_name" = "name",
-            "feature_display" = "display"
-          )
-      })
-
-      plot_data_function <- shiny::reactive({
+      sample_data_function <- shiny::reactive({
         function(.feature){
           cohort_obj()$get_feature_values(features = .feature) %>%
             dplyr::select(
@@ -26,19 +18,39 @@ immune_feature_distributions_server <- function(
               "feature_name",
               "feature_display",
               "feature_value",
-              "feature_order",
-              "group_description" = "group_characteristics",
-              "group_color"
-            )
+              "feature_order"
+            ) %>%
+            dplyr::mutate("dataset_name" = cohort_obj()$dataset_names)
         }
+      })
+
+      feature_data <- shiny::reactive({
+        cohort_obj()$feature_tbl %>%
+          dplyr::select(
+            "feature_class" = "class",
+            "feature_name" = "name",
+            "feature_display" = "display"
+          )
+      })
+
+      group_data <- shiny::reactive({
+        cohort_obj()$group_tbl %>%
+          dplyr::select(
+            "group_name" = "short_name",
+            "group_description" = "characteristics",
+            "group_color" = "color"
+          ) %>%
+          dplyr::mutate("group_display" = .data$group_name)
       })
 
       result <- iatlas.modules::distributions_plot_server(
         "distplot",
-        plot_data_function,
-        features      = features,
-        distplot_xlab = shiny::reactive(cohort_obj()$group_display),
-        drilldown     = shiny::reactive(T)
+        sample_data_function,
+        feature_data    = feature_data,
+        group_data      = group_data,
+        distplot_xlab   = shiny::reactive(cohort_obj()$group_display),
+        drilldown       = shiny::reactive(T),
+        mock_event_data = mock_event_data
       )
 
     }
--- apps/iatlas/app/R/til_map_distributions_server.R
+++ apps/iatlas/app/R/til_map_distributions_server.R
@@ -1,21 +1,15 @@
-til_map_distributions_server <- function(id, cohort_obj) {
+til_map_distributions_server <- function(
+  id,
+  cohort_obj,
+  mock_event_data = shiny::reactive(NULL)
+){
   shiny::moduleServer(
     id,
     function(input, output, session) {
 
       ns <- session$ns
 
-      features <- shiny::reactive({
-        cohort_obj()$feature_tbl %>%
-          dplyr::filter(.data$class %in% "TIL Map Characteristic") %>%
-          dplyr::select(
-            "feature_class" = "class",
-            "feature_name" = "name",
-            "feature_display" = "display"
-          )
-      })
-
-      plot_data_function <- shiny::reactive({
+      sample_data_function <- shiny::reactive({
         function(.feature){
           result <-
             cohort_obj()$get_feature_values(features = .feature) %>%
@@ -25,19 +19,40 @@ til_map_distributions_server <- function(id, cohort_obj) {
               "feature_name",
               "feature_display",
               "feature_value",
-              "feature_order",
-              "group_description" = "group_characteristics",
-              "group_color"
-            )
+              "feature_order"
+            ) %>%
+            dplyr::mutate("dataset_name" = cohort_obj()$dataset_names)
         }
+      })
+
+      feature_data <- shiny::reactive({
+        cohort_obj()$feature_tbl %>%
+          dplyr::filter(.data$class %in% "TIL Map Characteristic") %>%
+          dplyr::select(
+            "feature_class" = "class",
+            "feature_name" = "name",
+            "feature_display" = "display"
+          )
+      })
+
+      group_data <- shiny::reactive({
+        cohort_obj()$group_tbl %>%
+          dplyr::select(
+            "group_name" = "short_name",
+            "group_description" = "characteristics",
+            "group_color" = "color"
+          ) %>%
+          dplyr::mutate("group_display" = .data$group_name)
       })
 
       result <- iatlas.modules::distributions_plot_server(
         id = "distplot",
-        plot_data_function = plot_data_function,
-        features           = features,
-        distplot_xlab      = shiny::reactive(cohort_obj()$group_display),
-        drilldown          = shiny::reactive(T)
+        sample_data_function = sample_data_function,
+        feature_data         = feature_data,
+        group_data           = group_data,
+        distplot_xlab        = shiny::reactive(cohort_obj()$group_display),
+        drilldown            = shiny::reactive(T),
+        mock_event_data      = mock_event_data
       )
 
     }
