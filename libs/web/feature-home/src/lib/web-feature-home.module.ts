a2ffa5d5afa61e3b095ccc89b3320912b484795b
tschaffter <thomas.schaffter@gmail.com>
2022-03-11 14:29:08 -0800

    Add list of challenge hosts

--- libs/web/feature-home/src/lib/web-feature-home.module.ts
+++ libs/web/feature-home/src/lib/web-feature-home.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { SearchViewerModule } from './search-viewer/search-viewer.module';
 import { StatisticsViewerModule } from './statistics-viewer/statistics-viewer.module';
 import { TopicsViewerModule } from './topics-viewer/topics-viewer.module';
+import { ChallengeHostListModule } from './challenge-host-list/challenge-host-list.module';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [],
-  exports: [SearchViewerModule, StatisticsViewerModule, TopicsViewerModule],
+  exports: [
+    SearchViewerModule,
+    StatisticsViewerModule,
+    TopicsViewerModule,
+    ChallengeHostListModule,
+  ],
 })
 export class WebFeatureHomeModule {}
