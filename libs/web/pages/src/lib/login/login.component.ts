fc11607fcb291dcdeae28f541ea6030b13d1c45c
tschaffter <thomas.schaffter@gmail.com>
2022-03-13 14:40:00 -0700

    Generate login page

---
+++ libs/web/pages/src/lib/login/login.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'challenge-registry-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {}
---
+++ libs/web/pages/src/lib/login/login.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login.component';
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [CommonModule],
+  exports: [LoginComponent],
+})
+export class LoginModule {}
