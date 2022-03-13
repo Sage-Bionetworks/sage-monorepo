0acd7d00cf97de1e96d4666bc7db4d1088ed8414
tschaffter <thomas.schaffter@gmail.com>
2022-03-13 11:43:57 -0700

    Rename search-viewer to challenge-search

---
+++ libs/web/feature-home/src/lib/challenge-search/challenge-search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'challenge-registry-challenge-search',
+  templateUrl: './challenge-search.component.html',
+  styleUrls: ['./challenge-search.component.scss'],
+})
+export class ChallengeSearchComponent {
+  // constructor() {}
+  // ngOnInit(): void {}
+}
--- libs/web/feature-home/src/lib/challenge-search/challenge-search.module.ts
+++ libs/web/feature-home/src/lib/challenge-search/challenge-search.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { NgxTypedJsModule } from 'ngx-typed-js';
-import { SearchViewerComponent } from './search-viewer.component';
+import { ChallengeSearchComponent } from './challenge-search.component';
 
 @NgModule({
-  declarations: [SearchViewerComponent],
+  declarations: [ChallengeSearchComponent],
   imports: [CommonModule, MatButtonModule, NgxTypedJsModule],
-  exports: [SearchViewerComponent],
+  exports: [ChallengeSearchComponent],
 })
-export class SearchViewerModule {}
+export class ChallengeSearchModule {}
