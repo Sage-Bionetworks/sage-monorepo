30d8531c5cedb6e8963cbbb23befd8c8b16d7e0a
tschaffter <thomas.schaffter@gmail.com>
2022-03-13 13:35:38 -0700

    Generate signup page

---
+++ libs/web/pages/src/lib/signup/signup.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'challenge-registry-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss'],
+})
+export class SignupComponent {}
---
+++ libs/web/pages/src/lib/signup/signup.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignupComponent } from './signup.component';
+
+@NgModule({
+  declarations: [SignupComponent],
+  imports: [CommonModule],
+  exports: [SignupComponent],
+})
+export class SignupModule {}
