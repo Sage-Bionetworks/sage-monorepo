fe5a8e723643f6f137d2bf5e046e28cf3a73696c
tschaffter <thomas.schaffter@gmail.com>
2022-03-13 13:46:43 -0700

    Prepare Signup module and component

--- libs/web/pages/src/lib/signup/signup.component.ts
+++ libs/web/pages/src/lib/signup/signup.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { APP_CONFIG, AppConfig } from '@challenge-registry/web/config';
 
 @Component({
   selector: 'challenge-registry-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss'],
 })
-export class SignupComponent {}
+export class SignupComponent {
+  public appVersion: string;
+
+  constructor(@Inject(APP_CONFIG) private appConfig: AppConfig) {
+    this.appVersion = appConfig.appVersion;
+  }
+}
--- libs/web/pages/src/lib/signup/signup.module.ts
+++ libs/web/pages/src/lib/signup/signup.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 import { SignupComponent } from './signup.component';
+import { WebUiModule } from '@challenge-registry/web/ui';
+
+const routes: Routes = [{ path: '', component: SignupComponent }];
 
 @NgModule({
   declarations: [SignupComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    MatCardModule,
+    WebUiModule,
+  ],
   exports: [SignupComponent],
 })
 export class SignupModule {}
