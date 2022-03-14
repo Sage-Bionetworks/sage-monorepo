f814ccbf8f215e4e97d9fa928c4d76c94778ab26
tschaffter <thomas.schaffter@gmail.com>
2022-03-13 17:12:54 -0700

    Connect login page

--- libs/web/pages/src/index.ts
+++ libs/web/pages/src/index.ts
@@ -2,5 +2,6 @@ export * from './lib/web-pages.module';
 
 export * from './lib/about/about.module';
 export * from './lib/homepage/homepage.module';
+export * from './lib/login/login.module';
 export * from './lib/not-found/not-found.module';
 export * from './lib/signup/signup.module';
--- libs/web/pages/src/lib/login/login.module.ts
+++ libs/web/pages/src/lib/login/login.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { WebUiModule } from '@challenge-registry/web/ui';
+
+const routes: Routes = [{ path: '', component: LoginComponent }];
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    WebUiModule,
+  ],
   exports: [LoginComponent],
 })
 export class LoginModule {}
