de8e7157361622f830b2970cfc3130672e215ace
tschaffter <thomas.schaffter@gmail.com>
2022-03-15 16:57:50 -0700

    Display JSON object of user on user page

--- libs/web/pages/src/lib/user/user.component.ts
+++ libs/web/pages/src/lib/user/user.component.ts
@@ -1,8 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {
+  Account,
+  AccountService,
+  ModelError as ApiClientError,
+} from '@challenge-registry/api-angular';
+import { AppConfig, APP_CONFIG } from '@challenge-registry/web/config';
+import { isApiClientError } from '@challenge-registry/web/util';
+import { catchError, Observable, of, switchMap, throwError } from 'rxjs';
 
 @Component({
   selector: 'challenge-registry-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
 })
-export class UserComponent {}
+export class UserComponent implements OnInit {
+  public appVersion: string;
+  account$!: Observable<Account | undefined>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private accountService: AccountService,
+    @Inject(APP_CONFIG) private appConfig: AppConfig
+  ) {
+    this.appVersion = appConfig.appVersion;
+  }
+
+  ngOnInit(): void {
+    this.account$ = this.route.params.pipe(
+      switchMap((params) => this.accountService.getAccount(params['login'])),
+      catchError((err) => {
+        const error = err.error as ApiClientError;
+        if (isApiClientError(error)) {
+          if (error.status === 404) {
+            return of(undefined);
+          }
+        }
+        return throwError(err);
+      })
+    );
+
+    this.account$.subscribe((account) => {
+      const pageTitle = account ? `${account.login}` : 'Page not found';
+      console.log(pageTitle);
+      // this.pageTitleService.setTitle(`${pageTitle} · ROCC`);
+      // this.accountNotFound = !account;
+    });
+  }
+}
