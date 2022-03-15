eafd7b659493f502ee5170f97fef42598bb6ff4f
tschaffter <thomas.schaffter@gmail.com>
2022-03-15 16:44:48 -0700

    Add user page

---
+++ libs/web/pages/src/lib/user/user.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'challenge-registry-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss'],
+})
+export class UserComponent {}
--- libs/web/pages/src/lib/web-pages.module.ts
+++ libs/web/pages/src/lib/web-pages.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomepageModule } from './homepage/homepage.module';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [],
-  exports: [HomepageModule],
+  exports: [],
 })
 export class WebPagesModule {}
