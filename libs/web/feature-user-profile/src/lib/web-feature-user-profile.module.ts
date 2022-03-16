9f1588daca639adf5ffa4837e948b766a2172bed
tschaffter <thomas.schaffter@gmail.com>
2022-03-15 20:44:42 -0700

    Add user profile overview sub-page
    Co-authored-by: rrchai <rchai@sagebase.org>

--- libs/web/feature-user-profile/src/lib/web-feature-user-profile.module.ts
+++ libs/web/feature-user-profile/src/lib/web-feature-user-profile.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserProfileBarModule } from './user-profile-bar/user-profile-bar.module';
+import { UserProfileOverviewModule } from './user-profile-overview/user-profile-overview.module';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [],
-  exports: [UserProfileBarModule],
+  exports: [UserProfileBarModule, UserProfileOverviewModule],
 })
 export class WebFeatureUserProfileModule {}
