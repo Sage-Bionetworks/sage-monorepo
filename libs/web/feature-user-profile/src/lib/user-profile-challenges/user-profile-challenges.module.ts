db77420037b90100c47605250496f732cba28b5b
tschaffter <thomas.schaffter@gmail.com>
2022-03-15 20:49:19 -0700

    Add list of challenges sub-page to user profile
    Co-authored-by: rrchai <rrchai@sagebase.org>

---
+++ libs/web/feature-user-profile/src/lib/user-profile-challenges/user-profile-challenges.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserProfileChallengesComponent } from './user-profile-challenges.component';
+
+@NgModule({
+  declarations: [UserProfileChallengesComponent],
+  imports: [CommonModule],
+  exports: [UserProfileChallengesComponent],
+})
+export class UserProfileChallengesModule {}
--- libs/web/feature-user-profile/src/lib/web-feature-user-profile.module.ts
+++ libs/web/feature-user-profile/src/lib/web-feature-user-profile.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserProfileBarModule } from './user-profile-bar/user-profile-bar.module';
+import { UserProfileChallengesModule } from './user-profile-challenges/user-profile-challenges.module';
 import { UserProfileOverviewModule } from './user-profile-overview/user-profile-overview.module';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [],
-  exports: [UserProfileBarModule, UserProfileOverviewModule],
+  exports: [
+    UserProfileBarModule,
+    UserProfileChallengesModule,
+    UserProfileOverviewModule,
+  ],
 })
 export class WebFeatureUserProfileModule {}
