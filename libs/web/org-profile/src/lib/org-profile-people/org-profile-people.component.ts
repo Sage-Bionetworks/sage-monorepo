a5d48960c6774b6679f37cd5521950e6084d0094
tschaffter <thomas.schaffter@gmail.com>
2022-03-19 09:22:17 -0700

    Add remaining org-profile routes

---
+++ libs/web/org-profile/src/lib/org-profile-people/org-profile-people.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'challenge-registry-org-profile-people',
+  templateUrl: './org-profile-people.component.html',
+  styleUrls: ['./org-profile-people.component.scss'],
+})
+export class OrgProfilePeopleComponent {}
--- libs/web/org-profile/src/lib/org-profile.module.ts
+++ libs/web/org-profile/src/lib/org-profile.module.ts
@@ -1,32 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
 import { MatTabsModule } from '@angular/material/tabs';
 import { WebUiModule } from '@challenge-registry/web/ui';
 import { OrgProfileComponent } from './org-profile.component';
 import { OrgProfileHeaderModule } from './org-profile-header/org-profile-header.module';
 import { OrgProfileOverviewModule } from './org-profile-overview/org-profile-overview.module';
-import { OrgProfileOverviewComponent } from './org-profile-overview/org-profile-overview.component';
-import { OrgProfileChallengesComponent } from './org-profile-challenges/org-profile-challenges.component';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: OrgProfileComponent,
-    children: [
-      { path: '', component: OrgProfileOverviewComponent },
-      { path: 'challenges', component: OrgProfileChallengesComponent },
-    ],
-  },
-];
+import { OrgProfileRoutingModule } from './org-profile-routing.modules';
 
 @NgModule({
   declarations: [OrgProfileComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes),
     MatTabsModule,
     WebUiModule,
+    OrgProfileRoutingModule,
     OrgProfileHeaderModule,
     OrgProfileOverviewModule,
   ],
