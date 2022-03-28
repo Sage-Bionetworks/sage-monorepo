5f4dd10fd9186fa9fe7409ddccac66c7e9ddb198
tschaffter <thomas.schaffter@gmail.com>
2022-03-27 17:05:50 -0700

    Add unit tests for user-button

--- libs/web/ui/src/lib/user-button/user-button.component.spec.ts
+++ libs/web/ui/src/lib/user-button/user-button.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { AvatarModule } from '../avatar/avatar.module';
 
 import { UserButtonComponent } from './user-button.component';
 
@@ -9,6 +13,7 @@ describe('UserButtonComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [UserButtonComponent],
+      imports: [HttpClientModule, MatIconModule, MatMenuModule, AvatarModule],
     }).compileComponents();
   });
 
