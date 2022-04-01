fb95b5c4a8615d5574b4397f11227f82d9f17f94
tschaffter <thomas.schaffter@gmail.com>
2022-04-01 22:12:22 +0000

    Add library org-search

--- apps/web-app/src/app/app-routing.module.ts
+++ apps/web-app/src/app/app-routing.module.ts
@@ -15,6 +15,13 @@ export const routes: Routes = [
       ),
   },
   {
+    path: 'orgs',
+    loadChildren: () =>
+      import('@challenge-registry/web/org-search').then(
+        (m) => m.OrgSearchModule
+      ),
+  },
+  {
     path: 'login',
     loadChildren: () =>
       import('@challenge-registry/web/login').then((m) => m.LoginModule),
