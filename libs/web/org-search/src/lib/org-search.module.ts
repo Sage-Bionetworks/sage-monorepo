2e3a1f98f524feb0f7f442eeba4d22802f84a82e
tschaffter <thomas.schaffter@gmail.com>
2022-04-01 22:20:40 +0000

    Add org-search component and connect it to the app

--- libs/web/org-search/src/lib/org-search.module.ts
+++ libs/web/org-search/src/lib/org-search.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { WebUiModule } from '@challenge-registry/web/ui';
+import { OrgSearchComponent } from './org-search.component';
+
+const routes: Routes = [{ path: '', component: OrgSearchComponent }];
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forChild(routes), WebUiModule],
+  declarations: [OrgSearchComponent],
+  exports: [OrgSearchComponent],
 })
 export class OrgSearchModule {}
