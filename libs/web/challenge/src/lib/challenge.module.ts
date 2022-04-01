d75973f5315e8abde7763e31a512d4818fbe5a4b
tschaffter <thomas.schaffter@gmail.com>
2022-04-01 21:22:59 +0000

    Add Challenge header

--- libs/web/challenge/src/lib/challenge.module.ts
+++ libs/web/challenge/src/lib/challenge.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ChallengeComponent } from './challenge.component';
 import { WebUiModule } from '@challenge-registry/web/ui';
 import { ChallengeRoutingModule } from './challenge-routing.modules';
+import { ChallengeHeaderModule } from './challenge-header/challenge-header.module';
 
 @NgModule({
-  imports: [CommonModule, WebUiModule, ChallengeRoutingModule],
+  imports: [
+    CommonModule,
+    WebUiModule,
+    ChallengeRoutingModule,
+    ChallengeHeaderModule,
+  ],
   declarations: [ChallengeComponent],
   exports: [ChallengeComponent],
 })
