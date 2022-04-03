d467d570a9cdb706d582ec1cefa2681151393d47
tschaffter <thomas.schaffter@gmail.com>
2022-04-03 17:50:12 +0000

    Persist data in postgres db

--- apps/keycloak/docker-entrypoint-initdb.d/init-db.sql
+++ apps/keycloak/docker-entrypoint-initdb.d/init-db.sql
@@ -1,4 +1,4 @@
 -- keycloak
--- CREATE USER keycloak WITH ENCRYPTED PASSWORD 'changeme';
--- CREATE DATABASE keycloak;
--- GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
+CREATE USER keycloak WITH ENCRYPTED PASSWORD 'changeme';
+CREATE DATABASE keycloak;
+GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
