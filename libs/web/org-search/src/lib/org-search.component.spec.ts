1a026060c397892d094a1deb697af30b80154be2
Thomas Schaffter <thomas.schaffter@gmail.com>
2022-04-05 19:50:35 -0700

    Add org search page (#135)
    * Add library org-search
    
    * Add org-search component and connect it to the app

---
+++ libs/web/org-search/src/lib/org-search.component.spec.ts
@@ -0,0 +1,28 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { APP_CONFIG, MOCK_APP_CONFIG } from '@challenge-registry/web/config';
+
+import { OrgSearchComponent } from './org-search.component';
+
+describe('OrgSearchComponent', () => {
+  let component: OrgSearchComponent;
+  let fixture: ComponentFixture<OrgSearchComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [OrgSearchComponent],
+      providers: [{ provide: APP_CONFIG, useValue: MOCK_APP_CONFIG }],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OrgSearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
