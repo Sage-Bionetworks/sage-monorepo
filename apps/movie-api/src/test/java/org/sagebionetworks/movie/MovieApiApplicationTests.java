2a5bece0cee337b16c605e54fb1e118420e744ae
tschaffter <thomas.schaffter@gmail.com>
2022-05-20 20:55:02 +0000

    Add Spring Boot app movie-api

---
+++ apps/movie-api/src/test/java/org/sagebionetworks/movie/MovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sagebionetworks.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
