aa1d82f675986e8338d781a3f14dfc221f7a84fb
tschaffter <thomas.schaffter@gmail.com>
2022-05-24 20:18:41 +0000

    Import component and service code from article

--- libs/web/home/src/lib/movie-list/movie-list.component.ts
+++ libs/web/home/src/lib/movie-list/movie-list.component.ts
@@ -1,8 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTable } from '@angular/material/table';
+import { MovieListService } from './movie-list.service';
+import { Movie } from './movie';
+
+// interface Problem {
+//   code: number;
+//   reason: string;
+//   timestamp: string;
+//   message: string;
+// }
 
 @Component({
   selector: 'challenge-registry-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.scss'],
 })
-export class MovieListComponent {}
+export class MovieListComponent {
+  movies: Movie[] = [];
+  displayedColumns: string[] = ['title', 'director', 'year'];
+
+  @ViewChild(MatTable) table!: MatTable<any>;
+
+  constructor(
+    private backend: MovieListService,
+    private snackBar: MatSnackBar
+  ) {}
+
+  logout() {
+    console.log('Logout button was clicked');
+  }
+
+  getAllMovies() {
+    this.backend.getAllMovies().subscribe(
+      (response) => {
+        this.movies = response;
+        this.table.renderRows();
+      },
+      (error) => {
+        this.handleError(error.error);
+      }
+    );
+  }
+
+  onMovieIdChange(event: any) {
+    this.getMovieById(event.value);
+  }
+
+  private getMovieById(id: number) {
+    this.backend.getMovieById(id).subscribe(
+      (response) => {
+        this.movies = [response];
+        this.table.renderRows();
+      },
+      (error) => {
+        this.handleError(error.error);
+      }
+    );
+  }
+
+  private handleError(error: any) {
+    this.displayError(error.code + ' ' + error.reason + '. ' + error.message);
+  }
+
+  private displayError(message: string) {
+    this.snackBar.open(message, 'Close', { duration: 5000 });
+  }
+}
--- libs/web/home/src/lib/movie-list/movie-list.module.ts
+++ libs/web/home/src/lib/movie-list/movie-list.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
 import { MovieListComponent } from './movie-list.component';
+import { MovieListService } from './movie-list.service';
 
 @NgModule({
   declarations: [MovieListComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatSnackBarModule, MatTableModule],
+  providers: [MovieListService],
   exports: [MovieListComponent],
 })
 export class MovieListModule {}
