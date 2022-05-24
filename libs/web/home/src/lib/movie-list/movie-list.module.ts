976bf7b0c1cd238ea6f77a300fe2fd10a3d11663
tschaffter <thomas.schaffter@gmail.com>
2022-05-24 20:22:39 +0000

    Import HTML and CSS code

--- libs/web/home/src/lib/movie-list/movie-list.module.ts
+++ libs/web/home/src/lib/movie-list/movie-list.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { MovieListComponent } from './movie-list.component';
@@ -7,7 +8,7 @@ import { MovieListService } from './movie-list.service';
 
 @NgModule({
   declarations: [MovieListComponent],
-  imports: [CommonModule, MatSnackBarModule, MatTableModule],
+  imports: [CommonModule, MatSelectModule, MatSnackBarModule, MatTableModule],
   providers: [MovieListService],
   exports: [MovieListComponent],
 })
