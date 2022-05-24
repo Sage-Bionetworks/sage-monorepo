6922b4a69004423b6e0483aa10581c8dccc2d1a1
tschaffter <thomas.schaffter@gmail.com>
2022-05-24 19:50:47 +0000

    Import movie-list component to home page

--- libs/web/home/src/lib/home.module.ts
+++ libs/web/home/src/lib/home.module.ts
@@ -10,6 +10,7 @@ import { FeaturedChallengeListModule } from './featured-challenge-list/featured-
 import { SponsorListModule } from './sponsor-list/sponsor-list.module';
 import { StatisticsViewerModule } from './statistics-viewer/statistics-viewer.module';
 import { TopicsViewerModule } from './topics-viewer/topics-viewer.module';
+import { MovieListModule } from './movie-list/movie-list.module';
 
 const routes: Routes = [{ path: '', component: HomeComponent }];
 
@@ -26,6 +27,7 @@ const routes: Routes = [{ path: '', component: HomeComponent }];
     SponsorListModule,
     StatisticsViewerModule,
     TopicsViewerModule,
+    MovieListModule,
   ],
   exports: [HomeComponent],
 })
