ede9dcb203108dceb04d3ca3b72185bbd29e75f1
tschaffter <thomas.schaffter@gmail.com>
2022-05-28 02:52:04 +0000

    Create boot application for service registry

---
+++ apps/service-registry/src/test/java/org/sagebionetworks/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sagebionetworks.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
