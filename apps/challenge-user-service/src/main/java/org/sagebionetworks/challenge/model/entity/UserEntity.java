6bf01a5b019aebdb9aef2485ce99623dd49b0d9d
tschaffter <thomas.schaffter@gmail.com>
2022-05-30 02:33:24 +0000

    Import code from article

---
+++ apps/challenge-user-service/src/main/java/org/sagebionetworks/challenge/model/entity/UserEntity.java
@@ -0,0 +1,26 @@
+package org.sagebionetworks.challenge.model.entity;
+
+import org.sagebionetworks.challenge.model.dto.UserStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String authId;
+
+    private String identification;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+}
