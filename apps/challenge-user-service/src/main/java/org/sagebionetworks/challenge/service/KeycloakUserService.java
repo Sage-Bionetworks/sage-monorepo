0b41df885a915515dae193a9a087cfa13b22b755
tschaffter <thomas.schaffter@gmail.com>
2022-06-04 17:36:32 +0000

    Rebase `main` into this feature branch

---
+++ apps/challenge-user-service/src/main/java/org/sagebionetworks/challenge/service/KeycloakUserService.java
@@ -0,0 +1,41 @@
+package org.sagebionetworks.challenge.service;
+
+import org.sagebionetworks.challenge.configuration.KeycloakManager;
+import lombok.RequiredArgsConstructor;
+import org.keycloak.admin.client.resource.UserResource;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.stereotype.Service;
+
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class KeycloakUserService {
+
+    private final KeycloakManager keyCloakManager;
+
+    public Integer createUser(UserRepresentation userRepresentation) {
+        Response response = keyCloakManager.getKeyCloakInstanceWithRealm().users().create(userRepresentation);
+        return response.getStatus();
+    }
+
+    public void updateUser(UserRepresentation userRepresentation) {
+        keyCloakManager.getKeyCloakInstanceWithRealm().users().get(userRepresentation.getId()).update(userRepresentation);
+    }
+
+
+    public List<UserRepresentation> readUserByEmail(String email) {
+        return keyCloakManager.getKeyCloakInstanceWithRealm().users().search(email);
+    }
+
+
+    public UserRepresentation readUser(String authId) {
+        try {
+            UserResource userResource = keyCloakManager.getKeyCloakInstanceWithRealm().users().get(authId);
+            return userResource.toRepresentation();
+        } catch (Exception e) {
+            throw new RuntimeException("User not found under given ID");
+        }
+    }
+}
