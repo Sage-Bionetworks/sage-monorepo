c013ff8ef6d136287f899cfe02e1a2aaa3a1d4c5
tschaffter <thomas.schaffter@gmail.com>
2022-06-12 15:12:51 +0000

    Minor update

--- tools/add-hostnames.sh
+++ tools/add-hostnames.sh
@@ -5,7 +5,7 @@
 # list the names of the apps and services
 readarray -t app_names < <(find ./apps -mindepth 1 -maxdepth 1 -type d -printf '%P\n')
 
-# add hosts
+# add hostnames
 for app_name in "${app_names[@]}"; do
     line="127.0.0.1 $app_name"
     if ! grep -Fxq "$line" /etc/hosts; then
