2b74be8cfb9a233c830ac0b228fbca0e416d1f97
tschaffter <thomas.schaffter@gmail.com>
2022-06-13 23:32:28 +0000

    Create app challenge-core-service

---
+++ apps/challenge-core-service/src/main/java/org/sagebionetworks/challenge/ChallengeCoreServiceApplication.java
@@ -0,0 +1,13 @@
+package org.sagebionetworks.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeCoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeCoreServiceApplication.class, args);
+	}
+
+}
