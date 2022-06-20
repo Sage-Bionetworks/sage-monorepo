ae7da7adb4a6d533749f8caa44c2c39e142693c8
tschaffter <thomas.schaffter@gmail.com>
2022-06-20 17:20:08 +0000

    Prevent duplicated lines in /etc/hosts

--- tools/add-hostnames.sh
+++ tools/add-hostnames.sh
@@ -2,28 +2,27 @@
 #
 # Map the name of the apps in ./apps to 127.0.0.1.
 
-# # list the names of the apps and services
-# readarray -t app_names < <(find ./apps -mindepth 1 -maxdepth 1 -type d -printf '%P\n')
-
-# # add hostnames
-# for app_name in "${app_names[@]}"; do
-#     line="127.0.0.1 $app_name"
-#     if ! grep -Fxq "$line" /etc/hosts; then
-#       echo $line >> /etc/hosts
-#     fi
-# done
-
 # list of hostnames (defined in alphabetical order)
-echo "127.0.0.1 challenge-api-gateway" >> /etc/hosts
-echo "127.0.0.1 challenge-core-service" >> /etc/hosts
-echo "127.0.0.1 challenge-elasticsearch" >> /etc/hosts
-echo "127.0.0.1 challenge-kibana" >> /etc/hosts
-echo "127.0.0.1 challenge-logstash" >> /etc/hosts
-echo "127.0.0.1 challenge-keycloak" >> /etc/hosts
-echo "127.0.0.1 challenge-mariadb" >> /etc/hosts
-echo "127.0.0.1 challenge-mongodb" >> /etc/hosts
-echo "127.0.0.1 challenge-platform" >> /etc/hosts
-echo "127.0.0.1 challenge-postgresql" >> /etc/hosts
-echo "127.0.0.1 challenge-registry" >> /etc/hosts
-echo "127.0.0.1 challenge-service-registry" >> /etc/hosts
-echo "127.0.0.1 challenge-user-service" >> /etc/hosts
+declare -a hostnames=(
+  "127.0.0.1 challenge-api-gateway"
+  "127.0.0.1 challenge-core-service"
+  "127.0.0.1 challenge-elasticsearch"
+  "127.0.0.1 challenge-kibana"
+  "127.0.0.1 challenge-logstash"
+  "127.0.0.1 challenge-keycloak"
+  "127.0.0.1 challenge-mariadb"
+  "127.0.0.1 challenge-mongodb"
+  "127.0.0.1 challenge-platform"
+  "127.0.0.1 challenge-postgresql"
+  "127.0.0.1 challenge-registry"
+  "127.0.0.1 challenge-service-registry"
+  "127.0.0.1 challenge-user-service"
+)
+
+# add hostnames
+for hostname in "${hostnames[@]}"; do
+    line="$hostname"
+    if ! grep -Fxq "$line" /etc/hosts; then
+      echo $line >> /etc/hosts
+    fi
+done
