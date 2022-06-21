51dd6ab4905af6940d961c64c3c9fc58db83d55c
tschaffter <thomas.schaffter@gmail.com>
2022-06-21 15:27:02 +0000

    Add init DB file for mariadb (still debugging)

---
+++ apps/challenge-mariadb/docker-entrypoint-initdb.d/init-db.sql
@@ -0,0 +1,4 @@
+-- challenge-core-service
+CREATE USER 'plop' IDENTIFIED BY 'changeme';
+-- CREATE DATABASE challenge_core_servicea;
+-- GRANT ALL PRIVILEGES ON challenge_core_servicea TO challenge_core_servicea;
