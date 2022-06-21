b4ef9973bc681458c2300f0bea3ff6672f545d87
tschaffter <thomas.schaffter@gmail.com>
2022-06-21 02:21:09 +0000

    Plop

--- dev-env.sh
+++ dev-env.sh
@@ -14,7 +14,7 @@ export CHALLENGE_BUILD_PROCS
 # tput relies on the value of $TERM, which is not available when GH CI workflow
 # sources this file.
 if [ ! -z "${TERM}+x" ]; then
-  echo "plop"
+  echo "plop: $TERM"
   bold=$(tput bold)
   italic=$(tput sitm)
   reset=$(tput sgr0)
