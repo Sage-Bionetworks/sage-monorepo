fed019858263ed5714cf0d72f340acfc339bc4f0
Thomas Schaffter <thomas.schaffter@gmail.com>
2022-06-23 19:45:09 +0000

    Use sql grant (does not work yet)

--- apps/challenge-mariadb/docker-entrypoint-initdb.d/init-db.sql
+++ apps/challenge-mariadb/docker-entrypoint-initdb.d/init-db.sql
@@ -1,4 +1,8 @@
 -- challenge-core-service
 create user challenge_core_service identified by 'changeme';
+create role challenge_core_service_role;
 create database challenge_core_service;
-grant all on challenge_core_service.* to challenge_core_service;
+
+grant challenge_core_service_role to challenge_core_service;
+grant challenge_core_service_role to maria;
+grant all on challenge_core_service.* to challenge_core_service_role;
