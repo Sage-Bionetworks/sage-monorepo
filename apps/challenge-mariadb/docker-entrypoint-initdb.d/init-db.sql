73584eed545d77f3b5201391f6ce3c75872f4a93
tschaffter <thomas.schaffter@gmail.com>
2022-06-23 22:13:34 +0000

    Activate role

--- apps/challenge-mariadb/docker-entrypoint-initdb.d/init-db.sql
+++ apps/challenge-mariadb/docker-entrypoint-initdb.d/init-db.sql
@@ -1,8 +1,11 @@
 -- challenge-core-service
 create user challenge_core_service identified by 'changeme';
-create role challenge_core_service_role;
+create role challenge_core_service_role_admin;
 create database challenge_core_service;
+grant all on challenge_core_service.* to challenge_core_service_role_admin;
 
-grant challenge_core_service_role to challenge_core_service;
-grant challenge_core_service_role to maria;
-grant all on challenge_core_service.* to challenge_core_service_role;
+grant challenge_core_service_role_admin to challenge_core_service;
+set default role challenge_core_service_role_admin for challenge_core_service;
+
+grant challenge_core_service_role_admin to maria;
+set default role challenge_core_service_role_admin for maria;
