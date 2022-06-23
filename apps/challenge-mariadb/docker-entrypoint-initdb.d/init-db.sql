45dfd7cb1d0df853e239344842fcb12e8ad54400
Thomas Schaffter <thomas.schaffter@gmail.com>
2022-06-23 17:31:27 +0000

    Switch to lowercase SQL keywords

--- apps/challenge-mariadb/docker-entrypoint-initdb.d/init-db.sql
+++ apps/challenge-mariadb/docker-entrypoint-initdb.d/init-db.sql
@@ -1,4 +1,4 @@
 -- challenge-core-service
-CREATE USER challenge_core_service IDENTIFIED BY 'changeme';
-CREATE DATABASE challenge_core_service;
-GRANT ALL PRIVILEGES ON challenge_core_service TO challenge_core_service;
+create user challenge_core_service IDENTIFIED by 'changeme';
+create database challenge_core_service;
+grant ALL PRIVILEGES on challenge_core_service to challenge_core_service;
