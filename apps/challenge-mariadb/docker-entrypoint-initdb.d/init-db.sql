54e89d3101c5f2e3299fd6900f83f0188320c731
tschaffter <thomas.schaffter@gmail.com>
2022-06-23 22:19:01 +0000

    Cleanup

--- apps/challenge-mariadb/docker-entrypoint-initdb.d/init-db.sql
+++ apps/challenge-mariadb/docker-entrypoint-initdb.d/init-db.sql
@@ -2,10 +2,7 @@
 create user challenge_core_service identified by 'changeme';
 create role challenge_core_service_role_admin;
 create database challenge_core_service;
-grant all on challenge_core_service.* to challenge_core_service_role_admin;
 
+grant all on challenge_core_service.* to challenge_core_service_role_admin;
 grant challenge_core_service_role_admin to challenge_core_service;
 set default role challenge_core_service_role_admin for challenge_core_service;
-
-grant challenge_core_service_role_admin to maria;
-set default role challenge_core_service_role_admin for maria;
