2d29f963453a02d3db809f6c59058fdc8b87ab43
Thomas Schaffter <thomas.schaffter@gmail.com>
2022-06-24 05:22:19 +0000

    Create library `data-access-java`

---
+++ libs/shared/data-access-java/src/main/java/org/sagebionetworks/challenge/DataAccessJavaApplication.java
@@ -0,0 +1,13 @@
+package org.sagebionetworks.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAccessJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataAccessJavaApplication.class, args);
+	}
+
+}
