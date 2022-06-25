569288f7aac4d5f68728a628a10b976c4d85684f
tschaffter <thomas.schaffter@gmail.com>
2022-06-25 20:41:04 +0000

    Add target `lint` to api gateway

--- apps/challenge-api-gateway/src/main/java/org/sagebionetworks/challenge/configuration/SecurityConfiguration.java
+++ apps/challenge-api-gateway/src/main/java/org/sagebionetworks/challenge/configuration/SecurityConfiguration.java
@@ -9,20 +9,14 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 public class SecurityConfiguration {
-    @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        http
-                .authorizeExchange()
-                //ALLOWING REGISTER API FOR DIRECT ACCESS
-                .pathMatchers("/user/api/v1/register").permitAll()
-                //ALL OTHER APIS ARE AUTHENTICATED
-                .anyExchange().authenticated()
-                .and()
-                .csrf().disable()
-                .oauth2Login()
-                .and()
-                .oauth2ResourceServer()
-                .jwt();
-        return http.build();
-    }
+  @Bean
+  public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+    http.authorizeExchange()
+        // ALLOWING REGISTER API FOR DIRECT ACCESS
+        .pathMatchers("/user/api/v1/register").permitAll()
+        // ALL OTHER APIS ARE AUTHENTICATED
+        .anyExchange().authenticated().and().csrf().disable().oauth2Login().and()
+        .oauth2ResourceServer().jwt();
+    return http.build();
+  }
 }
