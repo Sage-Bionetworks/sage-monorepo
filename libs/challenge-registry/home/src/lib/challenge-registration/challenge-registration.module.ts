2360a63fb50310634dcb96680ac3a57214a90ee4
Verena Chung <9377970+vpchung@users.noreply.github.com>
2022-07-01 06:23:27 -0700

    Update home page (#358)
    * add app logo
    
    * copy over color palette
    
    * add initial export from teleporthq
    
    * extract theme-based styles to theme file
    
    * use values from theme
    
    * theme mixin not applying typography for some reason... writing font family directly for now
    
    * update html tags and css layouts
    
    * replace angular logo with registry logo
    
    * add theme
    
    * copy over initial layout
    
    * finalize layout and styling
    
    * add typography config to app theme
    
    * add logo in black; use asset for img
    
    * define typography levels
    
    * comment out movie-list component for now
    
    * update statistics section
    
    * update trending-cards section
    
    * update org-cards section
    
    * update add-challenge section
    
    * fix layout issues
    
    * update featured section
    
    * fix layout
    
    * update sponsor-list section
    
    * add images to `assets`
    
    * update src of images used

--- libs/challenge-registry/home/src/lib/challenge-registration/challenge-registration.module.ts
+++ libs/challenge-registry/home/src/lib/challenge-registration/challenge-registration.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 import { ChallengeRegistrationComponent } from './challenge-registration.component';
 
 @NgModule({
   declarations: [ChallengeRegistrationComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   exports: [ChallengeRegistrationComponent],
 })
 export class ChallengeRegistrationModule {}
