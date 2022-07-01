41db58bd8c1a8ecbe5f8c10a1deba0372b17e909
tschaffter <thomas.schaffter@gmail.com>
2022-07-01 23:44:46 +0000

    Push config

--- apps/challenge-config-service/src/main/java/org/sagebionetworks/challenge/ChallengeConfigServiceApplication.java
+++ apps/challenge-config-service/src/main/java/org/sagebionetworks/challenge/ChallengeConfigServiceApplication.java
@@ -2,6 +2,7 @@ package org.sagebionetworks.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
