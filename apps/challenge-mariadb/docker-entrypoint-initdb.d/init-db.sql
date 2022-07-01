a9c7fa0aee90f54553a48a481a076c5779ba4d2d
tschaffter <thomas.schaffter@gmail.com>
2022-07-01 19:39:14 +0000

    Update challenge db config

--- apps/challenge-mariadb/docker-entrypoint-initdb.d/init-db.sql
+++ apps/challenge-mariadb/docker-entrypoint-initdb.d/init-db.sql
@@ -1,17 +1,13 @@
+create database challenge;
+create role challenge_role_admin;
+grant all on challenge.* to challenge_role_admin;
+
 -- challenge-core-service
 create user challenge_core_service identified by 'changeme';
-create role challenge_core_service_role_admin;
-create database challenge_core_service;
-
-grant all on challenge_core_service.* to challenge_core_service_role_admin;
-grant challenge_core_service_role_admin to challenge_core_service;
-set default role challenge_core_service_role_admin for challenge_core_service;
+grant challenge_role_admin to challenge_core_service;
+set default role challenge_role_admin for challenge_core_service;
 
 -- challenge-user-service
 create user challenge_user_service identified by 'changeme';
-create role challenge_user_service_role_admin;
-create database challenge_user_service;
-
-grant all on challenge_user_service.* to challenge_user_service_role_admin;
-grant challenge_user_service_role_admin to challenge_user_service;
-set default role challenge_user_service_role_admin for challenge_user_service;
+grant challenge_role_admin to challenge_user_service;
+set default role challenge_role_admin for challenge_user_service;
