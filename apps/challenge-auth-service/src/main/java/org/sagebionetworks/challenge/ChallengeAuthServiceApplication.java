b8b24b8d7fc3b384ec488b05788e4ed8266a95ca
tschaffter <thomas.schaffter@gmail.com>
2022-07-07 23:27:15 +0000

    Various updates

--- apps/challenge-auth-service/src/main/java/org/sagebionetworks/challenge/ChallengeAuthServiceApplication.java
+++ apps/challenge-auth-service/src/main/java/org/sagebionetworks/challenge/ChallengeAuthServiceApplication.java
@@ -2,12 +2,14 @@ package org.sagebionetworks.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ChallengeAuthServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChallengeAuthServiceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ChallengeAuthServiceApplication.class, args);
+  }
 
 }
