5de6e656fbcd0ac9b9b4bde2c95afb408121b07d
verena <9377970+vpchung@users.noreply.github.com>
2022-07-08 18:40:25 +0000

    add RouterModule to module ts

--- libs/challenge-registry/team/src/lib/team.module.ts
+++ libs/challenge-registry/team/src/lib/team.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiModule } from '@sagebionetworks/challenge-registry/ui';
 import { TeamComponent } from './team.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: '', component: TeamComponent }];
 
 @NgModule({
-  imports: [CommonModule, UiModule],
+  imports: [CommonModule, RouterModule.forChild(routes), UiModule],
   declarations: [TeamComponent],
   exports: [TeamComponent],
 })
