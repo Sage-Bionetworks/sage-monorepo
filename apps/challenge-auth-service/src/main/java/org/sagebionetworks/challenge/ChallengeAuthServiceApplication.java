e9bba7f732b078b003295de9963f14e8745aef33
tschaffter <thomas.schaffter@gmail.com>
2022-07-08 02:33:35 +0000

    Can use auth service to login

--- apps/challenge-auth-service/src/main/java/org/sagebionetworks/challenge/ChallengeAuthServiceApplication.java
+++ apps/challenge-auth-service/src/main/java/org/sagebionetworks/challenge/ChallengeAuthServiceApplication.java
@@ -3,6 +3,9 @@ package org.sagebionetworks.challenge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -10,6 +13,11 @@ public class ChallengeAuthServiceApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ChallengeAuthServiceApplication.class, args);
+  }
+
+  @Bean
+  public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    return builder.build();
   }
 
 }
