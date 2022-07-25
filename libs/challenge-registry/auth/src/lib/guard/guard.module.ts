4aee96d9e5a89e3d043ec73ff7a79c3d385fc95c
tschaffter <thomas.schaffter@gmail.com>
2022-07-25 18:06:26 +0000

    Add KeycloakAngularModule to challenge-registry-auth

--- libs/challenge-registry/auth/src/lib/guard/guard.module.ts
+++ libs/challenge-registry/auth/src/lib/guard/guard.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthGuard } from './auth-guard.service';
 import { KAuthGuard } from './kauth.guard';
+import { KeycloakAngularModule } from 'keycloak-angular';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, KeycloakAngularModule],
   providers: [AuthGuard, KAuthGuard],
 })
 export class GuardModule {}
