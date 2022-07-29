2e10cd5947533e2a2eedd575171c6b43bd4f1631
tschaffter <thomas.schaffter@gmail.com>
2022-07-29 21:25:38 +0000

    Add directives

---
+++ libs/shared/util/src/lib/directives/app-shell-render.directive.spec.ts.off
@@ -0,0 +1,8 @@
+import { AppShellRenderDirective } from './app-shell-render.directive';
+
+describe('AppShellRenderDirective', () => {
+  it('should create an instance', () => {
+    const directive = new AppShellRenderDirective();
+    expect(directive).toBeTruthy();
+  });
+});
--- libs/shared/util/src/lib/shared-util.module.ts
+++ libs/shared/util/src/lib/shared-util.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppShellRenderDirective } from './directives/app-shell-render.directive';
+import { AppShellNoRenderDirective } from './directives/app-shell-no-render.directive';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [AppShellRenderDirective, AppShellNoRenderDirective],
 })
 export class SharedUtilModule {}
