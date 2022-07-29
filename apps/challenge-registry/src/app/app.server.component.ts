c079be5a62f8cd5b857033d01e9a70d01f4f32f2
tschaffter <thomas.schaffter@gmail.com>
2022-07-29 03:20:20 +0000

    Cleanup

--- apps/challenge-registry/src/app/app.server.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-
-@Component({
-  selector: 'challenge-registry-app-server',
-  template: '',
-  styleUrls: ['app.server.component.scss'],
-  encapsulation: ViewEncapsulation.None,
-})
-export class AppServerComponent {}
--- apps/challenge-registry/src/app/app.server.module.ts
+++ apps/challenge-registry/src/app/app.server.module.ts
@@ -3,11 +3,9 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
-// import { AppServerComponent } from './app.server.component';
 
 @NgModule({
   imports: [AppModule, ServerModule],
-  // declarations: [AppServerComponent],
   bootstrap: [AppComponent],
 })
 export class AppServerModule {}
