5829dffcccec9cf42630ca0066fa8c162ca7bbe2
tschaffter <thomas.schaffter@gmail.com>
2022-07-29 03:15:59 +0000

    Add SEO metadata to home page

--- libs/challenge-registry/home/src/lib/home.component.ts
+++ libs/challenge-registry/home/src/lib/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import {
   APP_CONFIG,
   AppConfig,
@@ -12,7 +13,18 @@ import {
 export class HomeComponent {
   public appVersion: string;
 
-  constructor(@Inject(APP_CONFIG) private appConfig: AppConfig) {
+  constructor(
+    @Inject(APP_CONFIG) private appConfig: AppConfig,
+    private title: Title,
+    private meta: Meta
+  ) {
     this.appVersion = appConfig.appVersion;
+
+    // SEO metadata
+    this.title.setTitle('Home - Challenge Registry');
+    this.meta.addTag({
+      name: 'description',
+      content: 'The homepage of the Challenge Registry',
+    });
   }
 }
