aed198efd9befbf0e0e41b5e7c3c20a7312681d7
tschaffter <thomas.schaffter@gmail.com>
2022-08-01 22:03:41 +0000

    Use different jmx name for the app adming MBean

---
+++ apps/challenge-user-service/src/test/java/org/sagebionetworks/challenge/controller/UserControllerIntegrationTest.java
@@ -0,0 +1,46 @@
+package org.sagebionetworks.challenge.controller;
+
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.sagebionetworks.challenge.ChallengeUserServiceApplication;
+import org.sagebionetworks.challenge.model.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+// import lombok.extern.slf4j.Slf4j;
+
+// @Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = ChallengeUserServiceApplication.class)
+@AutoConfigureMockMvc
+@ActiveProfiles("integration")
+public class UserControllerIntegrationTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  // @Autowired
+  // private UserRepository userRepository;
+
+  @BeforeEach
+  void setup() {
+    // userRepository.deleteAll();
+  }
+
+  // @DisplayName("Single test successful")
+  // @Disabled("Not implemented yet")
+  @Test
+  public void givenUserObject_whenCreateUser_thenStatus201() throws Exception {
+    // log.info("HELLO");
+    assumeTrue(5 > 1);
+  }
+}
