7a34f9fced59c4ec3bbbe5dacd6c11a337a80765
tschaffter <thomas.schaffter@gmail.com>
2022-08-02 17:56:42 +0000

    Test repository.deleteAll()

--- apps/challenge-user-service/src/main/java/org/sagebionetworks/challenge/model/entity/UserEntity.java
+++ apps/challenge-user-service/src/main/java/org/sagebionetworks/challenge/model/entity/UserEntity.java
@@ -14,14 +14,18 @@ import javax.persistence.*;
 @Entity
 @Table(name = "challenge_user")
 @NoArgsConstructor
-@AllArgsConstructor
 public class UserEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String username;
   private String authId;
-
   @Enumerated(EnumType.STRING)
   private UserStatus status;
+
+  public UserEntity(String username, String authId, UserStatus status) {
+    this.username = username;
+    this.authId = authId;
+    this.status = status;
+  }
 }
--- apps/challenge-user-service/src/test/java/org/sagebionetworks/challenge/model/repository/UserRepositoryTest.java
+++ apps/challenge-user-service/src/test/java/org/sagebionetworks/challenge/model/repository/UserRepositoryTest.java
@@ -31,11 +31,31 @@ public class UserRepositoryTest {
   @Test
   public void shouldStoreAUser() {
     UserEntity user = repository
-        .save(new UserEntity(1L, "test", "1212f921-6ab0-444f-a5ea-9dc154199a3c",
+        .save(new UserEntity("test", "1212f921-6ab0-444f-a5ea-9dc154199a3c",
             UserStatus.PENDING));
-    assertThat(user).hasFieldOrPropertyWithValue("id", 1L);
+    assertThat(user).hasFieldOrProperty("id");
     assertThat(user).hasFieldOrPropertyWithValue("username", "test");
     assertThat(user).hasFieldOrPropertyWithValue("authId", "1212f921-6ab0-444f-a5ea-9dc154199a3c");
     assertThat(user).hasFieldOrPropertyWithValue("status", UserStatus.PENDING);
+  }
+
+  @Test
+  public void shouldFindAllUsers() {
+    UserEntity user1 = new UserEntity("test1", "1212f921-6ab0-444f-a5ea-9dc154199a31",
+        UserStatus.PENDING);
+    UserEntity user2 = new UserEntity("test2", "1212f921-6ab0-444f-a5ea-9dc154199a32",
+        UserStatus.PENDING);
+    entityManager.persist(user1);
+    entityManager.persist(user2);
+    Iterable<UserEntity> users = repository.findAll();
+    assertThat(users).hasSize(2).contains(user1, user2);
+  }
+
+  @Test
+  public void shouldDeleteAllUsers() {
+    entityManager.persist(new UserEntity("test1", "1212f921-6ab0-444f-a5ea-9dc154199a31", UserStatus.PENDING));
+    entityManager.persist(new UserEntity("test2", "1212f921-6ab0-444f-a5ea-9dc154199a32", UserStatus.PENDING));
+    repository.deleteAll();
+    assertThat(repository.findAll()).isEmpty();
   }
 }
