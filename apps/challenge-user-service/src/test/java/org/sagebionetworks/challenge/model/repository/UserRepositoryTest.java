72dc0a375f81fac191f3ed87254b52c1efa7c383
tschaffter <thomas.schaffter@gmail.com>
2022-08-02 15:56:26 +0000

    Add unit test for UserRepository

--- apps/challenge-user-service/src/test/java/org/sagebionetworks/challenge/model/repository/UserRepositoryTest.java
+++ apps/challenge-user-service/src/test/java/org/sagebionetworks/challenge/model/repository/UserRepositoryTest.java
@@ -1,14 +1,18 @@
 package org.sagebionetworks.challenge.model.repository;
 
-// import static org.junit.jupiter.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.sagebionetworks.challenge.model.entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
+@AutoConfigureWebClient
+@ActiveProfiles("unit")
 public class UserRepositoryTest {
 
   @Autowired
@@ -17,10 +21,9 @@ public class UserRepositoryTest {
   @Autowired
   UserRepository repository;
 
-  // @Test
-  // public void shouldFindNoTutorialsIfRepositoryIsEmpty() {
-  // // Iterable<UserEntity> tutorials = repository.findAll();
-
-  // // assertThat(tutorials).isEmpty();
-  // }
+  @Test
+  public void shouldFindNoUsersIfRepositoryIsEmpty() {
+    Iterable<UserEntity> users = repository.findAll();
+    assertThat(users).isEmpty();
+  }
 }
