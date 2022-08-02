1c21151fc2df0f162234fbb4efd5726531bb4944
tschaffter <thomas.schaffter@gmail.com>
2022-08-02 22:45:12 +0000

    Fix most of the checkstyle issue in user service

--- apps/challenge-core-service/src/main/java/org/sagebionetworks/challenge/exception/GlobalExceptionHandler.java
+++ apps/challenge-core-service/src/main/java/org/sagebionetworks/challenge/exception/GlobalExceptionHandler.java
@@ -1,19 +1,18 @@
 package org.sagebionetworks.challenge.exception;
 
-import org.sagebionetworks.challenge.util.exception.SimpleChallengeGlobalException;
+import java.util.Locale;
 import org.sagebionetworks.challenge.util.exception.ErrorResponse;
+import org.sagebionetworks.challenge.util.exception.SimpleChallengeGlobalException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.Locale;
-
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
   @ExceptionHandler(SimpleChallengeGlobalException.class)
-  protected ResponseEntity handleGlobalException(
+  protected ResponseEntity<ErrorResponse> handleGlobalException(
       SimpleChallengeGlobalException simpleChallengeGlobalException, Locale locale) {
     return ResponseEntity.badRequest()
         .body(ErrorResponse.builder().code(simpleChallengeGlobalException.getCode())
