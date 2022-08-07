778fbfc8eb9344b11e50a9aa49358a5a4248b440
tschaffter <thomas.schaffter@gmail.com>
2022-08-07 18:52:46 +0000

    Remove terminal message about `yarn.lock`

--- dev-env.sh
+++ dev-env.sh
@@ -94,8 +94,7 @@ function challenge-welcome {
   if [ ! -d "node_modules" ]; then
     printf "%s\n" \
       "" \
-      "Run ${bold}challenge-install${reset} to install workspace tools like ${bold}nx${reset} and ${bold}jest${reset}." \
-      "Run this command each time ${italic}package-lock.json${reset} may have changed."
+      "Run ${bold}challenge-install${reset} to install workspace tools like ${bold}nx${reset} and ${bold}jest${reset}."
   fi
 
   if [ ! -f "nx-cloud.env" ]; then
