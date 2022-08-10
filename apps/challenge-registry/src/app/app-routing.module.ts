7e2d0516b8b3d0c470ce3cf608a43d868b9418fe
tschaffter <thomas.schaffter@gmail.com>
2022-08-10 22:37:44 +0000

    Add GOT home module

--- apps/challenge-registry/src/app/app-routing.module.ts
+++ apps/challenge-registry/src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // {
+  //   path: 'home',
+  //   loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  // },
   {
     path: 'home',
     loadChildren: () =>
--- apps/challenge-registry/src/main.ts
+++ apps/challenge-registry/src/main.ts
@@ -3,44 +3,45 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import {
-  AppConfig,
+  // AppConfig,
   APP_CONFIG,
-  Environment,
+  // Environment,
 } from '@sagebionetworks/challenge-registry/config';
+import { environment } from './environments/environment';
 // import { environment } from './environments/environment';
 
-function bootstrap() {
-  fetch('/config/config.json')
-    .then((response) => response.json() as Promise<AppConfig>)
-    .then((config: AppConfig) => {
-      if (
-        [Environment.Production, Environment.Staging].includes(
-          config.environment
-        )
-      ) {
-        enableProdMode();
-      }
+// function bootstrap() {
+//   fetch('/config/config.json')
+//     .then((response) => response.json() as Promise<AppConfig>)
+//     .then((config: AppConfig) => {
+//       if (
+//         [Environment.Production, Environment.Staging].includes(
+//           config.environment
+//         )
+//       ) {
+//         enableProdMode();
+//       }
 
-      console.log('challenge-registry config', config);
+//       console.log('challenge-registry config', config);
 
-      platformBrowserDynamic([{ provide: APP_CONFIG, useValue: config }])
-        .bootstrapModule(AppModule)
-        .catch((err) => console.error(err));
-    });
-}
-
-if (document.readyState === 'complete') {
-  bootstrap();
-} else {
-  document.addEventListener('DOMContentLoaded', bootstrap);
-}
-
-// if (environment.production) {
-//   enableProdMode();
+//       platformBrowserDynamic([{ provide: APP_CONFIG, useValue: config }])
+//         .bootstrapModule(AppModule)
+//         .catch((err) => console.error(err));
+//     });
 // }
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   platformBrowserDynamic()
-//     .bootstrapModule(AppModule)
-//     .catch((err) => console.error(err));
-// });
+// if (document.readyState === 'complete') {
+//   bootstrap();
+// } else {
+//   document.addEventListener('DOMContentLoaded', bootstrap);
+// }
+
+if (environment.production) {
+  enableProdMode();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  platformBrowserDynamic([{ provide: APP_CONFIG, useValue: {} }])
+    .bootstrapModule(AppModule)
+    .catch((err) => console.error(err));
+});
