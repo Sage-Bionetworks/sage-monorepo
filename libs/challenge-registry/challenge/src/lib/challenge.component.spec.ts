bb65aefbd6d31e94f48771d6dc78fab7ad812d27
tschaffter <thomas.schaffter@gmail.com>
2022-08-11 04:27:35 +0000

    Fix more tests

--- libs/challenge-registry/challenge/src/lib/challenge.component.spec.ts
+++ libs/challenge-registry/challenge/src/lib/challenge.component.spec.ts
@@ -3,7 +3,6 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ConfigService } from '@sagebionetworks/challenge-registry/config';
-
 import { ChallengeComponent } from './challenge.component';
 
 describe('ChallengeComponent', () => {
