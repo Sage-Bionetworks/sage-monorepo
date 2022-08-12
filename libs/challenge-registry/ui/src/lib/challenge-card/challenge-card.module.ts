886200c1e0b13a85906ee57d2bebfbf8a8f80c4b
Rongrong Chai <rchai@sagebase.org>
2022-08-12 13:50:02 +0000

    fix icon on challenge-card

--- libs/challenge-registry/ui/src/lib/challenge-card/challenge-card.module.ts
+++ libs/challenge-registry/ui/src/lib/challenge-card/challenge-card.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChallengeCardComponent } from './challenge-card.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [ChallengeCardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   exports: [ChallengeCardComponent],
 })
 export class ChallengeCardModule {}
