c3cc856b1243f77d34e28ddbc74980c67e6ab352
tschaffter <thomas.schaffter@gmail.com>
2022-08-13 00:59:36 +0000

    Fix test

--- libs/challenge-registry/user-profile/src/lib/user-profile.resolver.spec.ts
+++ libs/challenge-registry/user-profile/src/lib/user-profile.resolver.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { UserProfileResolver } from './user-profile.resolver';
@@ -6,7 +7,7 @@ describe('UserProfileResolver', () => {
   let resolver: UserProfileResolver;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ imports: [HttpClientModule] });
     resolver = TestBed.inject(UserProfileResolver);
   });
 
