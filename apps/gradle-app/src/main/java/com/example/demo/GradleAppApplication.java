a8d5f5a33629eb222749ac76d2a09af5921e42fe
Thomas Schaffter <thomas.schaffter@gmail.com>
2022-08-18 18:48:46 +0000

    Add gradle-app and gradle-lib

--- apps/gradle-app/src/main/java/com/example/demo/GradleAppApplication.java
+++ apps/gradle-app/src/main/java/com/example/demo/GradleAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PlopApplication {
+public class GradleAppApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(PlopApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(GradleAppApplication.class, args);
+	}
 
 }
