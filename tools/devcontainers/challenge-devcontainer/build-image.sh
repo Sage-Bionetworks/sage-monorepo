eff5aac07d18696fc304434cfc982bb1b6848125
tschaffter <thomas.schaffter@gmail.com>
2022-08-28 15:17:45 +0000

    Use devcontainer `test-dvc`

--- tools/devcontainers/challenge-devcontainer/build-image.sh
+++ tools/devcontainers/challenge-devcontainer/build-image.sh
@@ -2,7 +2,9 @@
 
 # This script must be run from the folder that includes it.
 
+imageTag="${1:-test}"
+
 devcontainer build \
   --no-cache \
   --disable-telemetry true \
-  --image-name sagebionetworks/challenge-devcontainer:test
+  --image-name "sagebionetworks/challenge-devcontainer:$imageTag"
