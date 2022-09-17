cfae75006d5fe1c812ec68024357b7aab27d2229
tschaffter <thomas.schaffter@gmail.com>
2022-09-17 03:01:36 +0000

    Rename app class

---
+++ apps/challenge-user-service/src/main/java/org/sagebionetworks/challenge/ChallengeUserServiceApplication.java
@@ -0,0 +1,24 @@
+package org.sagebionetworks.challenge;
+
+import com.fasterxml.jackson.databind.Module;
+import org.openapitools.jackson.nullable.JsonNullableModule;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"org.sagebionetworks.challenge", "org.sagebionetworks.challenge.api",
+    "org.sagebionetworks.challenge.configuration"})
+public class ChallengeUserServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ChallengeUserServiceApplication.class, args);
+  }
+
+  @Bean
+  public Module jsonNullableModule() {
+    return new JsonNullableModule();
+  }
+
+}
