a926fe9c8aac383c90f4fc2f5608891da2f8179c
tschaffter <thomas.schaffter@gmail.com>
2022-09-20 15:53:14 +0000

    Remove `Spotless Gradle`

--- apps/challenge-user-service/src/main/java/org/sagebionetworks/challenge/ChallengeUserServiceApplication.java
+++ apps/challenge-user-service/src/main/java/org/sagebionetworks/challenge/ChallengeUserServiceApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-// plop
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
