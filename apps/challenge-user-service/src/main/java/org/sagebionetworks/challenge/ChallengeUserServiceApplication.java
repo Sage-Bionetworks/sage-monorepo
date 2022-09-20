a9b70a9bb42c95fde6730e2e201a541b5f45364f
tschaffter <thomas.schaffter@gmail.com>
2022-09-20 03:42:32 +0000

    Dummy commit

--- apps/challenge-user-service/src/main/java/org/sagebionetworks/challenge/ChallengeUserServiceApplication.java
+++ apps/challenge-user-service/src/main/java/org/sagebionetworks/challenge/ChallengeUserServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+// plop
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
