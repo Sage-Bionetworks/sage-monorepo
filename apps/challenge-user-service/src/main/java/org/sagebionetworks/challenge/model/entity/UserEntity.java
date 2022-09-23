b07dab083d61e624551c8c958917e8a531945f24
Thomas Schaffter <thomas.schaffter@gmail.com>
2022-09-23 17:56:47 +0000

    Update UserEntity

--- apps/challenge-user-service/src/main/java/org/sagebionetworks/challenge/model/entity/UserEntity.java
+++ apps/challenge-user-service/src/main/java/org/sagebionetworks/challenge/model/entity/UserEntity.java
@@ -1,5 +1,6 @@
 package org.sagebionetworks.challenge.model.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -7,30 +8,37 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.sagebionetworks.challenge.model.dto.UserStatus;
 
-@Getter
-@Setter
+/**
+ * The User information saved to DB.
+ *
+ * <p>The following properties are saved in Keycloak: email, password (hash).
+ */
 @Entity
 @Table(name = "challenge_user")
+@Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(nullable = false, updatable = false)
   private Long id;
 
+  @Column(nullable = false)
   private String username;
+
+  @Column(nullable = false)
   private String authId;
 
   @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
   private UserStatus status;
-
-  public UserEntity(String username, String authId, UserStatus status) {
-    this.username = username;
-    this.authId = authId;
-    this.status = status;
-  }
 }
