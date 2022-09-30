cea545825a9533b9fe7342d832e2727b975998dd
Rongrong Chai <rchai@sagebase.org>
2022-09-30 13:50:10 +0000

    initial setup for primeng paginator

--- libs/challenge-registry/user-profile/src/lib/user-profile-starred/user-profile-starred.module.ts
+++ libs/challenge-registry/user-profile/src/lib/user-profile-starred/user-profile-starred.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiModule } from '@sagebionetworks/challenge-registry/ui';
+import { PaginatorModule } from 'primeng/paginator';
 import { UserProfileStarredComponent } from './user-profile-starred.component';
 
 @NgModule({
   declarations: [UserProfileStarredComponent],
-  imports: [CommonModule, UiModule],
+  imports: [CommonModule, PaginatorModule, UiModule],
   exports: [UserProfileStarredComponent],
 })
 export class UserProfileStarredModule {}
