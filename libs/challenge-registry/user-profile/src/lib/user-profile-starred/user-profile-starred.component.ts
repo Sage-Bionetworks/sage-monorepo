9e4b99bf0292c4ba9de65dd2929982c534963205
Rongrong Chai <rchai@sagebase.org>
2022-09-30 15:20:22 +0000

    connect paginator with results

--- libs/challenge-registry/user-profile/src/lib/user-profile-starred/user-profile-starred.component.ts
+++ libs/challenge-registry/user-profile/src/lib/user-profile-starred/user-profile-starred.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Challenge, Organization, User } from '@sagebionetworks/api-angular';
 import { Tab } from '../tab.model';
 import { USER_PROFILE_STARRED_TABS } from './user-profile-starred-tabs';
@@ -6,16 +6,19 @@ import {
   MOCK_CHALLENGES,
   MOCK_ORGANIZATIONS,
 } from '@sagebionetworks/challenge-registry/ui';
+import { Paginator } from 'primeng/paginator';
+import { BehaviorSubject, of, switchMap } from 'rxjs';
+import { assign } from 'lodash';
 
 @Component({
   selector: 'challenge-registry-user-profile-starred',
   templateUrl: './user-profile-starred.component.html',
   styleUrls: ['./user-profile-starred.component.scss'],
 })
-export class UserProfileStarredComponent {
+export class UserProfileStarredComponent implements OnInit {
   @Input() user!: User;
   @Input() loggedIn!: boolean;
-  challenges: Challenge[] = MOCK_CHALLENGES;
+  // challenges: Challenge[] = MOCK_CHALLENGES;
   organizations: Organization[] = MOCK_ORGANIZATIONS;
   stars: Challenge[] = [];
   starred!: boolean;
@@ -23,7 +26,53 @@ export class UserProfileStarredComponent {
   tabKeys: string[] = Object.keys(this.tabs);
   activeTab: Tab = this.tabs['challenges'];
 
+  // mock up challenge/organization search results
+  limit = 1;
+  offset = 0;
+  challengeResultsCount = 0;
+  challenges!: Challenge[];
+
+  private query: BehaviorSubject<any> = new BehaviorSubject<any>({
+    offset: 0,
+    limit: 1,
+  });
+
+  ngOnInit(): void {
+    this.query
+      .pipe(
+        switchMap((query: any) =>
+          of({
+            challenges: []
+              .concat(...new Array(100).fill(MOCK_CHALLENGES))
+              .slice(query.offset, query.offset + query.limit),
+            totalResults: 100 * MOCK_CHALLENGES.length,
+          })
+        )
+      )
+      .subscribe((page) => {
+        this.challenges = page.challenges;
+        console.log(this.challenges);
+        this.challengeResultsCount = page.totalResults;
+      });
+  }
+
+  updateQuery(): void {
+    const query = assign(this.query.getValue(), {
+      offset: this.offset,
+      limit: this.limit,
+    });
+    this.query.next(query);
+  }
+
   clickEvent(tab: string): void {
     this.activeTab = this.tabs[tab];
+  }
+
+  onPageChange(event: Paginator) {
+    console.log(event.first);
+    this.offset = event.first;
+    console.log(event.rows);
+    this.limit = event.rows;
+    this.updateQuery();
   }
 }
