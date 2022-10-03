d70fa3b58e98f3c27f9de2a31fb8071d4ea8bf79
Thomas Schaffter <thomas.schaffter@gmail.com>
2022-10-03 15:15:35 -0700

    feat(app): Authenticate the user using Keycloak (#444)
    * Upgrade package `keycloak-angular`
    
    * Add keycloak realm to CR config
    
    * Protect Challenges page that direct to user page
    
    * Protecting the About page
    
    * Parametrizing user menu items
    
    * Connect user menu logout button
    
    * Add KeycloakAngularModule to challenge-registry-auth
    
    * Disable home tests
    
    * Can now login and logout
    
    * Remove kauth from home page
    
    * Remove kauth from home page
    
    * Cleanup app component
    
    * Add methods to KAuthService
    
    * Specify flow in kc config
    
    * Minor update
    
    * Delete node_modules during post-merge hook
    
    * Restore Keycloak, logout issue
    
    * Can login and logout
    
    * Sort dependencies

--- libs/challenge-registry/ui/src/index.ts
+++ libs/challenge-registry/ui/src/index.ts
@@ -6,5 +6,5 @@ export * from './lib/avatar/mock-avatars';
 export * from './lib/navbar/navbar-section';
 export * from './lib/organization-card/mock-organizations';
 export * from './lib/user-button/menu-item';
-export * from './lib/user-button/mock-menu-items';
+export * from './lib/user-button/user-menu-items';
 export * from './lib/user-button/mock-user';
