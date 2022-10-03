ce4a6952dd68735f6467bc5965c13e5c65aa720c
Thomas Schaffter <thomas.schaffter@gmail.com>
2022-10-03 00:14:16 +0000

    Fix test import

--- apps/challenge-bot/test/app.spec.ts
+++ apps/challenge-bot/test/app.spec.ts
@@ -3,7 +3,7 @@
 
 import nock from 'nock';
 // Requiring our app implementation
-import myProbotApp from '../src/app';
+import myProbotApp from '../src/index';
 import { Probot, ProbotOctokit } from 'probot';
 // Requiring our fixtures
 import payload from './fixtures/issues.opened.json';
