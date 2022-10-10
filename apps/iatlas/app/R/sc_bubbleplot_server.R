ecadf2cdf710871490bafe546e045b08b81299e4
heimannch <carolina.heimann@gmail.com>
2022-10-10 15:55:12 -0700

    add bubble plot for single cell summary

---
+++ apps/iatlas/app/R/sc_bubbleplot_server.R
@@ -0,0 +1,47 @@
+sc_bubbleplot_server <- function(
+  id,
+  cohort_obj
+) {
+  shiny::moduleServer(
+    id,
+    function(input, output, session) {
+
+      ns <- session$ns
+
+      bubble_df <- shiny::reactive(read.csv("inst/tsv/bubble_plot_df.csv"))
+
+      output$select_cells <- shiny::renderUI({
+        shiny::req(bubble_df())
+        shiny::selectizeInput(
+          ns("cells"),
+          label = "Select cells",
+          choices = unique(bubble_df()$cell_type),
+          selected = c("B cell", "Plasma cell",  "CD8+ Teff"),
+          multiple = TRUE
+        )
+      })
+
+      output$select_genes <- shiny::renderUI({
+        shiny::req(bubble_df())
+        shiny::selectizeInput(
+          ns("genes"),
+          label = "Select genes",
+          choices = unique(bubble_df()$gene),
+          selected = c("CTLA4", "PDCD1", "LAG3"),
+          multiple = TRUE
+        )
+      })
+
+      output$bubble_plot <- plotly::renderPlotly({
+        shiny::req(bubble_df(), input$genes, input$cells)
+
+        bubble_df() %>%
+          dplyr::filter(cell_type %in% input$cells & gene %in% input$genes) %>%
+          plotly::plot_ly(
+            x = ~ gene, y = ~ cell_type, type = "scatter", size = ~ perc_expr, color = ~avg, mode = "markers"
+          )
+      })
+
+    }
+  )
+}
