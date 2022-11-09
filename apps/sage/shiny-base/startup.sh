00d74d3a3bb05992634da7ece06e507c022a877c
bhoff <bruce.hoff@sagebase.org>
2022-11-09 09:14:42 -0800

    improved env var handling

--- apps/sage/shiny-base/startup.sh
+++ apps/sage/shiny-base/startup.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
 
-# Pass environment variable from ECS to Shiny
-echo "" >> .Renviron
-echo $SECRETS_MANAGER_SECRETS | jq -r 'to_entries[] | [.key,.value] | join("=")' >> .Renviron
+# Split up SECRETS_MANAGER_SECRETS env var (from ECS) into individual variables, suitable for the application
+export $(echo $SECRETS_MANAGER_SECRETS | jq -r 'to_entries[] | [.key,.value] | join("=")')
 
 # Now, start up Shiny
 /usr/bin/shiny-server
