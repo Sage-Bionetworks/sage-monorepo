f84fe2f691a8b4eeb66738f963b26b919efc2083
bhoff <bruce.hoff@sagebase.org>
2022-11-09 17:01:50 -0800

    revert method to pass env var's

--- apps/sage/shiny-base/startup.sh
+++ apps/sage/shiny-base/startup.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/bash
 
-# Split up SECRETS_MANAGER_SECRETS env var (from ECS) into individual variables, suitable for the application
-# TODO This doesn't seem to work
-export $(echo $SECRETS_MANAGER_SECRETS | jq -r 'to_entries[] | [.key,.value] | join("=")')
-
-# TODO This is the 'old' way.  Remove once the above works
 # Pass environment variable from ECS to Shiny
 echo "" >> .Renviron
 echo $SECRETS_MANAGER_SECRETS | jq -r 'to_entries[] | [.key,.value] | join("=")' >> .Renviron
