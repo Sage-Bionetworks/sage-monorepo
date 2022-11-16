a531a12434c3891c65fa5e5db774e04eae0cd1dc
verena <9377970+vpchung@users.noreply.github.com>
2022-11-16 05:41:19 +0000

    initial layout for search page

--- libs/challenge-registry/challenge-search/src/lib/challenge-search.module.ts
+++ libs/challenge-registry/challenge-search/src/lib/challenge-search.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { UiModule } from '@sagebionetworks/challenge-registry/ui';
+import { MatIconModule } from '@angular/material/icon';
 import { ChallengeSearchComponent } from './challenge-search.component';
 
 const routes: Routes = [{ path: '', component: ChallengeSearchComponent }];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes), UiModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    RouterModule.forChild(routes),
+    UiModule,
+  ],
   declarations: [ChallengeSearchComponent],
   exports: [ChallengeSearchComponent],
 })
