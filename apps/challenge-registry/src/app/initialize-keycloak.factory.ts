2b4f6ead19d3954bfa75aa30020e65cb2619fe2f
Thomas Schaffter <thomas.schaffter@gmail.com>
2022-11-17 01:20:01 +0000

    Disable vs code auto formatting for TS files

--- apps/challenge-registry/src/app/initialize-keycloak.factory.ts
+++ apps/challenge-registry/src/app/initialize-keycloak.factory.ts
@@ -4,27 +4,33 @@ import { KeycloakService } from 'keycloak-angular';
 // import { fromPromise } from 'rxjs/internal-compatibility';
 import { ConfigService } from '@sagebionetworks/challenge-registry/config';
 // import { ConfigInitService } from './config-init.service';
+import { isPlatformBrowser } from '@angular/common';
 
 export function initializeKeycloakFactory(
   configService: ConfigService,
-  keycloak: KeycloakService
+  keycloak: KeycloakService,
+  platformId: Record<string, unknown>
 ) {
-  return () =>
-    keycloak.init({
-      config: {
-        url: 'http://localhost:8080',
-        realm: 'test', // configService.config.keycloakRealm,
-        clientId: 'test-client',
-        // url: config['KEYCLOAK_URL'] + '/auth',
-        // realm: config['KEYCLOAK_REALM'],
-        // clientId: config['KEYCLOAK_CLIENT_ID'],
-      },
-      initOptions: {
-        onLoad: 'check-sso',
-        silentCheckSsoRedirectUri:
-          window.location.origin + '/assets/silent-check-sso.html',
-        flow: 'standard',
-      },
-      bearerExcludedUrls: [],
-    });
+  return () => {
+    if (isPlatformBrowser(platformId)) {
+      keycloak.init({
+        config: {
+          url: 'http://localhost:8080',
+          realm: 'test', // configService.config.keycloakRealm,
+          clientId: 'test-client',
+          // url: config['KEYCLOAK_URL'] + '/auth',
+          // realm: config['KEYCLOAK_REALM'],
+          // clientId: config['KEYCLOAK_CLIENT_ID'],
+        },
+        initOptions: {
+          onLoad: 'check-sso',
+          silentCheckSsoRedirectUri:
+            'http://localhost:4200' + '/assets/silent-check-sso.html',
+          // window.location.origin + '/assets/silent-check-sso.html',
+          flow: 'standard',
+        },
+        bearerExcludedUrls: [],
+      });
+    }
+  };
 }
