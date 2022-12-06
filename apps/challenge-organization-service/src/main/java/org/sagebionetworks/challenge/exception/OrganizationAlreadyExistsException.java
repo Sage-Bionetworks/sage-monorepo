b718f7d98f984c2e267897e71708a8b02797aa93
Thomas Schaffter <thomas.schaffter@gmail.com>
2022-12-06 17:07:42 +0000

    Add code related to  error handling

--- apps/challenge-organization-service/src/main/java/org/sagebionetworks/challenge/exception/OrganizationAlreadyExistsException.java
+++ apps/challenge-organization-service/src/main/java/org/sagebionetworks/challenge/exception/OrganizationAlreadyExistsException.java
@@ -2,8 +2,8 @@ package org.sagebionetworks.challenge.exception;
 
 import org.sagebionetworks.challenge.util.exception.SimpleChallengeGlobalException;
 
-public class InvalidUserException extends SimpleChallengeGlobalException {
-  public InvalidUserException(String message, String code) {
+public class OrganizationAlreadyExistsException extends SimpleChallengeGlobalException {
+  public OrganizationAlreadyExistsException(String message, String code) {
     super(message, code);
   }
 }
