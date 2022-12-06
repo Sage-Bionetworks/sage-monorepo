c421b2d07187b07e0fb3f157d58415270c2e42e1
Thomas Schaffter <thomas.schaffter@gmail.com>
2022-12-06 23:18:44 +0000

    Add keycloak project

---
+++ apps/keycloak/src/main/java/com/baeldung/auth/config/RegularJsonConfigProviderFactory.java
@@ -0,0 +1,7 @@
+package com.baeldung.auth.config;
+
+import org.keycloak.services.util.JsonConfigProviderFactory;
+
+public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
+
+}
