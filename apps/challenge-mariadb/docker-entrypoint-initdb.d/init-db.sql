4df181e570fd7f40da0fe0c2e2bec7990472beda
Thomas Schaffter <thomas.schaffter@gmail.com>
2022-12-19 03:24:34 +0000

    Create SQL user for challenge-service

--- apps/challenge-mariadb/docker-entrypoint-initdb.d/init-db.sql
+++ apps/challenge-mariadb/docker-entrypoint-initdb.d/init-db.sql
@@ -6,17 +6,22 @@ grant all on challenge.* to challenge_role_admin;
 grant challenge_role_admin to maria;
 set default role challenge_role_admin for maria;
 
--- challenge-core-service
+-- Create the user for challenge-core-service
 create user challenge_core_service identified by 'changeme';
 grant challenge_role_admin to challenge_core_service;
 set default role challenge_role_admin for challenge_core_service;
 
--- challenge-user-service
+-- Create the user for challenge-user-service
 create user challenge_user_service identified by 'changeme';
 grant challenge_role_admin to challenge_user_service;
 set default role challenge_role_admin for challenge_user_service;
 
--- challenge-organization-service
+-- Create the user for challenge-organization-service
 create user challenge_organization_service identified by 'changeme';
 grant challenge_role_admin to challenge_organization_service;
 set default role challenge_role_admin for challenge_organization_service;
+
+-- Create the user for challenge-service
+create user challenge_service identified by 'changeme';
+grant challenge_role_admin to challenge_service;
+set default role challenge_role_admin for challenge_service;
