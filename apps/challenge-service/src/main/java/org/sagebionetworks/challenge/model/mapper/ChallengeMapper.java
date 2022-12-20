6bc16eabaab67b3949e6f55c91597b5b86a667fb
Thomas Schaffter <thomas.schaffter@gmail.com>
2022-12-20 00:24:20 +0000

    Fix entity mapper for `status`

--- apps/challenge-service/src/main/java/org/sagebionetworks/challenge/model/mapper/ChallengeMapper.java
+++ apps/challenge-service/src/main/java/org/sagebionetworks/challenge/model/mapper/ChallengeMapper.java
@@ -1,13 +1,17 @@
 package org.sagebionetworks.challenge.model.mapper;
 
+import lombok.extern.slf4j.Slf4j;
 import org.sagebionetworks.challenge.model.dto.ChallengeDto;
+import org.sagebionetworks.challenge.model.dto.ChallengeStatusDto;
 import org.sagebionetworks.challenge.model.entity.ChallengeEntity;
 import org.sagebionetworks.challenge.util.model.mapper.BaseMapper;
 import org.springframework.beans.BeanUtils;
 
+@Slf4j
 public class ChallengeMapper extends BaseMapper<ChallengeEntity, ChallengeDto> {
   @Override
   public ChallengeEntity convertToEntity(ChallengeDto dto, Object... args) {
+    log.info("DTO to Entity");
     ChallengeEntity ChallengeEntity = new ChallengeEntity();
     if (dto != null) {
       BeanUtils.copyProperties(dto, ChallengeEntity);
@@ -17,9 +21,11 @@ public class ChallengeMapper extends BaseMapper<ChallengeEntity, ChallengeDto> {
 
   @Override
   public ChallengeDto convertToDto(ChallengeEntity entity, Object... args) {
+    log.info("Entity to DTO");
     ChallengeDto user = new ChallengeDto();
     if (entity != null) {
       BeanUtils.copyProperties(entity, user);
+      user.setStatus(ChallengeStatusDto.fromValue(entity.getStatus()));
     }
     return user;
   }
