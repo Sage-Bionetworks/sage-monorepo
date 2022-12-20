bf87a5cdd3d02bc3c31546617348da2bf081517a
Thomas Schaffter <thomas.schaffter@gmail.com>
2022-12-20 00:26:29 +0000

    Cleanup

--- apps/challenge-service/src/main/java/org/sagebionetworks/challenge/model/mapper/ChallengeMapper.java
+++ apps/challenge-service/src/main/java/org/sagebionetworks/challenge/model/mapper/ChallengeMapper.java
@@ -1,17 +1,14 @@
 package org.sagebionetworks.challenge.model.mapper;
 
-import lombok.extern.slf4j.Slf4j;
 import org.sagebionetworks.challenge.model.dto.ChallengeDto;
 import org.sagebionetworks.challenge.model.dto.ChallengeStatusDto;
 import org.sagebionetworks.challenge.model.entity.ChallengeEntity;
 import org.sagebionetworks.challenge.util.model.mapper.BaseMapper;
 import org.springframework.beans.BeanUtils;
 
-@Slf4j
 public class ChallengeMapper extends BaseMapper<ChallengeEntity, ChallengeDto> {
   @Override
   public ChallengeEntity convertToEntity(ChallengeDto dto, Object... args) {
-    log.info("DTO to Entity");
     ChallengeEntity ChallengeEntity = new ChallengeEntity();
     if (dto != null) {
       BeanUtils.copyProperties(dto, ChallengeEntity);
@@ -21,7 +18,6 @@ public class ChallengeMapper extends BaseMapper<ChallengeEntity, ChallengeDto> {
 
   @Override
   public ChallengeDto convertToDto(ChallengeEntity entity, Object... args) {
-    log.info("Entity to DTO");
     ChallengeDto user = new ChallengeDto();
     if (entity != null) {
       BeanUtils.copyProperties(entity, user);
