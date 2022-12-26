53db9ec456574e82e7c373ba0810efac0797b3b0
Thomas Schaffter <thomas.schaffter@gmail.com>
2022-12-26 18:31:41 +0000

    Add RabbitMQ configuration

---
+++ apps/challenge-service/src/main/java/org/sagebionetworks/challenge/configuration/RabbitMQConfiguration.java
@@ -0,0 +1,43 @@
+package org.sagebionetworks.challenge.configuration;
+
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfiguration {
+
+    @Value("${spring.rabbitmq.host}")
+    String host;
+
+    @Value("${spring.rabbitmq.username}")
+    String username;
+
+    @Value("${spring.rabbitmq.password}")
+    String password;
+
+    @Bean
+    CachingConnectionFactory connectionFactory() {
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(host);
+        cachingConnectionFactory.setUsername(username);
+        cachingConnectionFactory.setPassword(password);
+        return cachingConnectionFactory;
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
+}
