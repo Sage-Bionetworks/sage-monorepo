65d979cdaebf48fb9283d3f8e0c3da5aff0977bb
Thomas Schaffter <thomas.schaffter@gmail.com>
2022-12-26 19:30:41 +0000

    Can send message to RabbitMQ

--- apps/challenge-service/src/main/java/org/sagebionetworks/challenge/configuration/RabbitMQConfiguration.java
+++ apps/challenge-service/src/main/java/org/sagebionetworks/challenge/configuration/RabbitMQConfiguration.java
@@ -1,43 +1,75 @@
 package org.sagebionetworks.challenge.configuration;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfiguration {
 
-    @Value("${spring.rabbitmq.host}")
-    String host;
+  @Value("${spring.rabbitmq.host}")
+  String host;
 
-    @Value("${spring.rabbitmq.username}")
-    String username;
+  @Value("${spring.rabbitmq.port}")
+  int port;
 
-    @Value("${spring.rabbitmq.password}")
-    String password;
+  @Value("${spring.rabbitmq.username}")
+  String username;
 
-    @Bean
-    CachingConnectionFactory connectionFactory() {
-        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(host);
-        cachingConnectionFactory.setUsername(username);
-        cachingConnectionFactory.setPassword(password);
-        return cachingConnectionFactory;
-    }
+  @Value("${spring.rabbitmq.password}")
+  String password;
 
-    @Bean
-    public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
+  @Value("${spring.rabbitmq.queue}")
+  private String queue;
 
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(jsonMessageConverter());
-        return rabbitTemplate;
-    }
+  @Value("${spring.rabbitmq.exchange}")
+  private String exchange;
+
+  @Value("${spring.rabbitmq.routingkey}")
+  private String routingKey;
+
+  @Bean
+  CachingConnectionFactory connectionFactory() {
+    CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(host, port);
+    cachingConnectionFactory.setUsername(username);
+    cachingConnectionFactory.setPassword(password);
+    return cachingConnectionFactory;
+  }
+
+  @Bean
+  public MessageConverter jsonMessageConverter() {
+    return new Jackson2JsonMessageConverter();
+  }
+
+  @Bean
+  public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+    final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+    rabbitTemplate.setMessageConverter(jsonMessageConverter());
+    return rabbitTemplate;
+  }
+
+  @Bean
+  Queue queue() {
+    return new Queue(queue, true);
+  }
+
+  @Bean
+  Exchange myExchange() {
+    return ExchangeBuilder.directExchange(exchange).durable(true).build();
+  }
+
+  @Bean
+  Binding binding() {
+    return BindingBuilder.bind(queue()).to(myExchange()).with(routingKey).noargs();
+  }
 }
