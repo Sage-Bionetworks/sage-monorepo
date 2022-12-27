988086ee24fc8171982b95889bd62364b4d0cc1c
Thomas Schaffter <thomas.schaffter@gmail.com>
2022-12-27 00:17:29 +0000

    Reserve port 7080 for schematic-api

--- apps/schematic/api/schematic_api/__main__.py
+++ apps/schematic/api/schematic_api/__main__.py
@@ -6,14 +6,14 @@ from schematic_api import encoder
 
 
 def main():
-    app = connexion.App(__name__, specification_dir='./openapi/')
+    app = connexion.App(__name__, specification_dir="./openapi/")
     app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('openapi.yaml',
-                arguments={'title': 'Schematic REST API'},
-                pythonic_params=True)
+    app.add_api(
+        "openapi.yaml", arguments={"title": "Schematic REST API"}, pythonic_params=True
+    )
 
-    app.run(port=8080)
+    app.run(port=7080)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
