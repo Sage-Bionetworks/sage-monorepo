32f36d932021b2aacd7da65a243829bcbb1b9730
Thomas Schaffter <thomas.schaffter@gmail.com>
2022-12-28 18:30:56 +0000

    Display datasets data in app

--- apps/schematic/app/src/app/app.module.ts
+++ apps/schematic/app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
 
 import {
   ApiModule as SchematicApiModule,
@@ -17,6 +18,7 @@ import { appRoutes } from './app.routes';
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
     HttpClientModule,
+    MatTableModule,
     SchematicApiModule,
   ],
   providers: [
