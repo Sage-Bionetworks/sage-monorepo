fe04738ece08f157108eb36cc8735d847920cd5d
Rongrong Chai <rchai@sagebase.org>
2023-01-04 15:09:48 +0000

    fix test

--- libs/challenge-registry/ui/src/lib/search-dropdown-filter/search-dropdown-filter.component.ts
+++ libs/challenge-registry/ui/src/lib/search-dropdown-filter/search-dropdown-filter.component.ts
@@ -10,7 +10,7 @@ import { Avatar } from '../avatar/avatar';
 export class SearchDropdownFilterComponent {
   @Input() values!: FilterValue[];
   @Input() selectedValues!: string[];
-  @Input() name!: string;
+  @Input() name = 'items';
   @Input() showAvatar = true;
   @Output() dropdownChange = new EventEmitter<string[]>();
 
