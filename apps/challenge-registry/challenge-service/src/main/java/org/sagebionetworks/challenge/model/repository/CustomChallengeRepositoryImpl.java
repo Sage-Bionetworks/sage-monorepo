80ebbbcc1d1d5062c9d9ecafaef5c0c1d3fe984f
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-01-07 04:05:42 +0000

    Remove references to QueryDSL from Java files

--- apps/challenge-registry/challenge-service/src/main/java/org/sagebionetworks/challenge/model/repository/CustomChallengeRepositoryImpl.java
+++ apps/challenge-registry/challenge-service/src/main/java/org/sagebionetworks/challenge/model/repository/CustomChallengeRepositoryImpl.java
@@ -1,7 +1,5 @@
 package org.sagebionetworks.challenge.model.repository;
 
-import com.querydsl.core.QueryResults;
-import com.querydsl.jpa.JPQLQuery;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
@@ -13,7 +11,6 @@ import org.hibernate.search.engine.search.query.SearchResult;
 import org.hibernate.search.mapper.orm.Search;
 import org.hibernate.search.mapper.orm.session.SearchSession;
 import org.sagebionetworks.challenge.model.entity.ChallengeEntity;
-import org.sagebionetworks.challenge.model.entity.QChallengeEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -31,34 +28,7 @@ public class CustomChallengeRepositoryImpl extends QuerydslRepositorySupport
   }
 
   @Override
-  public Page<ChallengeEntity> findAll(Pageable pageable, ChallengeFilter filter) {
-    QChallengeEntity challenge = QChallengeEntity.challengeEntity;
-
-    JPQLQuery<ChallengeEntity> query = from(challenge);
-
-    if (filter.getStatus() != null && filter.getStatus().size() > 0) {
-      query = query.where(challenge.status.in(filter.getStatus()));
-    }
-    if (filter.getPlatforms() != null && filter.getPlatforms().size() > 0) {
-      query = query.where(challenge.platform.name.in(filter.getPlatforms()));
-    }
-    if (filter.getDifficulties() != null && filter.getDifficulties().size() > 0) {
-      query = query.where(challenge.difficulty.in(filter.getDifficulties()));
-    }
-    if (filter.getSubmissionTypes() != null && filter.getSubmissionTypes().size() > 0) {
-      query = query.where(challenge.submissionTypes.any().name.in(filter.getSubmissionTypes()));
-    }
-    if (filter.getIncentives() != null && filter.getIncentives().size() > 0) {
-      query = query.where(challenge.incentives.any().name.in(filter.getIncentives()));
-    }
-    query = super.getQuerydsl().applyPagination(pageable, query);
-
-    QueryResults<ChallengeEntity> results = query.fetchResults();
-    return new PageImpl<>(results.getResults(), pageable, results.getTotal());
-  }
-
-  @Override
-  public Page<ChallengeEntity> searchBy(
+  public Page<ChallengeEntity> findAll(
       Pageable pageable, ChallengeFilter filter, String... fields) {
     SearchResult<ChallengeEntity> result = getSearchResult(pageable, filter, fields);
     return new PageImpl<>(result.hits(), pageable, result.total().hitCount());
