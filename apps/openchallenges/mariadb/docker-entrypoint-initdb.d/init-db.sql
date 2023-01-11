a9e8eccc8240becc5929b085fc4eb039278ebd6d
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-01-11 01:44:46 +0000

    Update organization service

--- apps/openchallenges/mariadb/docker-entrypoint-initdb.d/init-db.sql
+++ apps/openchallenges/mariadb/docker-entrypoint-initdb.d/init-db.sql
@@ -1,32 +1,32 @@
-create database openchallenges_challenge_service;
-create database openchallenges_organization_service;
-create database openchallenges_user_service;
+create database challenge_service;
+create database organization_service;
+create database user_service;
 
-create role openchallenges_role_admin;
-grant all on openchallenges_challenge_service.* to openchallenges_role_admin;
-grant all on openchallenges_organization_service.* to openchallenges_role_admin;
-grant all on openchallenges_user_service.* to openchallenges_role_admin;
+create role role_admin;
+grant all on challenge_service.* to role_admin;
+grant all on organization_service.* to role_admin;
+grant all on user_service.* to role_admin;
 
 -- Create the user maria
-grant openchallenges_role_admin to maria;
-set default role openchallenges_role_admin for maria;
+grant role_admin to maria;
+set default role role_admin for maria;
 
 -- Create the user for challenge-core-service
 -- create user challenge_core_service identified by 'changeme';
--- grant openchallenges_role_admin to challenge_core_service;
--- set default role openchallenges_role_admin for challenge_core_service;
+-- grant role_admin to challenge_core_service;
+-- set default role role_admin for challenge_core_service;
 
 -- Create the user for openchallenges-challenge-service
-create user openchallenges_challenge_service identified by 'changeme';
-grant openchallenges_role_admin to openchallenges_challenge_service;
-set default role openchallenges_role_admin for openchallenges_challenge_service;
+create user challenge_service identified by 'changeme';
+grant role_admin to challenge_service;
+set default role role_admin for challenge_service;
 
 -- Create the user for openchallenges-organization-service
-create user openchallenges_organization_service identified by 'changeme';
-grant openchallenges_role_admin to openchallenges_organization_service;
-set default role openchallenges_role_admin for openchallenges_organization_service;
+create user organization_service identified by 'changeme';
+grant role_admin to organization_service;
+set default role role_admin for organization_service;
 
 -- Create the user for openchallenges-user-service
-create user openchallenges_user_service identified by 'changeme';
-grant openchallenges_role_admin to openchallenges_user_service;
-set default role openchallenges_role_admin for openchallenges_user_service;
+create user user_service identified by 'changeme';
+grant role_admin to user_service;
+set default role role_admin for user_service;
