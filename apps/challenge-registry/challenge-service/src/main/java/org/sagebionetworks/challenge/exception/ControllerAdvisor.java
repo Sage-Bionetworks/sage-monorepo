8b9060ed75fd29fcacae07fd1ef50ad99e855bb1
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-01-12 02:28:03 +0000

    Create custom handler prototype

--- apps/challenge-registry/challenge-service/src/main/java/org/sagebionetworks/challenge/exception/ControllerAdvisor.java
+++ apps/challenge-registry/challenge-service/src/main/java/org/sagebionetworks/challenge/exception/ControllerAdvisor.java
@@ -2,13 +2,11 @@ package org.sagebionetworks.challenge.exception;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -19,10 +17,10 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
   protected ResponseEntity<Object> handleBindException(
       BindException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
 
-      Map<String, Object> body = new LinkedHashMap<>();
-      body.put("message", "City not found");
+    Map<String, Object> body = new LinkedHashMap<>();
+    body.put("message", "City not found");
 
-      return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
 
     // return handleExceptionInternal(ex, null, headers, status, request);
   }
