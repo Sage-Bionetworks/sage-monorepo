745c34b6a4d3a6cc8458b0e5926d83ca6831b6cf
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-01-13 03:45:58 +0000

    Create table and example data

--- apps/challenge-registry/challenge-service/src/main/resources/db/migration/V1.1.0__insert_data.sql
+++ apps/challenge-registry/challenge-service/src/main/resources/db/migration/V1.1.0__insert_data.sql
@@ -1,9 +1,32 @@
--- challenge.challenge_platform data
+-- challenge_platform data
 INSERT INTO challenge_platform (id, slug, name, avatar_url, website_url)
-VALUES ('1', 'synapse', 'Synapse', 'https://via.placeholder.com/300.png', 'https://synapse.org'),
-  ('2', 'codalab', 'CodaLab', 'https://via.placeholder.com/300.png', 'https://codalab.org'),
-  ('3', 'kaggle', 'Kaggle', 'https://via.placeholder.com/300.png', 'https://www.kaggle.com');
--- challenge.challenge data
+VALUES (
+    '1',
+    'synapse',
+    'Synapse',
+    'https://via.placeholder.com/300.png',
+    'https://synapse.org'
+  ),
+  (
+    '2',
+    'codalab',
+    'CodaLab',
+    'https://via.placeholder.com/300.png',
+    'https://codalab.org'
+  ),
+  (
+    '3',
+    'kaggle',
+    'Kaggle',
+    'https://via.placeholder.com/300.png',
+    'https://www.kaggle.com'
+  );
+-- challenge_input_data_type
+INSERT INTO challenge_input_data_type (id, slug, name)
+VALUES ('1', 'genomic', 'genomic'),
+  ('2', 'proteomic', 'proteomic'),
+  ('3', 'gene-expression', 'gene expression');
+-- challenge data
 INSERT INTO challenge (
     id,
     name,
@@ -48,17 +71,17 @@ VALUES (
     '2019-01-01',
     '2019-02-01'
   );
--- challenge.challenge_incentive data
+-- challenge_incentive data
 INSERT INTO challenge_incentive (id, name, challenge_id)
 VALUES ('1', 'monetary', 1),
   ('2', 'publication', 1),
   ('3', 'monetary', 2);
--- challenge.challenge_submission_type data
+-- challenge_submission_type data
 INSERT INTO challenge_submission_type (id, name, challenge_id)
 VALUES ('1', 'container_image', 1),
   ('2', 'prediction_file', 2),
   ('3', 'container_image', 2);
--- challenge.challenge_star data
+-- challenge_star data
 INSERT INTO challenge_star (id, challenge_id, user_id)
 VALUES ('1', 1, 1),
   ('2', 2, 1),
