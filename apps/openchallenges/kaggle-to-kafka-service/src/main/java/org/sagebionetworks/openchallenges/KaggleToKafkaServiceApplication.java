7a0ecd3051acec20afbcbdb108b3e94b0c66c356
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-01-21 05:00:23 +0000

    Add logger config file and print Kaggle search terms

--- apps/openchallenges/kaggle-to-kafka-service/src/main/java/org/sagebionetworks/openchallenges/KaggleToKafkaServiceApplication.java
+++ apps/openchallenges/kaggle-to-kafka-service/src/main/java/org/sagebionetworks/openchallenges/KaggleToKafkaServiceApplication.java
@@ -1,14 +1,21 @@
 package org.sagebionetworks.openchallenges;
 
+import lombok.extern.slf4j.Slf4j;
+import org.sagebionetworks.openchallenges.configuration.KaggleToKafkaServiceConfiguration;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Slf4j
 @SpringBootApplication
 public class KaggleToKafkaServiceApplication implements CommandLineRunner {
+
+  private final KaggleToKafkaServiceConfiguration config;
+
+  public KaggleToKafkaServiceApplication(
+      KaggleToKafkaServiceConfiguration kaggleToKafkaServiceConfiguration) {
+    this.config = kaggleToKafkaServiceConfiguration;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(KaggleToKafkaServiceApplication.class, args);
@@ -16,6 +23,7 @@ public class KaggleToKafkaServiceApplication implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
-    log.info("Hello");
+    log.info(this.config.getWelcomeMessage());
+    log.info("{}", this.config.getKaggleSearchTerms());
   }
 }
--- apps/openchallenges/kaggle-to-kafka-service/src/main/java/org/sagebionetworks/openchallenges/configuration/KaggleToKafkaServiceConfiguration.java
+++ apps/openchallenges/kaggle-to-kafka-service/src/main/java/org/sagebionetworks/openchallenges/configuration/KaggleToKafkaServiceConfiguration.java
@@ -1,15 +1,14 @@
 package org.sagebionetworks.openchallenges.configuration;
 
 import java.util.List;
-
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-
-import lombok.Data;
 
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "kaggle-to-kafka-service")
 public class KaggleToKafkaServiceConfiguration {
   private List<String> kaggleSearchTerms;
+  private String welcomeMessage;
 }
