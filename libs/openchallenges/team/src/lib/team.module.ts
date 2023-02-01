6a98bb40cbe18a3eb0d365e99df33468a6161eb5
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-02-01 03:17:12 +0000

    Fix three components

--- libs/openchallenges/team/src/lib/team.module.ts
+++ libs/openchallenges/team/src/lib/team.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UiModule } from '@sagebionetworks/openchallenges/ui';
+import { FooterComponent, UiModule } from '@sagebionetworks/openchallenges/ui';
 import { TeamComponent } from './team.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [{ path: '', component: TeamComponent }];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes), UiModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    UiModule,
+    FooterComponent,
+  ],
   declarations: [TeamComponent],
   exports: [TeamComponent],
 })
