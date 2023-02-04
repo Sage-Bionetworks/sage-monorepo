ab0d95d2e12055b0035e81c9a093e82741d42ea4
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-02-04 19:32:28 +0000

    Org service can get remote config

--- libs/openchallenges/app-config-data/src/main/java/org/sagebionetworks/openchallenges/app/config/data/OrganizationServiceConfigData.java
+++ libs/openchallenges/app-config-data/src/main/java/org/sagebionetworks/openchallenges/app/config/data/OrganizationServiceConfigData.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Data
 @Configuration
-@ConfigurationProperties(prefix = "organization-service")
+@ConfigurationProperties(prefix = "openchallenges-organization-service")
 public class OrganizationServiceConfigData {
   private String welcomeMessage;
 }
