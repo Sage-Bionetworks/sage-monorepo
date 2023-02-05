aaadd65ea382d359c38cc4ce73359c317ea712df
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-02-04 21:00:03 -0800

    feat: Add Vault project to OC (#1282)
    * Add Vault project for OC
    
    * Set default token
    
    * Add vault as the first backend of the config server
    
    * Troubleshooting config server registration
    
    * Register with eureka
    
    * Register with eureka

--- apps/openchallenges/config-server/src/main/java/org/sagebionetworks/openchallenges/config/server/ConfigServerApplication.java
+++ apps/openchallenges/config-server/src/main/java/org/sagebionetworks/openchallenges/config/server/ConfigServerApplication.java
@@ -3,13 +3,14 @@ package org.sagebionetworks.openchallenges.config.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigServerApplication.class, args);
+  }
 }
