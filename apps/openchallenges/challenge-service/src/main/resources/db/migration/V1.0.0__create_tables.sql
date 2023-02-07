3c6404e1cb4ba1e86700c05658617723e769390f
verena <9377970+vpchung@users.noreply.github.com>
2023-02-07 10:00:03 +0000

    revert changes to `challenge` table for now

--- apps/openchallenges/challenge-service/src/main/resources/db/migration/V1.0.0__create_tables.sql
+++ apps/openchallenges/challenge-service/src/main/resources/db/migration/V1.0.0__create_tables.sql
@@ -29,12 +29,12 @@ CREATE TABLE `challenge_input_data_type`
 CREATE TABLE `challenge`
 (
     `id`                    bigint(20) NOT NULL AUTO_INCREMENT,
-    `slug`                  varchar(255) NOT NULL UNIQUE,
+    -- `slug`                  varchar(255) NOT NULL UNIQUE,
     `name`                  varchar(255) DEFAULT NULL,
     `headline`              varchar(80),
     `description`           varchar(280) NOT NULL,
     -- `avatar_url`            varchar(255),
-    `website_url`           varchar(255) NOT NULL,
+    -- `website_url`           varchar(255) NOT NULL,
     `status`                ENUM('upcoming', 'active', 'completed'),
     `difficulty`            ENUM('good_for_beginners', 'intermediate', 'advanced'),
     `platform_id`           int,
