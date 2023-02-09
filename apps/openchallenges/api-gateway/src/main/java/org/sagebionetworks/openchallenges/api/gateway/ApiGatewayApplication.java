2c57024a4ce89d68c52481ae94be5bb6af8c26be
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-02-09 04:24:10 +0000

    Display welcome message in API gateway log

--- apps/openchallenges/api-gateway/src/main/java/org/sagebionetworks/openchallenges/api/gateway/ApiGatewayApplication.java
+++ apps/openchallenges/api-gateway/src/main/java/org/sagebionetworks/openchallenges/api/gateway/ApiGatewayApplication.java
@@ -1,15 +1,33 @@
 package org.sagebionetworks.openchallenges.api.gateway;
 
+import lombok.extern.slf4j.Slf4j;
+
+import org.sagebionetworks.openchallenges.app.config.data.ApiGatewayConfigData;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
+@Slf4j
 @EnableEurekaClient
 @SpringBootApplication
-public class ApiGatewayApplication {
+@ComponentScan(basePackages = "org.sagebionetworks.openchallenges")
+public class ApiGatewayApplication implements CommandLineRunner {
+
+  private final ApiGatewayConfigData apiGatewayConfigData;
+
+  public ApiGatewayApplication(ApiGatewayConfigData apiGatewayConfigData) {
+    this.apiGatewayConfigData = apiGatewayConfigData;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(ApiGatewayApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    log.info(apiGatewayConfigData.getWelcomeMessage());
   }
 
 }
