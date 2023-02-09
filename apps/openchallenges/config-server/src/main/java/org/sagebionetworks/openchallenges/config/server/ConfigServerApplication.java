f443971256a8c4d8805547785a1c03e1800f945e
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-02-09 16:23:45 +0000

    Display welcome message in config server

--- apps/openchallenges/config-server/src/main/java/org/sagebionetworks/openchallenges/config/server/ConfigServerApplication.java
+++ apps/openchallenges/config-server/src/main/java/org/sagebionetworks/openchallenges/config/server/ConfigServerApplication.java
@@ -1,14 +1,33 @@
 package org.sagebionetworks.openchallenges.config.server;
 
+import org.sagebionetworks.openchallenges.app.config.data.ConfigServerConfigData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigServerApplication {
+@ComponentScan(basePackages = "org.sagebionetworks.openchallenges")
+public class ConfigServerApplication implements CommandLineRunner {
+
+  private static final Logger LOG = LoggerFactory.getLogger(ConfigServerApplication.class);
+
+  private final ConfigServerConfigData configServerConfigData;
+
+  public ConfigServerApplication(ConfigServerConfigData configServerConfigData) {
+    this.configServerConfigData = configServerConfigData;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(ConfigServerApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    LOG.info(configServerConfigData.getWelcomeMessage());
   }
 }
