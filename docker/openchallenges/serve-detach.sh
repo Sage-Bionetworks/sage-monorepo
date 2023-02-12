983ec2ba73122804db8d4d6f5513b0c94ba94c35
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-02-12 19:18:26 +0000

    Add elasticsearch to docker

--- docker/openchallenges/serve-detach.sh
+++ docker/openchallenges/serve-detach.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-docker compose \
-  --file docker/openchallenges/services/api-gateway.yml \
-  --file docker/openchallenges/services/config-server.yml \
-  --file docker/openchallenges/services/mariadb.yml \
-  --file docker/openchallenges/services/service-registry.yml \
-  --file docker/openchallenges/services/vault.yml \
-  --file docker/openchallenges/services/zipkin.yml \
-  --file docker/openchallenges/networks.yml \
-  up "$1" --detach
+args=(
+  # List of services listed in alphanumeric order
+  --file docker/openchallenges/services/api-gateway.yml
+  --file docker/openchallenges/services/config-server.yml
+  --file docker/openchallenges/services/elasticsearch.yml
+  --file docker/openchallenges/services/mariadb.yml
+  --file docker/openchallenges/services/service-registry.yml
+  --file docker/openchallenges/services/vault.yml
+  --file docker/openchallenges/services/zipkin.yml
+
+  --file docker/openchallenges/networks.yml
+
+  up $1 --detach
+)
+
+docker compose "${args[@]}"
