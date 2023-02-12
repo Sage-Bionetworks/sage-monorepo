9de51ea749748a681fa600e3567a1b3284540701
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-02-12 18:10:28 +0000

    Add zipkin to docker/

--- docker/openchallenges/serve-detach.sh
+++ docker/openchallenges/serve-detach.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 docker compose \
-  --file docker/openchallenges/services/vault.yml \
   --file docker/openchallenges/services/config-server.yml \
+  --file docker/openchallenges/services/vault.yml \
+  --file docker/openchallenges/services/zipkin.yml \
   --file docker/openchallenges/networks.yml \
   up "$1" --detach
