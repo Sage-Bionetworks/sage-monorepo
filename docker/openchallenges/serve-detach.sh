a2b113e89304124b3bb6b481431ad448f96014c1
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-02-12 18:36:38 +0000

    Move mariadb docker files

--- docker/openchallenges/serve-detach.sh
+++ docker/openchallenges/serve-detach.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 docker compose \
+  --file docker/openchallenges/services/api-gateway.yml \
   --file docker/openchallenges/services/config-server.yml \
+  --file docker/openchallenges/services/mariadb.yml \
+  --file docker/openchallenges/services/service-registry.yml \
   --file docker/openchallenges/services/vault.yml \
   --file docker/openchallenges/services/zipkin.yml \
   --file docker/openchallenges/networks.yml \
