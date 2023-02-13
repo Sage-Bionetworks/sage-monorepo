869abca18aff11af9fa5d682b4ea1a9b494dfcf6
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-02-12 18:08:30 -0800

    feat: Refactor deployment with Docker and add healthchecks (#1302)
    * API gateway now get its config form the config server
    
    * Display welcome message in API gateway log
    
    * Add zipkin to API gateway
    
    * Decouple API gateway and service registry
    
    * Consolidate service registry
    
    * Config server no longer register with eureka
    
    * Display welcome message in config server
    
    * Many changes
    
    * Remove spring-security from service registry
    
    * Add config-server back as dependency of service registry
    
    * Enable health probes on security server
    
    * Add curl to config server image
    
    * Build image with helthcheck
    
    * Set condition n docker compose
    
    * Commit before moving to nx-container
    
    * vault image now has healthcheck
    
    * Remove @nx-tools/nx-docker
    
    * Finalize vault and config server
    
    * Trying to get healthcheck to work with service registry
    
    * Fix healthcheck of the service registry
    
    * Add health check to api gateway
    
    * Set SERVER_PORT in .env
    
    * Update config server to new serve tasks
    
    * Move docker file
    
    * Remove unused files
    
    * Add zipkin to docker/
    
    * Move mariadb docker files
    
    * Add simple healthcheck to mariadb
    
    * Add elasticsearch to docker
    
    * Add challenge-service to docker
    
    * Add healthcheck to challenge service
    
    * Remove rabbitmq
    
    * Add org service to docker setup
    
    * Update app project.json to use new dependency deployment
    
    * Remove ARG from vault dockerfile
    
    * Pin version of nx-container
    
    * Set max memory of more docker containers

---
+++ apps/openchallenges/api-gateway/src/main/java/org/sagebionetworks/openchallenges/api/gateway/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package org.sagebionetworks.openchallenges.api.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+public class SecurityConfig {
+  @Bean
+  SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+    http.authorizeExchange()
+        // ALLOWING REGISTER API FOR DIRECT ACCESS
+        .pathMatchers("/api/v1/users/register").permitAll().pathMatchers("/api/v1/auth/login")
+        .permitAll().pathMatchers("/api/v1/organizations/**")
+        .permitAll().pathMatchers("/api/v1/challenges/**")
+        .permitAll().pathMatchers("/api/v1/challengePlatforms/**")
+        .permitAll().pathMatchers("/api/v1/challengeInputDataTypes/**")
+        .permitAll().pathMatchers("/actuator/health/readiness")
+        .permitAll()
+        // ALL OTHER APIS ARE AUTHENTICATED
+        .anyExchange().authenticated().and().csrf().disable().oauth2Login().and()
+        .oauth2ResourceServer().jwt();
+    return http.build();
+  }
+}
---
+++ docker/openchallenges/serve-detach.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+args=(
+  # List of services in alphanumeric order
+  --file docker/openchallenges/services/api-gateway.yml
+  --file docker/openchallenges/services/challenge-service.yml
+  --file docker/openchallenges/services/config-server.yml
+  --file docker/openchallenges/services/elasticsearch.yml
+  --file docker/openchallenges/services/mariadb.yml
+  --file docker/openchallenges/services/organization-service.yml
+  --file docker/openchallenges/services/service-registry.yml
+  --file docker/openchallenges/services/vault.yml
+  --file docker/openchallenges/services/zipkin.yml
+
+  --file docker/openchallenges/networks.yml
+
+  up $1 --detach
+)
+
+docker compose "${args[@]}"
