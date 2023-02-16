61440ac6ba82083a13b8cb603059b55bc71bed86
verena <9377970+vpchung@users.noreply.github.com>
2023-02-16 02:30:08 +0000

    finalize command for seeding

--- apps/openchallenges/db-cli/src/database.ts
+++ apps/openchallenges/db-cli/src/database.ts
@@ -79,8 +79,8 @@ export const removeTables = async (conn: any): Promise<boolean> => {
       for (const table of tables) {
         conn
           .query(`DROP TABLE IF EXISTS ${table.name}`)
-          .then(() => logger.info(`Table dropped: ${table.name}`))
-          .catch((err: any) => logger.error('Unable to drop table:', err));
+          .then(() => logger.info(`table dropped: ${table.name}`))
+          .catch((err: any) => logger.error('unable to drop table:', err));
       }
     })
     .then(() => conn.query('SET FOREIGN_KEY_CHECKS = 1;'));
@@ -89,52 +89,30 @@ export const removeTables = async (conn: any): Promise<boolean> => {
 
 export const seedTables = async (conn: any, dir: string): Promise<boolean> => {
   await removeTables(conn);
-  await createTables(conn, dir);
+  await createTables(conn, dir).then(() => logger.info(`🎉 tables created`));
   const seedFiles = await listSeedFiles(dir);
   for (const seed of seedFiles) {
-    const res = (await readInputFile(seed.file)).split(/\r\n(?=\d+?,)/);
-    console.log(res);
+    const records: Array<any> = [];
+    const res = await readInputFile(seed.file, 'json');
+    for (const obj of res) {
+      records.push(Object.values(obj));
+    }
+    await seedTable(conn, seed.table_name, records);
   }
   return true;
 };
 
-const createTables = async (conn: any, dir: string): Promise<boolean> => {
-  const schema = await readInputFile(dir + '/_create.txt');
-  await conn
-    .query(schema)
-    .then(() => logger.info(`🎉 tables created`))
-    .catch(() => logger.error('tables not created'));
-  return true;
-};
-
-const readInputFile = async (seedFile: string): Promise<any> => {
-  return promises
-    .readFile(seedFile, 'utf8')
-    .then((data) => data.toString())
-    .catch((err: any) =>
-      logger.error('something wrong with the input file:', err.message)
-    );
-};
-
-// const seedCollection = async <any>(
-//   seedFile: string,
-//   name: string
-// ): Promise<any> => {
-//   return readInputFile(seedFile)
-//     .then(() => logger.info(`🌱 Seeding ${name} completed`))
-//     .catch((err: any) => logger.error(`Unable to seed ${name}`, err));
-// };
-
+// Helper: list files in directory
 const listSeedFiles = async (dir: string): Promise<Array<any>> => {
   return new Promise((resolve, reject) => {
-    glob(dir + '/*.csv', { ignore: 'nodir' }, function (err, files) {
+    glob(dir + '/*.json', { ignore: 'nodir' }, function (err, files) {
       if (err) {
         reject(err);
       } else {
         const seedFiles: Array<any> = [];
         for (const file of files) {
           seedFiles.push({
-            table_name: path.basename(file, '.csv'),
+            table_name: path.basename(file, '.json').replace(/^_/, ''),
             file,
           });
         }
@@ -142,4 +120,61 @@ const listSeedFiles = async (dir: string): Promise<Array<any>> => {
       }
     });
   });
+};
+
+// Helper: read in file
+const readInputFile = async (seedFile: string, type = 'text'): Promise<any> => {
+  return promises
+    .readFile(seedFile, 'utf8')
+    .then((data) => (type === 'json' ? JSON.parse(data) : data.toString()))
+    .catch((err: any) =>
+      logger.error('something wrong with the input file:', err.message)
+    );
+};
+
+// Helper: create tables from input file, using '--' as delimiter
+const createTables = async (conn: any, dir: string): Promise<boolean> => {
+  const schemas = (await readInputFile(dir + '/_create.txt')).split('--');
+  for (const schema of schemas) {
+    await conn.query(schema).catch(() => logger.error('table not created'));
+  }
+  return true;
+};
+
+// Helper: insert values into one table
+const seedTable = async (
+  conn: any,
+  table: string,
+  records: Array<any>
+): Promise<boolean> => {
+  const subQuery = await createInsertSql(conn, table);
+  await conn
+    .batch(
+      `INSERT INTO \`${table}\` (${subQuery.columns}) VALUES (${subQuery.values})`,
+      records
+    )
+    .then((res: any) =>
+      logger.info(`🌱 table seeded: ${table} (${res.affectedRows} records)`)
+    )
+    .catch((err: any) => logger.error(`unable to seed ${table}`, err));
+  return true;
+};
+
+// Helper: create colnames and placeholders for insert sql
+const createInsertSql = async (conn: any, table: string) => {
+  let columns = '';
+  let values = '';
+  await conn
+    .query(
+      `SELECT column_name FROM information_schema.columns \
+      WHERE table_name = '${table}' AND column_name NOT IN ('created_at', 'updated_at');`
+    )
+    .then((table: any) => {
+      columns = `${table[0].column_name}`;
+      for (let i = 1; i < table.length; i++) {
+        columns = columns.concat(`,${table[i].column_name}`);
+      }
+      values = `${'?,'.repeat(table.length - 1)}?`;
+    });
+  return { columns, values };
 };
