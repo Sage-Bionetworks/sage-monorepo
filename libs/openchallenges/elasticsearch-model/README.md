85c6939e3e4ed155c3ba7e6a6fc2e1072b8ee908
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-02-19 23:45:41 +0000

    Add initial lib elasticsearch-model

---
+++ libs/openchallenges/elasticsearch-model/README.md
@@ -0,0 +1,3 @@
+# OpenChallenges Elasticsearch Model
+
+TBD
---
+++ libs/openchallenges/elasticsearch-model/src/main/java/org/sagebionetworks/openchallenges/elasticsearch/model/index/IndexModel.java
@@ -0,0 +1,6 @@
+package org.sagebionetworks.openchallenges.elasticsearch.model.index;
+
+public interface IndexModel {
+
+  String getId();
+}
