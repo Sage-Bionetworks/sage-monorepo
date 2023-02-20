2f3ed7f75a4e29dce8d72c27ff0127e929d2e9c1
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-02-20 00:28:02 +0000

    Return ES rest client

--- libs/openchallenges/elasticsearch-config/src/main/java/org/sagebionetworks/openchallenges/elasticsearch/config/ElasticsearchConfig.java
+++ libs/openchallenges/elasticsearch-config/src/main/java/org/sagebionetworks/openchallenges/elasticsearch/config/ElasticsearchConfig.java
@@ -1,6 +1,40 @@
 package org.sagebionetworks.openchallenges.elasticsearch.config;
 
+import java.util.Objects;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.sagebionetworks.openchallenges.app.config.data.ElasticsearchConfigData;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Configuration
-public class ElasticsearchConfig {}
+public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
+
+  private final ElasticsearchConfigData elasticsearchConfigData;
+
+  public ElasticsearchConfig(ElasticsearchConfigData elasticsearchConfigData) {
+    this.elasticsearchConfigData = elasticsearchConfigData;
+  }
+
+  @Override
+  @Bean
+  public RestHighLevelClient elasticsearchClient() {
+    UriComponents serverUri =
+        UriComponentsBuilder.fromHttpUrl(elasticsearchConfigData.getConnectionUrl()).build();
+    return new RestHighLevelClient(
+        RestClient.builder(
+                new HttpHost(
+                    Objects.requireNonNull(serverUri.getHost()),
+                    serverUri.getPort(),
+                    serverUri.getScheme()))
+            .setRequestConfigCallback(
+                requestConfigBuildser ->
+                    requestConfigBuildser
+                        .setConnectTimeout(elasticsearchConfigData.getConnectTimeoutMs())
+                        .setSocketTimeout(elasticsearchConfigData.getSocketTimeoutMs())));
+  }
+}
