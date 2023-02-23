6faa478d6ba0831f136a330247e42ce8f4feb667
Rongrong Chai <rchai@sagebase.org>
2023-02-23 00:56:47 +0000

    fix routes on user-profile

--- libs/openchallenges/user-profile/src/lib/user-profile-routing.module.ts
+++ libs/openchallenges/user-profile/src/lib/user-profile-routing.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserProfileComponent } from './user-profile.component';
-import { UserProfileResolver } from './user-profile.resolver';
+// import { UserProfileResolver } from './user-profile.resolver';
 
 export const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: UserProfileComponent,
+  //   resolve: { userProfile: UserProfileResolver },
+  // },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   {
-    path: '',
+    path: ':slug',
     component: UserProfileComponent,
-    resolve: { userProfile: UserProfileResolver },
+    children: [{ path: '**', component: UserProfileComponent }],
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  declarations: [],
-  providers: [],
   exports: [RouterModule],
 })
 export class UserProfileRoutingModule {}
