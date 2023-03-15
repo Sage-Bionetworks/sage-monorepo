3bdb57044c17dcbf222f90da845a436ff862d63c
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-03-15 14:47:40 -0700

    fix: remove duplicated SQL property Organization.challenge_count (#1380)

--- apps/openchallenges/organization-service/src/main/resources/db/migration/V1.0.0__create_tables.sql
+++ apps/openchallenges/organization-service/src/main/resources/db/migration/V1.0.0__create_tables.sql
@@ -5,7 +5,6 @@ CREATE TABLE `organization` (
   `login` varchar(255) NOT NULL UNIQUE,
   `avatar_url` varchar(255) DEFAULT NULL,
   `website_url` varchar(255) DEFAULT NULL,
-  `challenge_count` int,
   `description` varchar(280) DEFAULT NULL,
   `challenge_count` int,
   `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
