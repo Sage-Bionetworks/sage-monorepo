5e4f83795dc08f225d155de92c076c49836a3f34
Rongrong Chai <rchai@sagebase.org>
2023-03-17 14:09:06 +0000

    enable to filter contribution roles

--- libs/openchallenges/org-search/src/lib/org-search-filters-values.ts
+++ libs/openchallenges/org-search/src/lib/org-search-filters-values.ts
@@ -1,9 +1,13 @@
 import { FilterValue } from '@sagebionetworks/openchallenges/ui';
 
-export const contributionRolesFilterValues: FilterValue[] = [
+export const challengeContributionRolesFilterValues: FilterValue[] = [
   {
-    value: 'owner',
-    label: 'Owner',
+    value: 'challenge_organizer',
+    label: 'Challenge Organizer',
+  },
+  {
+    value: 'data_contributor',
+    label: 'Data Contributor',
   },
   {
     value: 'sponsor',
