f67506bc5b374b23f1cc5103e1956381035415d7
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-03-23 00:14:38 +0000

    Rename imaging service to image service

---
+++ apps/openchallenges/image-service/src/main/java/org/sagebionetworks/openchallenges/image/service/ImageServiceApplication.java
@@ -0,0 +1,33 @@
+package org.sagebionetworks.openchallenges.image.service;
+
+import org.sagebionetworks.openchallenges.app.config.data.ImageServiceConfigData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableEurekaClient
+@SpringBootApplication
+@ComponentScan(basePackages = {"org.sagebionetworks.openchallenges.image.service"})
+public class ImageServiceApplication implements CommandLineRunner {
+
+  private static final Logger LOG = LoggerFactory.getLogger(ImageServiceApplication.class);
+
+  private final ImageServiceConfigData imageServiceConfigData;
+
+  public ImageServiceApplication(ImageServiceConfigData imageServiceConfigData) {
+    this.imageServiceConfigData = imageServiceConfigData;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(ImageServiceApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    LOG.info(imageServiceConfigData.getWelcomeMessage());
+  }
+}
---
+++ apps/openchallenges/image-service/src/test/java/org/sagebionetworks/openchallenges/image/service/ImageServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package org.sagebionetworks.openchallenges.image.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
