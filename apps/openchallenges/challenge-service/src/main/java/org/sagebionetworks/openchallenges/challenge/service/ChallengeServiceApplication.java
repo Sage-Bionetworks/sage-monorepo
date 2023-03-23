67e92ad63dd6a562485e31ac245acde5d1ffdde0
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-03-23 23:33:27 +0000

    Load challenge config data

--- apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/ChallengeServiceApplication.java
+++ apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/ChallengeServiceApplication.java
@@ -1,5 +1,9 @@
 package org.sagebionetworks.openchallenges.challenge.service;
 
+import org.sagebionetworks.openchallenges.app.config.data.ChallengeServiceConfigData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -7,15 +11,23 @@ import org.springframework.context.annotation.ComponentScan;
 
 @EnableEurekaClient
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.sagebionetworks.openchallenges.challenge.service"})
-public class ChallengeServiceApplication {
+@ComponentScan(basePackages = {"org.sagebionetworks.openchallenges"})
+public class ChallengeServiceApplication implements CommandLineRunner {
+
+  private static final Logger LOG = LoggerFactory.getLogger(ChallengeServiceApplication.class);
+
+  private final ChallengeServiceConfigData challengeServiceConfigData;
+
+  public ChallengeServiceApplication(ChallengeServiceConfigData challengeServiceConfigData) {
+    this.challengeServiceConfigData = challengeServiceConfigData;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(ChallengeServiceApplication.class, args);
   }
 
-  // @Bean
-  // public Module jsonNullableModule() {
-  //   return new JsonNullableModule();
-  // }
+  @Override
+  public void run(String... args) throws Exception {
+    LOG.info(challengeServiceConfigData.getWelcomeMessage());
+  }
 }
