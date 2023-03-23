3cb9efb3fb6d08aadcbc71c8c018211efb398fac
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-03-22 18:51:29 -0700

    feat: Create image service (#1413)
    * Add empty imaging service
    
    * Image service builds
    
    * Rename imaging service to image service in API description
    
    * Rename imaging service to image service
    
    * Rename imaging service to image service
    
    * The image service now starts

--- docker/openchallenges/serve-detach.sh
+++ docker/openchallenges/serve-detach.sh
@@ -8,15 +8,16 @@ args=(
   --file docker/openchallenges/services/config-server.yml
   --file docker/openchallenges/services/elasticsearch.yml
   --file docker/openchallenges/services/grafana.yml
+  --file docker/openchallenges/services/image-service.yml
   --file docker/openchallenges/services/kafka.yml
   --file docker/openchallenges/services/mariadb.yml
-  # --file docker/openchallenges/services/minio.yml
   --file docker/openchallenges/services/organization-service.yml
   --file docker/openchallenges/services/prometheus.yml
   --file docker/openchallenges/services/service-registry.yml
   --file docker/openchallenges/services/thumbor.yml
   --file docker/openchallenges/services/vault.yml
   --file docker/openchallenges/services/zipkin.yml
+  # --file docker/openchallenges/services/minio.yml
 
   --file docker/openchallenges/networks.yml
   --file docker/openchallenges/volumes.yml
