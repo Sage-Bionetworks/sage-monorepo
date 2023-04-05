98b462b7ccf553096e6de68261a958ec9efaaf8d
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-04-05 07:21:39 -0700

    feat: Add image from the image service to the About page (#1452)
    * Add image from image service to the About page
    
    * Document how to configure and use the image service
    
    * Add instructions to build Dockr images
    
    * Minor update

--- libs/openchallenges/about/src/lib/about.component.ts
+++ libs/openchallenges/about/src/lib/about.component.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  Image,
+  ImageService,
+} from '@sagebionetworks/openchallenges/api-client-angular';
 import { ConfigService } from '@sagebionetworks/openchallenges/config';
 import { FooterComponent } from '@sagebionetworks/openchallenges/ui';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'openchallenges-about',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, CommonModule],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
   public appVersion: string;
+  public triforceImg$: Observable<Image> | undefined;
 
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    private imageService: ImageService
+  ) {
     this.appVersion = this.configService.config.appVersion;
+  }
+
+  ngOnInit() {
+    this.triforceImg$ = this.imageService.getImage('triforce.png');
   }
 }
