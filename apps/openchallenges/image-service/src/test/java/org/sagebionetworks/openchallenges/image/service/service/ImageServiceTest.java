21cf22a986679b8fdd7154de74d92ed1d7cee426
mdage1 <maria.diaz@sagebase.org>
2023-04-26 19:06:01 +0000

    updat unit test

--- apps/openchallenges/image-service/src/test/java/org/sagebionetworks/openchallenges/image/service/service/ImageServiceTest.java
+++ apps/openchallenges/image-service/src/test/java/org/sagebionetworks/openchallenges/image/service/service/ImageServiceTest.java
@@ -13,7 +13,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.sagebionetworks.openchallenges.app.config.data.ImageServiceConfigData;
 import org.sagebionetworks.openchallenges.image.service.model.dto.ImageDto;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,9 +20,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class ImageServiceTest {
-  
+
   @Autowired private ImageService imageService;
-  
+
   @Mock private Logger LOG;
 
   @Mock private Thumbor thumbor;
@@ -31,21 +30,23 @@ public class ImageServiceTest {
   @Mock private ImageServiceConfigData imageServiceConfigData;
 
   @Value("${example.expectedURL}")
-  private String expectedURL = "http://localhost:8889/ucBCtKeFXF3rPUsmUGmxcTRnoj0=/300x300/triforce.png";
+  private String expectedURL =
+      "http://localhost:8889/ucBCtKeFXF3rPUsmUGmxcTRnoj0=/300x300/triforce.png";
 
   @Test
   void givenImage_ImageDto_MatchURL() {
-    
-    //given an image
+
+    // given an image
     String givenimg = "triforce.png";
 
-    //return the epected URL using Mocked Thumbor instance
-    String expectedImageUrl = "http://localhost:8889/ucBCtKeFXF3rPUsmUGmxcTRnoj0=/300x300/triforce.png";
+    // return the epected URL using Mocked Thumbor instance
+    String expectedImageUrl =
+        "http://localhost:8889/ucBCtKeFXF3rPUsmUGmxcTRnoj0=/300x300/triforce.png";
     when(thumbor.buildImage(anyString())).thenReturn(thumbor);
     when(thumbor.resize(isA(int.class), isA(int.class))).thenReturn(thumbor);
     when(thumbor.toUrl()).thenReturn(expectedImageUrl);
 
-    //assert that URL is as expected when ImageDto is run --PASS
+    // assert that URL is as expected when ImageDto is run --PASS
     ImageDto result = imageService.getImage(givenimg);
     assertThat(result.getUrl()).isEqualTo(expectedURL);
   }
