57dd55fcad54ad4b8c5fc6c47bc90f6d1f0325dd
mdage1 <maria.diaz@sagebase.org>
2023-04-28 17:01:06 +0000

    fix: remove excess checks from unit test

--- apps/openchallenges/image-service/src/test/java/org/sagebionetworks/openchallenges/image/service/service/ImageServiceTest.java
+++ apps/openchallenges/image-service/src/test/java/org/sagebionetworks/openchallenges/image/service/service/ImageServiceTest.java
@@ -1,9 +1,6 @@
 package org.sagebionetworks.openchallenges.image.service.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.isA;
-import static org.mockito.Mockito.when;
 
 import com.squareup.pollexor.Thumbor;
 import org.junit.jupiter.api.Test;
@@ -39,9 +36,6 @@ public class ImageServiceTest {
     // return the epected URL using Mocked Thumbor instance
     String expectedImageUrl =
         "http://localhost:8889/ucBCtKeFXF3rPUsmUGmxcTRnoj0=/300x300/triforce.png";
-    when(thumbor.buildImage(anyString())).thenReturn(thumbor);
-    when(thumbor.resize(isA(int.class), isA(int.class))).thenReturn(thumbor);
-    when(thumbor.toUrl()).thenReturn(expectedImageUrl);
 
     // assert that URL is as expected when ImageDto is run --PASS
     ImageDto result = imageService.getImage(givenimg);
