fc8a2e1259e10740270b3e91e5d89aa566a4df2e
mdage1 <maria.diaz@sagebase.org>
2023-05-01 18:51:01 +0000

    [Story] Write a unit test for the enpoint of the image service Fixes #1456

--- apps/openchallenges/image-service/src/test/java/org/sagebionetworks/openchallenges/image/service/service/ImageServiceTest.java
+++ apps/openchallenges/image-service/src/test/java/org/sagebionetworks/openchallenges/image/service/service/ImageServiceTest.java
@@ -2,30 +2,15 @@ package org.sagebionetworks.openchallenges.image.service.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.squareup.pollexor.Thumbor;
 import org.junit.jupiter.api.Test;
-import org.sagebionetworks.openchallenges.app.config.data.ImageServiceConfigData;
 import org.sagebionetworks.openchallenges.image.service.model.dto.ImageDto;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 public class ImageServiceTest {
 
   @Autowired private ImageService imageService;
-
-  @MockBean private Logger LOG;
-
-  @MockBean private Thumbor thumbor;
-
-  @MockBean private ImageServiceConfigData imageServiceConfigData;
-
-  @Value("${example.expectedURL}")
-  private String expectedURL =
-      "http://localhost:8889/ucBCtKeFXF3rPUsmUGmxcTRnoj0=/300x300/triforce.png";
 
   @Test
   void givenImage_ImageDto_MatchURL() {
@@ -35,10 +20,10 @@ public class ImageServiceTest {
 
     // return the epected URL using Mocked Thumbor instance
     String expectedImageUrl =
-        "http://localhost:8889/ucBCtKeFXF3rPUsmUGmxcTRnoj0=/300x300/triforce.png";
+        "http://localhost:8082/img/ucBCtKeFXF3rPUsmUGmxcTRnoj0=/300x300/triforce.png";
 
     // assert that URL is as expected when ImageDto is run --PASS
     ImageDto result = imageService.getImage(givenimg);
-    assertThat(result.getUrl()).isEqualTo(expectedURL);
+    assertThat(result.getUrl()).isEqualTo(expectedImageUrl);
   }
 }
