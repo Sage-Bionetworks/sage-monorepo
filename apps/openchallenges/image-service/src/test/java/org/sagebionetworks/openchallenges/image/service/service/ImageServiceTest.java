a374eab5f3170d3f82be80742ceb1474c035c199
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-05-01 21:49:54 +0000

    Commit WIP

--- apps/openchallenges/image-service/src/test/java/org/sagebionetworks/openchallenges/image/service/service/ImageServiceTest.java
+++ apps/openchallenges/image-service/src/test/java/org/sagebionetworks/openchallenges/image/service/service/ImageServiceTest.java
@@ -1,29 +1,65 @@
 package org.sagebionetworks.openchallenges.image.service.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.sagebionetworks.openchallenges.app.config.data.ImageServiceConfigData;
 import org.sagebionetworks.openchallenges.image.service.model.dto.ImageDto;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-public class ImageServiceTest {
+import com.squareup.pollexor.Thumbor;
 
-  @Autowired private ImageService imageService;
+@ExtendWith(MockitoExtension.class)
+public class ImageServiceTest {
+  @Mock
+  private ImageServiceConfigData imageServiceConfigData;
+
+  @Mock
+  private Thumbor thumbor;
+
+  // @InjectMocks
+  private ImageService imageService;
+
+  @BeforeEach
+  void setUp() {
+    imageService = new ImageService(imageServiceConfigData);
+  }
 
   @Test
-  void givenImage_ImageDto_MatchURL() {
+  void testGetImage() {
+    String image = "image.png";
+    String expectedUrl = "https://thumbor.example.com/image.png?resize=300x300";
 
-    // given an image
-    String givenimg = "triforce.png";
+    when(imageServiceConfigData.getThumborHost()).thenReturn("thumbor.example.com");
+    when(imageServiceConfigData.getThumborSecurityKey()).thenReturn("secret");
+    when(thumbor.buildImage(image).resize(300, 300).toUrl()).thenReturn(expectedUrl);
+    // when(thumbor.resize(300, 300)).thenReturn(thumbor);
+    // when(thumbor.toUrl()).thenReturn(expectedUrl);
 
-    // return the epected URL using Mocked Thumbor instance
-    String expectedImageUrl =
-        "http://localhost:8082/img/ucBCtKeFXF3rPUsmUGmxcTRnoj0=/300x300/triforce.png";
+    ImageDto actual = imageService.getImage(image);
 
-    // assert that URL is as expected when ImageDto is run --PASS
-    ImageDto result = imageService.getImage(givenimg);
-    assertThat(result.getUrl()).isEqualTo(expectedImageUrl);
+    assertEquals(expectedUrl, actual.getUrl());
   }
+
+  // @Test
+  // void givenImage_ImageDto_MatchURL() {
+
+  //   // given an image
+  //   String givenimg = "triforce.png";
+
+  //   // return the epected URL using Mocked Thumbor instance
+  //   String expectedImageUrl =
+  //       "http://localhost:8082/img/ucBCtKeFXF3rPUsmUGmxcTRnoj0=/300x300/triforce.png";
+
+  //   // assert that URL is as expected when ImageDto is run --PASS
+  //   ImageDto result = imageService.getImage(givenimg);
+  //   assertThat(result.getUrl()).isEqualTo(expectedImageUrl);
+  // }
 }
