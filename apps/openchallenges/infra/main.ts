0400ef0f0dfc5d0f64eec43d03bd504c35f90be1
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-05-02 16:41:57 +0000

    Generate CDKTF project

---
+++ apps/openchallenges/infra/main.ts
@@ -0,0 +1,14 @@
+import { Construct } from "constructs";
+import { App, TerraformStack } from "cdktf";
+
+class MyStack extends TerraformStack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    // define resources here
+  }
+}
+
+const app = new App();
+new MyStack(app, "infra");
+app.synth();
