8a6738b0ab21147b635639d2e293bfa1dce7f886
verena <9377970+vpchung@users.noreply.github.com>
2023-05-04 07:45:46 +0000

    home page: add graphics

--- libs/openchallenges/home/src/lib/statistics-viewer/statistics-viewer.component.ts
+++ libs/openchallenges/home/src/lib/statistics-viewer/statistics-viewer.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  Image,
+  ImageService,
+} from '@sagebionetworks/openchallenges/api-client-angular';
 import {
   Registry,
   RegistryService,
@@ -10,9 +14,28 @@ import { Observable } from 'rxjs';
   templateUrl: './statistics-viewer.component.html',
   styleUrls: ['./statistics-viewer.component.scss'],
 })
-export class StatisticsViewerComponent {
+export class StatisticsViewerComponent implements OnInit {
+  public challenges$: Observable<Image> | undefined;
+  public orgs$: Observable<Image> | undefined;
+  public users$: Observable<Image> | undefined;
   registry$!: Observable<Registry>;
-  constructor(private registryService: RegistryService) {
+
+  constructor(
+    private registryService: RegistryService,
+    private imageService: ImageService
+  ) {
     this.registry$ = this.registryService.getRegistry();
+  }
+
+  ngOnInit() {
+    this.challenges$ = this.imageService.getImage({
+      objectKey: 'home-challenges.svg',
+    });
+    this.orgs$ = this.imageService.getImage({
+      objectKey: 'home-hosts.svg',
+    });
+    this.users$ = this.imageService.getImage({
+      objectKey: 'home-users.svg',
+    });
   }
 }
