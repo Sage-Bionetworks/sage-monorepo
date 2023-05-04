2f2982e742d9db308f7556bad3d6224b8538cbd9
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-05-04 03:50:43 +0000

    Deploy EC2 + key pair

--- apps/openchallenges/infra/README.md
+++ apps/openchallenges/infra/README.md
@@ -8,6 +8,10 @@ This project describe how to deploy the OpenChallenges stack with [Terraform CDK
 
 ### Create an IAM user for deploying the stack
 
+1. Create the IAM user `OpenChallenges-GitHubActionsUser`
+
+TODO: Document the roles that this user should have
+
 ### Set AWS configuration
 
 Run this command to quickly set your credentials, Region, and output format. The following example
@@ -21,12 +25,31 @@ Default region name [None]: us-east-1
 Default output format [None]
 ```
 
+### Generate an SSH key for accessing the EC2 instance
+
+Generate the SSH key for connecting to the EC2 instance that we will create as part of the stack.
+
+```console
+ssh-keygen -t ed25519 -C "your_email@example.com" -f ~/.ssh/openchallenges-ec2 -N ''
+```
+
 ## Usage
 
 ### Deploy the stack
 
 ```console
 cdktf deploy
+```
+
+### Connect to the EC2 instance
+
+First, check that the security group of the EC2 instance to make sure that it access inbound SSH
+traffic.
+
+Connect to the instance using the private key:
+
+```console
+$ ssh -i ~/.ssh/openchallenges-ec2 ubuntu@44.206.227.254
 ```
 
 ### Destroy the stack
--- apps/openchallenges/infra/src/main.ts
+++ apps/openchallenges/infra/src/main.ts
@@ -1,31 +1,44 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable no-new */
 import { Construct } from 'constructs';
-import { App, TerraformStack } from 'cdktf';
+import { App, TerraformOutput, TerraformStack } from 'cdktf';
 import { logger, Level } from './logger';
 import { AwsProvider } from '@cdktf/provider-aws/lib/provider';
 import { Instance } from '@cdktf/provider-aws/lib/instance';
+import { KeyPair } from '@cdktf/provider-aws/lib/key-pair';
+import * as os from 'os';
+import * as fs from 'fs';
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    // eslint-disable-next-line no-new
+    const keyPath = `${os.homedir()}/.ssh/openchallenges-ec2.pub`;
+    const publicKey = fs.readFileSync(keyPath, 'utf-8');
+
     new AwsProvider(this, 'AWS', {
       region: 'us-east-1',
     });
 
-    // eslint-disable-next-line
+    const keyPair = new KeyPair(this, 'keypair', {
+      publicKey,
+      keyName: 'openchallenges-ec2-key',
+    });
+
     const ec2Instance = new Instance(this, 'compute', {
       ami: 'ami-0044130ca185d0880', // Ubuntu 22.04 LTS
       instanceType: 't2.micro',
+      keyName: keyPair.keyName,
+    });
+
+    new TerraformOutput(this, 'public_ip', {
+      value: ec2Instance.publicIp,
     });
   }
 }
 
 logger.setLevel(Level.Debug);
-logger.info('Welcome to OpenChallenges infra project');
+logger.info('Welcome to the deployment of the OpenChallenges stack.');
 
 const app = new App();
-// eslint-disable-next-line no-new
-new MyStack(app, 'openchallenges-infra');
+new MyStack(app, 'openchallenges-stack');
 app.synth();
