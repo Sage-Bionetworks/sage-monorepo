984532dbdfe0b598b3613186476600f0dc0b29db
verena <9377970+vpchung@users.noreply.github.com>
2023-05-04 07:01:01 +0000

    footer: placehold using img from image-service

--- libs/openchallenges/ui/src/lib/footer/footer.component.ts
+++ libs/openchallenges/ui/src/lib/footer/footer.component.ts
@@ -1,11 +1,32 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  Image,
+  ImageService,
+} from '@sagebionetworks/openchallenges/api-client-angular';
+import { ConfigService } from '@sagebionetworks/openchallenges/config';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'openchallenges-footer',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
   @Input() version = 'x.y.z';
+
+  public logo$: Observable<Image> | undefined;
+
+  constructor(
+    private readonly configService: ConfigService,
+    private imageService: ImageService
+  ) {}
+
+  ngOnInit() {
+    this.logo$ = this.imageService.getImage({
+      objectKey: 'openchallenges-white.svg',
+    });
+  }
 }
