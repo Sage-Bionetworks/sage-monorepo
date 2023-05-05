fa6d7cb990ec1eaac71d0a1836c63bb8d214dd48
Verena Chung <9377970+vpchung@users.noreply.github.com>
2023-05-05 16:46:57 -0700

    fix(data): organization data and table updates (#1507)
    * increase organization.description char limit to 1000
    
    * update descriptions

--- apps/openchallenges/organization-service/src/main/resources/db/migration/V1.0.0__create_tables.sql
+++ apps/openchallenges/organization-service/src/main/resources/db/migration/V1.0.0__create_tables.sql
@@ -7,7 +7,7 @@ CREATE TABLE `organization` (
   `login` varchar(64) NOT NULL UNIQUE,
   `avatar_url` varchar(255) DEFAULT NULL,
   `website_url` varchar(255) DEFAULT NULL,
-  `description` varchar(280) DEFAULT NULL,
+  `description` varchar(1000) DEFAULT NULL,
   `challenge_count` int,
   `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
   `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
