a300cb26153364516a801ec5746ef098b17a79b6
verena <9377970+vpchung@users.noreply.github.com>
2023-05-06 01:25:42 +0000

    home page: update sponsors list; increase search text

--- libs/openchallenges/home/src/lib/sponsor-list/sponsor-list.component.ts
+++ libs/openchallenges/home/src/lib/sponsor-list/sponsor-list.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  Image,
+  ImageService,
+} from '@sagebionetworks/openchallenges/api-client-angular';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'openchallenges-sponsor-list',
   templateUrl: './sponsor-list.component.html',
   styleUrls: ['./sponsor-list.component.scss'],
 })
-export class SponsorListComponent {}
+export class SponsorListComponent implements OnInit {
+  public sage$: Observable<Image> | undefined;
+
+  constructor(private imageService: ImageService) {}
+
+  ngOnInit() {
+    this.sage$ = this.imageService.getImage({
+      objectKey: 'logo/sage-bionetworks.png',
+    });
+  }
+}
