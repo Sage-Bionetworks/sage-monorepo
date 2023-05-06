f93b55aa684a38a2535e4d9fc6a6fe09d998f5ca
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-05-06 00:50:39 +0000

    Fix test

--- apps/openchallenges/image-service/src/test/java/org/sagebionetworks/openchallenges/image/service/service/ImageServiceTest.java
+++ apps/openchallenges/image-service/src/test/java/org/sagebionetworks/openchallenges/image/service/service/ImageServiceTest.java
@@ -1,9 +1,12 @@
 package org.sagebionetworks.openchallenges.image.service.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.squareup.pollexor.Thumbor;
+import com.squareup.pollexor.ThumborUrlBuilder;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,27 +16,35 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.sagebionetworks.openchallenges.app.config.data.ImageServiceConfigData;
 import org.sagebionetworks.openchallenges.image.service.model.dto.ImageDto;
 import org.sagebionetworks.openchallenges.image.service.model.dto.ImageQueryDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ExtendWith(MockitoExtension.class)
 public class ImageServiceTest {
 
-  @Mock private ImageServiceConfigData imageServiceConfigData;
+  private static final Logger LOG = LoggerFactory.getLogger(ImageServiceTest.class);
+
+  @Mock
+  private ImageServiceConfigData imageServiceConfigData;
 
   private Thumbor thumbor = Mockito.mock(Thumbor.class, Mockito.RETURNS_DEEP_STUBS);
 
-  @InjectMocks private ImageService imageService;
+  @InjectMocks
+  private ImageService imageService;
 
   @Test
   void getImage_ShouldReturnImage_WhenObjectKeyIsPassed() {
-    String expectedUrl =
-        "http://localhost:8082/img/ucBCtKeFXF3rPUsmUGmxcTRnoj0=/300x300/triforce.png";
+    String expectedUrl = "http://localhost:8082/img/S2_Nh1GysneL6qVEGuBdz5NK-wQ=/image.png";
 
     // given
     ImageQueryDto query = new ImageQueryDto();
     query.setObjectKey("image.png");
 
     // when an image is requested from the image service
-    when(thumbor.buildImage(query.getObjectKey()).resize(300, 300).toUrl()).thenReturn(expectedUrl);
+    ThumborUrlBuilder builder = mock(ThumborUrlBuilder.class);
+
+    when(thumbor.buildImage(query.getObjectKey())).thenReturn(builder); // why is it needed?
+    when(builder.toUrl()).thenReturn(expectedUrl);
     ImageDto actual = imageService.getImage(query);
 
     // then
