50c247095d4da81256d4e51578a7d0776d45a176
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-05-06 00:57:20 +0000

    Fix format

--- apps/openchallenges/image-service/src/test/java/org/sagebionetworks/openchallenges/image/service/service/ImageServiceTest.java
+++ apps/openchallenges/image-service/src/test/java/org/sagebionetworks/openchallenges/image/service/service/ImageServiceTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.when;
 
 import com.squareup.pollexor.Thumbor;
 import com.squareup.pollexor.ThumborUrlBuilder;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
