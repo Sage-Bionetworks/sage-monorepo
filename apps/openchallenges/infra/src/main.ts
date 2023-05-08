5a87fada924f212bc75c1d7ec44e6557b152a138
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-05-08 11:32:45 -0700

    feat: Add the CostCenter tag to AWS resources created with CDKTF (#1510)
    * Specify AWS profile name
    
    * Deploy using TF cloud backend
    
    * Update doc
    
    * Tag AWS resources
    
    * Add constants
    
    * Refer to official docs to add variable
    
    * Update README

--- apps/openchallenges/infra/src/main.ts
+++ apps/openchallenges/infra/src/main.ts
@@ -2,6 +2,7 @@
 import { Construct } from 'constructs';
 import {
   App,
+  Aspects,
   CloudBackend,
   NamedCloudWorkspace,
   TerraformOutput,
@@ -13,6 +14,13 @@ import { Instance } from '@cdktf/provider-aws/lib/instance';
 import { KeyPair } from '@cdktf/provider-aws/lib/key-pair';
 import * as os from 'os';
 import * as fs from 'fs';
+import { TagsAddingAspect } from './aspect/tags-adding-aspect';
+import {
+  Ami,
+  AmazonEc2InstanceType,
+  SageCostCenter,
+  AmazonRegion,
+} from './constants';
 
 class OpenChallengesStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
@@ -21,10 +29,10 @@ class OpenChallengesStack extends TerraformStack {
     const keyPath = `${os.homedir()}/.ssh/openchallenges-ec2.pub`;
     const publicKey = fs.readFileSync(keyPath, 'utf-8');
     const keyName = 'openchallenges-ec2-key';
+    const stackOwnerEmail = 'thomas.schaffter@sagebionetworks.org';
 
     new AwsProvider(this, 'AWS', {
-      region: 'us-east-1',
-      // profile: 'cdktf',
+      region: AmazonRegion.US_EAST_1,
     });
 
     const keyPair = new KeyPair(this, 'keypair', {
@@ -33,14 +41,22 @@ class OpenChallengesStack extends TerraformStack {
     });
 
     const ec2Instance = new Instance(this, 'compute', {
-      ami: 'ami-0044130ca185d0880', // Ubuntu 22.04 LTS
-      instanceType: 't2.micro',
+      ami: Ami.UBUNTU_22_04_LTS,
+      instanceType: AmazonEc2InstanceType.T2_MICRO,
       keyName: keyPair.keyName,
     });
 
     new TerraformOutput(this, 'public_ip', {
       value: ec2Instance.publicIp,
     });
+
+    // Add tags to every resource defined within this stack.
+    Aspects.of(this).add(
+      new TagsAddingAspect({
+        OwnerEmail: stackOwnerEmail,
+        CostCenter: SageCostCenter.ITCR,
+      })
+    );
   }
 }
 
