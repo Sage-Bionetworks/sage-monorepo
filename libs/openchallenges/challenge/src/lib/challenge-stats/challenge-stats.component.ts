74c854d7809e876fe9a2e513525b95d00bc0544b
verena <9377970+vpchung@users.noreply.github.com>
2023-05-09 08:42:07 +0000

    profile pages: use str abbrev. for large stat numbers

--- libs/openchallenges/challenge/src/lib/challenge-stats/challenge-stats.component.ts
+++ libs/openchallenges/challenge/src/lib/challenge-stats/challenge-stats.component.ts
@@ -1,10 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'openchallenges-challenge-stats',
   templateUrl: './challenge-stats.component.html',
   styleUrls: ['./challenge-stats.component.scss'],
 })
-export class ChallengeStatsComponent {
+export class ChallengeStatsComponent implements OnInit {
   @Input() loggedIn = false;
+  mockViews!: number;
+  mockStargazers!: number;
+
+  ngOnInit(): void {
+    this.mockViews = 2;
+    this.mockStargazers = 9_999;
+  }
+
+  shorthand(n: number) {
+    return Intl.NumberFormat('en-US', {
+      notation: 'compact',
+      maximumFractionDigits: 1,
+    }).format(n);
+  }
 }
--- libs/openchallenges/org-profile/src/lib/org-profile-stats/org-profile-stats.component.ts
+++ libs/openchallenges/org-profile/src/lib/org-profile-stats/org-profile-stats.component.ts
@@ -1,10 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'openchallenges-org-profile-stats',
   templateUrl: './org-profile-stats.component.html',
   styleUrls: ['./org-profile-stats.component.scss'],
 })
-export class OrgProfileStatsComponent {
+export class OrgProfileStatsComponent implements OnInit {
   @Input() loggedIn = false;
+  mockViews!: number;
+  mockStargazers!: number;
+
+  ngOnInit(): void {
+    this.mockViews = 2;
+    this.mockStargazers = 9_999;
+  }
+
+  shorthand(n: number) {
+    return Intl.NumberFormat('en-US', {
+      notation: 'compact',
+      maximumFractionDigits: 1,
+    }).format(n);
+  }
 }
