621bb4337113411b3621b459298296d131004a58
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-05-10 12:12:27 -0700

    feat: Use the org acronym when generating the org avatar (#1523)
    * Update API client for Angular
    
    * Fix mock organizations in app
    
    * Convert the avatar component to standalone
    
    * Enable to specify acronym to the avatar
    
    * Restore src
    
    * Add comment
    
    * Fix test
    
    * Update libs/openchallenges/ui/src/lib/avatar/avatar.component.ts
    
    Co-authored-by: Verena Chung <9377970+vpchung@users.noreply.github.com>
    
    * Restore images
    
    ---------
    
    Co-authored-by: Verena Chung <9377970+vpchung@users.noreply.github.com>

--- libs/openchallenges/ui/src/lib/avatar/mock-avatars.ts
+++ libs/openchallenges/ui/src/lib/avatar/mock-avatars.ts
@@ -4,6 +4,7 @@ export const EMPTY_AVATAR: Avatar = {
   name: '',
   src: '',
   size: 32,
+  value: '',
 };
 
 export const MOCK_AVATAR_32: Avatar = {
--- libs/openchallenges/ui/src/lib/search-dropdown-filter/search-dropdown-filter.module.ts
+++ libs/openchallenges/ui/src/lib/search-dropdown-filter/search-dropdown-filter.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchDropdownFilterComponent } from './search-dropdown-filter.component';
 import { MultiSelectModule } from 'primeng/multiselect';
-import { AvatarModule } from '../avatar/avatar.module';
 import { FormsModule } from '@angular/forms';
+import { AvatarComponent } from '../avatar/avatar.component';
 
 @NgModule({
   declarations: [SearchDropdownFilterComponent],
-  imports: [AvatarModule, CommonModule, FormsModule, MultiSelectModule],
+  imports: [AvatarComponent, CommonModule, FormsModule, MultiSelectModule],
   exports: [SearchDropdownFilterComponent],
 })
 export class SearchDropdownFilterModule {}
