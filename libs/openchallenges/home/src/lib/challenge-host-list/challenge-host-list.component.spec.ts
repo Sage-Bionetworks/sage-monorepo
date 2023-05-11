2670be03fa046eed852936962b54b3d2f501e9d6
verena <9377970+vpchung@users.noreply.github.com>
2023-05-11 22:46:31 +0000

    fix text

--- libs/openchallenges/home/src/lib/challenge-host-list/challenge-host-list.component.spec.ts
+++ libs/openchallenges/home/src/lib/challenge-host-list/challenge-host-list.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ChallengeHostListComponent } from './challenge-host-list.component';
 
@@ -9,6 +10,7 @@ describe('ChallengeHostListComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ChallengeHostListComponent],
+      imports: [HttpClientModule],
     }).compileComponents();
   });
 
