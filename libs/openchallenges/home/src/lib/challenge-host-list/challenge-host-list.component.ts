e2803460c2e598de1c26826845c20cbc1962fef0
Verena Chung <9377970+vpchung@users.noreply.github.com>
2023-05-12 09:39:47 -0700

    feat(ui): display top 4 org cards by challenge count (#1535)
    * display top 4 org cards by challenge count
    
    * add "see more" button
    
    * fix text
    
    * removing tracked change unrelated to pr

--- libs/openchallenges/home/src/lib/challenge-host-list/challenge-host-list.component.ts
+++ libs/openchallenges/home/src/lib/challenge-host-list/challenge-host-list.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
-import { Organization } from '@sagebionetworks/openchallenges/api-client-angular';
-import { MOCK_ORGANIZATIONS } from '@sagebionetworks/openchallenges/ui';
+import { Component, OnInit } from '@angular/core';
+import {
+  Organization,
+  OrganizationService,
+  OrganizationSearchQuery,
+} from '@sagebionetworks/openchallenges/api-client-angular';
+import { BehaviorSubject, switchMap } from 'rxjs';
 
 @Component({
   selector: 'openchallenges-challenge-host-list',
   templateUrl: './challenge-host-list.component.html',
   styleUrls: ['./challenge-host-list.component.scss'],
 })
-export class ChallengeHostListComponent {
-  // constructor() {}
-  // ngOnInit(): void {}
-  organizations: Organization[] = MOCK_ORGANIZATIONS;
+export class ChallengeHostListComponent implements OnInit {
+  private query: BehaviorSubject<OrganizationSearchQuery> =
+    new BehaviorSubject<OrganizationSearchQuery>({});
+
+  organizations: Organization[] = [];
+
+  constructor(private organizationService: OrganizationService) {}
+
+  ngOnInit() {
+    const defaultQuery: OrganizationSearchQuery = {
+      pageNumber: 0,
+      pageSize: 4,
+      searchTerms: '',
+      sort: 'challenge_count',
+    } as OrganizationSearchQuery;
+    this.query.next(defaultQuery);
+    this.query
+      .pipe(
+        switchMap((query) => this.organizationService.listOrganizations(query))
+      )
+      .subscribe((page) => {
+        this.organizations = page.organizations;
+      });
+  }
 }
