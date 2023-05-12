8ac7b4527d967b76106f7cb7914c03abdbf9f6ec
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-05-12 21:03:32 +0000

    Download sponsor logo with h=140px

--- libs/openchallenges/home/src/lib/sponsor-list/sponsor-list.component.ts
+++ libs/openchallenges/home/src/lib/sponsor-list/sponsor-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {
   Image,
+  ImageHeight,
   ImageService,
 } from '@sagebionetworks/openchallenges/api-client-angular';
 import { Observable } from 'rxjs';
@@ -11,21 +12,25 @@ import { Observable } from 'rxjs';
   styleUrls: ['./sponsor-list.component.scss'],
 })
 export class SponsorListComponent implements OnInit {
-  public itcr$: Observable<Image> | undefined;
-  public aws$: Observable<Image> | undefined;
-  public sage$: Observable<Image> | undefined;
+  itcr$: Observable<Image> | undefined;
+  aws$: Observable<Image> | undefined;
+  sage$: Observable<Image> | undefined;
+  readonly height = ImageHeight._140px;
 
   constructor(private imageService: ImageService) {}
 
   ngOnInit() {
     this.itcr$ = this.imageService.getImage({
       objectKey: 'logo/nci-itcr.png',
+      height: this.height,
     });
     this.aws$ = this.imageService.getImage({
       objectKey: 'logo/aws.svg',
+      height: this.height,
     });
     this.sage$ = this.imageService.getImage({
       objectKey: 'logo/sage-bionetworks.png',
+      height: this.height,
     });
   }
 }
