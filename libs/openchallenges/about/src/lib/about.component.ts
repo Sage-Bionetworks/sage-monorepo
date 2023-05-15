f19315e87b6651c4e754bfdfd6564dc2c244b615
Verena Chung <9377970+vpchung@users.noreply.github.com>
2023-05-15 15:03:12 -0700

    fix(ui): About page (+ other static pages/components) (#1556)
    * fix layout and increase text sizing
    
    * replace image service with image assets
    
    * replace image service in other static pages/components
    
    * fix format
    
    * update image name

--- libs/openchallenges/about/src/lib/about.component.ts
+++ libs/openchallenges/about/src/lib/about.component.ts
@@ -1,12 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import {
-  Image,
-  ImageService,
-} from '@sagebionetworks/openchallenges/api-client-angular';
+import { Component } from '@angular/core';
 import { ConfigService } from '@sagebionetworks/openchallenges/config';
 import { FooterComponent } from '@sagebionetworks/openchallenges/ui';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'openchallenges-about',
@@ -15,36 +10,10 @@ import { Observable } from 'rxjs';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent {
   public appVersion: string;
-  public logo$: Observable<Image> | undefined;
-  public bars$: Observable<Image> | undefined;
-  public fingerprint$: Observable<Image> | undefined;
-  public chart$: Observable<Image> | undefined;
-  public graph$: Observable<Image> | undefined;
 
-  constructor(
-    private readonly configService: ConfigService,
-    private imageService: ImageService
-  ) {
+  constructor(private readonly configService: ConfigService) {
     this.appVersion = this.configService.config.appVersion;
-  }
-
-  ngOnInit() {
-    this.logo$ = this.imageService.getImage({
-      objectKey: 'openchallenges-icon.svg',
-    });
-    this.bars$ = this.imageService.getImage({
-      objectKey: 'icon-bars.svg',
-    });
-    this.fingerprint$ = this.imageService.getImage({
-      objectKey: 'icon-fingerprint.svg',
-    });
-    this.chart$ = this.imageService.getImage({
-      objectKey: 'icon-chart.svg',
-    });
-    this.graph$ = this.imageService.getImage({
-      objectKey: 'icon-graph.svg',
-    });
   }
 }
