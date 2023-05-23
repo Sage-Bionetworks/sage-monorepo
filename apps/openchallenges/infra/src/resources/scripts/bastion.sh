f382b956b773d18702113bacb399df47e6ac99b9
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-05-23 21:14:44 +0000

    Move Docker install script to the preview instance

--- apps/openchallenges/infra/src/resources/scripts/bastion.sh
+++ apps/openchallenges/infra/src/resources/scripts/bastion.sh
@@ -1,27 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 apt update
 
 # Set the hostname
 hostnamectl set-hostname openchallenges-bastion
-
-# Install Docker Engine on Ubuntu
-apt-get install -y ca-certificates curl gnupg
-
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-chmod a+r /etc/apt/keyrings/docker.gpg
-
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-# Post-installation steps for Docker Engine
-user="ubuntu"
-
-groupadd --force docker
-usermod -a --groups docker $user
--- apps/openchallenges/infra/src/resources/scripts/preview-instance.sh
+++ apps/openchallenges/infra/src/resources/scripts/preview-instance.sh
@@ -1,7 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 apt update
 
 # Set the hostname
-# echo "openchallenges-preview-instance" > /etc/hostname
 hostnamectl set-hostname openchallenges-preview-instance
+
+# Install Docker Engine on Ubuntu
+apt-get install -y ca-certificates curl gnupg
+
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Post-installation steps for Docker Engine
+user="ubuntu"
+
+groupadd --force docker
+usermod -a --groups docker $user
