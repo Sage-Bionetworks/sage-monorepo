3b452b9ed28b51ed25b57973cd093a30e1e8b283
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-05-26 22:51:08 +0000

    Add role to ssm to the bastion

--- apps/openchallenges/infra/src/bastion/bastion.ts
+++ apps/openchallenges/infra/src/bastion/bastion.ts
@@ -1,13 +1,53 @@
+/* eslint-disable no-unused-vars */
 import { Instance } from '@cdktf/provider-aws/lib/instance';
 import { Construct } from 'constructs';
 import { readFileSync } from 'fs';
 import { BastionConfig } from './bastion-config';
+import { IamInstanceProfile } from '@cdktf/provider-aws/lib/iam-instance-profile';
+import { IamRole } from '@cdktf/provider-aws/lib/iam-role';
+import { DataAwsIamPolicyDocument } from '@cdktf/provider-aws/lib/data-aws-iam-policy-document';
 
 export class Bastion extends Construct {
   instance: Instance;
 
   constructor(scope: Construct, id: string, config: BastionConfig) {
     super(scope, id);
+
+    const policyDocument = new DataAwsIamPolicyDocument(
+      this,
+      'bastion_assume_role',
+      {
+        statement: [
+          {
+            actions: ['sts:AssumeRole'],
+            effect: 'Allow',
+            principals: [
+              {
+                identifiers: ['ec2.amazonaws.com'],
+                type: 'Service',
+              },
+            ],
+          },
+        ],
+      }
+    );
+
+    const ssmInstanceRole = new IamRole(this, 'bastion_iam_role', {
+      name: `${config.tagPrefix}-ec2-role`,
+      assumeRolePolicy: policyDocument.json,
+      managedPolicyArns: [
+        'arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore',
+      ],
+    });
+
+    const ssmInstanceProfile = new IamInstanceProfile(
+      this,
+      'ssm_instance_profile',
+      {
+        name: `${config.tagPrefix}-ssm-instance-profile`,
+        role: ssmInstanceRole.name,
+      }
+    );
 
     this.instance = new Instance(this, id, {
       ami: config.ami,
@@ -18,6 +58,7 @@ export class Bastion extends Construct {
       tags: { Name: `${config.tagPrefix}-bastion` },
       userData: readFileSync('./src/resources/scripts/bastion.sh', 'utf8'),
       vpcSecurityGroupIds: [config.securityGroupId],
+      iamInstanceProfile: ssmInstanceProfile.name,
     });
   }
 }
