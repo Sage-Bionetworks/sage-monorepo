617f1a8300727dbe11454816833ba22e9c7a1c5f
heimannch <carolina.heimann@gmail.com>
2023-06-06 15:50:27 -0700

    single cell bubble plot with MSK and Vanderbilt HTAN data

--- R/sc_bubbleplot_server.R
+++ R/sc_bubbleplot_server.R
@@ -8,7 +8,7 @@ sc_bubbleplot_server <- function(
 
       ns <- session$ns
 
-      bubble_df <- shiny::reactive(read.delim("inst/tsv/bubble_plot_df.tsv"))
+      bubble_df <- shiny::reactive(arrow::read_feather("inst/feather/bubble_plot_df.feather"))
 
       output$select_cells <- shiny::renderUI({
         shiny::req(bubble_df())
@@ -16,13 +16,13 @@ sc_bubbleplot_server <- function(
           ns("cells"),
           label = "Select cells",
           choices = unique(bubble_df()$cell_type),
-          selected = c("B cell", "Plasma cell",  "CD8+ Teff"),
+          selected = c("B cell"),
           multiple = TRUE
         )
       })
 
       shiny::observe({
-        #shiny::req(bubble_df())
+        # shiny::req(bubble_df())
         shiny::updateSelectizeInput(
           session,
           "genes",
@@ -32,25 +32,35 @@ sc_bubbleplot_server <- function(
         )
       })
 
-      output$bubble_plot <- plotly::renderPlotly({
+      plot_df <- shiny::reactive({
         shiny::req(bubble_df(), input$genes, input$cells)
-
         plot_df <- dplyr::filter(bubble_df(), cell_type %in% input$cells & gene %in% input$genes) %>%
           dplyr::mutate(text = paste(
                         paste0("% cells with expression for gene: ", round(perc_expr, 3)*100, "%"),
                         paste("Average value:", round(avg, 3)), sep = "\n"
                         ))
+      })
 
-        create_scatterplot(
-          df = plot_df,
-          x_col = "gene",
-          y_col = "cell_type",
-          color_col = "avg",
-          size_col = "perc_expr",
-          label_col = "text",
-          xlab = "Gene Symbol",
-          ylab = "Cell type"
-        )
+      output$bubble_plot <- plotly::renderPlotly({
+        shiny::req(plot_df())
+        datasets <- unique(plot_df()$dataset)
+
+        all_plots <- purrr::map(.x = datasets,
+                                .f = function(x){
+                                    create_scatterplot(
+                                      df = dplyr::filter(plot_df(), dataset == x),
+                                      x_col = "gene",
+                                      y_col = "cell_type",
+                                      color_col = "avg",
+                                      size_col = "perc_expr",
+                                      label_col = "text",
+                                      xlab = "Gene Symbol",
+                                      ylab = "Cell type"
+                                    ) %>%
+                                    add_title_subplot_plotly(x)
+                                 })
+
+        plotly::subplot(all_plots, nrows = 1, shareY = TRUE, titleX = TRUE, titleY= TRUE, margin = 0.1)
       })
 
     }
