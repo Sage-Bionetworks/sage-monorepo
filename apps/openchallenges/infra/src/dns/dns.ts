6b02c21929e8606baf317f98f2261f8e484b7d04
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-06-08 20:17:09 +0000

    Add cert

--- apps/openchallenges/infra/src/dns/dns.ts
+++ apps/openchallenges/infra/src/dns/dns.ts
@@ -2,13 +2,17 @@ import { Construct } from 'constructs';
 import { Route53Zone } from '@cdktf/provider-aws/lib/route53-zone';
 import { Route53Record } from '@cdktf/provider-aws/lib/route53-record';
 import { Alb } from '@cdktf/provider-aws/lib/alb';
+import { AcmCertificate } from '@cdktf/provider-aws/lib/acm-certificate';
 
 export class Dns extends Construct {
   devZone: Route53Zone;
   record: Route53Record;
+  cert: AcmCertificate;
 
   constructor(scope: Construct, id: string, lb: Alb) {
     super(scope, id);
+
+    const nameTagPrefix = 'openchallenges';
 
     // TODO This resource must be managed outside of this stack when more than one stacks are
     // deployed to this account.
@@ -28,6 +32,23 @@ export class Dns extends Construct {
         name: lb.dnsName,
         zoneId: lb.zoneId,
         evaluateTargetHealth: true,
+      },
+    });
+
+    this.cert = new AcmCertificate(this, 'cert', {
+      domainName: 'preview.openchallenges.io',
+      validationMethod: 'DNS',
+      validationOption: [
+        {
+          domainName: 'preview.openchallenges.io',
+          validationDomain: 'openchallenges.io',
+        },
+      ],
+      lifecycle: {
+        createBeforeDestroy: true,
+      },
+      tags: {
+        Name: `${nameTagPrefix}-preview-cert`,
       },
     });
   }
