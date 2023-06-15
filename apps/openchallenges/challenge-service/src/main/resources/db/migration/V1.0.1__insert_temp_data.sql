7d2c3221f6471ecf0fceb89e7e5d0c18e4893fb5
verena <9377970+vpchung@users.noreply.github.com>
2023-06-15 01:27:19 +0000

    update challenge status for kaggle challenge

--- apps/openchallenges/challenge-service/src/main/resources/db/migration/V1.0.1__insert_temp_data.sql
+++ apps/openchallenges/challenge-service/src/main/resources/db/migration/V1.0.1__insert_temp_data.sql
@@ -2289,7 +2289,7 @@ VALUES (
     'Find breast cancers in screening mammograms',
     'Find breast cancers in screening mammograms',
     'https://www.kaggle.com/competitions/rsna-breast-cancer-detection',
-    'active',
+    'completed',
     'intermediate',
     '8',
     '',
