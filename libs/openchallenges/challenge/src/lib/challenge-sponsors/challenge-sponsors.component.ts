29fb5a3c7dc8e65d8602ea6649158fd835d6f2b2
Rongrong Chai <rchai@sagebase.org>
2023-06-20 23:05:59 +0000

    abort updating, but keep the codes

--- libs/openchallenges/challenge/src/lib/challenge-sponsors/challenge-sponsors.component.ts
+++ libs/openchallenges/challenge/src/lib/challenge-sponsors/challenge-sponsors.component.ts
@@ -1,9 +1,27 @@
-import { Component, Input } from '@angular/core';
-import { Challenge } from '@sagebionetworks/openchallenges/api-client-angular';
+import {
+  Component,
+  Input,
+  // OnInit
+} from '@angular/core';
+import {
+  Challenge,
+  // ChallengeContribution,
+  // ChallengeContributionService,
+  // Image,
+  // ImageAspectRatio,
+  // ImageHeight,
+  // ImageQuery,
+  // ImageService,
+  // Organization,
+  // OrganizationService,
+} from '@sagebionetworks/openchallenges/api-client-angular';
 import {
   MOCK_CHALLENGE_SPONSORS,
   MOCK_ORGANIZATION_CARDS,
+  // OrganizationCard,
 } from '@sagebionetworks/openchallenges/ui';
+// import { forkJoinConcurrent } from '@sagebionetworks/openchallenges/util';
+// import { Observable, forkJoin, map, of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'openchallenges-challenge-sponsors',
@@ -14,4 +32,78 @@ export class ChallengeSponsorsComponent {
   @Input() challenge!: Challenge;
   sponsors = MOCK_CHALLENGE_SPONSORS;
   organizationCard = MOCK_ORGANIZATION_CARDS[0];
+
+  // contributionCards!: OrganizationCard[];
+
+  // constructor(
+  //   private challengeContributionService: ChallengeContributionService,
+  //   private organizationService: OrganizationService,
+  //   private imageService: ImageService
+  // ) {}
+
+  // ngOnInit() {
+  // TODO: need backend support to return org's login
+  // from ChallengeContributionService
+  //   const orgPage$ = this.challengeContributionService
+  //     .listChallengeContributions(this.challenge.id)
+  //     .pipe(
+  //       map((contributionPage) => contributionPage.challengeContributions),
+  //       switchMap((contributions) =>
+  //         forkJoin(
+  //           contributions.map((contribution) =>
+  //             this.organizationService.listOrganizations(
+  //               contribution.organizationId
+  //             )
+  //           )
+  //         )
+  //       )
+  //     );
+  //   orgPage$.pipe(
+  //     map((page) => page.organizations),
+  //     switchMap((orgs) =>
+  //       forkJoin({
+  //         orgs: of(orgs),
+  //         avatarUrls: forkJoinConcurrent(
+  //           orgs.map((org) => this.getOrganizationAvatarUrl(org)),
+  //           Infinity
+  //         ) as unknown as Observable<(Image | undefined)[]>,
+  //       })
+  //     ),
+  //     switchMap(({ orgs, avatarUrls }) =>
+  //       of(
+  //         orgs.map((org, index) =>
+  //           this.getOrganizationCard(org, avatarUrls[index])
+  //         )
+  //       )
+  //     )
+  //   ).subscribe(organizationCards =>
+  //     this.contributionCards = organizationCards;
+  //   )
+  // }
+  // private getOrganizationAvatarUrl(
+  //   org: Organization
+  // ): Observable<Image | undefined> {
+  //   if (org.avatarKey && org.avatarKey.length > 0) {
+  //     return this.imageService.getImage({
+  //       objectKey: org.avatarKey,
+  //       height: ImageHeight._140px,
+  //       aspectRatio: ImageAspectRatio._11,
+  //     } as ImageQuery);
+  //   } else {
+  //     return of(undefined);
+  //   }
+  // }
+  // private getOrganizationCard(
+  //   org: Organization,
+  //   avatarUrl: Image | undefined
+  // ): OrganizationCard {
+  //   return {
+  //     acronym: org.acronym,
+  //     avatarUrl: avatarUrl?.url,
+  //     challengeCount: org.challengeCount,
+  //     login: org.login,
+  //     name: org.name,
+  //   } as OrganizationCard;
+  // }
+  // }
 }
