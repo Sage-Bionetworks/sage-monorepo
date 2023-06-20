7315ede361fb0459d2234caadd8dd17c0ec45437
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-06-20 17:58:45 +0000

    Add initial contribution repository

--- apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/service/ChallengeContributionService.java
+++ apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/service/ChallengeContributionService.java
@@ -1,13 +1,33 @@
 package org.sagebionetworks.openchallenges.challenge.service.service;
 
+import java.util.List;
 import org.sagebionetworks.openchallenges.challenge.service.model.dto.ChallengeContributionsPageDto;
+import org.sagebionetworks.openchallenges.challenge.service.model.entity.ChallengeContributionEntity;
+import org.sagebionetworks.openchallenges.challenge.service.model.repository.ChallengeContributionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChallengeContributionService {
 
+  private static final Logger LOG = LoggerFactory.getLogger(ChallengeContributionService.class);
+
+  private final ChallengeContributionRepository challengeContributionRepository;
+
+  public ChallengeContributionService(
+      ChallengeContributionRepository challengeContributionRepository) {
+    this.challengeContributionRepository = challengeContributionRepository;
+  }
+
   public ChallengeContributionsPageDto listChallengeContributions(Long challengeId) {
 
+    LOG.info("listChallengeContributions {}", challengeId);
+
+    List<ChallengeContributionEntity> entities =
+        challengeContributionRepository.findAllByChallengeId(challengeId);
+
+    LOG.info("entities {}", entities);
     return ChallengeContributionsPageDto.builder().build();
   }
 }
