341fa9fee41b59013cde713aa429a3f429594b8d
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-06-27 00:05:27 +0000

    Add urls to the footer
    Co-authored-by: Hayley Sanchez <hayley.sanchez@sagebionetworks.org>
    Co-authored-by: Christine Suver <christine.suver@sagebionetworks.org>

--- libs/openchallenges/ui/src/lib/footer/footer.component.ts
+++ libs/openchallenges/ui/src/lib/footer/footer.component.ts
@@ -1,13 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ConfigService } from '@sagebionetworks/openchallenges/config';
 
 @Component({
   selector: 'openchallenges-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ConfigService],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
   @Input() version = 'x.y.z';
+  privacyPolicyUrl: string;
+  termsOfUseUrl: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.privacyPolicyUrl = configService.config.privacyPolicyUrl;
+    this.termsOfUseUrl = configService.config.termsOfUseUrl;
+  }
 }
