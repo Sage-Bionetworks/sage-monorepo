86f806ad780fea08fbbc8cab8f1e71ce73fd75f4
andrewelamb <andrewelamb@gmail.com>
2023-07-26 17:40:29 +0000

    add endpoint

--- apps/schematic/api/schematic_api/controllers/storage_controller.py
+++ apps/schematic/api/schematic_api/controllers/storage_controller.py
@@ -7,9 +7,31 @@ from typing import Union
 from schematic_api.models.asset_type import AssetType  # noqa: E501
 from schematic_api.models.basic_error import BasicError  # noqa: E501
 from schematic_api.models.datasets_page import DatasetsPage  # noqa: E501
+from schematic_api.models.files_page import FilesPage  # noqa: E501
 from schematic_api.models.manifests_page import ManifestsPage  # noqa: E501
 from schematic_api import util
 from schematic_api.controllers import storage_controller_impl
+
+
+def list_dataset_files(dataset_id, asset_view_id, asset_type, use_full_file_path=None):  # noqa: E501
+    """Gets all files associated with a dataset.
+
+    Gets all files associated with a dataset. # noqa: E501
+
+    :param dataset_id: The ID of a dataset.
+    :type dataset_id: str
+    :param asset_view_id: ID of view listing all project data assets. E.g. for Synapse this would be the Synapse ID of the fileview listing all data assets for a given project
+    :type asset_view_id: str
+    :param asset_type: Type of asset, such as Synapse
+    :type asset_type: dict | bytes
+    :param use_full_file_path: Whether or not to return the full path of output, or just the basename.
+    :type use_full_file_path: bool
+
+    :rtype: Union[FilesPage, Tuple[FilesPage, int], Tuple[FilesPage, int, Dict[str, str]]
+    """
+    if connexion.request.is_json:
+        asset_type =  AssetType.from_dict(connexion.request.get_json())  # noqa: E501
+    return storage_controller_impl.list_dataset_files(dataset_id, asset_view_id, asset_type, use_full_file_path=None)
 
 
 def list_storage_project_datasets(project_id, asset_view_id, asset_type):  # noqa: E501
@@ -27,10 +49,8 @@ def list_storage_project_datasets(project_id, asset_view_id, asset_type):  # noq
     :rtype: Union[DatasetsPage, Tuple[DatasetsPage, int], Tuple[DatasetsPage, int, Dict[str, str]]
     """
     if connexion.request.is_json:
-        asset_type = AssetType.from_dict(connexion.request.get_json())  # noqa: E501
-    return storage_controller_impl.list_storage_project_datasets(
-        project_id, asset_view_id, asset_type
-    )
+        asset_type =  AssetType.from_dict(connexion.request.get_json())  # noqa: E501
+    return storage_controller_impl.list_storage_project_datasets(project_id, asset_view_id, asset_type)
 
 
 def list_storage_project_manifests(project_id, asset_view_id, asset_type):  # noqa: E501
@@ -48,7 +68,5 @@ def list_storage_project_manifests(project_id, asset_view_id, asset_type):  # no
     :rtype: Union[ManifestsPage, Tuple[ManifestsPage, int], Tuple[ManifestsPage, int, Dict[str, str]]
     """
     if connexion.request.is_json:
-        asset_type = AssetType.from_dict(connexion.request.get_json())  # noqa: E501
-    return storage_controller_impl.list_storage_project_manifests(
-        project_id, asset_view_id, asset_type
-    )
+        asset_type =  AssetType.from_dict(connexion.request.get_json())  # noqa: E501
+    return storage_controller_impl.list_storage_project_manifests(project_id, asset_view_id, asset_type)
--- apps/schematic/api/schematic_api/test/__init__.py
+++ apps/schematic/api/schematic_api/test/__init__.py
@@ -7,9 +7,10 @@ from schematic_api.encoder import JSONEncoder
 
 
 class BaseTestCase(TestCase):
+
     def create_app(self):
-        logging.getLogger("connexion.operation").setLevel("ERROR")
-        app = connexion.App(__name__, specification_dir="../openapi/")
+        logging.getLogger('connexion.operation').setLevel('ERROR')
+        app = connexion.App(__name__, specification_dir='../openapi/')
         app.app.json_encoder = JSONEncoder
-        app.add_api("openapi.yaml", pythonic_params=True)
+        app.add_api('openapi.yaml', pythonic_params=True)
         return app.app
