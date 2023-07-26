2efe25f1df2fb617da4d3969190e62d4d015dda4
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-07-26 20:48:17 +0000

    Add debug message

--- apps/schematic/api/prepare-python.sh
+++ apps/schematic/api/prepare-python.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-echo $0
-
 pyenv install --skip-existing 3.10.0
+
+echo "pyenv local"
 pyenv local 3.10.0
+
+echo "poetry env use"
 poetry env use 3.10.0
+
 poetry run pip install "cython<3.0.0"
 poetry run pip install --no-build-isolation pyyaml==5.4.1
 poetry install --with prod,dev
