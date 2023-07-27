7fc471a74a4456b13ca7b80c0a882f1f69f694e0
andrewelamb <andrewelamb@gmail.com>
2023-07-27 16:16:47 +0000

    added two tests

--- apps/schematic/api/schematic_api/test/test_storage_controller_endpoints.py
+++ apps/schematic/api/schematic_api/test/test_storage_controller_endpoints.py
@@ -59,14 +59,11 @@ class TestListDatasetFiles(BaseTestCase):
             return_value=[("id1", "name1"), ("id2", "name2")],
         ) as mock_function:
             url = f"{FILES_URL}?fileNames=file.text"
-            import logging
-            logging.warning(url)
             response = self.client.open(url, method="GET", headers=HEADERS)
             self.assert200(
                 response, f"Response body is : {response.data.decode('utf-8')}"
             )
-            mock_function.assert_called_once_with('id2', 'synapse', ["file.text"], None)
-            assert False
+            mock_function.assert_called_once_with("id2", "synapse", ["file.text"], None)
 
     def test_use_full_file_path(self) -> None:
         """Test with use_full_file_path parameter"""
@@ -81,7 +78,7 @@ class TestListDatasetFiles(BaseTestCase):
             self.assert200(
                 response, f"Response body is : {response.data.decode('utf-8')}"
             )
-            mock_function.assert_called_once_with('id2', 'synapse', None, True)
+            mock_function.assert_called_once_with("id2", "synapse", None, True)
 
     def test_401(self) -> None:
         """Test for 401 result"""
