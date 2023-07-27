c7fa2ae274eb9ca4c30e6d5d8decbe8dedb2cd1e
andrewelamb <andrewelamb@gmail.com>
2023-07-27 17:11:23 +0000

    added new endpoint

--- apps/schematic/api/schematic_api/controllers/storage_controller_impl.py
+++ apps/schematic/api/schematic_api/controllers/storage_controller_impl.py
@@ -10,12 +10,15 @@ from synapseclient.core.exceptions import (  # type: ignore
 from schematic.store.synapse import SynapseStorage  # type: ignore
 from schematic.exceptions import AccessCredentialsError  # type: ignore
 from schematic import CONFIG  # type: ignore
+from schematic.schemas.explorer import SchemaExplorer  # type: ignore
 
 from schematic_api.models.basic_error import BasicError
 from schematic_api.models.dataset import Dataset
 from schematic_api.models.datasets_page import DatasetsPage
 from schematic_api.models.manifests_page import ManifestsPage
 from schematic_api.models.manifest import Manifest
+from schematic_api.models.attributes_page import AttributesPage
+from schematic_api.models.attribute import Attribute
 
 
 def config_handler(asset_view: Optional[str] = None) -> None:
@@ -214,6 +217,58 @@ def list_storage_project_manifests(
         manifests=manifests,
     )
     result: Union[ManifestsPage, BasicError] = page
+    status = 200
+
+    return result, status
+
+
+# schema controller functions -------------------------------------------------
+
+
+def get_component_attributes(schema_url: str, component_label: str) -> list[str]:
+    """Gets the attributes associated with the component
+
+    Args:
+        schema_url (str): _description_
+        component_label (str): _description_
+
+    Returns:
+        list[str]: _description_
+    """
+    schema_explorer = SchemaExplorer()
+    schema_explorer.load_schema(schema_url)
+    return schema_explorer.find_class_specific_properties(component_label)
+
+
+@handle_exceptions
+def list_component_attributes(
+    schema_url: str, component_label: str
+) -> tuple[Union[AttributesPage, BasicError], int]:
+    """Lists the attributes associated with the component
+
+    Args:
+        schema_url (str): _description_
+        component_label (str): _description_
+
+    Returns:
+        tuple[Union[AttributesPage, BasicError], int]: _description_
+    """
+
+    attributes = [
+        Attribute(attribute)
+        for attribute in get_component_attributes(schema_url, component_label)
+    ]
+
+    page = AttributesPage(
+        number=0,
+        size=100,
+        total_elements=len(attributes),
+        total_pages=1,
+        has_next=False,
+        has_previous=False,
+        attributes=attributes,
+    )
+    result: Union[AttributesPage, BasicError] = page
     status = 200
 
     return result, status
