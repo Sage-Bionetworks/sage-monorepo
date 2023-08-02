009fd390ef9719a1b5bba6a8e926bc73e98fe5c3
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-08-02 02:40:32 +0000

    Add script to prepare R environments

---
+++ tools/prepare-r-environments.js
@@ -0,0 +1,61 @@
+// For each Python project in the workspace, this script checks if their dependencies, as defined in
+// their lock files, have changed since the last git operation. The Nx target `prepare-python` is
+// then run for all the projects that had their dependencies updated.
+
+'use strict';
+
+const fs = require('fs');
+const { spawn } = require('child_process');
+const { getGitDiffFiles } = require('./git-util');
+const { getNxProjects, getNxProjectFiles } = require('./nx-util');
+
+// Returns true if the directory specified includes an renv lock file.
+const isRenvProject = (projectDir) => {
+  const filenames = fs.readdirSync(projectDir);
+  return filenames.includes('renv.lock');
+};
+
+// Returns true if the project dir specified includes an renv lock file that has changed.
+const hasRenvProjectDefinitionChanged = (projectDir, changedFiles) => {
+  if (isRenvProject(projectDir)) {
+    const projectDefinitionPaths = ['renv.lock'].map((filename) => `${projectDir}/${filename}`);
+    if (
+      projectDefinitionPaths.some((projectDefinitionPath) =>
+        changedFiles.includes(projectDefinitionPath)
+      )
+    ) {
+      return true;
+    }
+  }
+  return false;
+};
+
+// Installs the R dependencies of the comma-separated list of projects.
+const prepareREnvironment = (projectNames) => {
+  spawn('nx', ['run-many', '--target=prepare-r', `--projects=${projectNames}`], {
+    stdio: 'inherit',
+  }).on('exit', function (error) {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+  });
+};
+
+console.log('✨ Preparing R environments');
+getGitDiffFiles().then((changedFiles) => {
+  getNxProjects()
+    .then((projects) => {
+      const toUpdate = (project) =>
+      hasRenvProjectDefinitionChanged(project['projectDir'], changedFiles);
+      return projects.filter(toUpdate);
+    })
+    .then((projectsToUpdate) => {
+      // generate a list of comma-separated project names
+      let projectNames = projectsToUpdate.map((project) => project['projectName']);
+      projectNames = projectNames.join(',');
+      if (projectNames) {
+        prepareREnvironment(projectNames);
+      }
+    });
+});
