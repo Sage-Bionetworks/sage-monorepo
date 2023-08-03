d712a7bf880aceafc944f03c4f21b3eba519976c
heimannch <carolina.heimann@gmail.com>
2023-08-03 16:03:30 -0700

    initial changes to make app run with new db

--- R/ici_hazard_ratio_server.R
+++ R/ici_hazard_ratio_server.R
@@ -5,9 +5,6 @@ ici_hazard_ratio_server <- function(
   shiny::moduleServer(
     id,
     function(input, output, session) {
-
-      # ici_data <- load_io_data()
-
       ici_hazard_ratio_main_server(
         "ici_hazard_ratio_main",
         cohort_obj
