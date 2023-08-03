3ad89b55b249ca3c08762fefc5b8b2e612514ba4
andrewelamb <andrewelamb@gmail.com>
2023-08-03 17:28:42 +0000

    add component Display

--- apps/schematic/api/schematic_api/test/test_schema_controller_endpoints.py
+++ apps/schematic/api/schematic_api/test/test_schema_controller_endpoints.py
@@ -13,6 +13,9 @@ HEADERS = {
     "Authorization": "Bearer xxx",
 }
 COMPONENT_ATTRIBUTES_URL = "/api/v1/components/component1/attributes?schemaUrl=url1"
+COMPONENT_VALIDATION_RULES_URL = (
+    "/api/v1/components/component1/validationRules?schemaUrl=url1"
+)
 
 
 class TestComponentAttributes(BaseTestCase):
@@ -56,6 +59,53 @@ class TestComponentAttributes(BaseTestCase):
         ):
             response = self.client.open(
                 COMPONENT_ATTRIBUTES_URL, method="GET", headers=HEADERS
+            )
+            self.assert500(
+                response, f"Response body is : {response.data.decode('utf-8')}"
+            )
+
+
+class TestComponentValidationRules(BaseTestCase):
+    """Test case for component validation rules endpoint"""
+
+    def test_success(self) -> None:
+        """Test for successful result"""
+
+        with patch.object(
+            schematic_api.controllers.schema_controller_impl,
+            "get_component_validation_rules",
+            return_value=["rule1", "rule2"],
+        ) as mock_function:
+            response = self.client.open(
+                COMPONENT_VALIDATION_RULES_URL, method="GET", headers=HEADERS
+            )
+            self.assert200(
+                response, f"Response body is : {response.data.decode('utf-8')}"
+            )
+
+            mock_function.assert_called_once_with("component1", "url1")
+
+            assert not response.json["hasNext"]
+            assert not response.json["hasPrevious"]
+            assert response.json["number"] == 0
+            assert response.json["size"] == 100
+            assert response.json["totalElements"] == 2
+            assert response.json["totalPages"] == 1
+            validation_rules = response.json["validation_rules"]
+            assert len(validation_rules) == 2
+            validation_rule = validation_rules[0]
+            assert list(validation_rule.keys()) == ["name"]
+            assert validation_rule["name"] == "rule1"
+
+    def test_500(self) -> None:
+        """Test for 500 result"""
+        with patch.object(
+            schematic_api.controllers.schema_controller_impl,
+            "get_component_validation_rules",
+            side_effect=TypeError,
+        ):
+            response = self.client.open(
+                COMPONENT_VALIDATION_RULES_URL, method="GET", headers=HEADERS
             )
             self.assert500(
                 response, f"Response body is : {response.data.decode('utf-8')}"
