75e40d4eabe9124252892ed50649726839565f50
Rongrong Chai <73901500+rrchai@users.noreply.github.com>
2023-08-03 19:06:38 -0400

    Update libs/openchallenges/challenge-search/src/lib/challenge-search.component.ts
    Co-authored-by: Thomas Schaffter <thomas.schaffter@gmail.com>

--- libs/openchallenges/challenge-search/src/lib/challenge-search.component.ts
+++ libs/openchallenges/challenge-search/src/lib/challenge-search.component.ts
@@ -549,7 +549,7 @@ export class ChallengeSearchComponent
       .pipe(
         catchError(() => {
           console.error(
-            'Unable to get the image url. Please check the logs of image service'
+            'Unable to get the image url. Please check the logs of the image service.'
           );
           return of({ url: '' });
         })
