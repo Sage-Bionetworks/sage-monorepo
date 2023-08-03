35d5a8f82b12b81ee56f093beb5f93ba3b3ec3b7
Rongrong Chai <73901500+rrchai@users.noreply.github.com>
2023-08-03 12:58:37 -0400

    feat: Implement challenge contributors in Challenge Profile  (#1893)
    * add angular to get contributions
    
    * change orgId t o string
    
    * replace sponsors with contributors
    
    * update org card component
    
    * update some org card layouts
    
    * not show members by default and update org card layouts
    
    * use `grid` on card-group to better fit our cards layout
    
    * hide members
    
    * fix cards layout in home
    
    * add brief description to profile page sections (#2)
    
    * Update libs/openchallenges/challenge/src/lib/challenge-contributors/challenge-contributors.component.html
    
    Co-authored-by: Thomas Schaffter <thomas.schaffter@gmail.com>
    
    * fix grid space for person cards
    
    * fix org-member's space
    
    ---------
    
    Co-authored-by: Verena Chung <9377970+vpchung@users.noreply.github.com>
    Co-authored-by: Thomas Schaffter <thomas.schaffter@gmail.com>

---
+++ libs/openchallenges/challenge/src/lib/challenge-contributors/challenge-contributors.component.ts
@@ -0,0 +1,92 @@
+import { Component, Input, OnInit } from '@angular/core';
+import {
+  Challenge,
+  ChallengeContributionService,
+  Image,
+  ImageAspectRatio,
+  ImageHeight,
+  ImageQuery,
+  ImageService,
+  Organization,
+  OrganizationService,
+} from '@sagebionetworks/openchallenges/api-client-angular';
+import { OrganizationCard } from '@sagebionetworks/openchallenges/ui';
+import { forkJoinConcurrent } from '@sagebionetworks/openchallenges/util';
+import { Observable, forkJoin, of, switchMap } from 'rxjs';
+
+@Component({
+  selector: 'openchallenges-challenge-contributors',
+  templateUrl: './challenge-contributors.component.html',
+  styleUrls: ['./challenge-contributors.component.scss'],
+})
+export class ChallengeContributorsComponent implements OnInit {
+  @Input() challenge!: Challenge;
+  organizationCards$!: Observable<OrganizationCard[]>;
+  constructor(
+    private challengeContributionService: ChallengeContributionService,
+    private organizationService: OrganizationService,
+    private imageService: ImageService
+  ) {}
+
+  ngOnInit() {
+    this.organizationCards$ = this.challengeContributionService
+      .listChallengeContributions(this.challenge.id)
+      .pipe(
+        switchMap((page) =>
+          forkJoinConcurrent(
+            page.challengeContributions.map((contribution) =>
+              this.organizationService.getOrganization(
+                contribution.organizationId.toString()
+              )
+            ),
+            Infinity
+          )
+        ),
+        switchMap((orgs) =>
+          forkJoin({
+            orgs: of(orgs),
+            avatarUrls: forkJoinConcurrent(
+              orgs.map((org) => this.getOrganizationAvatarUrl(org)),
+              Infinity
+            ) as unknown as Observable<(Image | undefined)[]>,
+          })
+        ),
+        switchMap(({ orgs, avatarUrls }) =>
+          of(
+            orgs.map((org, index) =>
+              this.getOrganizationCard(org, avatarUrls[index])
+            )
+          )
+        )
+      );
+  }
+
+  // TODO Avoid duplicated code (see org search component)
+  private getOrganizationAvatarUrl(
+    org: Organization
+  ): Observable<Image | undefined> {
+    if (org.avatarKey && org.avatarKey.length > 0) {
+      return this.imageService.getImage({
+        objectKey: org.avatarKey,
+        height: ImageHeight._140px,
+        aspectRatio: ImageAspectRatio._11,
+      } as ImageQuery);
+    } else {
+      return of(undefined);
+    }
+  }
+
+  // TODO Avoid duplicated code (see org search component)
+  private getOrganizationCard(
+    org: Organization,
+    avatarUrl: Image | undefined
+  ): OrganizationCard {
+    return {
+      acronym: org.acronym,
+      avatarUrl: avatarUrl?.url,
+      challengeCount: org.challengeCount,
+      login: org.login,
+      name: org.name,
+    } as OrganizationCard;
+  }
+}
