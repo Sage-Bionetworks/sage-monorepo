48b2a9f127ac98c868972d6474b91df60fb12701
Rongrong Chai <rchai@sagebase.org>
2023-08-04 19:35:22 +0000

    generate initial challenge_cn over time

--- libs/openchallenges/home/src/lib/statistics-viewer/statistics-viewer.component.ts
+++ libs/openchallenges/home/src/lib/statistics-viewer/statistics-viewer.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  Challenge,
+  ChallengeService,
+} from '@sagebionetworks/openchallenges/api-client-angular';
 // import {
 //   ChallengeService,
 //   Image,
@@ -25,34 +29,46 @@ export class StatisticsViewerComponent implements OnInit {
   // challengeCount$: Observable<number> | undefined;
   // orgCount$: Observable<number> | undefined;
 
-  // constructor(
-  //   private organizationService: OrganizationService,
-  //   private challengeService: ChallengeService,
-  //   private imageService: ImageService
-  // ) {}
+  constructor(
+    // private organizationService: OrganizationService,
+    // private imageService: ImageService
+    private challengeService: ChallengeService
+  ) {}
 
   option!: EChartsOption;
-
   ngOnInit() {
     const chartDom = document.getElementById('statistics')!;
     const myChart = echarts.init(chartDom);
-    this.option = {
-      xAxis: {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-      },
-      yAxis: {
-        type: 'value',
-      },
-      series: [
-        {
-          data: [150, 230, 224, 218, 135, 147, 260],
-          type: 'line',
-        },
-      ],
-    };
 
-    this.option && myChart.setOption(this.option);
+    this.challengeService
+      .listChallenges({ pageSize: 1000 })
+      .subscribe((page) => {
+        const dataByYear = this.processData(page.challenges);
+
+        console.log(dataByYear);
+        this.option = {
+          title: {
+            text: 'Growth of Challenges',
+            left: 'center',
+          },
+          xAxis: {
+            type: 'category',
+            data: dataByYear.years,
+          },
+          yAxis: {
+            type: 'value',
+          },
+          series: [
+            {
+              data: dataByYear.cumulativeChallengeCounts,
+              type: 'line',
+            },
+          ],
+        };
+
+        this.option && myChart.setOption(this.option);
+      });
+
     // this.challengeImg$ = this.imageService.getImage({
     //   objectKey: 'home-challenges.svg',
     //   height: this.imgHeight,
@@ -77,5 +93,34 @@ export class StatisticsViewerComponent implements OnInit {
     //     pageSize: 1,
     //   })
     //   .pipe(map((page) => page.totalElements));
+  }
+
+  private processData(challenges: Challenge[]): {
+    years: string[];
+    cumulativeChallengeCounts: number[];
+  } {
+    const dataByYear: { [year: string]: number } = {};
+    const cumulativeChallengeCounts: number[] = [];
+
+    challenges.forEach((challenge) => {
+      const startYear = new Date(challenge.startDate as string)
+        .getFullYear()
+        .toString();
+      dataByYear[startYear] = (dataByYear[startYear] || 0) + 1;
+    });
+
+    const years = Object.keys(dataByYear);
+    years.sort(); // Sort years in ascending order
+
+    let cumulativeSum = 0;
+    years.forEach((year) => {
+      cumulativeSum += dataByYear[year];
+      cumulativeChallengeCounts.push(cumulativeSum);
+    });
+
+    return {
+      years,
+      cumulativeChallengeCounts,
+    };
   }
 }
