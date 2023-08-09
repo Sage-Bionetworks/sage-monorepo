7db010ebbb4936f7556b272e2c2811449a7054e2
verena <9377970+vpchung@users.noreply.github.com>
2023-08-09 09:50:53 +0000

    add text to truncated challenge desc

--- libs/openchallenges/challenge/src/lib/challenge-overview/challenge-overview.module.ts
+++ libs/openchallenges/challenge/src/lib/challenge-overview/challenge-overview.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiModule } from '@sagebionetworks/openchallenges/ui';
 import { ChallengeOverviewComponent } from './challenge-overview.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [ChallengeOverviewComponent],
-  imports: [CommonModule, UiModule],
+  imports: [CommonModule, MatIconModule, UiModule],
   exports: [ChallengeOverviewComponent],
 })
 export class ChallengeOverviewModule {}
