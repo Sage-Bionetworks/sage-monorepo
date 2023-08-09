6f31619925b08d961125e2760a7224475a06742d
verena <9377970+vpchung@users.noreply.github.com>
2023-08-09 10:13:30 +0000

    add text to truncated org desc

--- libs/openchallenges/org-profile/src/lib/org-profile-overview/org-profile-overview.module.ts
+++ libs/openchallenges/org-profile/src/lib/org-profile-overview/org-profile-overview.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiModule } from '@sagebionetworks/openchallenges/ui';
 import { OrgProfileOverviewComponent } from './org-profile-overview.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [OrgProfileOverviewComponent],
-  imports: [CommonModule, UiModule],
+  imports: [CommonModule, MatIconModule, UiModule],
   exports: [OrgProfileOverviewComponent],
 })
 export class OrgProfileOverviewModule {}
