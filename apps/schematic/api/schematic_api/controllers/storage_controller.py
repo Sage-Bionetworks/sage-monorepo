681f4864919e5dadae82d1975ad6a5c8cc755349
andrewelamb <andrewelamb@gmail.com>
2023-08-14 08:18:10 -0700

    add new endpoint (#1902)

--- apps/schematic/api/schematic_api/controllers/storage_controller.py
+++ apps/schematic/api/schematic_api/controllers/storage_controller.py
@@ -8,8 +8,26 @@ from schematic_api.models.asset_type import AssetType  # noqa: E501
 from schematic_api.models.basic_error import BasicError  # noqa: E501
 from schematic_api.models.datasets_page import DatasetsPage  # noqa: E501
 from schematic_api.models.manifests_page import ManifestsPage  # noqa: E501
+from schematic_api.models.projects_page import ProjectsPage  # noqa: E501
 from schematic_api import util
 from schematic_api.controllers import storage_controller_impl
+
+
+def list_projects(asset_view_id, asset_type):  # noqa: E501
+    """Gets all storage projects the current user has access to.
+
+    Gets all storage projects the current user has access to. # noqa: E501
+
+    :param asset_view_id: ID of view listing all project data assets. E.g. for Synapse this would be the Synapse ID of the fileview listing all data assets for a given project
+    :type asset_view_id: str
+    :param asset_type: Type of asset, such as Synapse
+    :type asset_type: dict | bytes
+
+    :rtype: Union[ProjectsPage, Tuple[ProjectsPage, int], Tuple[ProjectsPage, int, Dict[str, str]]
+    """
+    if connexion.request.is_json:
+        asset_type = AssetType.from_dict(connexion.request.get_json())  # noqa: E501
+    return storage_controller_impl.list_projects(asset_view_id, asset_type)
 
 
 def list_storage_project_datasets(project_id, asset_view_id, asset_type):  # noqa: E501
--- apps/schematic/api/schematic_api/models/__init__.py
+++ apps/schematic/api/schematic_api/models/__init__.py
@@ -12,3 +12,6 @@ from schematic_api.models.manifest import Manifest
 from schematic_api.models.manifests_page import ManifestsPage
 from schematic_api.models.manifests_page_all_of import ManifestsPageAllOf
 from schematic_api.models.page_metadata import PageMetadata
+from schematic_api.models.project import Project
+from schematic_api.models.projects_page import ProjectsPage
+from schematic_api.models.projects_page_all_of import ProjectsPageAllOf
