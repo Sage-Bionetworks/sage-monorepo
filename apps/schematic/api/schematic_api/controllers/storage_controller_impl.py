91cdfaec12285360643f1b072fb83400da4464a0
andrewelamb <andrewelamb@gmail.com>
2023-08-14 10:14:56 -0700

    feat: Add /components/{componentLabel}/attributes endpoint (#1903)
    * add endpoint
    
    * filled in docstrings
    
    * fix backward arguments
    
    * moved schemaUrl from path to query folder
    
    * fix c+p error
    
    * handle merge conflict
    
    * fix typo

--- apps/schematic/api/schematic_api/controllers/storage_controller_impl.py
+++ apps/schematic/api/schematic_api/controllers/storage_controller_impl.py
@@ -1,14 +1,8 @@
 """Implementation of all endpoints"""
-from typing import Optional, Union, Callable, Any
+from typing import Optional, Union, Callable
 from flask import request  # type: ignore
 
-from synapseclient.core.exceptions import (  # type: ignore
-    SynapseNoCredentialsError,
-    SynapseAuthenticationError,
-)
-
 from schematic.store.synapse import SynapseStorage  # type: ignore
-from schematic.exceptions import AccessCredentialsError  # type: ignore
 from schematic import CONFIG  # type: ignore
 
 from schematic_api.models.basic_error import BasicError
@@ -16,6 +10,7 @@ from schematic_api.models.dataset import Dataset
 from schematic_api.models.datasets_page import DatasetsPage
 from schematic_api.models.manifests_page import ManifestsPage
 from schematic_api.models.manifest import Manifest
+from schematic_api.controllers.utils import handle_exceptions
 from schematic_api.models.project import Project
 from schematic_api.models.projects_page import ProjectsPage
 
@@ -31,46 +26,6 @@ def get_access_token() -> Optional[str]:
         if auth_header.startswith("Bearer "):
             bearer_token = auth_header.split(" ")[1]
     return bearer_token
-
-
-def handle_exceptions(endpoint_function: Callable) -> Callable:
-    """
-    This is designed to be used as a decorator for endpoint functions.
-    The endpoint function is called in a try block, and then various
-      Synapse and Schematic exceptions are handled and returned as the
-      BasicError object.
-
-    Args:
-        f (Callable): A function that calls the input function
-    """
-
-    def func(*args: Any, **kwargs: Any) -> tuple[Union[Any, BasicError], int]:
-        try:
-            return endpoint_function(*args, **kwargs)
-
-        except SynapseNoCredentialsError as error:
-            status = 401
-            res = BasicError(
-                "Missing or invalid Synapse credentials error", status, str(error)
-            )
-            return res, status
-
-        except SynapseAuthenticationError as error:
-            status = 401
-            res = BasicError("Forbidden Synapse access error", status, str(error))
-            return res, status
-
-        except AccessCredentialsError as error:
-            status = 403
-            res = BasicError("Synapse entity access error", status, str(error))
-            return res, status
-
-        except Exception as error:  # pylint: disable=broad-exception-caught
-            status = 500
-            res = BasicError("Internal error", status, str(error))
-            return res, status
-
-    return func
 
 
 def get_asset_storage_class(asset_type: str) -> Callable:
