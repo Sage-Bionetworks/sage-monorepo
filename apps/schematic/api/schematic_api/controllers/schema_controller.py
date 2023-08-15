6f9450dd2093cdcb2240e0448eb024fe37544a70
andrewelamb <andrewelamb@gmail.com>
2023-08-15 17:19:54 +0000

    add new endpoint

--- apps/schematic/api/schematic_api/controllers/schema_controller.py
+++ apps/schematic/api/schematic_api/controllers/schema_controller.py
@@ -11,6 +11,27 @@ from schematic_api import util
 from schematic_api.controllers import schema_controller_impl
 
 
+def component_label(
+    component_display, schema_url, use_strict_camel_case=None
+):  # noqa: E501
+    """Gets the label of the component
+
+    Gets the label of the component # noqa: E501
+
+    :param component_display: The display name of a component in a schema
+    :type component_display: str
+    :param schema_url: The URL of a schema in jsonld form
+    :type schema_url: str
+    :param use_strict_camel_case: Whether or not to use the more strict way of converting to camel case
+    :type use_strict_camel_case: bool
+
+    :rtype: Union[str, Tuple[str, int], Tuple[str, int, Dict[str, str]]
+    """
+    return schema_controller_impl.component_label(
+        component_display, schema_url, use_strict_camel_case=None
+    )
+
+
 def list_component_attributes(component_label, schema_url):  # noqa: E501
     """Gets attributes associated with a given component
 
