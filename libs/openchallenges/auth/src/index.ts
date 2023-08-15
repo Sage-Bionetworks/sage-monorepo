e21242deb7a140c8847d70e1d20d63eae4dbdf8a
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-08-15 19:22:39 +0000

    Remove modules from openchallenges-auth

--- libs/openchallenges/auth/src/index.ts
+++ libs/openchallenges/auth/src/index.ts
@@ -1,7 +1,4 @@
-export * from './lib/auth.module';
-
 export * from './lib/auth.service';
-export * from './lib/kauth.service';
-
 export * from './lib/guard/kauth.guard';
+export * from './lib/kauth.service';
 export * from './lib/token/token.service';
