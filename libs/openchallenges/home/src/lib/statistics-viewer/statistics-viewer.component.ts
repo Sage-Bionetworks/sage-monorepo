71bf6833ef36662e06889a98ba363eef23c1aec8
Rongrong Chai <rchai@sagebase.org>
2023-08-23 23:13:21 +0000

    make an example plot

--- libs/openchallenges/home/src/lib/statistics-viewer/statistics-viewer.component.ts
+++ libs/openchallenges/home/src/lib/statistics-viewer/statistics-viewer.component.ts
@@ -1,64 +1,173 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import {
+  Component,
+  Inject,
+  // OnInit,
+  NgZone,
+  PLATFORM_ID,
+  OnDestroy,
+} from '@angular/core';
+import {
+  Challenge,
   ChallengeService,
-  Image,
-  ImageHeight,
-  ImageService,
-  OrganizationService,
 } from '@sagebionetworks/openchallenges/api-client-angular';
-import { CountUpModule } from 'ngx-countup';
-import { Observable, map } from 'rxjs';
+import { HomeDataService } from '../home-data-service';
+
+// amCharts imports
+import * as am5 from '@amcharts/amcharts5';
+import * as am5xy from '@amcharts/amcharts5/xy';
+import am5ThemesAnimated from '@amcharts/amcharts5/themes/Animated';
 
 @Component({
   selector: 'openchallenges-statistics-viewer',
   standalone: true,
-  imports: [CommonModule, CountUpModule],
+  imports: [CommonModule],
   templateUrl: './statistics-viewer.component.html',
   styleUrls: ['./statistics-viewer.component.scss'],
 })
-export class StatisticsViewerComponent implements OnInit {
-  challengeImg$: Observable<Image> | undefined;
-  orgImg$: Observable<Image> | undefined;
-  userImg$: Observable<Image> | undefined;
-
-  private imgHeight = ImageHeight._140px;
-
-  challengeCount$: Observable<number> | undefined;
-  orgCount$: Observable<number> | undefined;
-
+export class StatisticsViewerComponent implements OnDestroy {
   constructor(
-    private imageService: ImageService,
     private challengeService: ChallengeService,
-    private organizationService: OrganizationService
+    private homeDataService: HomeDataService,
+    @Inject(PLATFORM_ID) private platformId: any,
+    private zone: NgZone
   ) {}
 
-  ngOnInit() {
-    this.challengeImg$ = this.imageService.getImage({
-      objectKey: 'home-challenges.svg',
-      height: this.imgHeight,
+  // ngOnInit() {
+  //   this.homeDataService
+  //     .getAllChallenges()
+  //     .subscribe((challenges) => console.log(challenges));
+  // }
+
+  private root!: am5.Root;
+
+  // Run the function only in the browser
+  browserOnly(f: () => void) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.zone.runOutsideAngular(() => {
+        f();
+      });
+    }
+  }
+
+  ngAfterViewInit() {
+    // Chart code goes in here
+    this.browserOnly(() => {
+      const root = am5.Root.new('chartdiv');
+
+      root.setThemes([am5ThemesAnimated.new(root)]);
+
+      const chart = root.container.children.push(
+        am5xy.XYChart.new(root, {
+          panY: false,
+          layout: root.verticalLayout,
+        })
+      );
+
+      // Define data
+      const data = [
+        {
+          category: 'Research',
+          value1: 1000,
+          value2: 588,
+        },
+        {
+          category: 'Marketing',
+          value1: 1200,
+          value2: 1800,
+        },
+        {
+          category: 'Sales',
+          value1: 850,
+          value2: 1230,
+        },
+      ];
+
+      // Create Y-axis
+      const yAxis = chart.yAxes.push(
+        am5xy.ValueAxis.new(root, {
+          renderer: am5xy.AxisRendererY.new(root, {}),
+        })
+      );
+
+      // Create X-Axis
+      const xAxis = chart.xAxes.push(
+        am5xy.CategoryAxis.new(root, {
+          renderer: am5xy.AxisRendererX.new(root, {}),
+          categoryField: 'category',
+        })
+      );
+      xAxis.data.setAll(data);
+
+      // Create series
+      const series1 = chart.series.push(
+        am5xy.ColumnSeries.new(root, {
+          name: 'Series',
+          xAxis,
+          yAxis,
+          valueYField: 'value1',
+          categoryXField: 'category',
+        })
+      );
+      series1.data.setAll(data);
+
+      const series2 = chart.series.push(
+        am5xy.ColumnSeries.new(root, {
+          name: 'Series',
+          xAxis,
+          yAxis,
+          valueYField: 'value2',
+          categoryXField: 'category',
+        })
+      );
+      series2.data.setAll(data);
+
+      // Add legend
+      const legend = chart.children.push(am5.Legend.new(root, {}));
+      legend.data.setAll(chart.series.values);
+
+      // Add cursor
+      chart.set('cursor', am5xy.XYCursor.new(root, {}));
+
+      this.root = root;
     });
-    this.orgImg$ = this.imageService.getImage({
-      objectKey: 'home-hosts.svg',
-      height: this.imgHeight,
+  }
+
+  ngOnDestroy() {
+    // Clean up chart when the component is removed
+    this.browserOnly(() => {
+      if (this.root) {
+        this.root.dispose();
+      }
     });
-    this.userImg$ = this.imageService.getImage({
-      objectKey: 'home-users.svg',
-      height: this.imgHeight,
+  }
+
+  private processData(challenges: Challenge[]): {
+    years: string[];
+    cumulativeChallengeCounts: number[];
+  } {
+    const dataByYear: { [year: string]: number } = {};
+    const cumulativeChallengeCounts: number[] = [];
+
+    challenges.forEach((challenge) => {
+      const startYear = new Date(challenge.startDate as string)
+        .getFullYear()
+        .toString();
+      dataByYear[startYear] = (dataByYear[startYear] || 0) + 1;
     });
 
-    this.challengeCount$ = this.challengeService
-      .listChallenges({
-        pageNumber: 1,
-        pageSize: 1,
-      })
-      .pipe(map((page) => page.totalElements));
+    const years = Object.keys(dataByYear);
+    years.sort(); // Sort years in ascending order
 
-    this.orgCount$ = this.organizationService
-      .listOrganizations({
-        pageNumber: 1,
-        pageSize: 1,
-      })
-      .pipe(map((page) => page.totalElements));
+    let cumulativeSum = 0;
+    years.forEach((year) => {
+      cumulativeSum += dataByYear[year];
+      cumulativeChallengeCounts.push(cumulativeSum);
+    });
+
+    return {
+      years,
+      cumulativeChallengeCounts,
+    };
   }
 }
