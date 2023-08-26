5a2f8c012f63796c4d813db8002ebc4f52e94ddb
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-08-26 16:40:51 +0000

    Run OA generator

---
+++ apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/api/ChallengeAnalyticsApiDelegate.java
@@ -0,0 +1,51 @@
+package org.sagebionetworks.openchallenges.challenge.service.api;
+
+import java.util.Optional;
+import javax.annotation.Generated;
+import org.sagebionetworks.openchallenges.challenge.service.model.dto.ChallengesPerYearDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.NativeWebRequest;
+
+/**
+ * A delegate to be called by the {@link ChallengeAnalyticsApiController}}. Implement this interface
+ * with a {@link org.springframework.stereotype.Service} annotated class.
+ */
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen")
+public interface ChallengeAnalyticsApiDelegate {
+
+  default Optional<NativeWebRequest> getRequest() {
+    return Optional.empty();
+  }
+
+  /**
+   * GET /challengeAnalytics/challengesPerYear : Get the number of challenges started per year
+   * Returns the number of challenges started per year
+   *
+   * @return An object (status code 200) or The request cannot be fulfilled due to an unexpected
+   *     server error (status code 500)
+   * @see ChallengeAnalyticsApi#getChallengesPerYear
+   */
+  default ResponseEntity<ChallengesPerYearDto> getChallengesPerYear() {
+    getRequest()
+        .ifPresent(
+            request -> {
+              for (MediaType mediaType : MediaType.parseMediaTypes(request.getHeader("Accept"))) {
+                if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
+                  String exampleString =
+                      "{ \"challengeCounts\" : [ 0, 0 ], \"years\" : [ \"years\", \"years\" ] }";
+                  ApiUtil.setExampleResponse(request, "application/json", exampleString);
+                  break;
+                }
+                if (mediaType.isCompatibleWith(MediaType.valueOf("application/problem+json"))) {
+                  String exampleString =
+                      "Custom MIME type example not yet supported: application/problem+json";
+                  ApiUtil.setExampleResponse(request, "application/problem+json", exampleString);
+                  break;
+                }
+              }
+            });
+    return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+  }
+}
