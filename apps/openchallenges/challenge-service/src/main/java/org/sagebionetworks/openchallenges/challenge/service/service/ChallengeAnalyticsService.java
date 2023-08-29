e2d64252c4d068c6cf47b9e4df90ed27587473e7
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-08-29 23:46:48 +0000

    Update API Gateway security

--- apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/service/ChallengeAnalyticsService.java
+++ apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/service/ChallengeAnalyticsService.java
@@ -11,8 +11,12 @@ public class ChallengeAnalyticsService {
 
   @Transactional(readOnly = true)
   public ChallengesPerYearDto getChallengesPerYear() {
-    List<String> years = Arrays.asList("2010", "2011");
-    List<Integer> challengeCounts = Arrays.asList(100, 200);
+    List<String> years =
+        Arrays.asList(
+            "1969", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020",
+            "2021", "2022", "2023");
+    List<Integer> challengeCounts =
+        Arrays.asList(11, 12, 18, 22, 28, 35, 44, 69, 80, 99, 117, 159, 181, 199);
 
     return ChallengesPerYearDto.builder().years(years).challengeCounts(challengeCounts).build();
   }
