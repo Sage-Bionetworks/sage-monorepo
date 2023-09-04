e7596075f898120a0c15710ece491376923a425e
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-09-04 21:14:26 +0000

    Regenerate image service

--- apps/openchallenges/image-service/tools/pull-templates.sh
+++ apps/openchallenges/image-service/tools/pull-templates.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-openapiGeneratorVersion="6.1.x"
-destinationFolder="${PWD}/templates/v${openapiGeneratorVersion}"
+openapiGeneratorVersion="v7.0.0"
+destinationFolder="${PWD}/templates/${openapiGeneratorVersion}"
 
 mkdir -p "${destinationFolder}"
 
+curl -O --output-dir "${destinationFolder}" \
+  "https://raw.githubusercontent.com/OpenAPITools/openapi-generator/${openapiGeneratorVersion}/modules/openapi-generator/src/main/resources/JavaSpring/converter.mustache"
 curl -O --output-dir "${destinationFolder}" \
   "https://raw.githubusercontent.com/OpenAPITools/openapi-generator/${openapiGeneratorVersion}/modules/openapi-generator/src/main/resources/JavaSpring/libraries/spring-boot/openapi2SpringBoot.mustache"
 curl -O --output-dir "${destinationFolder}" \
