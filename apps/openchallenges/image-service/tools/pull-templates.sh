c4e01052ff44c8bfb439bccdb951010d4349629a
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-09-05 15:56:27 +0000

    Update tool

--- apps/openchallenges/image-service/tools/pull-templates.sh
+++ apps/openchallenges/image-service/tools/pull-templates.sh
@@ -6,8 +6,4 @@ destinationFolder="${PWD}/templates/${openapiGeneratorVersion}"
 mkdir -p "${destinationFolder}"
 
 curl -O --output-dir "${destinationFolder}" \
-  "https://raw.githubusercontent.com/OpenAPITools/openapi-generator/${openapiGeneratorVersion}/modules/openapi-generator/src/main/resources/JavaSpring/converter.mustache"
-curl -O --output-dir "${destinationFolder}" \
-  "https://raw.githubusercontent.com/OpenAPITools/openapi-generator/${openapiGeneratorVersion}/modules/openapi-generator/src/main/resources/JavaSpring/libraries/spring-boot/openapi2SpringBoot.mustache"
-curl -O --output-dir "${destinationFolder}" \
   "https://raw.githubusercontent.com/OpenAPITools/openapi-generator/${openapiGeneratorVersion}/modules/openapi-generator/src/main/resources/JavaSpring/pojo.mustache"
