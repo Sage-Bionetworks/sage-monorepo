fa6c48bdadd166b25bff40445b7b9d19aca77b11
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-09-06 19:34:06 +0000

    Update data returned by the new endpoint

--- apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/service/ChallengeAnalyticsService.java
+++ apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/service/ChallengeAnalyticsService.java
@@ -15,7 +15,8 @@ public class ChallengeAnalyticsService {
         Arrays.asList(
             "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021",
             "2022", "2023");
-    List<Integer> challengeCounts = Arrays.asList(1, 6, 4, 6, 7, 9, 25, 11, 19, 18, 42, 22, 18);
+    List<Integer> challengeCounts =
+        Arrays.asList(1, 7, 11, 17, 24, 33, 58, 69, 88, 106, 148, 170, 188);
 
     return ChallengesPerYearDto.builder().years(years).challengeCounts(challengeCounts).build();
   }
