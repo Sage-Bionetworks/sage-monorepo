6e120285376877e7ef8999023a99d4f391ef667d
heimannch <carolina.heimann@gmail.com>
2023-09-06 16:44:52 -0700

    first version of single cell immune features (with norm gsea results)

--- R/sc_module_server.R
+++ R/sc_module_server.R
@@ -3,15 +3,15 @@ sc_module_server <- function(id, cohort_obj){
     id,
     function(input, output, session) {
 
-      # sc_bubbleplot_server(
-      #   "sc_bubbleplot",
-      #   cohort_obj
-      # )
-      #
-      # sc_umap_server(
-      #   "sc_umap",
-      #   cohort_obj
-      # )
+      sc_bubbleplot_server(
+        "sc_bubbleplot",
+        cohort_obj
+      )
+
+      sc_umap_server(
+        "sc_umap",
+        cohort_obj
+      )
 
       sc_immune_features_server(
         "sc_distribution",
