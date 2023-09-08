f4cfaeffc379b059552928e60555945a53985f1b
mdsage1 <122999770+mdsage1@users.noreply.github.com>
2023-09-08 15:56:46 -0700

    Update ChallengeApiController.java
    remove changes

--- apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/api/ChallengeApiController.java
+++ apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/api/ChallengeApiController.java
@@ -3,6 +3,7 @@ package org.sagebionetworks.openchallenges.challenge.service.api;
 import java.util.Optional;
 import javax.annotation.Generated;
 import javax.validation.constraints.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,7 +14,7 @@ public class ChallengeApiController implements ChallengeApi {
 
   private final ChallengeApiDelegate delegate;
 
-  public ChallengeApiController(ChallengeApiDelegate delegate) {
+  public ChallengeApiController(@Autowired(required = false) ChallengeApiDelegate delegate) {
     this.delegate = Optional.ofNullable(delegate).orElse(new ChallengeApiDelegate() {});
   }
 
