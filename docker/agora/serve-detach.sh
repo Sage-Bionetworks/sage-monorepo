d3d6ac924c6c1643245887b1fa5a6ab43500bf8d
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-09-08 02:59:24 +0000

    Fix docker

--- docker/agora/serve-detach.sh
+++ docker/agora/serve-detach.sh
@@ -2,11 +2,11 @@
 
 args=(
   # List of services in alphanumeric order
-  --file docker/openchallenges/services/mongo.yml
-  --file docker/openchallenges/services/web-app.yml
+  --file docker/agora/services/mongo.yml
+  # --file docker/agora/services/web-app.yml
 
-  --file docker/openchallenges/networks.yml
-  --file docker/openchallenges/volumes.yml
+  --file docker/agora/networks.yml
+  --file docker/agora/volumes.yml
 
   up $1 --detach --remove-orphans
 )
