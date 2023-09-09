e2faafdc15d821c4bd39b797db5113cd978d536d
mdsage1 <maria.diaz@sagebase.org>
2023-09-09 03:04:41 +0000

    revert keycloak manager

--- apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/configuration/KeycloakManager.java
+++ apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/configuration/KeycloakManager.java
@@ -4,19 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.admin.client.resource.RealmResource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class KeycloakManager {
 
-  private KeycloakManagerProperties keycloakProperties;
+  @Autowired private KeycloakManagerProperties keycloakProperties;
 
   private static Keycloak keycloakInstance = null;
-
-  public KeycloakManager(KeycloakManagerProperties keycloakProperties) {
-    this.keycloakProperties = keycloakProperties;
-  }
 
   public RealmResource getKeycloakInstanceWithRealm() {
     return getInstance().realm(keycloakProperties.getRealm());
