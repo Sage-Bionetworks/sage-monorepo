2475d0b10cfa0333dc2ba1f8493b826a0d53da5d
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-09-15 22:14:07 +0000

    Cleanup app.component.ts

--- apps/openchallenges/app/src/app/app.config.ts
+++ apps/openchallenges/app/src/app/app.config.ts
@@ -1,32 +1,16 @@
-import {
-  ApplicationConfig,
-  APP_INITIALIZER,
-  importProvidersFrom,
-} from '@angular/core';
+import { ApplicationConfig, APP_INITIALIZER } from '@angular/core';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
   withInMemoryScrolling,
 } from '@angular/router';
-
-import { SharedUtilModule } from '@sagebionetworks/shared/util';
-import { MatLegacyProgressSpinnerModule as MatProgressSpinnerModule } from '@angular/material/legacy-progress-spinner';
-import { KeycloakAngularModule } from 'keycloak-angular';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import {
   withInterceptorsFromDi,
   provideHttpClient,
 } from '@angular/common/http';
-import { CountUpModule } from 'ngx-countup';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import {
-  BrowserModule,
-  provideClientHydration,
-} from '@angular/platform-browser';
-import {
-  BASE_PATH as API_CLIENT_BASE_PATH,
-  ApiModule,
-} from '@sagebionetworks/openchallenges/api-client-angular';
+import { provideClientHydration } from '@angular/platform-browser';
+import { BASE_PATH as API_CLIENT_BASE_PATH } from '@sagebionetworks/openchallenges/api-client-angular';
 import {
   configFactory,
   ConfigService,
@@ -35,20 +19,10 @@ import {
 import { routes } from './app.routes';
 
 // This index is used to remove the corresponding provider in app.config.server.ts.
-export const APP_BASE_URL_PROVIDER_INDEX = 1;
+export const APP_BASE_URL_PROVIDER_INDEX = 0;
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      BrowserModule.withServerTransition({ appId: 'openchallenges' }),
-      ApiModule,
-      CountUpModule,
-      MatButtonModule,
-      KeycloakAngularModule,
-      // AuthModule.forRoot(),
-      MatProgressSpinnerModule,
-      SharedUtilModule
-    ),
     {
       // This provider must be specified at the index defined by APP_BASE_URL_PROVIDER_INDEX.
       provide: 'APP_BASE_URL',
@@ -61,12 +35,6 @@ export const appConfig: ApplicationConfig = {
       deps: [ConfigService],
       multi: true,
     },
-    // {
-    //   provide: APP_INITIALIZER,
-    //   useFactory: initializeKeycloakFactory,
-    //   multi: true,
-    //   deps: [ConfigService, KeycloakService, PLATFORM_ID],
-    // },
     {
       provide: API_CLIENT_BASE_PATH,
       useFactory: (configService: ConfigService) => configService.config.apiUrl,
