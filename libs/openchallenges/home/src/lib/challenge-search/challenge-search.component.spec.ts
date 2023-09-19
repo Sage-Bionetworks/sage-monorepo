49362e5adc55477a4c12d965d7fd44f650198114
verena <9377970+vpchung@users.noreply.github.com>
2023-09-19 20:37:11 +0000

    fix test re: missing ResizeObserver polyfill

--- libs/openchallenges/home/src/lib/challenge-search/challenge-search.component.spec.ts
+++ libs/openchallenges/home/src/lib/challenge-search/challenge-search.component.spec.ts
@@ -4,6 +4,13 @@ import { ConfigService } from '@sagebionetworks/openchallenges/config';
 
 import { ChallengeSearchComponent } from './challenge-search.component';
 
+// import ResizeObserver polyfill
+global.ResizeObserver = jest.fn(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+}));
+
 describe('ChallengeSearchComponent', () => {
   let component: ChallengeSearchComponent;
   let fixture: ComponentFixture<ChallengeSearchComponent>;
