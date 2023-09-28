22fdb319c2c4c9f32cdd5140a8483be5471a59af
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-09-28 03:02:44 +0000

    Remove unused factory

--- apps/openchallenges/app/src/app/google-tag-manager/google-tag-manager-id.factory.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigService } from '@sagebionetworks/openchallenges/config';
-
-export const googleTagManagerIdFactory = (
-  configService: ConfigService
-): string => {
-  console.log(`GTM ID in Factory: ${configService.config.googleTagManagerId}`);
-  return configService.config.googleTagManagerId;
-};
