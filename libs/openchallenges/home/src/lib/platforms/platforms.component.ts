45b5a7d3c010b7f0e5dc2e6824a88880ffe1d012
Verena Chung <9377970+vpchung@users.noreply.github.com>
2023-10-02 13:12:28 -0700

    feat(ui): add platform logos to landing page (#2148)
    * add new section under Organizations
    
    * update text; update layout
    
    * typo
    
    * remove platforms section from challenge-host-list
    
    * add new platform component
    
    * update padding
    
    * update platform count
    
    * add svg of platform logos
    
    * add responsiveness
    
    * add bold emphasis on section titles
    
    * add CodaLab as platform
    
    * text update
    
    * remove OpenML and PapersWithCode as challenge platforms
    
    * remove subtexts
    
    * move list of platform logos to s3
    
    * fix test
    
    * add sub-texts back in
    
    * update spacing
    
    * update description
    
    * fix layout
    
    * use png image instead of svg
    
    * update description pt 2

---
+++ libs/openchallenges/home/src/lib/platforms/platforms.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  Image,
+  ImageService,
+} from '@sagebionetworks/openchallenges/api-client-angular';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'openchallenges-platforms',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './platforms.component.html',
+  styleUrls: ['./platforms.component.scss'],
+})
+export class PlatformsComponent implements OnInit {
+  public platforms$: Observable<Image> | undefined;
+
+  constructor(private imageService: ImageService) {}
+
+  ngOnInit() {
+    this.platforms$ = this.imageService.getImage({
+      objectKey: 'logo/platforms.png',
+    });
+  }
+}
