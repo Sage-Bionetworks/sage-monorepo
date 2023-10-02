0f0a1f9ddd925bfb5d21e6bd37187d46fb7b7bb0
Rongrong Chai <rchai@sagebase.org>
2023-10-02 14:46:00 +0000

    update plot data with latest database

--- apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/service/ChallengeAnalyticsService.java
+++ apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/service/ChallengeAnalyticsService.java
@@ -13,10 +13,10 @@ public class ChallengeAnalyticsService {
   public ChallengesPerYearDto getChallengesPerYear() {
     List<String> years =
         Arrays.asList(
-            "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017",
-            "2018", "2019", "2020", "2021", "2022", "2023");
+            "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", 
+            "2018", "2019", "2020", "2021", "2022", "2023");         
     List<Integer> challengeCounts =
-        Arrays.asList(5, 8, 12, 16, 21, 27, 31, 37, 44, 53, 79, 90, 109, 127, 174, 198, 219);
+        Arrays.asList(5, 8, 12, 16, 21, 27, 31, 38, 45, 54, 80, 91, 110, 129, 177, 203, 226);
 
     return ChallengesPerYearDto.builder().years(years).challengeCounts(challengeCounts).build();
   }
