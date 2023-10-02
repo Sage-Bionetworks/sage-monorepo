74d3380e6a9c4c9587cf7d8a2326b0f63552bfd6
verena <9377970+vpchung@users.noreply.github.com>
2023-10-02 19:01:15 +0000

    use png image instead of svg

--- libs/openchallenges/home/src/lib/platforms/platforms.component.ts
+++ libs/openchallenges/home/src/lib/platforms/platforms.component.ts
@@ -20,7 +20,7 @@ export class PlatformsComponent implements OnInit {
 
   ngOnInit() {
     this.platforms$ = this.imageService.getImage({
-      objectKey: 'logo/platforms.svg',
+      objectKey: 'logo/platforms.png',
     });
   }
 }
