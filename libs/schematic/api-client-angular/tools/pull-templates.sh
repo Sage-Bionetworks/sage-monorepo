3cff745001c266f70e95b6e03d1f136850f9a819
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-10-31 11:41:23 -0700

    chore(schematic): remove `schematic-app` and `schematic-api-client-angular` (#2293)
    * Remove schematic-app and schematic-api-client-angular
    
    * Update `tsconfig.base.json`

--- libs/schematic/api-client-angular/tools/pull-templates.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-openapiGeneratorVersion="6.1.x"
-destinationFolder="${PWD}/templates/v${openapiGeneratorVersion}"
-
-mkdir -p "${destinationFolder}"
-
-curl -O --output-dir "${destinationFolder}" \
-  "https://raw.githubusercontent.com/OpenAPITools/openapi-generator/${openapiGeneratorVersion}/modules/openapi-generator/src/main/resources/typescript-angular/api.module.mustache"
