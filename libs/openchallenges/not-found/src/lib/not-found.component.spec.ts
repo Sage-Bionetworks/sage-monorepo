0c7301854cd806e3f08bff16ee07e2e790b258ed
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-11-01 21:08:39 +0000

    Fix tests for the About and Not Found pages

--- libs/openchallenges/not-found/src/lib/not-found.component.spec.ts
+++ libs/openchallenges/not-found/src/lib/not-found.component.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ConfigService } from '@sagebionetworks/openchallenges/config';
 
 import { NotFoundComponent } from './not-found.component';
@@ -11,7 +12,7 @@ describe('NotFoundComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientModule, NotFoundComponent],
+      imports: [HttpClientModule, RouterTestingModule, NotFoundComponent],
       providers: [ConfigService],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
