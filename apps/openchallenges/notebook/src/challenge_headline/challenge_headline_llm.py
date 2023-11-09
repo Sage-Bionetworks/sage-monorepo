a984374fddd308f1458151534f4d5aceddf5336b
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-11-09 00:00:25 +0000

    Experimenting

--- apps/openchallenges/notebook/src/challenge_headline/challenge_headline_llm.py
+++ apps/openchallenges/notebook/src/challenge_headline/challenge_headline_llm.py
@@ -80,8 +80,11 @@ textgen_llm = Bedrock(
 
 def generate_challenge_headlines(text, num_headlines):
     prompt = (
-        f"Please generate {num_headlines} headlines that have a maximum ten words from the "
-        "following challenge description. The headline must summarize the goal of the challenge. "
+        f"Please generate {num_headlines} headlines that have less than 100 characters from the "
+        "following challenge description. The headlines must summarize the goal of the challenge. "
+        "The headlines must not include the name of the challenge. "
+        "The headlines must reads naturally and effectively communicates the topic of the page "
+        "content. "
         f"Description: \n{text}"
     )
     response = Bedrock(
@@ -118,7 +121,7 @@ def process_challenge(challenge):
     return obj
 
 
-challenge_headlines = list(map(process_challenge, challenges))
+challenge_headlines = list(map(process_challenge, challenges[:20]))
 
 
 # SAVE OUTPUT TO FILE
