6b6c9f3987b7fda5c263f83d895eaca839d0b3f2
mdsage1 <maria.diaz@sagebase.org>
2023-11-10 18:44:57 +0000

    formatting

--- apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/service/OrganizationServiceTest.java
+++ apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/service/OrganizationServiceTest.java
@@ -1,41 +1,27 @@
 package org.sagebionetworks.openchallenges.organization.service.service;
 
-
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
-import org.sagebionetworks.openchallenges.organization.service.model.entity.OrganizationEntity;
-import org.sagebionetworks.openchallenges.organization.service.model.repository.OrganizationRepository;
-import org.sagebionetworks.openchallenges.organization.service.service.OrganizationService;
-import org.sagebionetworks.openchallenges.organization.service.exception.OrganizationNotFoundException;
-import org.sagebionetworks.openchallenges.organization.service.model.dto.OrganizationDto;
-import org.sagebionetworks.openchallenges.organization.service.model.dto.OrganizationsPageDto;
-import static org.mockito.ArgumentMatchers.any;
-import org.sagebionetworks.openchallenges.organization.service.model.dto.OrganizationSearchQueryDto;
-import org.sagebionetworks.openchallenges.organization.service.model.mapper.OrganizationMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
+import static org.mockito.Mockito.when;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.sagebionetworks.openchallenges.organization.service.exception.OrganizationNotFoundException;
+import org.sagebionetworks.openchallenges.organization.service.model.dto.OrganizationSearchQueryDto;
+import org.sagebionetworks.openchallenges.organization.service.model.entity.OrganizationEntity;
+import org.sagebionetworks.openchallenges.organization.service.model.repository.OrganizationRepository;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
 
 @DataJpaTest
 public class OrganizationServiceTest {
-  @Mock
-  private OrganizationRepository organizationRepository;
+  @Mock private OrganizationRepository organizationRepository;
   private OrganizationService organizationService;
 
   @BeforeEach
@@ -50,18 +36,18 @@ public class OrganizationServiceTest {
     Long id = 1L;
     String login = "dream";
     String altId = "75";
-    
 
     OrganizationEntity organizationEntity = new OrganizationEntity();
 
     // Call the findByIdOrLogin method
     Optional<OrganizationEntity> entityResult = organizationRepository.findByIdOrLogin(id, login);
-    
-    //Create query to pass to list organizations
+
+    // Create query to pass to list organizations
     OrganizationSearchQueryDto query = new OrganizationSearchQueryDto();
     Page<OrganizationEntity> organizationEntityPage = mock(Page.class);
-    List<OrganizationEntity> organizationEntities = Collections.singletonList(new OrganizationEntity());
-    //Create mock results for the org entitites
+    List<OrganizationEntity> organizationEntities =
+        Collections.singletonList(new OrganizationEntity());
+    // Create mock results for the org entitites
     when(organizationEntityPage.getContent()).thenReturn(organizationEntities);
     when(organizationEntityPage.getNumber()).thenReturn(0);
     when(organizationEntityPage.getSize()).thenReturn(10);
@@ -70,11 +56,13 @@ public class OrganizationServiceTest {
     when(organizationEntityPage.hasNext()).thenReturn(false);
     when(organizationEntityPage.hasPrevious()).thenReturn(false);
 
-    //call methods and assert results match
-    assertThrows(OrganizationNotFoundException.class, () ->  organizationService.getOrganization(login));
+    // call methods and assert results match
+    assertThrows(
+        OrganizationNotFoundException.class, () -> organizationService.getOrganization(login));
     assertThrows(NullPointerException.class, () -> organizationService.listOrganizations(query));
-    assertThrows(OrganizationNotFoundException.class, () ->  organizationService.getOrganization(altId));
-    //assertThat(result.getOrganizations().size()).isEqualTo(1);
+    assertThrows(
+        OrganizationNotFoundException.class, () -> organizationService.getOrganization(altId));
+    // assertThat(result.getOrganizations().size()).isEqualTo(1);
     assertThat(organizationEntityPage.getNumber()).isEqualTo(0);
     assertThat(organizationEntityPage.getSize()).isEqualTo(10);
     assertThat(organizationEntityPage.getTotalElements()).isEqualTo(1L);
