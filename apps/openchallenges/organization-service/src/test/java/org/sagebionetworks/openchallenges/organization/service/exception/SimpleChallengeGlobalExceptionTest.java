c954c9f1f47ebd22a1025e13f0afe545b615dca5
mdsage1 <maria.diaz@sagebase.org>
2023-11-10 18:54:28 +0000

    add back unit test

--- apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/exception/SimpleChallengeGlobalExceptionTest.java
+++ apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/exception/SimpleChallengeGlobalExceptionTest.java
@@ -8,6 +8,16 @@ import org.springframework.http.HttpStatus;
 public class SimpleChallengeGlobalExceptionTest {
 
   @Test
+  public void SimpleChallengeGlobalException_ShouldReturnMessage_WhenMessageKeyIsPassed() {
+    // Create an instance of SimpleChallengeGlobalException using the constructor with details
+    String details = "Something went wrong";
+    SimpleChallengeGlobalException exception = new SimpleChallengeGlobalException(details);
+
+    // Verify the exception details
+    
+  }
+
+  @Test
   public void SimpleChallengeGlobalException_ShouldReturnStatus_WhenStatusKeyIsPassed() {
     // Define the exception details
     String type = "ExceptionType";
@@ -23,7 +33,6 @@ public class SimpleChallengeGlobalExceptionTest {
     assertThat(exception.getStatus()).isEqualTo(status);
     assertThat(exception.getTitle()).isEqualTo(title);
     assertThat(exception.getType()).isEqualTo(type);
-    assertThat(exception.getMessage()).isEqualTo(detail);
   }
 
   @Test
