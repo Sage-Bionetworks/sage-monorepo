6e267df097323f4e0877bd8dc014e08a248b00c4
mdsage1 <maria.diaz@sagebase.org>
2023-11-10 18:50:04 +0000

    add changes to condense tests

--- apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/exception/SimpleChallengeGlobalExceptionTest.java
+++ apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/exception/SimpleChallengeGlobalExceptionTest.java
@@ -8,48 +8,6 @@ import org.springframework.http.HttpStatus;
 public class SimpleChallengeGlobalExceptionTest {
 
   @Test
-  public void SimpleChallengeGlobalException_ShouldReturnMessage_WhenMessageKeyIsPassed() {
-    // Create an instance of SimpleChallengeGlobalException using the constructor with details
-    String details = "Something went wrong";
-    SimpleChallengeGlobalException exception = new SimpleChallengeGlobalException(details);
-
-    // Verify the exception details
-    assertThat(exception.getMessage()).isEqualTo(details);
-  }
-
-  @Test
-  public void SimpleChallengeGlobalException_ShouldReturnType_WhenTypeKeyIsPassed() {
-    // Define the exception details
-    String type = "ExceptionType";
-    String title = "Exception Title";
-    HttpStatus status = HttpStatus.BAD_REQUEST;
-    String detail = "Exception detail message";
-
-    // Create an instance of SimpleChallengeGlobalException using the all-args constructor
-    SimpleChallengeGlobalException exception =
-        new SimpleChallengeGlobalException(type, title, status, detail);
-
-    // Verify the exception details
-    assertThat(exception.getType()).isEqualTo(type);
-  }
-
-  @Test
-  public void SimpleChallengeGlobalException_ShouldReturnTitle_WhenTitleKeyIsPassed() {
-    // Define the exception details
-    String type = "ExceptionType";
-    String title = "Exception Title";
-    HttpStatus status = HttpStatus.BAD_REQUEST;
-    String detail = "Exception detail message";
-
-    // Create an instance of SimpleChallengeGlobalException using the all-args constructor
-    SimpleChallengeGlobalException exception =
-        new SimpleChallengeGlobalException(type, title, status, detail);
-
-    // Verify the exception details
-    assertThat(exception.getTitle()).isEqualTo(title);
-  }
-
-  @Test
   public void SimpleChallengeGlobalException_ShouldReturnStatus_WhenStatusKeyIsPassed() {
     // Define the exception details
     String type = "ExceptionType";
@@ -63,6 +21,9 @@ public class SimpleChallengeGlobalExceptionTest {
 
     // Verify the exception details
     assertThat(exception.getStatus()).isEqualTo(status);
+    assertThat(exception.getTitle()).isEqualTo(title);
+    assertThat(exception.getType()).isEqualTo(type);
+    assertThat(exception.getMessage()).isEqualTo(details);
   }
 
   @Test
