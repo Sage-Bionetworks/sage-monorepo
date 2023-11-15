d2bcde28b1a99949e480331807ce3a3da7ac0bd4
mdsage1 <maria.diaz@sagebase.org>
2023-11-15 20:35:39 +0000

    update test name

--- apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/exception/InvalidOrganizationExceptionTest.java
+++ apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/exception/InvalidOrganizationExceptionTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class InvalidOrganizationExceptionTest {
 
   @Test
-  public void InvalidOrganizationException_ShouldReturnExceptionObject_WhenDetailIsPassed() {
+  public void InvalidOrganizationException_ShouldReturnExceptionObject_WhenDetailKeyIsPassed() {
     // Set up the input detail
     String detail = "Errordetail";
 
