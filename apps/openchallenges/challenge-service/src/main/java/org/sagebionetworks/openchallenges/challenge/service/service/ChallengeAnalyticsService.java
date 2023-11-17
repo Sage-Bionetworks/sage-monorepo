078af47777e0f6002a36fd19dd4309de24721abb
verena <9377970+vpchung@users.noreply.github.com>
2023-11-17 00:26:19 +0000

    update plot counts

--- apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/service/ChallengeAnalyticsService.java
+++ apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/service/ChallengeAnalyticsService.java
@@ -14,9 +14,10 @@ public class ChallengeAnalyticsService {
     List<String> years =
         Arrays.asList(
             "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017",
-            "2018", "2019", "2020", "2021", "2022", "2023", "2024");
+            "2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025");
     List<Integer> challengeCounts =
-        Arrays.asList(6, 9, 13, 17, 23, 29, 34, 41, 49, 59, 86, 97, 116, 135, 183, 242, 302, 303);
+        Arrays.asList(
+            6, 9, 13, 17, 23, 29, 34, 41, 49, 59, 86, 97, 116, 135, 183, 242, 302, 304, 305);
     Integer undatedChallengeCount = 160;
 
     return ChallengesPerYearDto.builder()
