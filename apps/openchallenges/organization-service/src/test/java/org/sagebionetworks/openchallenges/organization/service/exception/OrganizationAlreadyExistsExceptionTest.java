4f10231d1ac44aa2d44c10be1e16f057e55cb37e
mdsage1 <maria.diaz@sagebase.org>
2023-11-17 22:31:29 +0000

    update test

--- apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/exception/OrganizationAlreadyExistsExceptionTest.java
+++ apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/exception/OrganizationAlreadyExistsExceptionTest.java
@@ -7,14 +7,14 @@ import org.junit.jupiter.api.Test;
 public class OrganizationAlreadyExistsExceptionTest {
 
   @Test
-  public void OrganizationAlreadyExistsException_ShouldReturnDetail_WhenDetailKeyIsPassed() {
+  public void OrganizationAlreadyExistsException_ShouldReturnMessage_WhenMessageKeyIsPassed() {
     // Set up the input detail
-    String detail = "Errordetail";
+    String message = "Errordetail";
 
     // Create an instance of OrganizationAlreadyExistsException
-    OrganizationAlreadyExistsException exception = new OrganizationAlreadyExistsException(detail);
+    OrganizationAlreadyExistsException exception = new OrganizationAlreadyExistsException(message);
 
     // Verify the properties of the exception
-    assertThat(exception.getDetail()).isEqualTo(detail);
+    assertThat(exception.getDetail()).isEqualTo(message);
   }
 }
