39e7154af0604208ed3c7d7a7f53cebd605fe4d8
Rongrong Chai <73901500+rrchai@users.noreply.github.com>
2023-11-20 20:51:15 -0500

    fix(openchallenges): reset pagination settings when filters change on search pages (#2370)

--- libs/openchallenges/challenge-search/src/lib/challenge-search.component.ts
+++ libs/openchallenges/challenge-search/src/lib/challenge-search.component.ts
@@ -130,6 +130,7 @@ export class ChallengeSearchComponent
   defaultSortedBy: ChallengeSort = 'relevance';
   defaultPageNumber = 0;
   defaultPageSize = 24;
+  @ViewChild('paginator', { static: true }) paginator!: PaginatorComponent;
 
   // define filters
   sortFilters: Filter[] = challengeSortFilter;
@@ -343,12 +344,28 @@ export class ChallengeSearchComponent
   }
 
   onParamChange(filteredQuery: any): void {
+    // reset pagination settings when filters change
+    if (!filteredQuery.pageNumber && !filteredQuery.pageSize) {
+      filteredQuery.pageNumber = this.defaultPageNumber;
+      filteredQuery.pageSize = this.defaultPageSize;
+      // this.selectedPageSize = this.defaultPageSize;
+      this.paginator.resetPageNumber();
+    }
     // update params of URL
     const currentParams = new HttpParams({
       fromString: this._location.path().split('?')[1] ?? '',
     });
     const params = Object.entries(filteredQuery)
-      .map(([key, value]) => [key, this.collapseParam(value as FilterValue)])
+      .map(([key, value]) => {
+        // avoid adding pageNumber and pageSize to the params if they are default
+        if (
+          (key === 'pageNumber' && value === this.defaultPageNumber) ||
+          (key === 'pageSize' && value === this.defaultPageSize)
+        ) {
+          return [key, ''];
+        }
+        return [key, this.collapseParam(value as FilterValue)];
+      })
       .reduce(
         // update with new param, or delete the param if empty string
         (params, [key, value]) =>
