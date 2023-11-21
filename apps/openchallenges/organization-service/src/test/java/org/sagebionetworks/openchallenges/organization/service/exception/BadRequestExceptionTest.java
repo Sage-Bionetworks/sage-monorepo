ef1fe76986ba7ade25088bb6d691d61dd65084c1
mdsage1 <maria.diaz@sagebase.org>
2023-11-21 16:02:44 +0000

    update changes

--- apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/exception/BadRequestExceptionTest.java
+++ apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/exception/BadRequestExceptionTest.java
@@ -1,6 +1,7 @@
 package org.sagebionetworks.openchallenges.organization.service.exception;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 
 public class BadRequestExceptionTest {
