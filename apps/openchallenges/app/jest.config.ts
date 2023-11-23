6102686d0289dd5dbc207a8a3c877494313844d7
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-11-23 18:18:14 +0000

    Ignore e2e folder by Jest

--- apps/openchallenges/app/jest.config.ts
+++ apps/openchallenges/app/jest.config.ts
@@ -14,7 +14,8 @@ export default {
       },
     ],
   },
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)', 'e2e/'],
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  testPathIgnorePatterns: ['<rootDir>/e2e'],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
