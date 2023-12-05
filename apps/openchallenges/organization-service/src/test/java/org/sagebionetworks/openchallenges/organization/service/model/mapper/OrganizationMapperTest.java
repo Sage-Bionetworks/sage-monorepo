491e2787b88dc2f4613a869fda00b3b1c24f167c
mdsage1 <maria.diaz@sagebase.org>
2023-12-05 07:44:17 +0000

    update mapper test

--- apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/model/mapper/OrganizationMapperTest.java
+++ apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/model/mapper/OrganizationMapperTest.java
@@ -4,42 +4,36 @@ import org.junit.jupiter.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.sagebionetworks.openchallenges.organization.service.model.dto.OrganizationDto;
 import org.sagebionetworks.openchallenges.organization.service.model.entity.OrganizationEntity;
-import org.sagebionetworks.openchallenges.organization.service.model.mapper.OrganizationMapper;
-import org.sagebionetworks.util.model.mapper.BaseMapper;
-import org.springframework.beans.BeanUtils;
-
 
 public class OrganizationMapperTest {
   @Test
-  public void ConvertToEntity_ShouldReturnEntityNameDescription_WhenEntityNameDescriptionKeysPassed() {
-    
+  public void
+      ConvertToEntity_ShouldReturnEntityNameDescription_WhenEntityNameDescriptionKeysPassed() {
+
     OrganizationDto dto = new OrganizationDto();
     dto.setName("Test Organization");
     dto.setDescription("This is a test organization");
-    
+
     OrganizationMapper mapper = new OrganizationMapper();
-    
-    
+
     OrganizationEntity entity = mapper.convertToEntity(dto);
-    
-    
+
     assertThat(entity.getName()).isEqualTo(dto.getName());
     assertThat(entity.getDescription()).isEqualTo(dto.getDescription());
   }
-  
+
   @Test
   public void ConvertToDto_ShouldReturndDtoNameDescription_WhenDtoNameDescriptionKeysPassed() {
 
     OrganizationEntity entity = new OrganizationEntity();
     entity.setName("Test Organization");
     entity.setDescription("This is a test organization");
-    
+
     OrganizationMapper mapper = new OrganizationMapper();
-    
+
     OrganizationDto dto = mapper.convertToDto(entity);
-    
+
     assertThat(dto.getName()).isEqualTo(entity.getName());
     assertThat(dto.getDescription()).isEqualTo(entity.getDescription());
   }
-  
 }
