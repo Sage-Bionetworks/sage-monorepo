ee9ca106c1f39598d66483ebbcf611fe016d7645
mdsage1 <maria.diaz@sagebase.org>
2023-12-05 08:59:39 +0000

    add comment to document integration test

--- apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/OrganizationServiceApplicationTests.java
+++ apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/OrganizationServiceApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OrganizationServiceApplicationTests {
 
+  //integration test
   @Test
   void contextLoads() {}
 }
