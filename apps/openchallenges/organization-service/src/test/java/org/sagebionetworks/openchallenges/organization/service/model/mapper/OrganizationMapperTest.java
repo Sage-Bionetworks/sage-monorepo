8f1e632a47a847d4423bcc6a7c58b523ad62d6e1
mdsage1 <maria.diaz@sagebase.org>
2023-12-05 08:23:17 +0000

    add unit tests for null objects entities and dto

--- apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/model/mapper/OrganizationMapperTest.java
+++ apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/model/mapper/OrganizationMapperTest.java
@@ -8,11 +8,19 @@ import org.sagebionetworks.openchallenges.organization.service.model.entity.Orga
 public class OrganizationMapperTest {
   @Test
   public void
-      ConvertToEntity_ShouldReturnEntityNameDescription_WhenEntityNameDescriptionKeysPassed() {
+      ConvertToEntity_ShouldReturnDtoProperties_WhenDtoPropertiesPassed() {
 
+    //create new Dto with properties to be copied to organization entity
     OrganizationDto dto = new OrganizationDto();
     dto.setName("Test Organization");
     dto.setDescription("This is a test organization");
+    dto.setId(4599L);
+    dto.setEmail("testemail@test.edu");
+    dto.setLogin("login");
+    dto.setAvatarKey("avatar Key");
+    dto.setWebsiteUrl("url");
+    dto.setChallengeCount(7);
+    dto.setAcronym(null);
 
     OrganizationMapper mapper = new OrganizationMapper();
 
@@ -20,14 +28,53 @@ public class OrganizationMapperTest {
 
     assertThat(entity.getName()).isEqualTo(dto.getName());
     assertThat(entity.getDescription()).isEqualTo(dto.getDescription());
+    assertThat(entity.getId()).isEqualTo(dto.getId());
+    assertThat(entity.getEmail()).isEqualTo(dto.getEmail());
+    assertThat(entity.getLogin()).isEqualTo(dto.getLogin());
+    assertThat(entity.getAvatarKey()).isEqualTo(dto.getAvatarKey());
+    assertThat(entity.getWebsiteUrl()).isEqualTo(dto.getWebsiteUrl());
+    assertThat(entity.getChallengeCount()).isEqualTo(dto.getChallengeCount());
+    assertThat(entity.getAcronym()).isEqualTo(dto.getAcronym());
+
+
   }
 
   @Test
-  public void ConvertToDto_ShouldReturndDtoNameDescription_WhenDtoNameDescriptionKeysPassed() {
+  public void
+    convertToEntity_ShouldReturnNullProperties_WhenDtoPropertiesNotPassed() {
+      
+      OrganizationDto dto = new OrganizationDto(null);
+      //don't set dto properties to avoid BeanUtils.copyProperties from passing them to the new entity
+
+      OrganizationMapper mapper = new OrganizationMapper();
+
+      OrganizationEntity entity = mapper.convertToEntity(dto);
+
+      assertThat(entity.getName()).isEqualTo(null);
+      assertThat(entity.getDescription()).isEqualTo(null);
+      assertThat(entity.getId()).isEqualTo(null);
+      assertThat(entity.getEmail()).isEqualTo(null);
+      assertThat(entity.getLogin()).isEqualTo(null);
+      assertThat(entity.getAvatarKey()).isEqualTo(null);
+      assertThat(entity.getWebsiteUrl()).isEqualTo(null);
+      assertThat(entity.getChallengeCount()).isEqualTo(null);
+      assertThat(entity.getAcronym()).isEqualTo(null);
+ 
+  }
+
+  @Test
+  public void ConvertToDto_ShouldReturndDtoWithMatchingEntityProperties_WhenEntityPropertiesPassed() {
 
     OrganizationEntity entity = new OrganizationEntity();
     entity.setName("Test Organization");
     entity.setDescription("This is a test organization");
+    entity.setId(4599L);
+    entity.setEmail("testemail@test.edu");
+    entity.setLogin("login");
+    entity.setAvatarKey("avatar Key");
+    entity.setWebsiteUrl("url");
+    entity.setChallengeCount(7);
+    entity.setAcronym(null);
 
     OrganizationMapper mapper = new OrganizationMapper();
 
@@ -35,5 +82,36 @@ public class OrganizationMapperTest {
 
     assertThat(dto.getName()).isEqualTo(entity.getName());
     assertThat(dto.getDescription()).isEqualTo(entity.getDescription());
+    assertThat(dto.getId()).isEqualTo(entity.getId());
+    assertThat(dto.getEmail()).isEqualTo(entity.getEmail());
+    assertThat(dto.getLogin()).isEqualTo(entity.getLogin());
+    assertThat(dto.getAvatarKey()).isEqualTo(entity.getAvatarKey());
+    assertThat(dto.getWebsiteUrl()).isEqualTo(entity.getWebsiteUrl());
+    assertThat(dto.getChallengeCount()).isEqualTo(entity.getChallengeCount());
+    assertThat(dto.getAcronym()).isEqualTo(entity.getAcronym());
+  }
+
+  @Test
+  public void
+    convertToDto_ShouldReturnNullProperties_WhenEntityPropertiesNotPassed() {
+      
+      OrganizationEntity entity = new OrganizationEntity(null);
+      //don't set entity properties to avoid BeanUtils.copyProperties from passing them to the new entity
+
+      OrganizationMapper mapper = new OrganizationMapper();
+
+      OrganizationDto dto = mapper.convertToDto(entity);
+      //don't set dto properties to avoid BeanUtils.copyProperties from passing them to the new entity
+
+      assertThat(dto.getName()).isEqualTo(null);
+      assertThat(dto.getDescription()).isEqualTo(null);
+      assertThat(dto.getId()).isEqualTo(null);
+      assertThat(dto.getEmail()).isEqualTo(null);
+      assertThat(dto.getLogin()).isEqualTo(null);
+      assertThat(dto.getAvatarKey()).isEqualTo(null);
+      assertThat(dto.getWebsiteUrl()).isEqualTo(null);
+      assertThat(dto.getChallengeCount()).isEqualTo(null);
+      assertThat(dto.getAcronym()).isEqualTo(null);
+ 
   }
 }
