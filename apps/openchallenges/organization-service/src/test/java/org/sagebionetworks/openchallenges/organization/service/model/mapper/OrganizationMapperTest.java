c6351287f39463b37324dc848966f7fcf66b9558
mdsage1 <maria.diaz@sagebase.org>
2023-12-05 07:42:45 +0000

    unit test model mapper

---
+++ apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/model/mapper/OrganizationMapperTest.java
@@ -0,0 +1,45 @@
+package org.sagebionetworks.openchallenges.organization.service.model.mapper;
+
+import org.junit.jupiter.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.sagebionetworks.openchallenges.organization.service.model.dto.OrganizationDto;
+import org.sagebionetworks.openchallenges.organization.service.model.entity.OrganizationEntity;
+import org.sagebionetworks.openchallenges.organization.service.model.mapper.OrganizationMapper;
+import org.sagebionetworks.util.model.mapper.BaseMapper;
+import org.springframework.beans.BeanUtils;
+
+
+public class OrganizationMapperTest {
+  @Test
+  public void ConvertToEntity_ShouldReturnEntityNameDescription_WhenEntityNameDescriptionKeysPassed() {
+    
+    OrganizationDto dto = new OrganizationDto();
+    dto.setName("Test Organization");
+    dto.setDescription("This is a test organization");
+    
+    OrganizationMapper mapper = new OrganizationMapper();
+    
+    
+    OrganizationEntity entity = mapper.convertToEntity(dto);
+    
+    
+    assertThat(entity.getName()).isEqualTo(dto.getName());
+    assertThat(entity.getDescription()).isEqualTo(dto.getDescription());
+  }
+  
+  @Test
+  public void ConvertToDto_ShouldReturndDtoNameDescription_WhenDtoNameDescriptionKeysPassed() {
+
+    OrganizationEntity entity = new OrganizationEntity();
+    entity.setName("Test Organization");
+    entity.setDescription("This is a test organization");
+    
+    OrganizationMapper mapper = new OrganizationMapper();
+    
+    OrganizationDto dto = mapper.convertToDto(entity);
+    
+    assertThat(dto.getName()).isEqualTo(entity.getName());
+    assertThat(dto.getDescription()).isEqualTo(entity.getDescription());
+  }
+  
+}
