354ddb3b8b1da7c71ebf31b7f480581788325de4
mdsage1 <maria.diaz@sagebase.org>
2023-12-05 23:10:33 +0000

    update comment

--- apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/OrganizationServiceApplicationTests.java
+++ apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/OrganizationServiceApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OrganizationServiceApplicationTests {
 
-  // integration test
+  // prepare the context for the unit tests
   @Test
   void contextLoads() {}
 }
