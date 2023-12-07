9620ad785cd71387bbd29543de93e01ad88de8b7
verena <9377970+vpchung@users.noreply.github.com>
2023-12-07 22:35:52 +0000

    update table specifications

--- apps/openchallenges/challenge-service/src/main/resources/db/migration/V1.0.0__create_tables.sql
+++ apps/openchallenges/challenge-service/src/main/resources/db/migration/V1.0.0__create_tables.sql
@@ -1,19 +1,18 @@
 -- challenge_platform definition
-
 CREATE TABLE `challenge_platform`
 (
     `id`                    int NOT NULL AUTO_INCREMENT,
     `slug`                  varchar(255) NOT NULL UNIQUE,
     `name`                  varchar(255) NOT NULL UNIQUE,
-    `avatar_key`            varchar(255) NOT NULL,
-    `website_url`           varchar(255) NOT NULL,
+    `avatar_key`            varchar(255),
+    `website_url`           varchar(500) NOT NULL,
     `created_at`            DATETIME DEFAULT CURRENT_TIMESTAMP,
     `updated_at`            DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
 );
 
--- challenge_input_data_type definition
 
+-- challenge_input_data_type definition
 CREATE TABLE `challenge_input_data_type`
 (
     `id`                    int NOT NULL AUTO_INCREMENT,
@@ -24,36 +23,31 @@ CREATE TABLE `challenge_input_data_type`
     PRIMARY KEY (`id`)
 );
 
--- challenge definition
 
+-- challenge definition
 CREATE TABLE `challenge`
 (
     `id`                    bigint(20) NOT NULL AUTO_INCREMENT,
-    `slug`                  varchar(255) NOT NULL,
-    `name`                  varchar(255) DEFAULT NULL,
+    `slug`                  varchar(255) NOT NULL UNIQUE,
+    `name`                  varchar(255) NOT NULL,
     `headline`              varchar(80),
-    `description`           varchar(1000) NOT NULL,
-    `avatar_url`            varchar(255),
-    `website_url`           varchar(255) NOT NULL,
+    `description`           varchar(1000),
+    `avatar_url`            varchar(500),
+    `website_url`           varchar(500),
     `status`                ENUM('upcoming', 'active', 'completed'),
     `difficulty`            ENUM('good_for_beginners', 'intermediate', 'advanced'),
     `platform_id`           int,
-    `doi`                   varchar(80),
+    `doi`                   varchar(120),
     `start_date`            DATE,
     `end_date`              DATE,
-    -- `email`                 varchar(255) DEFAULT NULL,
-    -- `login`                 varchar(255) UNIQUE,
     `created_at`            DATETIME DEFAULT CURRENT_TIMESTAMP,
     `updated_at`            DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`platform_id`) REFERENCES challenge_platform(`id`)
 );
 
--- KEY                `FKk9w2ogq595jbe8r2due7vv3xr` (`account_id`),
--- CONSTRAINT `FKk9w2ogq595jbe8r2due7vv3xr` FOREIGN KEY (`account_id`) REFERENCES `banking_core_account` (`id`)
 
 -- challenge_organization_role definition
-
 CREATE TABLE `challenge_contribution`
 (
     `id`                    int NOT NULL AUTO_INCREMENT,
@@ -62,11 +56,11 @@ CREATE TABLE `challenge_contribution`
     `role`                  ENUM('challenge_organizer', 'data_contributor', 'sponsor'),
     PRIMARY KEY (`id`),
     FOREIGN KEY (`challenge_id`) REFERENCES challenge(`id`),
-    CONSTRAINT unique_item UNIQUE (`id`)
+    CONSTRAINT unique_item UNIQUE (`challenge_id`, `organization_id`, `role`)
 );
 
--- challenge_incentive definition
 
+-- challenge_incentive definition
 CREATE TABLE `challenge_incentive`
 (
     `id`                    int NOT NULL AUTO_INCREMENT,
@@ -78,8 +72,8 @@ CREATE TABLE `challenge_incentive`
     CONSTRAINT unique_item UNIQUE (`name`, `challenge_id`)
 );
 
--- challenge_submission_type definition
 
+-- challenge_submission_type definition
 CREATE TABLE `challenge_submission_type`
 (
     `id`                    int NOT NULL AUTO_INCREMENT,
@@ -91,8 +85,8 @@ CREATE TABLE `challenge_submission_type`
     CONSTRAINT unique_item UNIQUE (`name`, `challenge_id`)
 );
 
--- challenge_x_challenge_input_data_type definition
 
+-- challenge_x_challenge_input_data_type definition
 CREATE TABLE `challenge_x_challenge_input_data_type`
 (
     `id`                              int NOT NULL AUTO_INCREMENT,
@@ -105,8 +99,8 @@ CREATE TABLE `challenge_x_challenge_input_data_type`
     CONSTRAINT unique_item UNIQUE (`challenge_id`, `challenge_input_data_type_id`)
 );
 
--- challenge_star definition
 
+-- challenge_star definition
 CREATE TABLE `challenge_star`
 (
     `id`                    int NOT NULL AUTO_INCREMENT,
@@ -118,14 +112,13 @@ CREATE TABLE `challenge_star`
     CONSTRAINT unique_item UNIQUE (`challenge_id`, `user_id`)
 );
 
--- challenge_category definition
 
+-- challenge_category definition
 CREATE TABLE `challenge_category`
 (
     `id`                    int NOT NULL AUTO_INCREMENT,
     `challenge_id`          bigint(20) NOT NULL,
     `category`              ENUM('featured'),
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`challenge_id`) REFERENCES challenge(`id`),
-    CONSTRAINT unique_item UNIQUE (`id`)
+    FOREIGN KEY (`challenge_id`) REFERENCES challenge(`id`)
 );
--- apps/openchallenges/organization-service/src/main/resources/db/migration/V1.0.0__create_tables.sql
+++ apps/openchallenges/organization-service/src/main/resources/db/migration/V1.0.0__create_tables.sql
@@ -1,35 +1,34 @@
 -- organization definition
 
 CREATE TABLE `organization` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
+  `id`                    bigint(20) NOT NULL AUTO_INCREMENT,
+  `name`                  varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `login` varchar(64) NOT NULL UNIQUE,
-  `avatar_key` varchar(255) DEFAULT NULL,
-  `website_url` varchar(255) DEFAULT NULL,
-  `description` varchar(1000) DEFAULT NULL,
-  `challenge_count` int,
-  `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `acronym` varchar(255) DEFAULT NULL,
+  `login`                 varchar(64) NOT NULL UNIQUE,
+  `avatar_key`            varchar(255) DEFAULT NULL,
+  `website_url`           varchar(500) DEFAULT NULL,
+  `description`           varchar(1000) DEFAULT NULL,
+  `challenge_count`       int DEFAULT 0,
+  `created_at`            DATETIME DEFAULT CURRENT_TIMESTAMP,
+  `updated_at`            DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `acronym`               varchar(10) DEFAULT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT login_check CHECK (char_length(`login`) >= 2 and `login` regexp '^[a-z]')
 );
 
--- organization_category definition
 
+-- organization_category definition
 CREATE TABLE `organization_category`
 (
     `id`                    int NOT NULL AUTO_INCREMENT,
     `organization_id`       bigint(20) NOT NULL,
     `category`              ENUM('featured'),
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`organization_id`) REFERENCES organization(`id`),
-    CONSTRAINT unique_item UNIQUE (`id`)
+    FOREIGN KEY (`organization_id`) REFERENCES organization(`id`)
 );
 
--- contributor_roles definition
 
+-- contributor_roles definition
 CREATE TABLE `challenge_contribution`
 (
     `id`                    int NOT NULL AUTO_INCREMENT,
@@ -38,5 +37,5 @@ CREATE TABLE `challenge_contribution`
     `role`                  ENUM('challenge_organizer', 'data_contributor', 'sponsor'),
     PRIMARY KEY (`id`),
     FOREIGN KEY (`organization_id`) REFERENCES organization(`id`),
-    CONSTRAINT unique_item UNIQUE (`id`)
+    CONSTRAINT unique_item UNIQUE (`challenge_id`, `organization_id`, `role`)
 );
