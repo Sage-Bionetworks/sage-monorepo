60d147f871a96b3ec86e97691c5625ba1f5c8f0e
Thomas Schaffter <thomas.schaffter@gmail.com>
2023-12-14 17:37:36 +0000

    Can now start container

--- docker/iatlas/serve-detach.sh
+++ docker/iatlas/serve-detach.sh
@@ -2,10 +2,10 @@
 
 args=(
   # List of services in alphanumeric order
-  --file docker/openchallenges/services/postgres.yml
+  --file docker/iatlas/services/postgres.yml
 
-  --file docker/openchallenges/networks.yml
-  --file docker/openchallenges/volumes.yml
+  --file docker/iatlas/networks.yml
+  --file docker/iatlas/volumes.yml
 
   up $1 --detach --remove-orphans
 )
