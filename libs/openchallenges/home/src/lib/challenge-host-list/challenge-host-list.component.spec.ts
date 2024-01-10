6399d1c1e8f2a25efff44bef8f78e734c0a3e438
Rongrong Chai <rchai@sagebase.org>
2024-01-10 19:54:41 +0000

    fix ci

--- libs/openchallenges/home/src/lib/challenge-host-list/challenge-host-list.component.spec.ts
+++ libs/openchallenges/home/src/lib/challenge-host-list/challenge-host-list.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 
 import { ChallengeHostListComponent } from './challenge-host-list.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ChallengeHostListComponent', () => {
   let component: ChallengeHostListComponent;
@@ -9,7 +10,11 @@ describe('ChallengeHostListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientModule, ChallengeHostListComponent],
+      imports: [
+        HttpClientModule,
+        ChallengeHostListComponent,
+        RouterTestingModule,
+      ],
     }).compileComponents();
   });
 
