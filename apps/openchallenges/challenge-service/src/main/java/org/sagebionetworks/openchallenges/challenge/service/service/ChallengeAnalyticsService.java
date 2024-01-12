2d448218016a030bfb603c3ff65d57ab78952e83
verena <9377970+vpchung@users.noreply.github.com>
2024-01-12 18:41:46 +0000

    add 2024 to plot; update plot numbers

--- apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/service/ChallengeAnalyticsService.java
+++ apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/service/ChallengeAnalyticsService.java
@@ -14,9 +14,9 @@ public class ChallengeAnalyticsService {
     List<String> years =
         Arrays.asList(
             "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017",
-            "2018", "2019", "2020", "2021", "2022", "2023");
+            "2018", "2019", "2020", "2021", "2022", "2023", "2024");
     List<Integer> challengeCounts =
-        Arrays.asList(6, 9, 13, 17, 23, 29, 34, 41, 49, 59, 86, 97, 116, 135, 183, 242, 302);
+    Arrays.asList(6, 9, 13, 17, 23, 29, 34, 41, 49, 59, 86, 97, 116, 135, 184, 243, 305, 315);
     Integer undatedChallengeCount = 171;
 
     // int currentYear = Year.now().getValue();
