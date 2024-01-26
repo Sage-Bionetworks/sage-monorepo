a82867c5fb593a9fc338239ffeb901fbccc126a0
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-01-25 18:38:36 -0800

    Touch two oc projects (#2462)

--- apps/openchallenges/organization-service/src/main/java/org/sagebionetworks/openchallenges/organization/service/OrganizationServiceApplication.java
+++ apps/openchallenges/organization-service/src/main/java/org/sagebionetworks/openchallenges/organization/service/OrganizationServiceApplication.java
@@ -10,7 +10,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-// Add comment to trigger Sonar
 @ComponentScan(basePackages = {"org.sagebionetworks.openchallenges"})
 @EnableEurekaClient
 @EnableFeignClients
