1d23dea62caf5007f759f0fcfe612bb5d19ec04e
heimannch <carolina.heimann@gmail.com>
2024-02-21 15:08:45 -0800

    add links for umap visualizations on cellxgene

--- R/sc_umap_ui.R
+++ R/sc_umap_ui.R
@@ -1,13 +1,18 @@
 sc_umap_ui <- function(id){
-#TODO: add links to HTAN; add gene info for Vanderbilt
-#TODO: incorporate clinical annotation (especially response to treatment)
 
   ns <- shiny::NS(id)
 
   shiny::tagList(
+    iatlas.modules::titleBox(
+      "iAtlas Explorer — UMAP Viewer of Single cell RNA-Seq data"
+    ),
+    iatlas.modules::textBox(
+      width = 12,
+      shiny::p("Explore UMAP visualizations of single-cell RNA-Seq datasets.")
+    ),
     iatlas.modules::messageBox(
       width = 24,
-      shiny::p("UMAP")
+      shiny::p("Explore UMAP visualizations of single-cell RNA-Seq datasets. Links for visualizations on CELLxGENE are provided for further exploration.")
     ),
     iatlas.modules::optionsBox(
       width=24,
@@ -16,24 +21,24 @@ sc_umap_ui <- function(id){
         shiny::selectInput(
           ns("color"),
           label = "Color by",
-          choices = c("Cell Type"="cell_type", "Type" = "type", "Gene Symbol" = "gene"),
+          choices = c("Cell Type"="cell_type", "Type" = "type"),
           selected = "cell_type",
           multiple = FALSE
         )
         ),
-      shiny::column(
-        width = 6,
-        parameter_tabs <- tabsetPanel(
-          id = ns("params"),
-          type = "hidden",
-          tabPanel("normal",
-                   shiny::p("")
-          ),
-          tabPanel("gene",
-                   shiny::uiOutput(ns("select_gene"))
-          )
-        )
-      )
+      # shiny::column(
+      #   width = 6,
+      #   parameter_tabs <- tabsetPanel(
+      #     id = ns("params"),
+      #     type = "hidden",
+      #     tabPanel("normal",
+      #              shiny::p("")
+      #     ),
+      #     tabPanel("gene",
+      #              shiny::uiOutput(ns("select_gene"))
+      #     )
+      #   )
+      # )
     ),
     iatlas.modules::plotBox(
       width=24,
