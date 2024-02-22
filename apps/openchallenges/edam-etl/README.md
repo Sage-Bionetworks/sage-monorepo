4b555286474bfe2d47382a19f851b1dcd361cbe9
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-02-22 22:13:37 +0000

    create initial project `openchallenges-edam-etl`

---
+++ apps/openchallenges/edam-etl/README.md
@@ -0,0 +1,6 @@
+# OpenChallenges EDAM ETL
+
+## Introduction
+
+This Python project downloads the EDAM ontolgy from X, processes the data for the needs of OC and
+export them to the OC DB.
---
+++ apps/openchallenges/edam-etl/src/main.py
@@ -0,0 +1,7 @@
+"""Extract, transform and load EDAM concepts"""
+
+# TODO Download EDAM concepts from GitHub or S3 bucket (CSV file)
+
+# TODO Process the EDAM concepts
+
+# TODO Push the EDAM concept to the OC challenge service DB
