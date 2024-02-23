02108399a85f4821f38bac49c194bd5a52a7fc0d
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-02-23 19:16:15 +0000

    read config from env vars (example)

--- apps/openchallenges/edam-etl/src/main.py
+++ apps/openchallenges/edam-etl/src/main.py
@@ -1,5 +1,11 @@
 """Extract, transform and load EDAM concepts"""
 
+from os import getenv
+
+# Get config from the environment variables
+oc_db_url = getenv("OC_DB_URL")
+print(f"OC DB URL: {oc_db_url}")
+
 # TODO Download EDAM concepts from GitHub or S3 bucket (CSV file)
 print("[TODO] Download EDAM concepts from GitHub or S3 bucket (CSV file)")
 
