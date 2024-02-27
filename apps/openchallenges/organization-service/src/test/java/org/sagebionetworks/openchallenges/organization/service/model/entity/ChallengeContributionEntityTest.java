5c06507da9424b47986bae285e5e5cef9f17bac9
mdsage1 <maria.diaz@sagebase.org>
2024-02-27 23:44:19 +0000

    update test format

--- apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/model/entity/ChallengeContributionEntityTest.java
+++ apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/model/entity/ChallengeContributionEntityTest.java
@@ -6,73 +6,71 @@ import org.junit.jupiter.api.Test;
 
 class ChallengeContributionEntityTest {
 
-    private OrganizationEntity organization;
-    private ChallengeContributionEntity contributor1;
-    private ChallengeContributionEntity contributor2;
-    private ChallengeContributionEntity contributor3;
-    private Long entityId1 = 1L;
-    private Long entityId2 = 2L;
-    private String role = "role";
+  private OrganizationEntity organization;
+  private ChallengeContributionEntity contributor1;
+  private ChallengeContributionEntity contributor2;
+  private ChallengeContributionEntity contributor3;
+  private Long entityId1 = 1L;
+  private Long entityId2 = 2L;
+  private String role = "role";
 
-    @BeforeEach
-    void setup() {
-        // Create an OrganizationEntity
-        organization = new OrganizationEntity();
+  @BeforeEach
+  void setup() {
+    // Create an OrganizationEntity
+    organization = new OrganizationEntity();
 
-        // Create different ChallengeContributionEntity using different methods
-        contributor1 = new ChallengeContributionEntity();
-        contributor1.setId(1L);
-        contributor1.setRole("role");
-        contributor1.setOrganization(organization);
+    // Create different ChallengeContributionEntity using different methods
+    contributor1 = new ChallengeContributionEntity();
+    contributor1.setId(1L);
+    contributor1.setRole("role");
+    contributor1.setOrganization(organization);
 
-        contributor2 = new ChallengeContributionEntity(1L, "role", organization);
+    contributor2 = new ChallengeContributionEntity(1L, "role", organization);
 
-        contributor3 = ChallengeContributionEntity.builder()
-                .id(2L)
-                .organization(organization)
-                .role(role)
-                .build();
-    }
+    contributor3 =
+        ChallengeContributionEntity.builder().id(2L).organization(organization).role(role).build();
+  }
 
-    @Test
-    void ChallengeContributionEntityGetters_ShouldReturnChallengeContributionEntityValues_WhenArgsPassedViaSetters() {
+  @Test
+  void
+      ChallengeContributionEntityGetters_ShouldReturnChallengeContributionEntityValues_WhenArgsPassedViaSetters() {
 
-        // Verify the values are set
-        Assertions.assertEquals(entityId1, contributor1.getId());
-        Assertions.assertEquals(organization, contributor1.getOrganization());
-        Assertions.assertEquals(role, contributor1.getRole());
+    // Verify the values are set
+    Assertions.assertEquals(entityId1, contributor1.getId());
+    Assertions.assertEquals(organization, contributor1.getOrganization());
+    Assertions.assertEquals(role, contributor1.getRole());
+  }
 
-    }
+  @Test
+  void
+      ChallengeContributionEntityGetters_ShouldReturnChallengeContributionEntityValues_WhenAllArgsPassedtoConstructor() {
 
-    @Test
-    void ChallengeContributionEntityGetters_ShouldReturnChallengeContributionEntityValues_WhenAllArgsPassedtoConstructor() {
+    // Verify the values are set
+    Assertions.assertEquals(entityId1, contributor2.getId());
+    Assertions.assertEquals(organization, contributor2.getOrganization());
+    Assertions.assertEquals(role, contributor2.getRole());
+  }
 
-        // Verify the values are set
-        Assertions.assertEquals(entityId1, contributor2.getId());
-        Assertions.assertEquals(organization, contributor2.getOrganization());
-        Assertions.assertEquals(role, contributor2.getRole());
+  @Test
+  void
+      ChallengeContributionEntityGetters_ShouldReturnArgumentsPassed_WhenOrganizationEntityBuiltWithClassBuilder() {
 
-    }
+    // Verify the values are set
+    Assertions.assertEquals(entityId2, contributor3.getId());
+    Assertions.assertEquals(organization, contributor3.getOrganization());
+    Assertions.assertEquals(role, contributor3.getRole());
+  }
 
-    @Test
-    void ChallengeContributionEntityGetters_ShouldReturnArgumentsPassed_WhenOrganizationEntityBuiltWithClassBuilder() {
+  @Test
+  void
+      DataMethods_ShouldReturnRecognizeIdenticalAndDifferentChallengeContributionEntity_WhenChallengeContributionEntityCompared() {
 
-        // Verify the values are set
-        Assertions.assertEquals(entityId2, contributor3.getId());
-        Assertions.assertEquals(organization, contributor3.getOrganization());
-        Assertions.assertEquals(role, contributor3.getRole());
+    // Verify the generated equals() method
+    Assertions.assertEquals(contributor1, contributor2);
+    Assertions.assertNotEquals(contributor1, contributor3);
 
-    }
-
-    @Test
-    void DataMethods_ShouldReturnRecognizeIdenticalAndDifferentChallengeContributionEntity_WhenChallengeContributionEntityCompared() {
-
-        // Verify the generated equals() method
-        Assertions.assertEquals(contributor1, contributor2);
-        Assertions.assertNotEquals(contributor1, contributor3);
-
-        // Verify the generated hashCode() method
-        Assertions.assertEquals(contributor1.hashCode(), contributor2.hashCode());
-        Assertions.assertNotEquals(contributor1.hashCode(), contributor3.hashCode());
-    }
+    // Verify the generated hashCode() method
+    Assertions.assertEquals(contributor1.hashCode(), contributor2.hashCode());
+    Assertions.assertNotEquals(contributor1.hashCode(), contributor3.hashCode());
+  }
 }
