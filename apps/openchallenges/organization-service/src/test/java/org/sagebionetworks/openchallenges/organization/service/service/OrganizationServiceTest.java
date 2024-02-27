8d5ea5d271c78be1827f212eba7b58431a0f4dcd
mdsage1 <maria.diaz@sagebase.org>
2024-02-27 23:54:50 +0000

    update

--- apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/service/OrganizationServiceTest.java
+++ apps/openchallenges/organization-service/src/test/java/org/sagebionetworks/openchallenges/organization/service/service/OrganizationServiceTest.java
@@ -56,7 +56,6 @@ class OrganizationServiceTest {
     /* Test that the organization is in the repo, is not null, that the id can be pulled,
     findByIdOrLogin method is called, and no interactions with the organizationMapper (verifies
     interactions w/Mock object) */
-    
 
     verify(organizationRepository).findByIdOrLogin(orgId, orgLogin);
     assertNotNull(response);
