a1c076dbd8db7e4a64788a0920460780a1447e70
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-03-22 14:44:29 -0700

    chore(sage-monorepo): migrate to Nx v18 and Angular v17 (#2591)

--- libs/agora/home/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/lib.routes';
-
-export * from './lib/agora-home/agora-home.component';
--- libs/agora/home/src/lib/lib.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Route } from '@angular/router';
-import { AgoraHomeComponent } from './agora-home/agora-home.component';
-
-export const agoraHomeRoutes: Route[] = [
-  { path: '', component: AgoraHomeComponent },
-];
