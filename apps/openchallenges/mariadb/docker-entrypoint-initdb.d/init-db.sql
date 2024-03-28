03f9e1613032fdde0ecc687d6f91690f6da112bc
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-03-28 18:37:13 +0000

    create edam DB and user

--- apps/openchallenges/mariadb/docker-entrypoint-initdb.d/init-db.sql
+++ apps/openchallenges/mariadb/docker-entrypoint-initdb.d/init-db.sql
@@ -3,11 +3,13 @@ SET GLOBAL local_infile = 'ON';
 create database challenge_service;
 create database organization_service;
 create database user_service;
+create database edam;
 
 create role role_admin;
 grant all on challenge_service.* to role_admin;
 grant all on organization_service.* to role_admin;
 grant all on user_service.* to role_admin;
+grant all on edam.* to role_admin;
 
 -- Create the user maria
 grant role_admin to maria;
@@ -37,3 +39,12 @@ set default role role_admin for user_service;
 create user 'mysqld-exporter' identified by 'changeme' with max_user_connections 3;;
 grant process, slave monitor on *.* to 'mysqld-exporter';
 grant select on performance_schema.* to 'mysqld-exporter';
+
+-- Create the user for edam-etl
+create user 'edam-etl' identified by 'changeme';
+grant role_admin to 'edam-etl';
+set default role role_admin for 'edam-etl';
+
+-- Create the read-only user to the EDAM DB for the challenge service
+create user 'edam-challenge-service' identified by 'changeme';
+grant select on edam.* to 'edam-challenge-service';
