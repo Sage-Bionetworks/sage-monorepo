554792a0ecc99edb7a4db431201e292918808ab1
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-04-18 01:16:14 +0000

    update comment

--- apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/model/search/EdamSectionValueBridge.java
+++ apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/model/search/EdamSectionValueBridge.java
@@ -9,10 +9,10 @@ public class EdamSectionValueBridge implements ValueBridge<String, String> {
   @Override
   public String toIndexedValue(String value, ValueBridgeToIndexedValueContext context) {
     for (EdamSectionDto section : EdamSectionDto.values()) {
-      // The first condition is used to map EDAM concept class IDs to concept sections during mass
-      // indexing. This value bridge is also called when sending search queries to Elasticsearch,
-      // which is why the second condition is required to map the user input to an existing EDAM
-      // concept.
+      // The first condition is used to map EDAM concept class IDs to concept sections when new
+      // concept are created (e.g. during mass indexing). This value bridge is also called when
+      // sending search queries to Elasticsearch, which is why the second condition is required to
+      // map the user input to an existing EDAM concept.
       if (value.contains("/" + section.getValue() + "_") || section.getValue().equals(value)) {
         return section.getValue();
       }
