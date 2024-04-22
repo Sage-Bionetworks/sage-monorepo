4fe98b7781bd3494a8de3863922ef33be3728615
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-04-22 21:00:53 +0000

    implement sort for edam concept

--- apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/model/repository/CustomEdamConceptRepositoryImpl.java
+++ apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/model/repository/CustomEdamConceptRepositoryImpl.java
@@ -8,8 +8,13 @@ import org.hibernate.search.engine.search.common.BooleanOperator;
 import org.hibernate.search.engine.search.predicate.SearchPredicate;
 import org.hibernate.search.engine.search.predicate.dsl.SearchPredicateFactory;
 import org.hibernate.search.engine.search.query.SearchResult;
+import org.hibernate.search.engine.search.sort.SearchSort;
+import org.hibernate.search.engine.search.sort.dsl.SearchSortFactory;
+import org.hibernate.search.engine.search.sort.dsl.SortOrder;
 import org.hibernate.search.mapper.orm.Search;
 import org.hibernate.search.mapper.orm.session.SearchSession;
+import org.sagebionetworks.openchallenges.challenge.service.exception.BadRequestException;
+import org.sagebionetworks.openchallenges.challenge.service.model.dto.EdamConceptDirectionDto;
 import org.sagebionetworks.openchallenges.challenge.service.model.dto.EdamConceptSearchQueryDto;
 import org.sagebionetworks.openchallenges.challenge.service.model.dto.EdamSectionDto;
 import org.sagebionetworks.openchallenges.challenge.service.model.entity.EdamConceptEntity;
@@ -34,6 +39,7 @@ public class CustomEdamConceptRepositoryImpl implements CustomEdamConceptReposit
       Pageable pageable, EdamConceptSearchQueryDto query, String[] fields) {
     SearchSession searchSession = Search.session(entityManager);
     SearchPredicateFactory pf = searchSession.scope(EdamConceptEntity.class).predicate();
+    SearchSortFactory sf = searchSession.scope(EdamConceptEntity.class).sort();
     List<SearchPredicate> predicates = new ArrayList<>();
 
     if (query.getSearchTerms() != null && !query.getSearchTerms().isBlank()) {
@@ -43,11 +49,18 @@ public class CustomEdamConceptRepositoryImpl implements CustomEdamConceptReposit
       predicates.add(getEdamSectionsPredicate(pf, query));
     }
 
+    SearchSort sort = getSearchSort(sf, query);
+    SearchPredicate sortPredicate = getSearchSortPredicate(pf, query);
+    if (sortPredicate != null) {
+      predicates.add(sortPredicate);
+    }
+
     SearchPredicate topLevelPredicate = buildTopLevelPredicate(pf, predicates);
 
     return searchSession
         .search(EdamConceptEntity.class)
         .where(topLevelPredicate)
+        .sort(sort)
         .fetch((int) pageable.getOffset(), pageable.getPageSize());
   }
 
@@ -103,5 +116,43 @@ public class CustomEdamConceptRepositoryImpl implements CustomEdamConceptReposit
               }
             })
         .toPredicate();
+  }
+
+  private SearchSort getSearchSort(SearchSortFactory sf, EdamConceptSearchQueryDto query) {
+    SortOrder orderWithDefaultAsc =
+        query.getDirection() == EdamConceptDirectionDto.DESC ? SortOrder.DESC : SortOrder.ASC;
+    SortOrder orderWithDefaultDesc =
+        query.getDirection() == EdamConceptDirectionDto.ASC ? SortOrder.ASC : SortOrder.DESC;
+
+    SearchSort preferredLabelSort =
+        sf.field("preferred_label_sort").order(orderWithDefaultAsc).toSort();
+    SearchSort scoreSort = sf.score().order(orderWithDefaultDesc).toSort();
+    SearchSort relevanceSort =
+        (query.getSearchTerms() == null || query.getSearchTerms().isBlank())
+            ? preferredLabelSort
+            : scoreSort;
+
+    switch (query.getSort()) {
+      case PREFERRED_LABEL -> {
+        return preferredLabelSort;
+      }
+      case RELEVANCE -> {
+        return relevanceSort;
+      }
+      default -> {
+        throw new BadRequestException(
+            String.format("Unhandled sorting strategy '%s'", query.getSort()));
+      }
+    }
+  }
+
+  private SearchPredicate getSearchSortPredicate(
+      SearchPredicateFactory pf, EdamConceptSearchQueryDto query) {
+
+    switch (query.getSort()) {
+      default -> {
+        return null;
+      }
+    }
   }
 }
