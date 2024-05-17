8a7f83f22569f758cb9cd957f66aa5b60341a4df
Rongrong Chai <rchai@sagebase.org>
2024-05-17 20:08:01 +0000

    Revert "move the dropdown variables outside of ngOnit"
    This reverts commit 84b6d68ad74a1163d97b99bce1e55f096865d508.

--- libs/openchallenges/challenge-search/src/lib/challenge-search.component.ts
+++ libs/openchallenges/challenge-search/src/lib/challenge-search.component.ts
@@ -146,19 +146,9 @@ export class ChallengeSearchComponent
   statusFilter = challengeStatusFilterPanel;
   submissionTypesFilter = challengeSubmissionTypesFilterPanel;
 
-  // set dropdown filter loaded pages
-  loadedPages: { [key: string]: Set<number> } = {
-    inputDataTypes: new Set(),
-    organizations: new Set(),
-    platforms: new Set(),
-  };
-
   // set dropdown filter placeholders
-  dropdownFilters: { [key: string]: FilterPanel } = {
-    inputDataTypes: challengeInputDataTypesFilterPanel,
-    organizations: challengeOrganizationsFilterPanel,
-    platforms: challengePlatformsFilterPanel,
-  };
+  dropdownFilters!: { [key: string]: FilterPanel };
+  loadedPages!: { [key: string]: Set<number> };
 
   // define selected filter values
   selectedCategories!: ChallengeCategory[];
@@ -249,6 +239,18 @@ export class ChallengeSearchComponent
     this.challengeService.listChallenges().subscribe((page) => {
       this.totalChallengesCount = page.totalElements;
     });
+
+    // update loaded pages and dropdown filters
+    this.loadedPages = {
+      inputDataTypes: new Set(),
+      organizations: new Set(),
+      platforms: new Set(),
+    };
+    this.dropdownFilters = {
+      inputDataTypes: challengeInputDataTypesFilterPanel,
+      organizations: challengeOrganizationsFilterPanel,
+      platforms: challengePlatformsFilterPanel,
+    };
 
     // load initial data and listen to fetch data as query changes
     this.loadInitialDropdownData();
