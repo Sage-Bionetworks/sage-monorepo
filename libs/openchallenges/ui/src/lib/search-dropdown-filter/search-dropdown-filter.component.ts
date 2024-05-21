6108c071acdbec2017b8610b0b98ff5d877bf6f7
Rongrong Chai <rchai@sagebase.org>
2024-05-21 01:04:49 +0000

    adjust bottom space

--- libs/openchallenges/ui/src/lib/search-dropdown-filter/search-dropdown-filter.component.ts
+++ libs/openchallenges/ui/src/lib/search-dropdown-filter/search-dropdown-filter.component.ts
@@ -57,7 +57,7 @@ export class SearchDropdownFilterComponent implements OnInit {
       delay: this.showLoader ? this.delays : 0, // if no loader is applied, load data seamlessly
       showLoader: this.showLoader,
       step: this.optionsPerPage,
-      scrollHeight: `${this.itemHeight * this.optionsPerPage + 24}px`, // set height
+      scrollHeight: `${this.itemHeight * this.optionsPerPage + 12}px`, // set height
     };
 
     this.showAvatar = this.showAvatar ? this.showAvatar : false;
