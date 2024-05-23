b61fbf5de68b41bf4b534bdcec57830ba38ebcfb
hallieswan <26949006+hallieswan@users.noreply.github.com>
2024-05-23 21:38:40 +0000

    AG-1085: add testing-library to charts-ngx

--- libs/shared/typescript/charts-ngx/src/lib/boxplot/boxplot.directive.spec.ts
+++ libs/shared/typescript/charts-ngx/src/lib/boxplot/boxplot.directive.spec.ts
@@ -1,8 +1,23 @@
+import { Component } from '@angular/core';
+import { render, screen } from '@testing-library/angular';
 import { BoxplotDirective } from './boxplot.directive';
 
+const renderTestComponent = async (testText: string) => {
+  @Component({
+    template: `<p sageBoxplot [text]="'${testText}'">{{ text }}</p>`,
+  })
+  class TestComponent {}
+
+  await render(TestComponent, {
+    imports: [BoxplotDirective],
+  });
+};
+
 describe('BoxplotDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BoxplotDirective();
-    expect(directive).toBeTruthy();
+  it('should render text', async () => {
+    const testText = 'Hello, World!';
+    await renderTestComponent(testText);
+
+    expect(screen.getByText(testText)).toBeVisible();
   });
 });
