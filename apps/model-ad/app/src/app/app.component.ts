e60570be0c4dba906cdf6c500a054eeb7a7d07da
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-05-29 12:14:30 -0700

    feat(model-ad): create a footer component and add it to the MODEL-AD app (#2689)

--- apps/model-ad/app/src/app/app.component.ts
+++ apps/model-ad/app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { FooterComponent } from '@sagebionetworks/model-ad/ui';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, FooterComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
