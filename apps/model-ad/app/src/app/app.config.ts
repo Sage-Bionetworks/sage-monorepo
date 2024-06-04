d421cd1305c377f315cdea2ba582bcff7d0cb9b2
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-06-04 23:12:42 +0000

    create config library and inject it in the not-found page

--- apps/model-ad/app/src/app/app.config.ts
+++ apps/model-ad/app/src/app/app.config.ts
@@ -1,8 +1,53 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { ApplicationConfig, APP_INITIALIZER, APP_ID } from '@angular/core';
+import {
+  provideRouter,
+  withEnabledBlockingInitialNavigation,
+  withInMemoryScrolling,
+} from '@angular/router';
+import {
+  withInterceptorsFromDi,
+  provideHttpClient,
+} from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+// import { BASE_PATH as API_CLIENT_BASE_PATH } from '@sagebionetworks/model-ad/api-client-angular';
+import { configFactory, ConfigService } from '@sagebionetworks/model-ad/config';
+
+import { routes } from './app.routes';
+
+// This index is used to remove the corresponding provider in app.config.server.ts.
+export const APP_BASE_URL_PROVIDER_INDEX = 1;
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideClientHydration(), provideRouter(appRoutes)],
+  providers: [
+    { provide: APP_ID, useValue: 'model-ad-app' },
+    {
+      // This provider must be specified at the index defined by APP_BASE_URL_PROVIDER_INDEX.
+      provide: 'APP_BASE_URL',
+      useFactory: () => '.',
+      deps: [],
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: configFactory,
+      deps: [ConfigService],
+      multi: true,
+    },
+    // {
+    //   provide: API_CLIENT_BASE_PATH,
+    //   useFactory: (configService: ConfigService) =>
+    //     configService.config.isPlatformServer
+    //       ? configService.config.ssrApiUrl
+    //       : configService.config.csrApiUrl,
+    //   deps: [ConfigService],
+    // },
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideRouter(
+      routes,
+      withEnabledBlockingInitialNavigation(),
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+      })
+    ),
+  ],
 };
---
+++ libs/model-ad/config/README.md
@@ -0,0 +1,7 @@
+# openchallenges-config
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test openchallenges-config` to execute the unit tests.
