a87d6cd510ced877a76c2c4e7c2a31bc3814e8c5
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-06-07 22:16:01 +0000

    add metadata to the not-found page

--- libs/model-ad/not-found/src/lib/not-found.component.ts
+++ libs/model-ad/not-found/src/lib/not-found.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { FooterComponent } from '@sagebionetworks/model-ad/ui';
 import { ConfigService } from '@sagebionetworks/model-ad/config';
+import { SeoService } from '@sagebionetworks/shared/util';
+import { getSeoData } from './seo-data';
 
 @Component({
   selector: 'model-ad-not-found',
@@ -26,11 +28,17 @@ export class NotFoundComponent {
   public termsOfUseUrl: string;
   public apiDocsUrl: string;
 
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    private seoService: SeoService,
+    private renderer2: Renderer2
+  ) {
     this.appVersion = this.configService.config.appVersion;
     this.dataUpdatedOn = this.configService.config.dataUpdatedOn;
     this.privacyPolicyUrl = this.configService.config.privacyPolicyUrl;
     this.termsOfUseUrl = this.configService.config.termsOfUseUrl;
     this.apiDocsUrl = this.configService.config.apiDocsUrl;
+
+    this.seoService.setData(getSeoData(), this.renderer2);
   }
 }
