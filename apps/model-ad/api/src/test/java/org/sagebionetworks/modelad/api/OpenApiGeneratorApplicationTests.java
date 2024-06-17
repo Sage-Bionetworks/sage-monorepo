9aa0c9cce579278de15e6b8945d3a881a35aeb1c
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-06-17 16:34:04 +0000

    generate code for MODEL-AD OpenAPI description

---
+++ apps/model-ad/api/src/test/java/org/sagebionetworks/modelad/api/OpenApiGeneratorApplicationTests.java
@@ -0,0 +1,11 @@
+package org.sagebionetworks.modelad.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiGeneratorApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
