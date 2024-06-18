391b45a6fce65aaa1dc0ceffb5877838801630b4
hallieswan <26949006+hallieswan@users.noreply.github.com>
2024-06-18 17:34:24 +0000

    AG-1462: build agora-app docker image

---
+++ apps/agora/app/docker-entrypoint.d/10-envsubst-on-app-config-template.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+cd "${APP_DIR}/dist/apps/model-ad/app/browser/browser/config"
+# envsubst < config.json.template > config.json
--- docker/agora/serve-detach.sh
+++ docker/agora/serve-detach.sh
@@ -2,8 +2,8 @@
 
 args=(
   # List of services in alphanumeric order
+  --file docker/agora/services/app.yml
   --file docker/agora/services/mongo.yml
-  # --file docker/agora/services/web-app.yml
 
   --file docker/agora/networks.yml
   --file docker/agora/volumes.yml
