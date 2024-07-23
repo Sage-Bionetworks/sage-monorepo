70a066837f10c686436988e992355bdb15adcf65
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-07-23 14:11:02 +0000

    update delegate impl to detect requested mime type

--- apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/api/ChallengeApiDelegateImpl.java
+++ apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/api/ChallengeApiDelegateImpl.java
@@ -1,14 +1,24 @@
 package org.sagebionetworks.openchallenges.challenge.service.api;
 
+import java.util.Optional;
 import org.sagebionetworks.openchallenges.challenge.service.model.dto.ChallengeDto;
 import org.sagebionetworks.openchallenges.challenge.service.model.dto.ChallengeSearchQueryDto;
 import org.sagebionetworks.openchallenges.challenge.service.model.dto.ChallengesPageDto;
 import org.sagebionetworks.openchallenges.challenge.service.service.ChallengeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.NativeWebRequest;
 
 @Component
 public class ChallengeApiDelegateImpl implements ChallengeApiDelegate {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ChallengeApiDelegateImpl.class);
+
+  @Autowired private NativeWebRequest request;
 
   private final ChallengeService challengeService;
 
@@ -17,7 +27,26 @@ public class ChallengeApiDelegateImpl implements ChallengeApiDelegate {
   }
 
   @Override
+  public Optional<NativeWebRequest> getRequest() {
+    return Optional.ofNullable(request);
+  }
+
+  @Override
   public ResponseEntity<ChallengeDto> getChallenge(Long challengeId) {
+    LOGGER.info("getChallenge()");
+    getRequest()
+        .ifPresent(
+            request -> {
+              for (MediaType mediaType : MediaType.parseMediaTypes(request.getHeader("Accept"))) {
+                LOGGER.info("mediaType: {}", mediaType);
+                if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
+                  LOGGER.info("Requested application/json");
+                }
+                if (mediaType.isCompatibleWith(MediaType.valueOf("application/ld+json"))) {
+                  LOGGER.info("Requested application/ld+json");
+                }
+              }
+            });
     return ResponseEntity.ok(challengeService.getChallenge(challengeId));
   }
 
