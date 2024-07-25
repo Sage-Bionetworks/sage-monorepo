82771382d9d40de6230df14f91c5aad5b7f0aa99
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-07-25 12:16:45 -0700

    fix(openchallenges): make the challenge and organization services AWS aware (IT-3800) (#2751)

--- libs/openchallenges/app-config-data/src/main/java/org/sagebionetworks/openchallenges/app/config/data/OrganizationServiceConfigData.java
+++ libs/openchallenges/app-config-data/src/main/java/org/sagebionetworks/openchallenges/app/config/data/OrganizationServiceConfigData.java
@@ -10,4 +10,5 @@ import org.springframework.context.annotation.Configuration;
 public class OrganizationServiceConfigData {
 
   private String welcomeMessage;
+  private boolean isDeployedOnAws;
 }
