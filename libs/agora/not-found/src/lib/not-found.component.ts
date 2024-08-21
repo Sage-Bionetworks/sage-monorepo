1e54f887311a00fb9582969575c5654a80640435
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-08-21 20:18:38 +0000

    fix api path

--- libs/agora/not-found/src/lib/not-found.component.ts
+++ libs/agora/not-found/src/lib/not-found.component.ts
@@ -1,12 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component, Renderer2 } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { FooterComponent } from '@sagebionetworks/agora/ui';
 import { ConfigService } from '@sagebionetworks/agora/config';
 import { SeoService } from '@sagebionetworks/shared/util';
+import {
+  DataversionService,
+  Dataversion,
+} from '@sagebionetworks/agora/api-client-angular';
 import { getSeoData } from './seo-data';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'agora-not-found',
@@ -21,15 +26,18 @@ import { getSeoData } from './seo-data';
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss'],
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements OnInit {
   public appVersion: string;
   public dataUpdatedOn: string;
   public privacyPolicyUrl: string;
   public termsOfUseUrl: string;
   public apiDocsUrl: string;
 
+  dataversion$!: Observable<Dataversion>;
+
   constructor(
     private readonly configService: ConfigService,
+    private dataVersionService: DataversionService,
     private seoService: SeoService,
     private renderer2: Renderer2,
   ) {
@@ -40,5 +48,9 @@ export class NotFoundComponent {
     this.apiDocsUrl = this.configService.config.apiDocsUrl;
 
     this.seoService.setData(getSeoData(), this.renderer2);
+  }
+
+  ngOnInit(): void {
+    this.dataversion$ = this.dataVersionService.getDataversion();
   }
 }
