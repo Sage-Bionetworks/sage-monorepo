c0b2678bb4600d2ded22a686306f1c36ff6f1841
heimannch <carolina.heimann@gmail.com>
2024-09-10 13:43:50 -0700

    sc distribution working with cohort_obj

--- R/sc_immune_features_server.R
+++ R/sc_immune_features_server.R
@@ -10,44 +10,23 @@ sc_immune_features_server <- function(id, cohort_obj){
       )
 
       pseudobulk_df <- shiny::reactive({
-        iatlasGraphQLClient::query_pseudobulk_feature_values() %>% #TODO: update when data in cohort_obj
-          dplyr::inner_join(iatlasGraphQLClient::query_dataset_samples(single_cell_datasets()$name), by = "sample_name") %>%
+        iatlasGraphQLClient::query_pseudobulk_feature_values() %>%
+          dplyr::inner_join(cohort_obj()$sample_tbl, by = "sample_name") %>%
           dplyr::select(
             "sample_name",
             "group" = "cell_type",
             "feature_name" ,
             "feature_display",
             "feature_value" = "value",
-            "dataset_name",
-            "dataset_display"
+            "dataset_name"
           )
       })
-
-      categories_df <- shiny::reactive(iatlasGraphQLClient::query_tags(datasets = single_cell_datasets()$name) %>% #TODO: update when data is in cohort_obj
-                                         dplyr::mutate(class = dplyr::case_when(
-                                           tag_name %in% c( "Response", "Responder", "Progression", "Clinical_Benefit") ~ "Response to ICI",
-                                           TRUE ~ "Treatment Data")) %>%
-                                         create_nested_list_by_class(.,
-                                                                     class_column = "class",
-                                                                     internal_column = "tag_name",
-                                                                     display_column = "tag_short_display")
-      )
 
       sc_immune_features_distribution_server(
         "sc_immune_features_distribution",
         cohort_obj,
         pseudobulk_df,
-        feature_op = shiny::reactive(c( #TODO: change this when data is in cohort_obj
-          "Wound Healing" = "CHANG_CORE_SERUM_RESPONSE_UP",
-          "Macrophage Regulation" = "CSF1_response",
-          "Lymphocyte Infiltration" = "LIexpression_score",
-          "Proliferation" = "Module11_Prolif_score",
-          "IFN-gamma Response" = "Module3_IFN_score",
-          "TGF-beta Response" = "TGFB_score_21050467",
-          "Th1 Cells" = "Th1_cells",
-          "Th2 Cells" = "Th2_cells"
-        )),
-        categories_df
+        feature_op = dplyr::filter(cohort_obj()$feature_tbl, !class %in% c("Clinical", "umap"))
       )
 
       observeEvent(input$method_link,{
