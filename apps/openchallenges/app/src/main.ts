75ce2c564e6237a2cc7a3be6bef516286625e35e
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-09-11 16:46:41 +0000

    test auto git add after formatting

--- apps/openchallenges/app/src/main.ts
+++ apps/openchallenges/app/src/main.ts
@@ -2,4 +2,5 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+// add a comment
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
