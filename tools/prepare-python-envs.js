3ea3837cab385fb0599e47a57f6e5e0526fdd869
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-09-11 22:10:45 +0000

    debug

--- tools/prepare-python-envs.js
+++ tools/prepare-python-envs.js
@@ -56,7 +56,7 @@ const installWorkspacePythonDependencies = () => {
 
 console.log('✨ Preparing Python dependencies');
 getGitDiffFiles().then((changedFiles) => {
-  console.log(`changedFiles: ${changeFiles}`);
+  console.log(`changedFiles: ${changedFiles}`);
   // if (hasPoetryDefinitionChanged('.', changedFiles)) {
   // console.log('root poetry lock file has changed');
   // installWorkspacePythonDependencies();
