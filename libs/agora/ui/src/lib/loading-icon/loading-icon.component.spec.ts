342f04d43f14daaffc648bf7c475043264b10612
sagely1 <114952739+sagely1@users.noreply.github.com>
2024-09-16 02:51:55 +0000

    adding loading-icon component

---
+++ libs/agora/ui/src/lib/loading-icon/loading-icon.component.spec.ts
@@ -0,0 +1,35 @@
+// -------------------------------------------------------------------------- //
+// External
+// -------------------------------------------------------------------------- //
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+// -------------------------------------------------------------------------- //
+// Internal
+// -------------------------------------------------------------------------- //
+import { LoadingIconComponent } from './loading-icon.component';
+
+// -------------------------------------------------------------------------- //
+// Tests
+// -------------------------------------------------------------------------- //
+describe('Component: Loading Icon', () => {
+  let fixture: ComponentFixture<LoadingIconComponent>;
+  let component: LoadingIconComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LoadingIconComponent],
+      imports: [RouterTestingModule],
+    }).compileComponents();
+  });
+
+  beforeEach(async () => {
+    fixture = TestBed.createComponent(LoadingIconComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
