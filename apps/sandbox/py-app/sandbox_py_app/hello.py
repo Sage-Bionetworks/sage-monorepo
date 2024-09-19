3cb93759f1d1ebdabf8aea7730e0e858093273e7
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-09-19 03:08:44 +0000

    use py-lib in py-app

--- apps/sandbox/py-app/sandbox_py_app/hello.py
+++ apps/sandbox/py-app/sandbox_py_app/hello.py
@@ -1,6 +1,11 @@
+from sandbox_py_lib.hello import hello
+
 """Sample Hello World application."""
 
 
-def hello():
+def hello2():
     """Return a friendly greeting."""
     return "Hello sandbox-py-app"
+
+
+print(hello())
