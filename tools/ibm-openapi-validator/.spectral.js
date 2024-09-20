e045ab3616de3e6e1aa291287b3d24017f251b6d
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-09-20 22:37:40 +0000

    require path segment to be camel case

--- tools/ibm-openapi-validator/.spectral.js
+++ tools/ibm-openapi-validator/.spectral.js
@@ -1,5 +1,5 @@
 const ibmCloudValidationRules = require('@ibm-cloud/openapi-ruleset');
-const { propertyCasingConvention } = require('@ibm-cloud/openapi-ruleset/src/functions');
+const { pathSegmentCasingConvention } = require('@ibm-cloud/openapi-ruleset/src/functions');
 const { schemas } = require('@ibm-cloud/openapi-ruleset-utilities/src/collections');
 
 module.exports = {
@@ -9,20 +9,19 @@ module.exports = {
     'ibm-enum-casing-convention': 'off',
     'ibm-operation-summary-length': 'warn',
     'ibm-parameter-casing-convention': 'off',
-    'ibm-path-segment-casing-convention': 'off',
+    'ibm-path-segment-casing-convention': {
+      description: 'Path segments must follow camel case',
+      message: '{{error}}',
+      resolved: true,
+      given: schemas,
+      severity: 'warn',
+      then: {
+        function: pathSegmentCasingConvention,
+        functionOptions: {
+          type: 'camel',
+        },
+      },
+    },
     'ibm-property-casing-convention': 'off',
-    // 'ibm-path-segment-casing-convention': {
-    //   description: 'Path segments must follow camel case',
-    //   message: '{{error}}',
-    //   resolved: true,
-    //   given: schemas,
-    //   severity: 'warn',
-    //   then: {
-    //     function: propertyCasingConvention,
-    //     functionOptions: {
-    //       type: 'camel',
-    //     },
-    //   },
-    // },
   },
 };
