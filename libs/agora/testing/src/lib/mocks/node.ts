e2c3986824878daf5fe3c616519046615c8d4d1d
sagely1 <114952739+sagely1@users.noreply.github.com>
2024-09-23 18:13:43 +0000

    code cleanup

--- libs/agora/testing/src/lib/mocks/node.ts
+++ libs/agora/testing/src/lib/mocks/node.ts
@@ -1,4 +1,4 @@
 import { setupServer } from 'msw/node';
-import { synapseHandlers as synapseHandlers } from './synapse-handlers';
+import { synapseHandlers } from './synapse-handlers';
 
 export const server = setupServer(...synapseHandlers);
