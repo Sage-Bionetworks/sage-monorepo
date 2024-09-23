8b8a6dcdb4cb0ffb57dc2fd80a1c5993bc0d60c9
sagely1 <114952739+sagely1@users.noreply.github.com>
2024-09-23 18:24:44 +0000

    code cleanup

--- libs/agora/wiki/jest.config.ts
+++ libs/agora/wiki/jest.config.ts
@@ -25,7 +25,4 @@ export default {
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment',
   ],
-  // testEnvironmentOptions: {
-  //   customExportConditions: [''],
-  // },
 };
