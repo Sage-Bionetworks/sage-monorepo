29076a98985d91ccd0bd34e004237828a154a972
sagely1 <114952739+sagely1@users.noreply.github.com>
2024-09-26 23:02:20 +0000

    update footer test, bug fix

--- libs/agora/ui/src/lib/components/footer/footer.component.spec.ts
+++ libs/agora/ui/src/lib/components/footer/footer.component.spec.ts
@@ -1,7 +1,7 @@
-import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { FooterComponent } from './footer.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -9,7 +9,8 @@ describe('FooterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientModule, RouterTestingModule],
+      imports: [],
+      providers: [provideRouter([]), provideHttpClient()],
     }).compileComponents();
   });
 
