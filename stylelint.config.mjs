84fd7943aacff9d278c2dde1e321ec2eb74e96e1
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-09-26 22:53:49 +0000

    add stylelint

---
+++ stylelint.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: ['stylelint-config-standard', 'stylelint-config-recommended-scss'],
+  plugins: ['stylelint-scss'],
+  rules: {},
+};
