86f8c80879debcb5d83af894d570adccb277aeed
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-10-09 23:37:59 +0000

    generate nx plugin

---
+++ libs/sage-monorepo/nx-sage-monorepo/jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+  displayName: 'sage-monorepo',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/libs/sage-monorepo/nx-sage-monorepo',
+};
