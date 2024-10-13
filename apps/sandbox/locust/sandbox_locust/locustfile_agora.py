60c7fc9e12eec151d473c5cb8ea9fd0fa3dbbc2a
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-10-13 23:00:44 +0000

    go headless and add GH workflow

--- apps/sandbox/locust/sandbox_locust/locustfile_agora.py
+++ apps/sandbox/locust/sandbox_locust/locustfile_agora.py
@@ -1,7 +1,9 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, between
 
 
 class AgoraUser(HttpUser):
+    wait_time = between(1, 5)
+
     @task
     def hello_world(self):
         self.client.get("/")
