96062941478d1baea365db8654017268d03f19c6
sagely1 <114952739+sagely1@users.noreply.github.com>
2024-11-04 22:08:15 +0000

    adding svg-image and svg-icon tweaks

--- libs/agora/ui/src/lib/components/svg-image/svg-image.component.spec.ts
+++ libs/agora/ui/src/lib/components/svg-image/svg-image.component.spec.ts
@@ -1,14 +1,12 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { SvgImageComponent } from './svg-image.component';
 import { provideHttpClient } from '@angular/common/http';
-import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
-import { SimpleChanges } from '@angular/core';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('SvgImageComponent', () => {
   let fixture: ComponentFixture<SvgImageComponent>;
   let component: SvgImageComponent;
   let element: HTMLElement;
-  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -19,12 +17,6 @@ describe('SvgImageComponent', () => {
     fixture = TestBed.createComponent(SvgImageComponent);
     component = fixture.componentInstance;
     element = fixture.nativeElement;
-    httpMock = TestBed.inject(HttpTestingController);
-  });
-
-  afterEach(() => {
-    // Check that there are no outstanding requests
-    httpMock.verify();
   });
 
   it('should create', () => {
@@ -33,42 +25,13 @@ describe('SvgImageComponent', () => {
 
   it('should populate innerHTML in the div when imagePath changes', () => {
     // Mock input change to trigger ngOnChanges
-    component.imagePath = 'assets/images/sample.svg';
-
-    // Simulate ngOnChanges call
-    const changes: SimpleChanges = {
-      imagePath: {
-        currentValue: component.imagePath,
-        previousValue: null,
-        firstChange: true,
-        isFirstChange: () => true,
-      },
-    };
-    component.ngOnChanges(changes); // Manually trigger ngOnChanges
-
-    // Mock the HTTP request for the SVG file
-    const req = httpMock.expectOne('assets/images/sample.svg');
-    const mockSvgContent = `<svg
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        viewBox="0 0 20 20"
-        xml:space="preserve"
-      >
-        <circle class="svg-icon-bg" fill="transparent" cx="10" cy="10" r="9.5" />
-        <path
-          d="M9,15h2V9H9V15z M10,0C4.5,0,0,4.5,0,10s4.5,10,10,10s10-4.5,10-10S15.5,0,10,0z M10,18c-4.4,0-8-3.6-8-8s3.6-8,8-8 s8,3.6,8,8S14.4,18,10,18z M9,7h2V5H9V7z"
-          fill="currentColor"
-        />
-      </svg>`;
-    req.flush(mockSvgContent); // Simulate a successful HTTP response
+    component.imagePath = '/agora-assets/images/background1.svg';
 
     // Trigger change detection
     fixture.detectChanges();
 
     // Now the div should be populated with the sanitized SVG content
-    const divElement = element.querySelector('div');
-    expect(divElement?.innerHTML).toBeTruthy(); // Check that innerHTML is populated
-    expect(divElement?.innerHTML).toContain('svg'); // Check that it contains the SVG content
+    const parentElement = element.querySelector('img');
+    expect(parentElement).toBeTruthy(); // Check that innerHTML is populated
   });
 });
