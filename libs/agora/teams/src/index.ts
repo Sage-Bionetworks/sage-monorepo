29f7ce68c291a551c3df76d2dfae6fff80deac92
sagely1 <114952739+sagely1@users.noreply.github.com>
2024-11-04 21:07:22 +0000

    standardizing teams file structure and syntax

--- libs/agora/teams/src/index.ts
+++ libs/agora/teams/src/index.ts
@@ -1,3 +1,3 @@
-export * from './lib/teams/teams.routes';
+export * from './lib/teams.routes';
 export * from './lib/team-list/team-list.routes';
 export * from './lib/team-member-list/team-member-list.routes';
