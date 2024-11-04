730d8ef80f1504b456017b57c6578c60ff9e2e37
sagely1 <114952739+sagely1@users.noreply.github.com>
2024-11-04 21:07:22 +0000

    standardizing teams file structure and syntax

--- libs/agora/teams/src/lib/teams.component.ts
+++ libs/agora/teams/src/lib/teams.component.ts
@@ -2,16 +2,16 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Team, TeamsList, TeamsService } from '@sagebionetworks/agora/api-client-angular';
 import { HelperService } from '@sagebionetworks/agora/services';
-import { TeamListComponent } from '../team-list/team-list.component';
+import { TeamListComponent } from './team-list/team-list.component';
 import { catchError, finalize, map, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'agora-teams',
   standalone: true,
   imports: [CommonModule, TeamListComponent],
+  providers: [HelperService, TeamsService],
   templateUrl: './teams.component.html',
   styleUrls: ['./teams.component.scss'],
-  providers: [HelperService, TeamsService],
 })
 export class TeamsComponent implements OnInit {
   teams$!: Observable<Team[]>;
