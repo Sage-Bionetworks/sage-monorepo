badaf77333e4431727c0ee2c95f49f9981aade6b
rrchai <73901500+rrchai@users.noreply.github.com>
2024-11-09 05:24:40 +0000

    revert changes for current not found page

--- libs/openchallenges/not-found/src/lib/not-found.component.ts
+++ libs/openchallenges/not-found/src/lib/not-found.component.ts
@@ -4,26 +4,27 @@ import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { ConfigService } from '@sagebionetworks/openchallenges/config';
-// import { FooterComponent } from '@sagebionetworks/openchallenges/ui';
+import { FooterComponent } from '@sagebionetworks/openchallenges/ui';
 
 @Component({
   selector: 'openchallenges-not-found',
   standalone: true,
-  imports: [CommonModule, RouterModule, MatCardModule, MatButtonModule],
+  imports: [CommonModule, RouterModule, MatCardModule, MatButtonModule, FooterComponent],
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss'],
 })
 export class NotFoundComponent {
-  // public appVersion: string;
-  // public dataUpdatedOn: string;
-  // public privacyPolicyUrl: string;
-  // public termsOfUseUrl: string;
-  // public apiDocsUrl: string;
-  // constructor(private readonly configService: ConfigService) {
-  //   this.appVersion = this.configService.config.appVersion;
-  //   this.dataUpdatedOn = this.configService.config.dataUpdatedOn;
-  //   this.privacyPolicyUrl = this.configService.config.privacyPolicyUrl;
-  //   this.termsOfUseUrl = this.configService.config.termsOfUseUrl;
-  //   this.apiDocsUrl = this.configService.config.apiDocsUrl;
-  // }
+  public appVersion: string;
+  public dataUpdatedOn: string;
+  public privacyPolicyUrl: string;
+  public termsOfUseUrl: string;
+  public apiDocsUrl: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.appVersion = this.configService.config.appVersion;
+    this.dataUpdatedOn = this.configService.config.dataUpdatedOn;
+    this.privacyPolicyUrl = this.configService.config.privacyPolicyUrl;
+    this.termsOfUseUrl = this.configService.config.termsOfUseUrl;
+    this.apiDocsUrl = this.configService.config.apiDocsUrl;
+  }
 }
