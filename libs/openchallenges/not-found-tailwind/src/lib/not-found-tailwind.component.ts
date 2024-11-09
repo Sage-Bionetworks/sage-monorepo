a0aae838a2b6677c9588c94d560b391d10f7ef75
rrchai <73901500+rrchai@users.noreply.github.com>
2024-11-09 05:22:38 +0000

    inital migration to tailwind for not-found page

--- libs/openchallenges/not-found-tailwind/src/lib/not-found-tailwind.component.ts
+++ libs/openchallenges/not-found-tailwind/src/lib/not-found-tailwind.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'openchallenges-not-found-tailwind',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, MatButtonModule],
   templateUrl: './not-found-tailwind.component.html',
   styleUrl: './not-found-tailwind.component.css',
 })
