e391dc891de6e5f77e564592ab6910d08a0c289e
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-12-02 19:25:18 -0800

    feat(sage-monorepo): create project template for developing AWS Lambda node.js 20.x (ARCH-351) (#2932)

---
+++ docker/sandbox/serve-detach.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+args=(
+  # List of services in alphanumeric order
+  --file docker/sandbox/services/lambda-nodejs.yml
+
+  --file docker/sandbox/networks.yml
+  --file docker/sandbox/volumes.yml
+
+  up $1 --detach --remove-orphans
+)
+
+docker compose "${args[@]}"
