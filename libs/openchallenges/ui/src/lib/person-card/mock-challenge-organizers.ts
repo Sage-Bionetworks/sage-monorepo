7070281e8aa8866126cbec0a84c35bbc49993ae8
verena <9377970+vpchung@users.noreply.github.com>
2024-12-19 00:02:26 +0000

    remove dummy values from mock lists

--- libs/openchallenges/ui/src/lib/person-card/mock-challenge-organizers.ts
+++ libs/openchallenges/ui/src/lib/person-card/mock-challenge-organizers.ts
@@ -1,18 +1,18 @@
 import { ChallengeOrganizer } from './challenge-organizer';
 
 export const MOCK_CHALLENGE_ORGANIZERS: ChallengeOrganizer[] = [
-  {
-    id: '',
-    challengeId: '1',
-    login: 'awesome-lead',
-    name: 'Awesome Lead',
-    roles: ['ChallengeLead'],
-  },
-  {
-    id: '',
-    challengeId: '2',
-    name: 'Awesome Engineer',
-    login: 'awesome-engineer',
-    roles: ['InfrastructureLead'],
-  },
+  // {
+  //   id: '',
+  //   challengeId: '1',
+  //   login: 'awesome-lead',
+  //   name: 'Awesome Lead',
+  //   roles: ['ChallengeLead'],
+  // },
+  // {
+  //   id: '',
+  //   challengeId: '2',
+  //   name: 'Awesome Engineer',
+  //   login: 'awesome-engineer',
+  //   roles: ['InfrastructureLead'],
+  // },
 ];
