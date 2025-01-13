f83caf601fc2909f42ef558fe4a54fc4f622de64
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-01-13 15:05:42 -0800

    chore: remove OC Prometheus, Grafana and mysql-exporter (#2963)

--- apps/openchallenges/grafana/tools/backup-to-s3.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-docker run \
-  --rm \
-  --name grafana-backup-tool \
-  --env-file tools/grafana-backup-tool/.env \
-  --network openchallenges \
-  ysde/docker-grafana-backup-tool:1.2.6-slim
