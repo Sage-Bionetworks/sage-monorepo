bef30ec7fc68ffa213528f002dd32d4e9765d9c9
verena <9377970+vpchung@users.noreply.github.com>
2025-01-14 22:53:41 +0000

    extract all config parameters from env

--- apps/openchallenges/data-lambda/openchallenges_data_lambda/app.py
+++ apps/openchallenges/data-lambda/openchallenges_data_lambda/app.py
@@ -74,17 +74,21 @@ def lambda_handler(event, context):
     }
 
 
-def write_credentials_file(template, output_json):
+def write_credentials_file(output_json):
     """Write credentials JSON file for Google Sheets API authentication."""
     with open(output_json, "w") as out:
         credentials = {
-            **template.get("google_sheets_api_auth"),
-            # "project_id": os.getenv("PROJECT_ID"),
-            # "private_key_id": os.getenv("PRIVATE_KEY_ID"),
-            # "private_key": os.getenv("PRIVATE_KEY"),
-            # "client_email": os.getenv("client_email"),
-            # "client_id": os.getenv("CLIENT_ID"),
-            # "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL")
+            "type": os.getenv("TYPE"),
+            "project_id": os.getenv("PROJECT_ID"),
+            "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+            "private_key": os.getenv("PRIVATE_KEY"),
+            "client_email": os.getenv("client_email"),
+            "client_id": os.getenv("CLIENT_ID"),
+            "auth_uri": os.getenv("AUTH_URI"),
+            "token_uri": os.getenv("TOKEN_URI"),
+            "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+            "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
+            "universe_domain": os.getenv("UNIVERSE_DOMAIN"),
         }
         out.write(json.dumps(credentials))
 
