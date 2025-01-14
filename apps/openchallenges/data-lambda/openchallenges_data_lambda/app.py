fda911d42334508e9722d6ba1c12f025219e4157
verena <9377970+vpchung@users.noreply.github.com>
2025-01-14 22:14:22 +0000

    implement writing credentials file for Google Sheets API

--- apps/openchallenges/data-lambda/openchallenges_data_lambda/app.py
+++ apps/openchallenges/data-lambda/openchallenges_data_lambda/app.py
@@ -1,9 +1,12 @@
+import os
 import json
+
 import gspread
 import numpy as np
 import pandas as pd
 
 
+GOOGLE_SHEET_CREDENTIALS_FILE = "service_account.json"
 GOOGLE_SHEET_TITLE = "OpenChallenges Data"
 
 
@@ -29,45 +32,61 @@ def lambda_handler(event, context):
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
 
-    google_client = gspread.service_account(filename="service_account.json")
+    write_credentials_file(event, GOOGLE_SHEET_CREDENTIALS_FILE)
 
     try:
-        wks = google_client.open(GOOGLE_SHEET_TITLE)
-
-        platforms = get_platform_data(wks)
-        print(platforms.head())
-
-        roles = get_roles(wks)
-        print(roles.head())
-
-        categories = get_challenge_categories(wks)
-        print(categories.head())
-
-        organizations = get_organization_data(wks)
-        print(organizations.head())
-
-        edam_data_annotations = get_edam_annotations(wks)
-        print(edam_data_annotations.head())
-
-        challenges, incentives, sub_types = get_challenge_data(wks)
-        print(challenges.head())
-        print(incentives.head())
-        print(sub_types.head())
-
-        message = "Data successfully pulled from OC Data google sheet."
-
+        google_client = gspread.service_account(filename=GOOGLE_SHEET_CREDENTIALS_FILE)
     except Exception as err:
-        message = (
-            f"Something went wrong with pulling the data: {err}."
-            "\n\nPlease see logs for more details."
-        )
+        message = "Private key not found in the credentials file. Please try again."
+    else:
+        try:
+            wks = google_client.open(GOOGLE_SHEET_TITLE)
+
+            platforms = get_platform_data(wks)
+            print(platforms.head())
+
+            roles = get_roles(wks)
+            print(roles.head())
+
+            categories = get_challenge_categories(wks)
+            print(categories.head())
+
+            organizations = get_organization_data(wks)
+            print(organizations.head())
+
+            edam_data_annotations = get_edam_annotations(wks)
+            print(edam_data_annotations.head())
+
+            challenges, incentives, sub_types = get_challenge_data(wks)
+            print(challenges.head())
+            print(incentives.head())
+            print(sub_types.head())
+
+            message = "Data successfully pulled from OC Data google sheet."
+
+        except Exception as err:
+            message = f"Something went wrong with pulling the data: {err}."
 
     data = {"message": message}
-
     return {
         "statusCode": 200,
         "body": json.dumps(data),
     }
+
+
+def write_credentials_file(template, output_json):
+    """Write credentials JSON file for Google Sheets API authentication."""
+    with open(output_json, "w") as out:
+        credentials = {
+            **template.get("google_sheets_api_auth"),
+            # "project_id": os.getenv("PROJECT_ID"),
+            # "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+            # "private_key": os.getenv("PRIVATE_KEY"),
+            # "client_email": os.getenv("client_email"),
+            # "client_id": os.getenv("CLIENT_ID"),
+            # "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL")
+        }
+        out.write(json.dumps(credentials))
 
 
 def get_challenge_data(wks, sheet_name="challenges"):
