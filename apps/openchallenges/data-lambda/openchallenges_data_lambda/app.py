e0470f1194debd8842767cf5a67ace561d12b0a8
verena <9377970+vpchung@users.noreply.github.com>
2025-01-16 20:03:37 +0000

    extract data pull functions into a separate module

--- apps/openchallenges/data-lambda/openchallenges_data_lambda/app.py
+++ apps/openchallenges/data-lambda/openchallenges_data_lambda/app.py
@@ -2,8 +2,8 @@ import os
 import json
 
 import gspread
-import numpy as np
-import pandas as pd
+
+import oc_data_sheet
 
 
 GOOGLE_SHEET_CREDENTIALS_FILE = "service_account.json"
@@ -43,22 +43,22 @@ def lambda_handler(event, context):
         try:
             wks = google_client.open(GOOGLE_SHEET_TITLE)
 
-            platforms = get_platform_data(wks)
+            platforms = oc_data_sheet.get_platform_data(wks)
             print(platforms.head())
 
-            roles = get_roles(wks)
+            roles = oc_data_sheet.get_roles(wks)
             print(roles.head())
 
-            categories = get_challenge_categories(wks)
+            categories = oc_data_sheet.get_challenge_categories(wks)
             print(categories.head())
 
-            organizations = get_organization_data(wks)
+            organizations = oc_data_sheet.get_organization_data(wks)
             print(organizations.head())
 
-            edam_data_annotations = get_edam_annotations(wks)
+            edam_data_annotations = oc_data_sheet.get_edam_annotations(wks)
             print(edam_data_annotations.head())
 
-            challenges, incentives, sub_types = get_challenge_data(wks)
+            challenges, incentives, sub_types = oc_data_sheet.get_challenge_data(wks)
             print(challenges.head())
             print(incentives.head())
             print(sub_types.head())
@@ -94,186 +94,6 @@ def write_credentials_file(output_json):
             "universe_domain": os.getenv("UNIVERSE_DOMAIN"),
         }
         out.write(json.dumps(credentials))
-
-
-def get_challenge_data(wks, sheet_name="challenges"):
-    """Get challenges data and clean up as needed.
-
-    Output:
-        - challenges
-        - challenge incentives
-        - challenge submission types
-    """
-    df = pd.DataFrame(wks.worksheet(sheet_name).get_all_records()).fillna("")
-    df.loc[df._platform == "Other", "platform"] = "\\N"
-
-    challenges = df[
-        [
-            "id",
-            "slug",
-            "name",
-            "headline",
-            "description",
-            "avatar_url",
-            "website_url",
-            "status",
-            "platform",
-            "doi",
-            "start_date",
-            "end_date",
-            "operation_id",
-            "created_at",
-            "updated_at",
-        ]
-    ]
-    challenges = (
-        challenges.replace({r"\s+$": "", r"^\s+": ""}, regex=True)
-        .replace(r"\n", " ", regex=True)
-        .replace("'", "''")
-        .replace("\u2019", "''", regex=True)  # replace curly right-quote
-        .replace("\u202f", " ", regex=True)  # replace narrow no-break space
-        .replace("\u2060", "", regex=True)  # remove word joiner
-    )
-    challenges["headline"] = (
-        challenges["headline"]
-        .astype(str)
-        .apply(lambda x: x[:76] + "..." if len(x) > 80 else x)
-    )
-    challenges["description"] = (
-        challenges["description"]
-        .astype(str)
-        .apply(lambda x: x[:995] + "..." if len(x) > 1000 else x)
-    )
-    challenges.loc[challenges.start_date == "", "start_date"] = "\\N"
-    challenges.loc[challenges.end_date == "", "end_date"] = "\\N"
-    challenges.loc[challenges.operation_id == "", "operation_id"] = "\\N"
-
-    incentives = pd.concat(
-        [
-            df[df.monetary_incentive == "TRUE"][["id", "created_at"]].assign(
-                incentives="monetary"
-            ),
-            df[df.publication_incentive == "TRUE"][["id", "created_at"]].assign(
-                incentives="publication"
-            ),
-            df[df.speaking_incentive == "TRUE"][["id", "created_at"]].assign(
-                incentives="speaking_engagement"
-            ),
-            df[df.other_incentive == "TRUE"][["id", "created_at"]].assign(
-                incentives="other"
-            ),
-        ]
-    ).rename(columns={"id": "challenge_id"})
-    incentives["incentives"] = pd.Categorical(
-        incentives["incentives"],
-        categories=["monetary", "publication", "speaking_engagement", "other"],
-    )
-    incentives = incentives.sort_values(["challenge_id", "incentives"])
-    incentives.index = np.arange(1, len(incentives) + 1)
-
-    sub_types = pd.concat(
-        [
-            df[df.file_submission == "TRUE"][["id", "created_at"]].assign(
-                submission_types="prediction_file"
-            ),
-            df[df.container_submission == "TRUE"][["id", "created_at"]].assign(
-                submission_types="container_image"
-            ),
-            df[df.notebook_submission == "TRUE"][["id", "created_at"]].assign(
-                submission_types="notebook"
-            ),
-            df[df.mlcube_submission == "TRUE"][["id", "created_at"]].assign(
-                submission_types="mlcube"
-            ),
-            df[df.other_submission == "TRUE"][["id", "created_at"]].assign(
-                submission_types="other"
-            ),
-        ]
-    ).rename(columns={"id": "challenge_id"})
-    sub_types["submission_types"] = pd.Categorical(
-        sub_types["submission_types"],
-        categories=[
-            "prediction_file",
-            "container_image",
-            "notebook",
-            "mlcube",
-            "other",
-        ],
-    )
-    sub_types = sub_types.sort_values(["challenge_id", "submission_types"])
-    sub_types.index = np.arange(1, len(sub_types) + 1)
-
-    return (
-        challenges,
-        incentives[["incentives", "challenge_id", "created_at"]],
-        sub_types[["submission_types", "challenge_id", "created_at"]],
-    )
-
-
-def get_challenge_categories(wks, sheet_name="challenge_category"):
-    """Get challenge categories."""
-    return pd.DataFrame(wks.worksheet(sheet_name).get_all_records()).fillna("")[
-        ["id", "challenge_id", "category"]
-    ]
-
-
-def get_platform_data(wks, sheet_name="platforms"):
-    """Get platform data and clean up as needed."""
-    platforms = pd.DataFrame(wks.worksheet(sheet_name).get_all_records()).fillna("")
-    return platforms[platforms._public == "TRUE"][
-        ["id", "slug", "name", "avatar_url", "website_url", "created_at", "updated_at"]
-    ]
-
-
-def get_organization_data(wks, sheet_name="organizations"):
-    """Get organization data and clean up as needed."""
-    organizations = pd.DataFrame(wks.worksheet(sheet_name).get_all_records()).fillna("")
-    organizations = organizations[organizations._public == "TRUE"][
-        [
-            "id",
-            "name",
-            "login",
-            "avatar_url",
-            "website_url",
-            "description",
-            "challenge_count",
-            "created_at",
-            "updated_at",
-            "acronym",
-        ]
-    ]
-    organizations = (
-        organizations.replace({r"\s+$": "", r"^\s+": ""}, regex=True)
-        .replace(r"\n", " ", regex=True)
-        .replace("'", "''")
-        .replace("\u2019", "''", regex=True)  # replace curly right-quote
-        .replace("\u202f", " ", regex=True)  # replace narrow no-break space
-        .replace("\u2060", "", regex=True)  # remove word joiner
-    )
-    organizations["description"] = (
-        organizations["description"]
-        .astype(str)
-        .apply(lambda x: x[:995] + "..." if len(x) > 1000 else x)
-    )
-    return organizations
-
-
-def get_roles(wks, sheet_name="contribution_role"):
-    """Get data on organization's role(s) in challenges."""
-    return (
-        pd.DataFrame(wks.worksheet(sheet_name).get_all_records())
-        .fillna("")
-        .drop(["_challenge", "_organization"], axis=1)
-    )
-
-
-def get_edam_annotations(wks, sheet_name="challenge_data"):
-    """Get data on challenge's EDAM annotations."""
-    return (
-        pd.DataFrame(wks.worksheet(sheet_name).get_all_records())
-        .fillna("")
-        .drop(["_challenge", "_edam_name"], axis=1)
-    )
 
 
 if __name__ == "__main__":
