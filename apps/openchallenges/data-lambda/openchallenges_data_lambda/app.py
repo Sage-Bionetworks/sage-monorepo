6772f63c9a0e4b963add9adbf50115fb02aaa33b
verena <9377970+vpchung@users.noreply.github.com>
2025-01-16 20:04:40 +0000

    add default "" value for PRIVATE_KEY to allow for encoding

--- apps/openchallenges/data-lambda/openchallenges_data_lambda/app.py
+++ apps/openchallenges/data-lambda/openchallenges_data_lambda/app.py
@@ -84,7 +84,9 @@ def write_credentials_file(output_json):
             "type": os.getenv("TYPE"),
             "project_id": os.getenv("PROJECT_ID"),
             "private_key_id": os.getenv("PRIVATE_KEY_ID"),
-            "private_key": os.getenv("PRIVATE_KEY").encode().decode("unicode_escape"),
+            "private_key": os.getenv("PRIVATE_KEY", "")
+            .encode()
+            .decode("unicode_escape"),
             "client_email": os.getenv("CLIENT_EMAIL"),
             "client_id": os.getenv("CLIENT_ID"),
             "auth_uri": os.getenv("AUTH_URI"),
