6f09736857bab7bd3caa1a28e3f5550925f39007
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-02-04 22:08:12 +0000

    chore: remove `sandbox-lambda-python`

--- docker/sandbox/serve-detach.sh
+++ docker/sandbox/serve-detach.sh
@@ -3,7 +3,6 @@
 args=(
   # List of services in alphanumeric order
   --file docker/sandbox/services/lambda-nodejs.yml
-  --file docker/sandbox/services/lambda-python.yml
 
   --file docker/sandbox/networks.yml
   --file docker/sandbox/volumes.yml
