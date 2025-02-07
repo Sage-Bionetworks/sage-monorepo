f7f62695b56ed5fb61ab435fb0f67bc140faf028
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-02-07 11:24:48 -0800

    chore: prepare Python projects in parallel and update active dev container (SMR-14) (#3000)

--- dev-env.sh
+++ dev-env.sh
@@ -32,9 +32,9 @@ function workspace-install {
   workspace-install-python-dependencies
   nx run-many --target=create-config
   # Projects that can be prepared in parallel
-  nx run-many --target=prepare --projects=!tag:language:java,!tag:language:python
-  # Python and Java projects must be installed one at a time
-  nx run-many --target=prepare --projects=tag:language:java,tag:language:python --parallel=1
+  nx run-many --target=prepare --projects=!tag:language:java
+  # Java projects must be installed one at a time
+  nx run-many --target=prepare --projects=tag:language:java --parallel=1
 }
 
 function workspace-install-affected {
@@ -42,9 +42,9 @@ function workspace-install-affected {
   workspace-install-python-dependencies
   nx affected --target=create-config
   # Projects that can be prepared in parallel
-  nx affected --target=prepare --exclude='tag:language:java,tag:language:python'
-  # Python and Java projects must be installed one at a time
-  nx affected --target=prepare --exclude='!tag:language:java,!tag:language:python' --parallel=1
+  nx affected --target=prepare --exclude='tag:language:java'
+  # Java projects must be installed one at a time
+  nx affected --target=prepare --exclude='!tag:language:java' --parallel=1
 }
 
 # Setup Python virtualenvs
--- tools/workspace-install.sh
+++ tools/workspace-install.sh
@@ -9,7 +9,7 @@ uv sync
 # Prepare projects
 pnpm dlx nx run-many --target=create-config
 # Projects that can be prepared in parallel
-pnpm dlx nx run-many --target=prepare --projects=!tag:language:java,!tag:language:python
+pnpm dlx nx run-many --target=prepare --projects=!tag:language:java
 # Python and Java projects must be installed one at a time
-pnpm dlx nx run-many --target=prepare --projects=tag:language:java,tag:language:python --parallel=1
+pnpm dlx nx run-many --target=prepare --projects=tag:language:java --parallel=1
 
