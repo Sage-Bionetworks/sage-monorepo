7cdbe82f7bf198722645e8aa43f8898076515909
verena <9377970+vpchung@users.noreply.github.com>
2025-02-14 11:27:09 +0000

    reformat; remove commented out lines

--- apps/openchallenges/data-lambda/openchallenges_data_lambda/app.py
+++ apps/openchallenges/data-lambda/openchallenges_data_lambda/app.py
@@ -14,6 +14,27 @@ GOOGLE_SHEET_CREDENTIALS_FILE = "service_account.json"
 GOOGLE_SHEET_TITLE = "OpenChallenges Data"
 
 
+def write_credentials_file(output_json):
+    """Write credentials JSON file for Google Sheets API authentication."""
+    with open(output_json, "w") as out:
+        credentials = {
+            "type": os.getenv("TYPE"),
+            "project_id": os.getenv("PROJECT_ID"),
+            "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+            "private_key": os.getenv("PRIVATE_KEY", "")
+            .encode()
+            .decode("unicode_escape"),
+            "client_email": os.getenv("CLIENT_EMAIL"),
+            "client_id": os.getenv("CLIENT_ID"),
+            "auth_uri": os.getenv("AUTH_URI"),
+            "token_uri": os.getenv("TOKEN_URI"),
+            "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+            "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
+            "universe_domain": os.getenv("UNIVERSE_DOMAIN"),
+        }
+        out.write(json.dumps(credentials))
+
+
 def connect_to_db(db: str = "challenge_service") -> mariadb.Connection:
     """Establishes connection to the MariaDB database."""
     credentials = {
@@ -87,6 +108,9 @@ def update_table(conn: mariadb.Connection, table_name: str, data: pd.DataFrame):
     """Updates the specified table."""
     truncate_table(conn, table_name)
     insert_data(conn, table_name, data)
+
+
+def lambda_handler(event, context) -> dict:
     """Sample pure Lambda function
 
     Parameters
@@ -119,29 +143,18 @@ def update_table(conn: mariadb.Connection, table_name: str, data: pd.DataFrame):
         try:
             wks = google_client.open(GOOGLE_SHEET_TITLE)
 
-            platforms = oc_data_sheet.get_platform_data(wks)
-            print(platforms.head())
+            platforms = oc_data_sheet.get_platform_data(wks).rename(
+                columns={"avatar_url": "avatar_key"}
+            )
+            platforms["avatar_url"] = (
+                ""  # FIXME: table in local db has this column for some reason?
+            )
 
             # roles = oc_data_sheet.get_roles(wks)
-            # print(roles.head())
-
             # categories = oc_data_sheet.get_challenge_categories(wks)
-            # print(categories.head())
-
             # organizations = oc_data_sheet.get_organization_data(wks)
-            # print(organizations.head())
-
             # edam_data_annotations = oc_data_sheet.get_edam_annotations(wks)
-            # print(edam_data_annotations.head())
-
             # challenges, incentives, sub_types = oc_data_sheet.get_challenge_data(wks)
-            # print(challenges.head())
-            # print(incentives.head())
-            # print(sub_types.head())
-
-            status_code = 200
-            message = "Data successfully pulled from OC Data google sheet."
-
         except Exception as err:
             status_code = 400
             message = f"Something went wrong with pulling the data: {err}."
@@ -159,6 +172,8 @@ def update_table(conn: mariadb.Connection, table_name: str, data: pd.DataFrame):
 
         conn = connect_to_db()
         update_table(conn, table_name="challenge_platform", data=platforms)
+        # update_table(conn, table_name="challenge", data=challenges)
+        # update_table(conn, table_name="challenge_contribution", data=roles)
         conn.close()
 
         status_code = 200
@@ -172,27 +187,6 @@ def update_table(conn: mariadb.Connection, table_name: str, data: pd.DataFrame):
         "statusCode": status_code,
         "body": json.dumps(data),
     }
-
-
-def write_credentials_file(output_json):
-    """Write credentials JSON file for Google Sheets API authentication."""
-    with open(output_json, "w") as out:
-        credentials = {
-            "type": os.getenv("TYPE"),
-            "project_id": os.getenv("PROJECT_ID"),
-            "private_key_id": os.getenv("PRIVATE_KEY_ID"),
-            "private_key": os.getenv("PRIVATE_KEY", "")
-            .encode()
-            .decode("unicode_escape"),
-            "client_email": os.getenv("CLIENT_EMAIL"),
-            "client_id": os.getenv("CLIENT_ID"),
-            "auth_uri": os.getenv("AUTH_URI"),
-            "token_uri": os.getenv("TOKEN_URI"),
-            "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
-            "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
-            "universe_domain": os.getenv("UNIVERSE_DOMAIN"),
-        }
-        out.write(json.dumps(credentials))
 
 
 if __name__ == "__main__":
