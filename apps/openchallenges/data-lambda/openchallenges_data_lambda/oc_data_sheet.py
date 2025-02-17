5e3e74b0133c5dff07a074a2f4243dd99a7a77ab
verena <9377970+vpchung@users.noreply.github.com>
2025-02-17 22:17:52 +0000

    replace \\N with None; rename select columns

--- apps/openchallenges/data-lambda/openchallenges_data_lambda/oc_data_sheet.py
+++ apps/openchallenges/data-lambda/openchallenges_data_lambda/oc_data_sheet.py
@@ -2,6 +2,18 @@ import numpy as np
 import pandas as pd
 
 
+def _reformat_df_values(df: pd.DataFrame) -> pd.DataFrame:
+    df = (
+        df.replace({r"\s+$": "", r"^\s+": ""}, regex=True)
+        .replace(r"\n", " ", regex=True)
+        .replace("'", "''")
+        .replace("\u2019", "''", regex=True)  # replace curly right-quote
+        .replace("\u202f", " ", regex=True)  # replace narrow no-break space
+        .replace("\u2060", "", regex=True)  # remove word joiner
+    )
+    return df
+
+
 def get_challenge_data(wks, sheet_name="challenges"):
     """Get challenges data and clean up as needed.
 
@@ -11,8 +23,9 @@ def get_challenge_data(wks, sheet_name="challenges"):
         - challenge submission types
     """
     df = pd.DataFrame(wks.worksheet(sheet_name).get_all_records()).fillna("")
-    df.loc[df._platform == "Other", "platform"] = "\\N"
+    df.loc[df._platform == "Other", "platform"] = None
 
+    # Challenges
     challenges = df[
         [
             "id",
@@ -32,72 +45,65 @@ def get_challenge_data(wks, sheet_name="challenges"):
             "updated_at",
         ]
     ]
-    challenges = (
-        challenges.replace({r"\s+$": "", r"^\s+": ""}, regex=True)
-        .replace(r"\n", " ", regex=True)
-        .replace("'", "''")
-        .replace("\u2019", "''", regex=True)  # replace curly right-quote
-        .replace("\u202f", " ", regex=True)  # replace narrow no-break space
-        .replace("\u2060", "", regex=True)  # remove word joiner
-    )
+    challenges = _reformat_df_values(challenges)
     challenges["headline"] = (
         challenges["headline"]
         .astype(str)
-        .apply(lambda x: x[:76] + "..." if len(x) > 80 else x)
+        .apply(lambda x: x[:75] + "..." if len(x) > 80 else x)
     )
     challenges["description"] = (
         challenges["description"]
         .astype(str)
         .apply(lambda x: x[:995] + "..." if len(x) > 1000 else x)
     )
-    challenges.loc[challenges.start_date == "", "start_date"] = "\\N"
-    challenges.loc[challenges.end_date == "", "end_date"] = "\\N"
-    challenges.loc[challenges.operation_id == "", "operation_id"] = "\\N"
+    challenges.loc[challenges.start_date == "", "start_date"] = None
+    challenges.loc[challenges.end_date == "", "end_date"] = None
+    challenges.loc[challenges.operation_id == "", "operation_id"] = None
 
+    # Challenge incentive(s)
     incentives = pd.concat(
         [
             df[df.monetary_incentive == "TRUE"][["id", "created_at"]].assign(
-                incentives="monetary"
+                name="monetary"
             ),
             df[df.publication_incentive == "TRUE"][["id", "created_at"]].assign(
-                incentives="publication"
+                name="publication"
             ),
             df[df.speaking_incentive == "TRUE"][["id", "created_at"]].assign(
-                incentives="speaking_engagement"
+                name="speaking_engagement"
             ),
-            df[df.other_incentive == "TRUE"][["id", "created_at"]].assign(
-                incentives="other"
-            ),
+            df[df.other_incentive == "TRUE"][["id", "created_at"]].assign(name="other"),
         ]
     ).rename(columns={"id": "challenge_id"})
-    incentives["incentives"] = pd.Categorical(
-        incentives["incentives"],
+    incentives["name"] = pd.Categorical(
+        incentives["name"],
         categories=["monetary", "publication", "speaking_engagement", "other"],
     )
-    incentives = incentives.sort_values(["challenge_id", "incentives"])
+    incentives = incentives.sort_values(["challenge_id", "name"])
     incentives.index = np.arange(1, len(incentives) + 1)
 
+    # Challenge submission type(s)
     sub_types = pd.concat(
         [
             df[df.file_submission == "TRUE"][["id", "created_at"]].assign(
-                submission_types="prediction_file"
+                name="prediction_file"
             ),
             df[df.container_submission == "TRUE"][["id", "created_at"]].assign(
-                submission_types="container_image"
+                name="container_image"
             ),
             df[df.notebook_submission == "TRUE"][["id", "created_at"]].assign(
-                submission_types="notebook"
+                name="notebook"
             ),
             df[df.mlcube_submission == "TRUE"][["id", "created_at"]].assign(
-                submission_types="mlcube"
+                name="mlcube"
             ),
             df[df.other_submission == "TRUE"][["id", "created_at"]].assign(
-                submission_types="other"
+                name="other"
             ),
         ]
     ).rename(columns={"id": "challenge_id"})
-    sub_types["submission_types"] = pd.Categorical(
-        sub_types["submission_types"],
+    sub_types["name"] = pd.Categorical(
+        sub_types["name"],
         categories=[
             "prediction_file",
             "container_image",
@@ -106,28 +112,34 @@ def get_challenge_data(wks, sheet_name="challenges"):
             "other",
         ],
     )
-    sub_types = sub_types.sort_values(["challenge_id", "submission_types"])
+    sub_types = sub_types.sort_values(["challenge_id", "name"])
     sub_types.index = np.arange(1, len(sub_types) + 1)
 
     return (
-        challenges,
-        incentives[["incentives", "challenge_id", "created_at"]],
-        sub_types[["submission_types", "challenge_id", "created_at"]],
+        challenges.rename(columns={"platform": "platform_id"}),
+        incentives[["name", "challenge_id", "created_at"]],
+        sub_types[["name", "challenge_id", "created_at"]],
     )
 
 
 def get_challenge_categories(wks, sheet_name="challenge_category"):
     """Get challenge categories."""
-    return pd.DataFrame(wks.worksheet(sheet_name).get_all_records()).fillna("")[
-        ["id", "challenge_id", "category"]
-    ]
+    return (
+        pd.DataFrame(wks.worksheet(sheet_name).get_all_records())
+        .fillna("")
+        .rename(columns={"category": "name"})[["id", "challenge_id", "name"]]
+    )
 
 
 def get_platform_data(wks, sheet_name="platforms"):
     """Get platform data and clean up as needed."""
-    platforms = pd.DataFrame(wks.worksheet(sheet_name).get_all_records()).fillna("")
+    platforms = (
+        pd.DataFrame(wks.worksheet(sheet_name).get_all_records())
+        .fillna("")
+        .rename(columns={"avatar_url": "avatar_key"})
+    )
     return platforms[platforms._public == "TRUE"][
-        ["id", "slug", "name", "avatar_url", "website_url", "created_at", "updated_at"]
+        ["id", "slug", "name", "avatar_key", "website_url", "created_at", "updated_at"]
     ]
 
 
@@ -148,14 +160,7 @@ def get_organization_data(wks, sheet_name="organizations"):
             "acronym",
         ]
     ]
-    organizations = (
-        organizations.replace({r"\s+$": "", r"^\s+": ""}, regex=True)
-        .replace(r"\n", " ", regex=True)
-        .replace("'", "''")
-        .replace("\u2019", "''", regex=True)  # replace curly right-quote
-        .replace("\u202f", " ", regex=True)  # replace narrow no-break space
-        .replace("\u2060", "", regex=True)  # remove word joiner
-    )
+    organizations = _reformat_df_values(organizations)
     organizations["description"] = (
         organizations["description"]
         .astype(str)
@@ -179,4 +184,5 @@ def get_edam_annotations(wks, sheet_name="challenge_data"):
         pd.DataFrame(wks.worksheet(sheet_name).get_all_records())
         .fillna("")
         .drop(["_challenge", "_edam_name"], axis=1)
+        .rename(columns={"edam_id": "edam_concept_id"})
     )
