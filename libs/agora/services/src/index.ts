afd7caacfddd320407388dcf8c84394b6818ba32
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2025-02-25 21:12:58 +0000

    AG-1660: fix similar genes network chart and table

--- libs/agora/services/src/index.ts
+++ libs/agora/services/src/index.ts
@@ -1,5 +1,4 @@
 export * from './lib/error.service';
-export * from './lib/gene.service';
 export * from './lib/github.service';
 export * from './lib/helper.service';
 export * from './lib/http-error-interceptor';
