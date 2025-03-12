d93b77d5ef3cdffda1610199a836b77445369388
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2025-03-12 21:33:58 +0000

    AG-1677: display v before data manifest file version in footer

---
+++ libs/agora/testing/src/lib/mocks/config-mocks.ts
@@ -0,0 +1,10 @@
+import { AppConfig } from '@sagebionetworks/agora/config';
+
+export const configMock: AppConfig = {
+  apiDocsUrl: 'http://localhost:8000/api-docs',
+  appVersion: 'edge',
+  csrApiUrl: 'http://localhost:4200/v1',
+  ssrApiUrl: 'http://agora-api:3333/v1',
+  tagName: 'edge',
+  isPlatformServer: false,
+};
