98cbdc75bc2fd618f2e15ca7b0ba9b6091934cc3
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-03-29 00:48:26 +0000

    feat: add Docker Compose files to deploy the MariaDB instance

---
+++ docker/amp-als/serve-detach.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+args=(
+  # List of services in alphanumeric order
+  --file docker/amp-als/services/mariadb.yml
+
+  --file docker/amp-als/networks.yml
+
+  up $1 --detach --remove-orphans
+)
+
+docker compose "${args[@]}"
