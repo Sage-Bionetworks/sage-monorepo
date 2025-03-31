16a0ef69a4b47bbfb508191ad4730ab117a54d9e
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-03-31 17:30:50 +0000

    chore: use config data from library

--- apps/amp-als/dataset-service/src/main/java/org/sagebionetworks/amp/als/dataset/service/DatasetServiceApplication.java
+++ apps/amp-als/dataset-service/src/main/java/org/sagebionetworks/amp/als/dataset/service/DatasetServiceApplication.java
@@ -1,13 +1,11 @@
 package org.sagebionetworks.amp.als.dataset.service;
 
-// TODO: next line
-// import org.sagebionetworks.openchallenges.app.config.data.ChallengeServiceConfigData;
+import org.sagebionetworks.amp.als.app.config.data.DatasetServiceConfigData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -16,13 +14,11 @@ public class DatasetServiceApplication implements CommandLineRunner {
 
   private static final Logger LOG = LoggerFactory.getLogger(DatasetServiceApplication.class);
 
-  // private final ChallengeServiceConfigData challengeServiceConfigData;
+  private final DatasetServiceConfigData datasetServiceConfigData;
 
-  // public DatasetServiceApplication(ChallengeServiceConfigData challengeServiceConfigData) {
-  //   this.challengeServiceConfigData = challengeServiceConfigData;
-  // }
-
-  public DatasetServiceApplication() {}
+  public DatasetServiceApplication(DatasetServiceConfigData datasetServiceConfigData) {
+    this.datasetServiceConfigData = datasetServiceConfigData;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(DatasetServiceApplication.class, args);
@@ -30,6 +26,6 @@ public class DatasetServiceApplication implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
-    // LOG.info(challengeServiceConfigData.getWelcomeMessage());
+    LOG.info(datasetServiceConfigData.getWelcomeMessage());
   }
 }
