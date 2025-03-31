32e6827df0ddb06344f13024d045521a2c430201
sagely1 <114952739+sagely1@users.noreply.github.com>
2025-03-31 11:09:27 +0000

    upgrade svg handling

--- libs/agora/shared/src/lib/svg-icon/svg-icon.component.ts
+++ libs/agora/shared/src/lib/svg-icon/svg-icon.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'agora-svg-icon',
@@ -8,18 +10,31 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./svg-icon.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class SvgIconComponent {
-  @Input() imagePath = '';
+export class SvgIconComponent implements OnInit {
+  @Input() imagePath!: string;
   @Input() altText = '';
-  @Input() customClasses = '';
+  @Input() color = 'currentColor'; // Default to inherited color
 
-  getClasses() {
-    const classes = ['svg-icon'];
+  http = inject(HttpClient);
+  sanitizer = inject(DomSanitizer);
 
-    if (this.customClasses) {
-      classes.push(this.customClasses);
+  svgContent: SafeHtml | null = null;
+
+  ngOnInit() {
+    if (this.imagePath) {
+      this.http.get(this.imagePath, { responseType: 'text' }).subscribe(
+        (svgContent) => {
+          // Sanitize the SVG content to prevent XSS attacks
+          console.log('svg loaded:', svgContent);
+          this.svgContent = this.sanitizer.bypassSecurityTrustHtml(svgContent);
+          // // Replace the inner HTML of the component with the sanitized SVG content
+          // const svgContainer = document.querySelector('.svg-icon-container');
+          // if (svgContainer) {
+          //   svgContainer.innerHTML = sanitizedSvg as string;
+          // }
+        },
+        (error) => console.error('Error loading svg', error),
+      );
     }
-
-    return classes.join(' ');
   }
 }
