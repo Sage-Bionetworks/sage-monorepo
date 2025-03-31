241854dee876d50cd178cc7215a68194490b41f4
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2025-03-31 21:32:18 +0000

    AG-1618: improvements from PR, skip tests that depend on query parameter handling

--- apps/agora/app/e2e/gene-comparison-tool-pinning-cache.spec.ts
+++ apps/agora/app/e2e/gene-comparison-tool-pinning-cache.spec.ts
@@ -113,170 +113,176 @@ test.describe('GCT: Caching pinned genes', () => {
     });
   });
 
-  test('Pinned proteins are maintained when switching between Protein subcategories', async ({
-    page,
-  }) => {
-    const gene1 = geneWithMultipleProteinsTMT.name;
-    const gene2 = gene2WithMultipleProteinsTMT.name;
-    const genes = [gene1, gene2];
-    const nGenes = genes.length;
+  // TODO: re-enable as part of AG-1739
+  test.fixme(
+    'Pinned proteins are maintained when switching between Protein subcategories',
+    async ({ page }) => {
+      const gene1 = geneWithMultipleProteinsTMT.name;
+      const gene2 = gene2WithMultipleProteinsTMT.name;
+      const genes = [gene1, gene2];
+      const nGenes = genes.length;
 
-    const proteins1 = geneWithMultipleProteinsTMT.uniProtIds;
-    const proteins2 = gene2WithMultipleProteinsTMT.uniProtIds;
-    const nProteins = proteins1.length + proteins2.length;
+      const proteins1 = geneWithMultipleProteinsTMT.uniProtIds;
+      const proteins2 = gene2WithMultipleProteinsTMT.uniProtIds;
+      const nProteins = proteins1.length + proteins2.length;
 
-    await page.goto(URL_GCT_PROTEIN_TMT);
-    await expectGctPageLoaded(page, GCT_CATEGORIES.PROTEIN, GCT_PROTEIN_SUBCATEGORIES.TMT);
-
-    for (const gene of genes) {
-      await pinAllItemsViaSearchByGene(page, gene);
-    }
-
-    await test.step('confirm # genes and proteins pinned on Protein page', async () => {
-      await expectPinnedGenesCountText(page, nGenes);
-      await expectPinnedProteinsCountText(page, nProteins);
-      await confirmPinnedItemsCount(page, nProteins);
-    });
-
-    await changeGctSubcategory(
-      page,
-      GCT_CATEGORIES.PROTEIN,
-      GCT_PROTEIN_SUBCATEGORIES.TMT,
-      GCT_PROTEIN_SUBCATEGORIES.SRM,
-    );
-
-    await changeGctSubcategory(
-      page,
-      GCT_CATEGORIES.PROTEIN,
-      GCT_PROTEIN_SUBCATEGORIES.SRM,
-      GCT_PROTEIN_SUBCATEGORIES.LFQ,
-    );
-
-    // back to original subcategory
-    await changeGctSubcategory(
-      page,
-      GCT_CATEGORIES.PROTEIN,
-      GCT_PROTEIN_SUBCATEGORIES.LFQ,
-      GCT_PROTEIN_SUBCATEGORIES.TMT,
-    );
-
-    await test.step('confirm same genes and proteins pinned on Protein page', async () => {
-      await expectPinnedGenesCountText(page, nGenes);
-      await expectPinnedProteinsCountText(page, nProteins);
-      await confirmPinnedItemsCount(page, nProteins);
-
-      await confirmPinnedProteins(page, gene1, proteins1);
-      await confirmPinnedProteins(page, gene2, proteins2);
-    });
-  });
-
-  test('Pinned proteins are maintained when switching categories: Protein -> RNA -> Protein', async ({
-    page,
-  }) => {
-    const gene1 = geneWithMultipleProteinsTMT.name;
-    const gene2 = gene2WithMultipleProteinsTMT.name;
-    const genes = [gene1, gene2];
-    const nGenes = genes.length;
-
-    const proteins1 = geneWithMultipleProteinsTMT.uniProtIds;
-    const proteins2 = gene2WithMultipleProteinsTMT.uniProtIds;
-    const nProteins = proteins1.length + proteins2.length;
-
-    await page.goto(URL_GCT_PROTEIN_TMT);
-    await expectGctPageLoaded(page, GCT_CATEGORIES.PROTEIN, GCT_PROTEIN_SUBCATEGORIES.TMT);
-
-    for (const gene of genes) {
-      await pinAllItemsViaSearchByGene(page, gene);
-    }
-
-    await test.step('confirm # genes and proteins pinned on Protein page', async () => {
-      await expectPinnedGenesCountText(page, nGenes);
-      await expectPinnedProteinsCountText(page, nProteins);
-      await confirmPinnedItemsCount(page, nProteins);
-    });
-
-    await changeGctCategory(page, GCT_CATEGORIES.PROTEIN, GCT_CATEGORIES.RNA);
-
-    await changeGctSubcategory(
-      page,
-      GCT_CATEGORIES.RNA,
-      GCT_RNA_SUBCATEGORIES.AD,
-      GCT_RNA_SUBCATEGORIES.AD_AOD,
-    );
-
-    // back to original category
-    await changeGctCategory(page, GCT_CATEGORIES.RNA, GCT_CATEGORIES.PROTEIN);
-
-    // back to original subcategory
-    await changeGctSubcategory(
-      page,
-      GCT_CATEGORIES.PROTEIN,
-      GCT_PROTEIN_SUBCATEGORIES.SRM,
-      GCT_PROTEIN_SUBCATEGORIES.TMT,
-    );
-
-    await test.step('confirm same genes and proteins pinned on Protein page', async () => {
-      await expectPinnedGenesCountText(page, nGenes);
-      await expectPinnedProteinsCountText(page, nProteins);
-
-      await confirmPinnedItemsCount(page, nProteins);
-      await confirmPinnedProteins(page, gene1, proteins1);
-      await confirmPinnedProteins(page, gene2, proteins2);
-    });
-  });
-
-  test('Last pinned genes are maintained even if proteins were pinned initially', async ({
-    page,
-  }) => {
-    const rnaCategoryGene = geneWithMultipleProteinsTMT.name;
-    const rnaCategoryProteins = geneWithMultipleProteinsTMT.uniProtIds;
-    const proteinCategoryGene = gene2WithMultipleProteinsTMT.name;
-
-    await test.step('pin proteins in Protein category', async () => {
       await page.goto(URL_GCT_PROTEIN_TMT);
       await expectGctPageLoaded(page, GCT_CATEGORIES.PROTEIN, GCT_PROTEIN_SUBCATEGORIES.TMT);
 
-      await pinAllItemsViaSearchByGene(page, proteinCategoryGene);
-    });
+      for (const gene of genes) {
+        await pinAllItemsViaSearchByGene(page, gene);
+      }
 
-    await test.step('pin genes in RNA category', async () => {
+      await test.step('confirm # genes and proteins pinned on Protein page', async () => {
+        await expectPinnedGenesCountText(page, nGenes);
+        await expectPinnedProteinsCountText(page, nProteins);
+        await confirmPinnedItemsCount(page, nProteins);
+      });
+
+      await changeGctSubcategory(
+        page,
+        GCT_CATEGORIES.PROTEIN,
+        GCT_PROTEIN_SUBCATEGORIES.TMT,
+        GCT_PROTEIN_SUBCATEGORIES.SRM,
+      );
+
+      await changeGctSubcategory(
+        page,
+        GCT_CATEGORIES.PROTEIN,
+        GCT_PROTEIN_SUBCATEGORIES.SRM,
+        GCT_PROTEIN_SUBCATEGORIES.LFQ,
+      );
+
+      // back to original subcategory
+      await changeGctSubcategory(
+        page,
+        GCT_CATEGORIES.PROTEIN,
+        GCT_PROTEIN_SUBCATEGORIES.LFQ,
+        GCT_PROTEIN_SUBCATEGORIES.TMT,
+      );
+
+      await test.step('confirm same genes and proteins pinned on Protein page', async () => {
+        await expectPinnedGenesCountText(page, nGenes);
+        await expectPinnedProteinsCountText(page, nProteins);
+        await confirmPinnedItemsCount(page, nProteins);
+
+        await confirmPinnedProteins(page, gene1, proteins1);
+        await confirmPinnedProteins(page, gene2, proteins2);
+      });
+    },
+  );
+
+  // TODO: re-enable as part of AG-1739
+  test.fixme(
+    'Pinned proteins are maintained when switching categories: Protein -> RNA -> Protein',
+    async ({ page }) => {
+      const gene1 = geneWithMultipleProteinsTMT.name;
+      const gene2 = gene2WithMultipleProteinsTMT.name;
+      const genes = [gene1, gene2];
+      const nGenes = genes.length;
+
+      const proteins1 = geneWithMultipleProteinsTMT.uniProtIds;
+      const proteins2 = gene2WithMultipleProteinsTMT.uniProtIds;
+      const nProteins = proteins1.length + proteins2.length;
+
+      await page.goto(URL_GCT_PROTEIN_TMT);
+      await expectGctPageLoaded(page, GCT_CATEGORIES.PROTEIN, GCT_PROTEIN_SUBCATEGORIES.TMT);
+
+      for (const gene of genes) {
+        await pinAllItemsViaSearchByGene(page, gene);
+      }
+
+      await test.step('confirm # genes and proteins pinned on Protein page', async () => {
+        await expectPinnedGenesCountText(page, nGenes);
+        await expectPinnedProteinsCountText(page, nProteins);
+        await confirmPinnedItemsCount(page, nProteins);
+      });
+
       await changeGctCategory(page, GCT_CATEGORIES.PROTEIN, GCT_CATEGORIES.RNA);
 
-      await test.step('clear all items', async () => {
-        await page.getByRole('button', { name: 'Clear All' }).click();
-        await expect(page.getByText(/Pinned Genes/i)).toBeHidden();
-      });
+      await changeGctSubcategory(
+        page,
+        GCT_CATEGORIES.RNA,
+        GCT_RNA_SUBCATEGORIES.AD,
+        GCT_RNA_SUBCATEGORIES.AD_AOD,
+      );
 
-      await pinGeneViaSearch(page, rnaCategoryGene);
-
-      await test.step('confirm pinned gene', async () => {
-        await expectPinnedGenesCountText(page, 1);
-        await confirmPinnedItemsByGeneName(page, rnaCategoryGene, 1);
-      });
-    });
-
-    await test.step('Protein category pins have changed', async () => {
+      // back to original category
       await changeGctCategory(page, GCT_CATEGORIES.RNA, GCT_CATEGORIES.PROTEIN);
+
+      // back to original subcategory
       await changeGctSubcategory(
         page,
         GCT_CATEGORIES.PROTEIN,
         GCT_PROTEIN_SUBCATEGORIES.SRM,
         GCT_PROTEIN_SUBCATEGORIES.TMT,
       );
-      await expectPinnedGenesCountText(page, 1);
-      await expectPinnedProteinsCountText(page, rnaCategoryProteins.length);
-      await confirmPinnedProteins(page, rnaCategoryGene, rnaCategoryProteins);
-    });
 
-    await test.step('RNA category pins are maintained', async () => {
-      await changeGctCategory(page, GCT_CATEGORIES.PROTEIN, GCT_CATEGORIES.RNA);
+      await test.step('confirm same genes and proteins pinned on Protein page', async () => {
+        await expectPinnedGenesCountText(page, nGenes);
+        await expectPinnedProteinsCountText(page, nProteins);
 
-      await test.step('confirm pinned gene', async () => {
-        await expectPinnedGenesCountText(page, 1);
-        await confirmPinnedItemsByGeneName(page, rnaCategoryGene, 1);
+        await confirmPinnedItemsCount(page, nProteins);
+        await confirmPinnedProteins(page, gene1, proteins1);
+        await confirmPinnedProteins(page, gene2, proteins2);
       });
-    });
-  });
+    },
+  );
+
+  // TODO: re-enable as part of AG-1739
+  test.fixme(
+    'Last pinned genes are maintained even if proteins were pinned initially',
+    async ({ page }) => {
+      const rnaCategoryGene = geneWithMultipleProteinsTMT.name;
+      const rnaCategoryProteins = geneWithMultipleProteinsTMT.uniProtIds;
+      const proteinCategoryGene = gene2WithMultipleProteinsTMT.name;
+
+      await test.step('pin proteins in Protein category', async () => {
+        await page.goto(URL_GCT_PROTEIN_TMT);
+        await expectGctPageLoaded(page, GCT_CATEGORIES.PROTEIN, GCT_PROTEIN_SUBCATEGORIES.TMT);
+
+        await pinAllItemsViaSearchByGene(page, proteinCategoryGene);
+      });
+
+      await test.step('pin genes in RNA category', async () => {
+        await changeGctCategory(page, GCT_CATEGORIES.PROTEIN, GCT_CATEGORIES.RNA);
+
+        await test.step('clear all items', async () => {
+          await page.getByRole('button', { name: 'Clear All' }).click();
+          await expect(page.getByText(/Pinned Genes/i)).toBeHidden();
+        });
+
+        await pinGeneViaSearch(page, rnaCategoryGene);
+
+        await test.step('confirm pinned gene', async () => {
+          await expectPinnedGenesCountText(page, 1);
+          await confirmPinnedItemsByGeneName(page, rnaCategoryGene, 1);
+        });
+      });
+
+      await test.step('Protein category pins have changed', async () => {
+        await changeGctCategory(page, GCT_CATEGORIES.RNA, GCT_CATEGORIES.PROTEIN);
+        await changeGctSubcategory(
+          page,
+          GCT_CATEGORIES.PROTEIN,
+          GCT_PROTEIN_SUBCATEGORIES.SRM,
+          GCT_PROTEIN_SUBCATEGORIES.TMT,
+        );
+        await expectPinnedGenesCountText(page, 1);
+        await expectPinnedProteinsCountText(page, rnaCategoryProteins.length);
+        await confirmPinnedProteins(page, rnaCategoryGene, rnaCategoryProteins);
+      });
+
+      await test.step('RNA category pins are maintained', async () => {
+        await changeGctCategory(page, GCT_CATEGORIES.PROTEIN, GCT_CATEGORIES.RNA);
+
+        await test.step('confirm pinned gene', async () => {
+          await expectPinnedGenesCountText(page, 1);
+          await confirmPinnedItemsByGeneName(page, rnaCategoryGene, 1);
+        });
+      });
+    },
+  );
 
   test('Last pinned proteins are maintained even if genes were pinned initially', async ({
     page,
