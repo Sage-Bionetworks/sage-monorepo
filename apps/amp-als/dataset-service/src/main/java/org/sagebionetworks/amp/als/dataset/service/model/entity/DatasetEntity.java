f9126ee866c0dbe78ce8d44ef3b1a2137da798d4
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-04-01 23:07:47 +0000

    chore: cleanup

--- apps/amp-als/dataset-service/src/main/java/org/sagebionetworks/amp/als/dataset/service/model/entity/DatasetEntity.java
+++ apps/amp-als/dataset-service/src/main/java/org/sagebionetworks/amp/als/dataset/service/model/entity/DatasetEntity.java
@@ -1,18 +1,11 @@
 package org.sagebionetworks.amp.als.dataset.service.model.entity;
 
-import java.time.LocalDate;
 import java.time.OffsetDateTime;
-import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
