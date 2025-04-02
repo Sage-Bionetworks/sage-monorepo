8413efbad57fa3754b14436dc9e241c66ed68cff
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-04-02 03:36:00 +0000

    chore: update service and api

--- apps/amp-als/dataset-service/src/main/java/org/sagebionetworks/amp/als/dataset/service/service/DatasetService.java
+++ apps/amp-als/dataset-service/src/main/java/org/sagebionetworks/amp/als/dataset/service/service/DatasetService.java
@@ -1,11 +1,17 @@
 package org.sagebionetworks.amp.als.dataset.service.service;
 
+import java.util.Arrays;
+import java.util.List;
 import org.sagebionetworks.amp.als.dataset.service.exception.DatasetNotFoundException;
 import org.sagebionetworks.amp.als.dataset.service.model.dto.DatasetDto;
+import org.sagebionetworks.amp.als.dataset.service.model.dto.DatasetSearchQueryDto;
 import org.sagebionetworks.amp.als.dataset.service.model.dto.DatasetsPageDto;
 import org.sagebionetworks.amp.als.dataset.service.model.entity.DatasetEntity;
 import org.sagebionetworks.amp.als.dataset.service.model.mapper.DatasetMapper;
 import org.sagebionetworks.amp.als.dataset.service.model.repository.DatasetRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +25,32 @@ public class DatasetService {
   }
 
   private DatasetMapper datasetMapper = new DatasetMapper();
+
+  private static final List<String> SEARCHABLE_FIELDS = Arrays.asList("name", "description");
+
+  @Transactional(readOnly = true)
+  public DatasetsPageDto listDatasets(DatasetSearchQueryDto query) {
+    Pageable pageable = PageRequest.of(query.getPageNumber(), query.getPageSize());
+
+    List<String> fieldsToSearchBy = SEARCHABLE_FIELDS;
+    Page<DatasetEntity> datasetEntitiesPage = datasetRepository.findAll(
+      pageable,
+      query,
+      fieldsToSearchBy.toArray(new String[0])
+    );
+
+    List<DatasetDto> datasets = datasetMapper.convertToDtoList(datasetEntitiesPage.getContent());
+
+    return DatasetsPageDto.builder()
+      .datasets(datasets)
+      .number(datasetEntitiesPage.getNumber())
+      .size(datasetEntitiesPage.getSize())
+      .totalElements(datasetEntitiesPage.getTotalElements())
+      .totalPages(datasetEntitiesPage.getTotalPages())
+      .hasNext(datasetEntitiesPage.hasNext())
+      .hasPrevious(datasetEntitiesPage.hasPrevious())
+      .build();
+  }
 
   @Transactional(readOnly = true)
   public DatasetDto getDataset(Long datasetId) {
