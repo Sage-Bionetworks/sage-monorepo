a6c8d2007fff57c7d87e838aa54ba4871cf1366a
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-04-02 03:38:36 +0000

    chore: rename log to logger

--- apps/amp-als/dataset-service/src/main/java/org/sagebionetworks/amp/als/dataset/service/configuration/FlywayConfiguration.java
+++ apps/amp-als/dataset-service/src/main/java/org/sagebionetworks/amp/als/dataset/service/configuration/FlywayConfiguration.java
@@ -9,14 +9,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FlywayConfiguration {
 
-  private static final Logger log = LoggerFactory.getLogger(FlywayConfiguration.class);
+  private static final Logger logger = LoggerFactory.getLogger(FlywayConfiguration.class);
 
   // TODO: Clean the DB only in dev mode, never in production.
   // @Profile("dev")
   @Bean
   public FlywayMigrationStrategy cleanMigrationStrategy() {
     return flyway -> {
-      log.info("Executing custom Flyway migration: clean and migrate");
+      logger.info("Executing custom Flyway migration: clean and migrate");
       flyway.clean();
       flyway.migrate();
     };
