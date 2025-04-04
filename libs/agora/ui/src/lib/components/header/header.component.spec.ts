a2fbb8e3f9d696b8b33f810618513fe02d70e152
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2025-04-04 22:45:53 +0000

    AG-1629: add home, nominated-targets, not-found, shared, and ui tests

--- libs/agora/ui/src/lib/components/header/header.component.spec.ts
+++ libs/agora/ui/src/lib/components/header/header.component.spec.ts
@@ -1,9 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HeaderComponent } from './header.component';
 import { provideHttpClient } from '@angular/common/http';
-import { provideRouter, RouterModule } from '@angular/router';
-import { GeneSearchComponent } from '@sagebionetworks/agora/genes';
-import { SvgImageComponent } from '@sagebionetworks/agora/ui';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -11,7 +9,6 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterModule, GeneSearchComponent, SvgImageComponent],
       providers: [provideHttpClient(), provideRouter([])],
     }).compileComponents();
   });
