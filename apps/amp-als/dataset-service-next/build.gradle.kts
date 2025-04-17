360bdb7a556eaad56976fbd0c24ca00dfc4f2a17
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-04-17 16:23:21 -0700

    chore(amp-als): create Spring Boot 3 stub for the dataset service with OpenAPI Generator (SMR-62) (#3088)

---
+++ apps/amp-als/dataset-service-next/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("org.springframework.boot") version "3.4.4"
+    id("io.spring.dependency-management") version "1.1.7"
+    java
+}
+
+group = "org.sagebionetworks.amp.als"
+version = "1.0.0"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenLocal()
+    maven { url = uri("https://repo.spring.io/milestone") }
+    mavenCentral()
+}
+
+dependencies {
+    // Spring Boot
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.boot.starter.validation)
+    runtimeOnly(libs.spring.boot.devtools)
+    testImplementation(libs.spring.boot.starter.test)
+
+    // SpringDoc
+    implementation(libs.springdoc.openapi.ui)
+
+    // Jackson
+    implementation(libs.jackson.databind)
+    implementation(libs.jackson.dataformat.yaml)
+    implementation(libs.jackson.datatype.jsr310)
+
+    // Misc
+    implementation(libs.findbugs.jsr305)
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Javadoc> {
+    options.encoding = "UTF-8"
+}
---
+++ apps/amp-als/dataset-service-next/src/test/java/org/sagebionetworks/amp/als/dataset/service/OpenApiGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sagebionetworks.amp.als.dataset.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
