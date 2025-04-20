865ddcb730095ac031664aff6493f14d99802c3c
Rongrong Chai <73901500+rrchai@users.noreply.github.com>
2025-04-21 05:50:57 +0800

    feat(openchallenges): add config to toggle the home page announcement banner (SMR-26) (#3087)

--- apps/openchallenges/app/docker-entrypoint.d/10-envsubst-on-app-config-template.sh
+++ apps/openchallenges/app/docker-entrypoint.d/10-envsubst-on-app-config-template.sh
@@ -2,4 +2,11 @@
 
 # Generate 'config.json' from 'config.json.template' using environment variables.
 cd "${APP_DIR}/browser/config"
-envsubst < config.json.template > config.json
+envsubst < config.json.template | jq '
+  with_entries(
+    if .value == "true" then .value = true
+    elif .value == "false" then .value = false
+    else .
+    end
+  )
+' > config.json
