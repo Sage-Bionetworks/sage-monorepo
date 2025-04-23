330b9c303115bdf6c2595276633705408e76b129
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-04-23 02:45:14 +0000

    chore: format kt files and add rule to lint-staged

--- apps/amp-als/dataset-service/build.gradle.kts
+++ apps/amp-als/dataset-service/build.gradle.kts
@@ -1,76 +1,76 @@
 plugins {
-    id("org.springframework.boot") version "3.4.4"
-    id("io.spring.dependency-management") version "1.1.7"
-    java
+  id("org.springframework.boot") version "3.4.4"
+  id("io.spring.dependency-management") version "1.1.7"
+  java
 }
 
 group = "org.sagebionetworks.amp.als"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+  sourceCompatibility = JavaVersion.VERSION_21
+  targetCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
-    mavenLocal()
-    maven { url = uri("https://repo.spring.io/milestone") }
-    mavenCentral()
+  mavenLocal()
+  maven { url = uri("https://repo.spring.io/milestone") }
+  mavenCentral()
 }
 
 dependencies {
-    // Spring Boot
-    implementation(libs.spring.boot.starter.data.jpa)
-    implementation(libs.spring.boot.starter.jdbc)
-    implementation(libs.spring.boot.starter.security)
-    implementation(libs.spring.boot.starter.validation)
-    implementation(libs.spring.boot.starter.web)
-    runtimeOnly(libs.spring.boot.devtools)
-    testImplementation(libs.spring.boot.starter.test)
+  // Spring Boot
+  implementation(libs.spring.boot.starter.data.jpa)
+  implementation(libs.spring.boot.starter.jdbc)
+  implementation(libs.spring.boot.starter.security)
+  implementation(libs.spring.boot.starter.validation)
+  implementation(libs.spring.boot.starter.web)
+  runtimeOnly(libs.spring.boot.devtools)
+  testImplementation(libs.spring.boot.starter.test)
 
-    // SpringDoc
-    implementation(libs.springdoc.openapi.ui)
+  // SpringDoc
+  implementation(libs.springdoc.openapi.ui)
 
-    // Jackson
-    implementation(libs.jackson.databind)
-    implementation(libs.jackson.dataformat.yaml)
-    implementation(libs.jackson.datatype.jsr310)
+  // Jackson
+  implementation(libs.jackson.databind)
+  implementation(libs.jackson.dataformat.yaml)
+  implementation(libs.jackson.datatype.jsr310)
 
-    // Flyway
-    implementation(libs.flyway.core)
-    implementation(libs.flyway.mysql)
+  // Flyway
+  implementation(libs.flyway.core)
+  implementation(libs.flyway.mysql)
 
-    // MySQL
-    runtimeOnly(libs.mysql.driver)
+  // MySQL
+  runtimeOnly(libs.mysql.driver)
 
-    // Lombok
-    annotationProcessor(libs.lombok)
-    compileOnly(libs.lombok)
-    testAnnotationProcessor(libs.lombok)
-    testCompileOnly(libs.lombok)
+  // Lombok
+  annotationProcessor(libs.lombok)
+  compileOnly(libs.lombok)
+  testAnnotationProcessor(libs.lombok)
+  testCompileOnly(libs.lombok)
 
-    // Hibernate Search
-    implementation(libs.hibernate.search.mapper.orm)
-    implementation(libs.hibernate.search.backend.elasticsearch)
+  // Hibernate Search
+  implementation(libs.hibernate.search.mapper.orm)
+  implementation(libs.hibernate.search.backend.elasticsearch)
 
-    // Sage Bionetworks
-    implementation(libs.sage.util)
+  // Sage Bionetworks
+  implementation(libs.sage.util)
 
-    // AMP-ALS
-    implementation(libs.amp.als.app.config.data)
+  // AMP-ALS
+  implementation(libs.amp.als.app.config.data)
 
-    // Misc
-    implementation(libs.findbugs.jsr305)
+  // Misc
+  implementation(libs.findbugs.jsr305)
 }
 
 tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
+  options.encoding = "UTF-8"
 }
 
 tasks.withType<Javadoc> {
-    options.encoding = "UTF-8"
+  options.encoding = "UTF-8"
 }
 
 tasks.named<org.springframework.boot.gradle.tasks.bundling.BootBuildImage>("bootBuildImage") {
-    imageName.set("ghcr.io/sage-bionetworks/${project.name}-base:local")
+  imageName.set("ghcr.io/sage-bionetworks/${project.name}-base:local")
 }
