975ecdadf8160c02e2e8260b6624b30a0377369c
sagely1 <114952739+sagely1@users.noreply.github.com>
2025-04-25 18:35:34 +0000

    add primeng and app skeleton

--- apps/model-ad/app/src/app/app.component.ts
+++ apps/model-ad/app/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FooterComponent } from '@sagebionetworks/model-ad/ui';
+import { HeaderComponent, FooterComponent } from '@sagebionetworks/model-ad/ui';
 
 @Component({
-  imports: [RouterModule, FooterComponent],
+  imports: [RouterModule, HeaderComponent, FooterComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- apps/model-ad/app/src/app/app.routes.ts
+++ apps/model-ad/app/src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Route } from '@angular/router';
 
 export const routes: Route[] = [
   {
+    path: '',
+    loadChildren: () => import('@sagebionetworks/model-ad/home').then((routes) => routes.routes),
+  },
+  {
     path: 'not-found',
     loadChildren: () =>
       import('@sagebionetworks/model-ad/not-found').then((routes) => routes.routes),
