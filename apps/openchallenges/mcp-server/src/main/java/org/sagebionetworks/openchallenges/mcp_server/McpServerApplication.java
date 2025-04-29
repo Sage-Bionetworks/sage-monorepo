1d4179ffc9065fa266fb102295c24be87ffe9ecb
rrchai <73901500+rrchai@users.noreply.github.com>
2025-04-29 19:45:41 +0000

    add an initial mcp-server project folder

---
+++ apps/openchallenges/mcp-server/src/main/java/org/sagebionetworks/openchallenges/mcp_server/McpServerApplication.java
@@ -0,0 +1,13 @@
+package org.sagebionetworks.openchallenges.mcp_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpServerApplication.class, args);
+	}
+
+}
