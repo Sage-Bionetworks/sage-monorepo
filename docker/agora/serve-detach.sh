b0e6ea05c0a45b481129574de237d1fcd452bac9
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-05-03 21:55:45 +0000

    feat: add Docker Compose files

--- docker/agora/serve-detach.sh
+++ docker/agora/serve-detach.sh
@@ -6,6 +6,7 @@ args=(
   --file docker/agora/services/api-docs.yml
   --file docker/agora/services/api.yml
   --file docker/agora/services/app.yml
+  --file docker/agora/services/cgt-v2beta-service.yml
   --file docker/agora/services/data.yml
   --file docker/agora/services/mongo.yml
 
