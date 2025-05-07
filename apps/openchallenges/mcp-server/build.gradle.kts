64ef66c198c9174bd0d5ee97d1e0a23f3dad0363
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-05-07 16:55:05 -0700

    refactor(openchallenges): update the OC MCP server to use Gradle Version Catalogs (SMR-105) (#3143)

--- apps/openchallenges/mcp-server/build.gradle.kts
+++ apps/openchallenges/mcp-server/build.gradle.kts
@@ -1,34 +1,25 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.4.5"
-	id("io.spring.dependency-management") version "1.1.7"
+	alias(libs.plugins.spring.boot)
+	alias(libs.plugins.spring.dependency.management)
 }
 
 group = "org.sagebionetworks.openchallenges"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+  sourceCompatibility = JavaVersion.VERSION_21
+  targetCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
 	mavenCentral()
 }
 
-extra["springAiVersion"] = "1.0.0-M7"
-
 dependencies {
-	implementation("org.springframework.ai:spring-ai-starter-mcp-server-webmvc")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-dependencyManagement {
-	imports {
-		mavenBom("org.springframework.ai:spring-ai-bom:${property("springAiVersion")}")
-	}
+	implementation(libs.spring.ai.starter.mcp.server.webmvc)
+	testImplementation(libs.spring.boot.starter.test)
+	testRuntimeOnly(libs.junit.platform.launcher)
 }
 
 tasks.withType<Test> {
