d041c118ae145b249e8f8efd0b720ce3a90d5183
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2025-05-08 23:15:34 +0000

    MG-33: home page minus background

--- libs/explorers/ui/src/index.ts
+++ libs/explorers/ui/src/index.ts
@@ -1,4 +1,6 @@
+export * from './lib/components/card/card.component';
 export * from './lib/components/footer/footer.component';
 export * from './lib/components/header/header.component';
 export * from './lib/components/hero/hero.component';
+export * from './lib/components/search-input/search-input.component';
 export * from './lib/components/svg-image/svg-image.component';
