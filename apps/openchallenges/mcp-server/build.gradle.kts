83f956c324360e12700c93b016a9424414f05182
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-05-08 22:31:35 +0000

    chore: disable static-linking

--- apps/openchallenges/mcp-server/build.gradle.kts
+++ apps/openchallenges/mcp-server/build.gradle.kts
@@ -25,8 +25,8 @@ graalvmNative {
       })
 
       imageName.set(project.name)
-      buildArgs.add("--static")
-      buildArgs.add("--libc=musl")
+      // buildArgs.add("--static")
+      // buildArgs.add("--libc=musl")
       // Optional: disables fallback to a dynamically linked image
       buildArgs.add("--no-fallback")
     }
