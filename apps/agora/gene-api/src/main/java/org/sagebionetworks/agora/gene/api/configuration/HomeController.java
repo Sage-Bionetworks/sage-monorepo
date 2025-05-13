325956b5e9109994c86596fcc65ba730c2c8d38d
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-05-13 03:03:27 +0000

    chore: create `agora-gene-api` project based off of `amp-als-dataset-service`

---
+++ apps/agora/gene-api/src/main/java/org/sagebionetworks/agora/gene/api/configuration/HomeController.java
@@ -0,0 +1,19 @@
+package org.sagebionetworks.agora.gene.api.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Home redirection to OpenAPI api documentation
+ */
+@Controller
+public class HomeController {
+
+  @RequestMapping("/")
+  public String index() {
+    return "redirect:swagger-ui.html";
+  }
+}
---
+++ apps/agora/gene-api/src/test/java/org/sagebionetworks/agora/gene/api/GeneApiApplicationTests.java
@@ -0,0 +1,11 @@
+package org.sagebionetworks.agora.gene.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneApiApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
