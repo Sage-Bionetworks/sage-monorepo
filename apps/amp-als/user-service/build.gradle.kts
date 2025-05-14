189edd1b7b87b6a60179dd818c127d82df66176b
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-05-14 16:29:48 -0700

    feat(amp-als): create a Spring Boot 3 skeleton for the user service (SMR-132) (#3177)

---
+++ apps/amp-als/user-service/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+	java
+	alias(libs.plugins.spring.dependency.management)
+	alias(libs.plugins.spring.boot)
+}
+
+group = "org.sagebionetworks.amp.als"
+version = "0.0.1-SNAPSHOT"
+
+java {
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+  }
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation(libs.findbugs.jsr305)
+  implementation(libs.jackson.databind)
+  implementation(libs.jackson.dataformat.yaml)
+  implementation(libs.jackson.datatype.jsr310)
+  implementation(libs.spring.boot.starter.validation)
+  implementation(libs.spring.boot.starter.web)
+  implementation(libs.springdoc.openapi.ui)
+  runtimeOnly(libs.spring.boot.devtools)
+  testImplementation(libs.spring.boot.starter.test)
+}
+
+tasks.withType<JavaCompile> {
+  options.encoding = "UTF-8"
+}
+
+tasks.withType<Javadoc> {
+  options.encoding = "UTF-8"
+}
+
+tasks.named<org.springframework.boot.gradle.tasks.bundling.BootBuildImage>("bootBuildImage") {
+  imageName.set("ghcr.io/sage-bionetworks/${project.name}-base:local")
+}
