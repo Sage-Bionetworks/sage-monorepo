cc4020027664f4078afc27ccc6ff2a9396932313
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-05-17 03:17:39 +0000

    feat: create the amp-als-grafana project

--- tools/configure-hostnames.sh
+++ tools/configure-hostnames.sh
@@ -10,6 +10,7 @@ declare -a hostnames=(
   "127.0.0.1 amp-als-apex"
   "127.0.0.1 amp-als-dataset-service"
   "127.0.0.1 amp-als-elasticsearch"
+  "127.0.0.1 amp-als-grafana"
   "127.0.0.1 amp-als-mariadb"
   "127.0.0.1 amp-als-opensearch"
   "127.0.0.1 amp-als-prometheus"
