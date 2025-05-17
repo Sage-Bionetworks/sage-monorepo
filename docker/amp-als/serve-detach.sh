103786592d34557b17be2c88a5e592d6c95ec2d4
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-05-16 20:23:45 -0700

    feat(amp-als): add Grafana to the AMP-ALS stack (SMR-144) (#3183)

--- docker/amp-als/serve-detach.sh
+++ docker/amp-als/serve-detach.sh
@@ -5,6 +5,7 @@ args=(
   --file docker/amp-als/services/apex.yml
   --file docker/amp-als/services/api-docs.yml
   --file docker/amp-als/services/dataset-service.yml
+  --file docker/amp-als/services/grafana.yml
   --file docker/amp-als/services/keycloak.yml
   --file docker/amp-als/services/mariadb.yml
   --file docker/amp-als/services/opensearch.yml
