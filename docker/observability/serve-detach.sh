8f4f3ac7de659404869412266e01f4586d1a4d67
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-05-19 22:27:06 +0000

    chore: services must be listed first

--- docker/observability/serve-detach.sh
+++ docker/observability/serve-detach.sh
@@ -2,9 +2,10 @@
 
 args=(
   # List of services in alphanumeric order
-  --file docker/observability/networks.yml
   --file docker/observability/services/grafana.yml
   --file docker/observability/services/prometheus.yml
+
+  --file docker/observability/networks.yml
 
   --project-name observability
 
