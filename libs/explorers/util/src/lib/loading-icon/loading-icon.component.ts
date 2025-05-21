ec6df3dfba31a6decad2960cda6e19cacf8c92db
sagely1 <114952739+sagely1@users.noreply.github.com>
2025-05-21 14:44:16 -0700

    feat(model-ad): created loading-container and configurable loading-icon (MG-225) (#3192)

--- libs/explorers/util/src/lib/loading-icon/loading-icon.component.ts
+++ libs/explorers/util/src/lib/loading-icon/loading-icon.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'explorers-loading-icon',
@@ -8,4 +8,16 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./loading-icon.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class LoadingIconComponent {}
+export class LoadingIconComponent {
+  @Input() colorOutermost = '#8B8AD1';
+  @Input() colorCentral = '#8B8AD1';
+  @Input() colorInnermost = '#8B8AD1';
+
+  get customColors() {
+    return {
+      '--hex-outermost-color': this.colorOutermost,
+      '--hex-central-color': this.colorCentral,
+      '--hex-innermost-color': this.colorInnermost,
+    };
+  }
+}
