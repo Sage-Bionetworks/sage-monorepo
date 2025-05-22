9bbc53564f6c1ed2ccbd6c6632b3e9be55d39826
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2025-05-22 18:14:56 +0000

    MG-33: remove prefix from preset names

--- apps/agora/app/src/app/primeNGPreset.ts
+++ apps/agora/app/src/app/primeNGPreset.ts
@@ -1,4 +1,4 @@
 import { definePreset } from '@primeng/themes';
 import { BasePreset } from '@sagebionetworks/explorers/themes';
 
-export const MyPreset = definePreset(BasePreset);
+export const AgoraPreset = definePreset(BasePreset);
