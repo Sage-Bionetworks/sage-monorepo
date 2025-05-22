64b930524dd86565db495500528c0e0476576602
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2025-05-22 22:07:07 +0000

    MG-213: add handler to server

---
+++ apps/model-ad/api/src/models/modeldetails.ts
@@ -0,0 +1,56 @@
+import {
+  GeneticInfo,
+  IndividualData,
+  ModelData,
+  ModelDetails,
+} from '@sagebionetworks/model-ad/api-client-angular';
+import { Schema, model } from 'mongoose';
+export { ModelDetails } from '@sagebionetworks/model-ad/api-client-angular';
+
+const IndividualDataSchema = new Schema<IndividualData>({
+  genotype: { type: String, required: true },
+  sex: { type: String, required: true, enum: ['Male', 'Female'] },
+  individual_id: { type: String, required: true },
+  value: { type: Number, required: true },
+});
+
+const ModelDataSchema = new Schema<ModelData>({
+  model: { type: String, required: true },
+  evidence_type: { type: String, required: true },
+  tissue: { type: String, required: true },
+  age: { type: String, required: true },
+  units: { type: String, required: true },
+  data: { type: [IndividualDataSchema], required: true },
+});
+
+const GeneticInfoSchema = new Schema<GeneticInfo>({
+  modified_gene: { type: String, required: true },
+  ensembl_gene_id: { type: String, required: true },
+  allele: { type: String, required: true },
+  allele_type: { type: String, required: true },
+  mgi_allele_id: { type: Number, required: true },
+});
+
+const ModelDetailsSchema = new Schema<ModelDetails>(
+  {
+    model: { type: String, required: true },
+    matched_controls: { type: [String], required: true },
+    model_type: { type: String, required: true },
+    contributing_group: { type: String, required: true },
+    study_synid: { type: String, required: true },
+    rrid: { type: String, required: true },
+    jax_id: { type: Number, required: true },
+    alzforum_id: { type: String, required: true },
+    genotype: { type: String, required: true },
+    aliases: { type: [String], required: true },
+    genetic_info: { type: [GeneticInfoSchema], required: true },
+    biomarkers: { type: [ModelDataSchema], required: true },
+    pathology: { type: [ModelDataSchema], required: true },
+  },
+  { collection: 'modeldetails' },
+);
+
+export const ModelDetailsCollection = model<ModelDetails>(
+  'ModelDetailsCollection',
+  ModelDetailsSchema,
+);
