a71f6b34280a5ee772aa9a1ed546fd60a2f4a243
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-05-22 18:44:07 +0000

    chore: create empty service

---
+++ apps/agora/gene-api/src/main/java/org/sagebionetworks/agora/gene/api/service/DifferentialExpressionProfileRnaService.java
@@ -0,0 +1,25 @@
+package org.sagebionetworks.agora.gene.api.service;
+
+import org.sagebionetworks.agora.gene.api.model.dto.DifferentialExpressionProfileRnaSearchQueryDto;
+import org.sagebionetworks.agora.gene.api.model.dto.DifferentialExpressionProfilesRnaPageDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DifferentialExpressionProfileRnaService {
+
+  private static final Logger logger = LoggerFactory.getLogger(
+    DifferentialExpressionProfileRnaService.class
+  );
+
+  public DifferentialExpressionProfilesRnaPageDto listDifferentialExpressionProfilesRna(
+    DifferentialExpressionProfileRnaSearchQueryDto query
+  ) {
+    logger.info("listDifferentialExpressionProfilesRna query: {}", query);
+
+    throw new UnsupportedOperationException(
+      "Unimplemented method 'listDifferentialExpressionProfilesRna'"
+    );
+  }
+}
