3fe11e48adf325e37b5255612b303b6f0bbacfe9
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2025-05-22 22:09:10 +0000

    MG-213: add mocks

---
+++ libs/model-ad/testing/README.md
@@ -0,0 +1,7 @@
+# model-ad-testing
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test model-ad-testing` to execute the unit tests.
---
+++ libs/model-ad/testing/src/lib/mocks/dataversion-mocks.ts
@@ -0,0 +1,6 @@
+import { Dataversion } from '@sagebionetworks/model-ad/api-client-angular';
+
+export const dataversionMock: Dataversion = {
+  data_file: 'syn63540270',
+  data_version: '39',
+};
