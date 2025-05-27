b02008d9bb500c089c15e624ed0a5fe9b5c30022
sagely1 <114952739+sagely1@users.noreply.github.com>
2025-05-27 22:33:12 +0000

    remove references that will be added in another PR

--- libs/explorers/comparison-tools/src/index.ts
+++ libs/explorers/comparison-tools/src/index.ts
@@ -1,3 +1,1 @@
 export * from './lib/base-comparison-tool/base-comparison-tool.component';
-export * from './lib/comparison-tool-legend-panel/comparison-tool-legend-panel.component';
-export * from './lib/comparison-tool-how-to-panel/comparison-tool-how-to-panel.component';
