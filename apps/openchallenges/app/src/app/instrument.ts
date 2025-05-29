2c7e9c975cfe5cf80337b21815af008d7d80e205
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-05-29 00:45:49 +0000

    chore: cleanup

--- apps/openchallenges/app/src/app/instrument.ts
+++ apps/openchallenges/app/src/app/instrument.ts
@@ -1,18 +1,9 @@
-import opentelemetry from '@opentelemetry/api';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
-import {
-  WebTracerProvider,
-  ConsoleSpanExporter,
-  SimpleSpanProcessor,
-  BatchSpanProcessor,
-} from '@opentelemetry/sdk-trace-web';
-import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+import { WebTracerProvider, BatchSpanProcessor } from '@opentelemetry/sdk-trace-web';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { resourceFromAttributes } from '@opentelemetry/resources';
 import { B3Propagator } from '@opentelemetry/propagator-b3';
 import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from '@opentelemetry/semantic-conventions';
-import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
-import { MeterProvider, PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load';
 import { W3CTraceContextPropagator } from '@opentelemetry/core/build/src/trace/W3CTraceContextPropagator';
 import { CompositePropagator } from '@opentelemetry/core';
@@ -56,37 +47,3 @@ provider.register({
 registerInstrumentations({
   instrumentations: [new DocumentLoadInstrumentation(), new UserInteractionInstrumentation()],
 });
-
-// registerInstrumentations({
-//   instrumentations: [
-//     getWebAutoInstrumentations({
-//       '@opentelemetry/instrumentation-document-load': {},
-//       '@opentelemetry/instrumentation-user-interaction': {},
-//       '@opentelemetry/instrumentation-fetch': {
-//         propagateTraceHeaderCorsUrls: /.+/,
-//       },
-//       '@opentelemetry/instrumentation-xml-http-request': {
-//         propagateTraceHeaderCorsUrls: /.+/,
-//       },
-//     }),
-//   ],
-// });
-
-// const metricReader = new PeriodicExportingMetricReader({
-//   exporter: new OTLPMetricExporter({
-//     url: 'https://ingest.<REGION>.signoz.cloud:443/v1/metrics',
-//     headers: {
-//       'signoz-ingestion-key': '<SIGNOZ_INGESTION_KEY>',
-//     },
-//   }),
-//   // Default is 60000ms (60 seconds). Set to 10 seconds for demonstrative purposes only.
-//   exportIntervalMillis: 6000,
-// });
-
-// const myServiceMeterProvider = new MeterProvider({
-//   resource,
-//   readers: [metricReader],
-// });
-
-// // Set this MeterProvider to be global to the app being instrumented.
-// opentelemetry.metrics.setGlobalMeterProvider(myServiceMeterProvider);
