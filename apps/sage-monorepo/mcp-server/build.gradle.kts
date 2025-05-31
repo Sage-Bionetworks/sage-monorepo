e7d71ece42186b4c9a0ea59e3458c5773ae74701
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-05-31 03:47:05 +0000

    feat: create sage-monorepo-mcp-server

---
+++ apps/sage-monorepo/mcp-server/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+	java
+  alias(libs.plugins.graalvm.native)
+	alias(libs.plugins.spring.dependency.management)
+	alias(libs.plugins.spring.boot)
+}
+
+group = "org.sagebionetworks"
+version = "0.0.1-SNAPSHOT"
+
+java {
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+    nativeImageCapable = true
+  }
+}
+
+graalvmNative {
+  binaries {
+    named("main") {
+      // Enable GraalVM Toolchain detection
+      javaLauncher.set(javaToolchains.launcherFor {
+        languageVersion.set(JavaLanguageVersion.of(21))
+        vendor.set(JvmVendorSpec.matching("Oracle"))
+      })
+
+      imageName.set(project.name)
+      buildArgs.add("--static")
+      buildArgs.add("--libc=musl")
+      buildArgs.add("--no-fallback")
+    }
+    named("test") {
+      // Enable GraalVM Toolchain detection
+      javaLauncher.set(javaToolchains.launcherFor {
+        languageVersion.set(JavaLanguageVersion.of(21))
+        vendor.set(JvmVendorSpec.matching("Oracle"))
+      })
+
+      buildArgs.add("-O0")
+    }
+  }
+}
+
+repositories {
+	mavenCentral()
+  mavenLocal()
+}
+
+dependencies {
+	implementation(libs.spring.ai.starter.mcp.server.webmvc)
+  implementation(libs.openchallenges.api.client.java)
+  compileOnly(libs.lombok)
+  annotationProcessor(libs.lombok)
+	testImplementation(libs.spring.boot.starter.test)
+  testCompileOnly(libs.lombok)
+  testAnnotationProcessor(libs.lombok)
+	testRuntimeOnly(libs.junit.platform.launcher)
+}
+
+tasks.withType<Test>().configureEach {
+  maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).coerceAtLeast(1)
+
+  useJUnitPlatform()
+
+  testLogging {
+    events("passed", "skipped", "failed")
+  }
+}
+
+tasks.named<org.springframework.boot.gradle.tasks.bundling.BootBuildImage>("bootBuildImage") {
+  imageName.set("ghcr.io/sage-bionetworks/${project.name}-base:local")
+}
---
+++ apps/sage-monorepo/mcp-server/src/main/java/org/sagebionetworks/sage/monorepo/mcp/server/McpServerApplication.java
@@ -0,0 +1,34 @@
+package org.sagebionetworks.sage.monorepo.mcp.server;
+
+import java.util.Arrays;
+import java.util.List;
+import org.sagebionetworks.sage.monorepo.mcp.server.config.McpServerConfigData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.ai.tool.ToolCallbacks;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class McpServerApplication implements CommandLineRunner {
+
+  private static final Logger logger = LoggerFactory.getLogger(McpServerApplication.class);
+
+  private final McpServerConfigData mcpServerConfigData;
+
+  public McpServerApplication(McpServerConfigData mcpServerConfigData) {
+    this.mcpServerConfigData = mcpServerConfigData;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(McpServerApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    logger.info(mcpServerConfigData.getWelcomeMessage());
+  }
+}
