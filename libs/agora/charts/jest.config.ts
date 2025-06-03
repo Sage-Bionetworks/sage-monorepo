3f5846fd9c64c91824322d93e6c4f7ef50d2d122
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2025-06-03 18:32:01 +0000

    MG-33: ensure that mocks are restored

--- libs/agora/charts/jest.config.ts
+++ libs/agora/charts/jest.config.ts
@@ -14,7 +14,13 @@ export default {
       },
     ],
   },
+  maxWorkers: '50%',
+  testEnvironment: 'jest-fixed-jsdom',
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  moduleNameMapper: {
+    d3: '<rootDir>/../../../node_modules/d3/dist/d3.min.js',
+    '^d3-(.*)$': '<rootDir>/../../../node_modules/d3-$1/dist/d3-$1.min.js',
+  },
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
