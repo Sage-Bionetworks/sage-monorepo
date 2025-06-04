22e0610ef9e1c34840ee65f9ce36fff7bbe649eb
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2025-06-04 02:52:34 +0000

    MG-179: add resource card

--- libs/explorers/ui/src/index.ts
+++ libs/explorers/ui/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/components/footer/footer.component';
 export * from './lib/components/header/header.component';
 export * from './lib/components/hero/hero.component';
+export * from './lib/components/resource-card/resource-card.component';
 export * from './lib/components/svg-image/svg-image.component';
---
+++ libs/explorers/ui/src/lib/components/resource-card/resource-card.component.spec.ts
@@ -0,0 +1,69 @@
+import { Router } from '@angular/router';
+import { render, screen } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
+import { ResourceCardComponent } from './resource-card.component';
+
+const navigateMock = jest.fn();
+
+const mockTitle = 'Test title';
+const mockDescription = 'Test description';
+
+const mockInternalLink = '/internal-link';
+const mockExternalLink = 'https://synapse.org';
+const mockAltText = 'Test alt text';
+
+describe('ResourceCardComponent', () => {
+  async function setup(inputs?: Partial<ResourceCardComponent>) {
+    const user = userEvent.setup();
+    const component = await render(ResourceCardComponent, {
+      componentInputs: {
+        imagePath: '/assets/test.svg',
+        description: mockDescription,
+        link: mockInternalLink,
+        altText: mockAltText,
+        ...inputs,
+      },
+      providers: [
+        {
+          provide: Router,
+          useValue: { navigate: navigateMock },
+        },
+      ],
+    });
+    return { user, component };
+  }
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should render title, description, and image', async () => {
+    await setup({ title: mockTitle });
+
+    expect(screen.getByText(mockTitle)).toBeInTheDocument();
+    expect(screen.getByText(mockDescription)).toBeInTheDocument();
+
+    const img = screen.getByAltText(mockAltText);
+    expect(img).toBeInTheDocument();
+  });
+
+  it('should open external link in new tab when links starts with http', async () => {
+    const openSpy = jest.spyOn(window, 'open').mockImplementation();
+
+    const { user } = await setup({ link: mockExternalLink });
+    const button = screen.getByRole('button', { name: mockDescription });
+    await user.click(button);
+
+    expect(openSpy).toHaveBeenCalledWith(mockExternalLink, '_blank');
+    openSpy.mockRestore();
+  });
+
+  it('should navigate internally when link does not start with http', async () => {
+    const { user } = await setup({ link: mockInternalLink });
+
+    const button = screen.getByRole('button', { name: mockDescription });
+    await user.click(button);
+
+    expect(navigateMock).toHaveBeenCalledWith([mockInternalLink]);
+  });
+});
