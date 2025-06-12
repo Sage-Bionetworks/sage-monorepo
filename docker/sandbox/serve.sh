8b2da8d451b7d32c7eaaf3bdf7379d70632ba2ae
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-06-12 15:38:05 +0000

    chore: containerize the Python app and push profiles data

--- docker/sandbox/serve.sh
+++ docker/sandbox/serve.sh
@@ -5,6 +5,7 @@ product_name="sandbox"
 args=(
   # List of services in alphanumeric order
   --file docker/"$product_name"/services/lambda-nodejs.yml
+  --file docker/"$product_name"/services/observability-python.yml
 
   --file docker/"$product_name"/networks.yml
   --file docker/"$product_name"/volumes.yml
