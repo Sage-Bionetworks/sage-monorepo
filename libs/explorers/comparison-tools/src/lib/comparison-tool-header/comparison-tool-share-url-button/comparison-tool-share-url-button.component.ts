0657382354b7e24ef6a7cac80a6a71dd9b997267
sagely1 <114952739+sagely1@users.noreply.github.com>
2025-06-12 21:27:20 +0000

    updated tooltip text, added toast and tooltip styling

--- libs/explorers/comparison-tools/src/lib/comparison-tool-header/comparison-tool-share-url-button/comparison-tool-share-url-button.component.ts
+++ libs/explorers/comparison-tools/src/lib/comparison-tool-header/comparison-tool-share-url-button/comparison-tool-share-url-button.component.ts
@@ -2,10 +2,11 @@ import { Component, inject, input } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { TooltipModule } from 'primeng/tooltip';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'explorers-comparison-tool-share-url-button',
-  imports: [ButtonModule, TooltipModule],
+  imports: [ButtonModule, TooltipModule, ToastModule],
   templateUrl: './comparison-tool-share-url-button.component.html',
   styleUrls: ['./comparison-tool-share-url-button.component.scss'],
 })
@@ -14,18 +15,17 @@ export class ComparisonToolShareURLButtonComponent {
 
   tooltip = input('');
 
+  toastDuration = 1000;
+
   copyUrl() {
     navigator.clipboard.writeText(window.location.href);
     this.messageService.clear();
     this.messageService.add({
+      key: 'share-url-button-toast',
       severity: 'info',
-      sticky: true,
       summary: '',
       detail:
         'URL copied to clipboard! Use this URL to share or bookmark the current table configuration.',
     });
-    setTimeout(() => {
-      this.messageService.clear();
-    }, 5000);
   }
 }
