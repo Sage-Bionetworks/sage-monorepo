6f6c219179a3ff2f81b5aed920bb40742e65f87a
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-06-20 20:50:59 +0000

    chore: migrate openchallenges-about to Angular `inject()`

--- libs/openchallenges/about/src/lib/about.component.ts
+++ libs/openchallenges/about/src/lib/about.component.ts
@@ -1,5 +1,4 @@
-
-import { Component, Renderer2 } from '@angular/core';
+import { Component, Renderer2, inject } from '@angular/core';
 import { ConfigService } from '@sagebionetworks/openchallenges/config';
 import { FooterComponent } from '@sagebionetworks/openchallenges/ui';
 import { getSeoData } from './about-seo-data';
@@ -18,11 +17,11 @@ export class AboutComponent {
   public termsOfUseUrl: string;
   public apiDocsUrl: string;
 
-  constructor(
-    private readonly configService: ConfigService,
-    private seoService: SeoService,
-    private renderer2: Renderer2,
-  ) {
+  private readonly configService = inject(ConfigService);
+  private seoService = inject(SeoService);
+  private renderer2 = inject(Renderer2);
+
+  constructor() {
     this.appVersion = this.configService.config.appVersion;
     this.dataUpdatedOn = this.configService.config.dataUpdatedOn;
     this.privacyPolicyUrl = this.configService.config.privacyPolicyUrl;
