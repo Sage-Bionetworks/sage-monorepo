4c316727296dd11f3aac8e8e042a75f3023bfe5a
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-06-27 18:54:30 +0000

    chore: remove APP_BASE_URL_PROVIDER_INDEX

--- apps/agora/app/src/app/app.config.ts
+++ apps/agora/app/src/app/app.config.ts
@@ -29,19 +29,9 @@ import { CustomUrlSerializer } from './app.custom-uri-serializer';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
-// This index is used to remove the corresponding provider in app.config.server.ts.
-// TODO: This index could be out of sync if we are not careful. Find a more elegant way.
-export const APP_BASE_URL_PROVIDER_INDEX = 1;
-
 export const appConfig: ApplicationConfig = {
   providers: [
     { provide: APP_ID, useValue: 'agora-app' },
-    {
-      // This provider must be specified at the index defined by APP_BASE_URL_PROVIDER_INDEX.
-      provide: 'APP_BASE_URL',
-      useFactory: () => '.',
-      deps: [],
-    },
     {
       provide: SYNAPSE_API_CLIENT_BASE_PATH,
       useFactory: () => 'https://repo-prod.prod.sagebase.org',
