718a9f668897c8d6428a608428041d0c5eb936fd
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-06-29 02:27:10 +0000

    chore: generate challenge per year dynamically

--- apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/model/repository/ChallengeRepository.java
+++ apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/model/repository/ChallengeRepository.java
@@ -1,7 +1,22 @@
 package org.sagebionetworks.openchallenges.challenge.service.model.repository;
 
+import java.util.List;
 import org.sagebionetworks.openchallenges.challenge.service.model.entity.ChallengeEntity;
+import org.sagebionetworks.openchallenges.challenge.service.model.projection.YearlyChallengeCount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ChallengeRepository
-  extends JpaRepository<ChallengeEntity, Long>, CustomChallengeRepository {}
+  extends JpaRepository<ChallengeEntity, Long>, CustomChallengeRepository {
+  @Query(
+    value = "SELECT EXTRACT(YEAR FROM start_date) AS year, COUNT(*) AS count " +
+    "FROM challenge " +
+    "WHERE start_date IS NOT NULL " +
+    "GROUP BY year ORDER BY year",
+    nativeQuery = true
+  )
+  List<YearlyChallengeCount> findChallengeCountsByYear();
+
+  @Query(value = "SELECT COUNT(*) FROM challenge WHERE start_date IS NULL", nativeQuery = true)
+  int countUndatedChallenges();
+}
--- apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/service/ChallengeAnalyticsService.java
+++ apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/service/ChallengeAnalyticsService.java
@@ -1,45 +1,35 @@
 package org.sagebionetworks.openchallenges.challenge.service.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.sagebionetworks.openchallenges.challenge.service.model.dto.ChallengesPerYearDto;
+import org.sagebionetworks.openchallenges.challenge.service.model.projection.YearlyChallengeCount;
+import org.sagebionetworks.openchallenges.challenge.service.model.repository.ChallengeRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ChallengeAnalyticsService {
 
+  private final ChallengeRepository challengeRepository;
+
+  public ChallengeAnalyticsService(ChallengeRepository challengeRepository) {
+    this.challengeRepository = challengeRepository;
+  }
+
   @Transactional(readOnly = true)
   public ChallengesPerYearDto getChallengesPerYear() {
-    List<String> years = Arrays.asList(
-      "2007",
-      "2008",
-      "2009",
-      "2010",
-      "2011",
-      "2012",
-      "2013",
-      "2014",
-      "2015",
-      "2016",
-      "2017",
-      "2018",
-      "2019",
-      "2020",
-      "2021",
-      "2022",
-      "2023",
-      "2024",
-      "2025"
-    );
+    List<YearlyChallengeCount> results = challengeRepository.findChallengeCountsByYear();
+    int undatedChallengeCount = challengeRepository.countUndatedChallenges();
 
-    // The following line will be auto-updated by a script and should NOT be modified manually.
-    List<Integer> challengeCounts = /* AUTO-UPDATE MARKER */Arrays.asList(11, 17, 25, 34, 45, 53, 61, 73, 85, 100, 138, 155, 178, 203, 254, 316, 387, 452, 484);
-    Integer undatedChallengeCount = 171;
+    List<String> years = new ArrayList<>();
+    List<Integer> challengeCounts = new ArrayList<>();
 
-    // int currentYear = Year.now().getValue();
-    // years.removeIf(year -> Integer.parseInt(year) > currentYear);
-    // challengeCounts = challengeCounts.subList(0, years.size());
+    for (YearlyChallengeCount row : results) {
+      years.add(String.valueOf(row.getYear()));
+      challengeCounts.add(row.getCount());
+    }
 
     return ChallengesPerYearDto.builder()
       .years(years)
