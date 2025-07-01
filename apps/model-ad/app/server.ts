f59d8f474be2c6e36ea96d014ac1df580d473242
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2025-07-01 17:37:32 +0000

    SMR-213: use inject() in model-ad-config and fix APP_PORT DI

--- apps/model-ad/app/server.ts
+++ apps/model-ad/app/server.ts
@@ -73,10 +73,12 @@ export function app(): express.Express {
 }
 
 function run(): void {
+  const port = process.env['PORT'] || '4200';
+
   // Start up the Node server
   const server = app();
-  server.listen(PORT, () => {
-    console.log(`Node Express server listening on http://localhost:${PORT}`);
+  server.listen(port, () => {
+    console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
 
