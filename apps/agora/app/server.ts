871a12e6e497428eb5089dfc6a3a6594886d7b6c
sagely1 <114952739+sagely1@users.noreply.github.com>
2025-07-01 22:24:46 +0000

    fix module check does not work for ESM setups

--- apps/agora/app/server.ts
+++ apps/agora/app/server.ts
@@ -71,6 +71,15 @@ function run(): void {
   });
 }
 
-if (require.main === module) {
+// Webpack will replace 'require' with '__webpack_require__'
+// '__non_webpack_require__' is a proxy to Node 'require'
+// The below code is to ensure that the server is run only when not requiring the bundle.
+/* eslint-disable camelcase,no-undef */
+declare const __non_webpack_require__: NodeRequire;
+const mainModule = __non_webpack_require__.main;
+const moduleFilename = (mainModule && mainModule.filename) || '';
+if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
   run();
 }
+
+export default bootstrap;
