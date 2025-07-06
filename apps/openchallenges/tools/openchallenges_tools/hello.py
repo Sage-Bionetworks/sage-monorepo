db77986758a95c22be1fbab26585709ad1e5752d
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-07-06 23:34:15 +0000

    chore: demonstrate lib import into the app hello.py

--- apps/openchallenges/tools/openchallenges_tools/hello.py
+++ apps/openchallenges/tools/openchallenges_tools/hello.py
@@ -1,6 +1,18 @@
 """Sample Hello World application."""
 
+from openchallenges_api_client_python.hello import hello as api_hello
 
-def hello():
+
+def hello() -> str:
     """Return a friendly greeting."""
     return "Hello openchallenges-tools"
+
+
+def hello_lib() -> str:
+    """Return greeting from the API client library."""
+    return api_hello()
+
+
+if __name__ == "__main__":
+    print(hello())
+    print(hello_lib())
