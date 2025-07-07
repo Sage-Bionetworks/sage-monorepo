d1442c9cad3fa0670168cf48cd92b11941fd2618
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-07-07 22:18:04 +0000

    test: fix tests

--- apps/openchallenges/tools/openchallenges_tools/get_challenge.py
+++ apps/openchallenges/tools/openchallenges_tools/get_challenge.py
@@ -3,7 +3,6 @@
 import openchallenges_api_client_python
 from openchallenges_api_client_python.models.challenge import Challenge
 from openchallenges_api_client_python.rest import ApiException
-from pprint import pprint
 
 # Defining the host is optional and defaults to http://localhost/v1
 # See configuration.py for a list of all supported configuration parameters.
@@ -17,15 +16,12 @@ def get_challenge(challenge_id: int) -> Challenge:
     with openchallenges_api_client_python.ApiClient(configuration) as api_client:
         # Create an instance of the API class
         challenge_api = openchallenges_api_client_python.ChallengeApi(api_client)
-        # challenge_id = 516  # int | The unique identifier of the challenge.
 
         try:
             # Get a challenge
-            api_response = challenge_api.get_challenge(challenge_id)
-            print("The response of ChallengeApi->get_challenge:\n")
-            return api_response
+            return challenge_api.get_challenge(challenge_id)
         except ApiException as e:
-            print("Exception when calling ChallengeApi->get_challenge: %s\n" % e)
+            print(f"Exception when calling ChallengeApi->get_challenge: {e}\n")
             raise
 
 
