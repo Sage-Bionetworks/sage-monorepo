5c21fa61940e4125050c22f9a57dd7daba144ba2
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-07-09 19:39:40 -0700

    feat(amp-als): add health actuator to the AMP-ALS dataset service to support K8s deployment (SMR-260) (#3287)

--- apps/amp-als/dataset-service/build.gradle.kts
+++ apps/amp-als/dataset-service/build.gradle.kts
@@ -21,7 +21,6 @@ java {
 }
 
 repositories {
-  // maven { url = uri("https://repo.spring.io/milestone") }
   mavenCentral()
   mavenLocal()
 }
@@ -32,13 +31,13 @@ dependencies {
   implementation(libs.amp.als.app.config.data)
   implementation(libs.findbugs.jsr305)
   implementation(libs.flyway.core)
-  implementation(libs.flyway.database.postgresql)
   implementation(libs.hibernate.search.backend.elasticsearch)
   implementation(libs.hibernate.search.mapper.orm)
   implementation(libs.jackson.databind)
   implementation(libs.jackson.dataformat.yaml)
   implementation(libs.jackson.datatype.jsr310)
   implementation(libs.sagebionetworks.util)
+  implementation(libs.spring.boot.starter.actuator)
   implementation(libs.spring.boot.starter.data.jpa)
   implementation(libs.spring.boot.starter.jdbc)
   implementation(libs.spring.boot.starter.security)
@@ -46,6 +45,7 @@ dependencies {
   implementation(libs.spring.boot.starter.web)
   implementation(libs.springdoc.openapi.ui)
   implementation(platform(libs.spring.boot.dependencies))
+  runtimeOnly(libs.flyway.database.postgresql)
   runtimeOnly(libs.postgresql)
   runtimeOnly(libs.spring.boot.devtools)
   testAnnotationProcessor(libs.lombok)
