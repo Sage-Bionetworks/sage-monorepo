5209de1d71035d46ce91b326040aa6b1605fe46b
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2025-07-17 20:52:37 +0000

    MG-219: add unit tests

--- libs/model-ad/model-details/src/lib/components/model-details-boxplots-selector/model-details-boxplots-selector.component.spec.ts
+++ libs/model-ad/model-details/src/lib/components/model-details-boxplots-selector/model-details-boxplots-selector.component.spec.ts
@@ -6,7 +6,7 @@ import {
   validWikiParams,
 } from '@sagebionetworks/explorers/testing';
 import { modelMock } from '@sagebionetworks/model-ad/testing';
-import { render, screen } from '@testing-library/angular';
+import { render, screen, waitFor } from '@testing-library/angular';
 import { ModelDetailsBoxplotsSelectorComponent } from './model-details-boxplots-selector.component';
 
 const mockTitle = 'Pathology';
@@ -39,5 +39,24 @@ describe('ModelDetailsBoxplotsSelectorComponent', () => {
     await setup();
     expect(screen.getByRole('combobox', { name: /tissue/i })).toBeVisible();
     expect(screen.getByRole('combobox', { name: /sex/i })).toBeVisible();
+  });
+
+  it('should automatically select first tissue when data loads', async () => {
+    await setup();
+    const tissueSelect = screen.getByRole('combobox', { name: /tissue/i });
+
+    const firstTissue = modelMock.pathology[0].tissue;
+    await waitFor(() => {
+      expect(tissueSelect).toHaveAccessibleName(firstTissue);
+    });
+  });
+
+  it('should use default value for sex', async () => {
+    await setup();
+    const sexSelect = screen.getByRole('combobox', { name: /sex/i });
+
+    await waitFor(() => {
+      expect(sexSelect).toHaveAccessibleName('Male & Female');
+    });
   });
 });
