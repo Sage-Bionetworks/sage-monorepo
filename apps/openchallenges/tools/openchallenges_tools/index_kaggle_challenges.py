6d42e022fa7a89740f4f701aa7bc9b9901080a12
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-07-18 16:13:46 +0000

    chore: cleanup

--- apps/openchallenges/tools/openchallenges_tools/index_kaggle_challenges.py
+++ apps/openchallenges/tools/openchallenges_tools/index_kaggle_challenges.py
@@ -9,7 +9,7 @@ import kaggle
 from opensearchpy import OpenSearch
 
 # Configure logging
-LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper() or "INFO"
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
 logging.basicConfig(
     level=getattr(logging, LOG_LEVEL, logging.INFO),
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
