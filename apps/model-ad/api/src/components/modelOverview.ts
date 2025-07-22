c4aef463d2be8d76b089ba0c00f3259fcfeb09cd
sagely1 <114952739+sagely1@users.noreply.github.com>
2025-07-22 23:29:57 +0000

    add filters to api

--- apps/model-ad/api/src/components/modelOverview.ts
+++ apps/model-ad/api/src/components/modelOverview.ts
@@ -3,7 +3,7 @@ import { cache, setHeaders } from '../helpers';
 import { ModelOverviewCollection } from '../models';
 import { ModelOverview } from '@sagebionetworks/model-ad/api-client-angular';
 
-export async function getModelOverview() {
+export async function getModelOverviews() {
   const cacheKey = 'modelOverview';
   const cachedResult: ModelOverview[] | null | undefined = cache.get(cacheKey);
 
@@ -19,7 +19,7 @@ export async function getModelOverview() {
 
 export async function modelOverviewRoute(req: Request, res: Response, next: NextFunction) {
   try {
-    const result = await getModelOverview();
+    const result = await getModelOverviews();
 
     if (!result || result.length === 0) {
       res.status(404).contentType('application/problem+json').json({
--- apps/model-ad/api/src/models/comparisonToolConfig.ts
+++ apps/model-ad/api/src/models/comparisonToolConfig.ts
@@ -1,13 +1,19 @@
 import {
   ComparisonToolConfig,
   ComparisonToolConfigColumn,
+  ComparisonToolConfigFilter,
 } from '@sagebionetworks/model-ad/api-client-angular';
 import { Schema, model } from 'mongoose';
+
+const ComparisonToolConfigFilterSchema = new Schema<ComparisonToolConfigFilter>({
+  name: { type: String, required: true },
+  field: { type: String, required: true },
+  values: { type: [String], required: true },
+});
 
 const ComparisonToolConfigColumnSchema = new Schema<ComparisonToolConfigColumn>({
   name: { type: String, required: true },
   type: { type: String, required: true },
-  column_type: { type: String, required: true },
   tooltip: { type: String, required: true },
   sort_tooltip: { type: String, required: true },
 });
@@ -20,6 +26,7 @@ const ComparisonToolConfigSchema = new Schema<ComparisonToolConfig>(
       type: [ComparisonToolConfigColumnSchema],
       required: true,
     },
+    filters: { type: [ComparisonToolConfigFilterSchema], required: true },
   },
   {
     collection: 'ui_config',
