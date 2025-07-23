c89b88c723919a168222e720f36913c562c24805
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-07-23 03:25:19 +0000

    chore: generate the organization service

---
+++ apps/openchallenges/organization-service/src/main/java/org/sagebionetworks/openchallenges/organization/service/model/dto/ChallengeParticipationRoleDto.java
@@ -0,0 +1,57 @@
+package org.sagebionetworks.openchallenges.organization.service.model.dto;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonValue;
+import java.time.OffsetDateTime;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import jakarta.annotation.Generated;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+/**
+ * The nature of a challenge participation.
+ */
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", comments = "Generator version: 7.14.0")
+public enum ChallengeParticipationRoleDto {
+  
+  CHALLENGE_ORGANIZER("challenge_organizer"),
+  
+  DATA_CONTRIBUTOR("data_contributor"),
+  
+  SPONSOR("sponsor");
+
+  private final String value;
+
+  ChallengeParticipationRoleDto(String value) {
+    this.value = value;
+  }
+
+  @JsonValue
+  public String getValue() {
+    return value;
+  }
+
+  @Override
+  public String toString() {
+    return String.valueOf(value);
+  }
+
+  @JsonCreator
+  public static ChallengeParticipationRoleDto fromValue(String value) {
+    for (ChallengeParticipationRoleDto b : ChallengeParticipationRoleDto.values()) {
+      if (b.value.equals(value)) {
+        return b;
+      }
+    }
+    throw new IllegalArgumentException("Unexpected value '" + value + "'");
+  }
+}
+
