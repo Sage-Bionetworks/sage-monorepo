9679ea0d914ad2facc306c1b5e47d464f0cd4506
hallieswan <26949006+hallieswan@users.noreply.github.com>
2025-07-30 09:29:06 -0700

    feat(model-ad): add sticky table of contents for model details boxplots (MG-300) (#3347)

--- libs/model-ad/model-details/src/lib/components/model-details-boxplots-selector/model-details-boxplots-selector.component.ts
+++ libs/model-ad/model-details/src/lib/components/model-details-boxplots-selector/model-details-boxplots-selector.component.ts
@@ -1,18 +1,38 @@
-import { Component, computed, effect, input, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  effect,
+  ElementRef,
+  inject,
+  input,
+  signal,
+  ViewChild,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SynapseWikiParams } from '@sagebionetworks/explorers/models';
-import { ModalLinkComponent } from '@sagebionetworks/explorers/util';
+import { HelperService } from '@sagebionetworks/explorers/services';
+import { DecodeGreekEntityPipe, ModalLinkComponent } from '@sagebionetworks/explorers/util';
 import { IndividualData, ModelData } from '@sagebionetworks/model-ad/api-client-angular';
 import { SelectModule } from 'primeng/select';
 import { ModelDetailsBoxplotsGridComponent } from '../model-details-boxplots-grid/model-details-boxplots-grid.component';
 
 @Component({
   selector: 'model-ad-model-details-boxplots-selector',
-  imports: [FormsModule, SelectModule, ModelDetailsBoxplotsGridComponent, ModalLinkComponent],
+  imports: [
+    FormsModule,
+    SelectModule,
+    ModelDetailsBoxplotsGridComponent,
+    ModalLinkComponent,
+    DecodeGreekEntityPipe,
+  ],
   templateUrl: './model-details-boxplots-selector.component.html',
   styleUrls: ['./model-details-boxplots-selector.component.scss'],
 })
 export class ModelDetailsBoxplotsSelectorComponent {
+  private readonly helperService = inject(HelperService);
+
+  @ViewChild('boxplotsContainer', { static: false }) boxplotsContainer!: ElementRef<HTMLElement>;
+
   title = input.required<string>();
   modelName = input.required<string>();
   modelControls = input.required<string[]>();
@@ -30,6 +50,8 @@ export class ModelDetailsBoxplotsSelectorComponent {
     return Array.from(new Set(this.modelDataList().map((item) => item.tissue)));
   });
   selectedTissueOption = signal('');
+
+  private readonly SCROLL_PADDING = 15;
 
   constructor() {
     effect(() => {
@@ -66,5 +88,38 @@ export class ModelDetailsBoxplotsSelectorComponent {
     return this.selectedModelDataList().filter(
       (modelData) => modelData.evidence_type === evidenceType,
     );
+  }
+
+  generateAnchorId(evidenceType: string): string {
+    return evidenceType
+      .toLowerCase()
+      .replace(/[^a-z0-9]/g, '-')
+      .replace(/-+/g, '-');
+  }
+
+  scrollToSection(anchorId: string): void {
+    if (
+      typeof document !== 'undefined' &&
+      typeof window !== 'undefined' &&
+      this.boxplotsContainer
+    ) {
+      const element = this.boxplotsContainer.nativeElement.querySelector(
+        `#${anchorId}`,
+      ) as HTMLElement;
+
+      if (element) {
+        const tocElement = document.querySelector('.table-of-contents-container');
+        const tocHeight = tocElement ? tocElement.getBoundingClientRect().height : 0;
+
+        const panelNavHeight = this.helperService.getNumberFromCSSValue(
+          getComputedStyle(document.documentElement).getPropertyValue('--panel-nav-height'),
+        );
+
+        const yOffset = -(tocHeight + panelNavHeight + this.SCROLL_PADDING);
+        const elementOffset = this.helperService.getOffset(element);
+        const y = elementOffset.top + yOffset;
+        window.scrollTo({ top: y, behavior: 'smooth' });
+      }
+    }
   }
 }
