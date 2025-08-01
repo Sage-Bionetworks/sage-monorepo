ad806ef56a9e66668428c6779d9e77852133a0f4
rrchai <73901500+rrchai@users.noreply.github.com>
2025-08-01 15:44:10 +0000

    disable controller

--- apps/bixarena/app/docker-entrypoint.sh
+++ apps/bixarena/app/docker-entrypoint.sh
@@ -5,6 +5,6 @@ python generate_model_config.py
 
 # Run the application as app user
 exec gosu "$APP_USERNAME" python -m fastchat.serve.gradio_web_server_multi \
-    --controller-url "http://${CONTROLLER_HOST}:${CONTROLLER_PORT}" \
+    --controller-url "" \
     --port "${APP_PORT}" \
     --register-api-endpoint-file model_config.json
