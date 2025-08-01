32448b7f782f7b3b44f7d591f39e83cf20cedb2c
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-08-01 21:22:53 +0000

    chore: update project files but gradle version catalog and src

--- apps/openchallenges/image-service/tools/pull-templates.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-openapiGeneratorVersion="v7.0.0"
-destinationFolder="${PWD}/templates/${openapiGeneratorVersion}"
-
-mkdir -p "${destinationFolder}"
-
-curl -O --output-dir "${destinationFolder}" \
-  "https://raw.githubusercontent.com/OpenAPITools/openapi-generator/${openapiGeneratorVersion}/modules/openapi-generator/src/main/resources/JavaSpring/pojo.mustache"
