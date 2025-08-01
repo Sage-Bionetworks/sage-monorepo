01a27ff86070f589a4bd7f409b9ed508f514dd7d
rrchai <73901500+rrchai@users.noreply.github.com>
2025-08-01 21:37:00 +0000

    use envsubst to generate model config

--- apps/bixarena/app/docker-entrypoint.sh
+++ apps/bixarena/app/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ "$1" = 'python' ]; then
     cd ${APP_DIR}
-    gosu "$APP_USERNAME" python generate_model_config.py
+    gosu "$APP_USERNAME" envsubst < model_config.template.json > model_config.json
     exec gosu "$APP_USERNAME" python -m fastchat.serve.gradio_web_server_multi \
         --controller-url "" \
         --port "${APP_PORT}" \
