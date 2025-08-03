cf894c3319011723ed86e7cd81debd1c62cb9c77
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-08-03 22:15:11 +0000

    chore: upgrade to spring-cloud-starter-gateway-server-webflux

--- apps/openchallenges/api-gateway/build.gradle.kts
+++ apps/openchallenges/api-gateway/build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
   implementation(libs.openchallenges.app.config.data)
   implementation(libs.spring.boot.starter.actuator)
   implementation(libs.spring.boot.starter.security)
-  implementation(libs.spring.cloud.starter.gateway)
+  implementation(libs.spring.cloud.starter.gateway.server.webflux)
   implementation(platform(libs.spring.boot.dependencies))
   runtimeOnly(libs.spring.boot.devtools)
   testImplementation(libs.spring.boot.starter.test)
