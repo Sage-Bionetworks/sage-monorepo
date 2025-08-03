3b52b4285009af7da685fba13b2c8e79c5dec490
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-08-03 02:48:08 +0000

    chore: remove the service registry from Docker Compose files

--- docker/openchallenges/serve-detach.sh
+++ docker/openchallenges/serve-detach.sh
@@ -9,7 +9,6 @@ args=(
   --file docker/"$product_name"/services/app.yml
   --file docker/"$product_name"/services/auth-service.yml
   --file docker/"$product_name"/services/challenge-service.yml
-  --file docker/"$product_name"/services/config-server.yml
   --file docker/"$product_name"/services/image-service.yml
   --file docker/"$product_name"/services/kafka.yml
   --file docker/"$product_name"/services/mcp-server.yml
@@ -17,7 +16,6 @@ args=(
   --file docker/"$product_name"/services/opensearch.yml
   --file docker/"$product_name"/services/organization-service.yml
   --file docker/"$product_name"/services/postgres.yml
-  --file docker/"$product_name"/services/service-registry.yml
   --file docker/"$product_name"/services/thumbor.yml
 
   --file docker/"$product_name"/networks.yml
