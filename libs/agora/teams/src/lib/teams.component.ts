d068e94c1157da8a7591b5181c67240ef12f6867
sagely1 <114952739+sagely1@users.noreply.github.com>
2025-08-15 21:16:38 +0000

    fix teams page memory leak

--- libs/agora/teams/src/lib/teams.component.ts
+++ libs/agora/teams/src/lib/teams.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { Team, TeamsList, TeamsService } from '@sagebionetworks/agora/api-client-angular';
 import { HelperService } from '@sagebionetworks/agora/services';
 import { catchError, finalize, map, Observable, of } from 'rxjs';
 import { TeamListComponent } from './team-list/team-list.component';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'agora-teams',
@@ -12,6 +13,8 @@ import { TeamListComponent } from './team-list/team-list.component';
   styleUrls: ['./teams.component.scss'],
 })
 export class TeamsComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+
   helperService = inject(HelperService);
   teamsService = inject(TeamsService);
 
@@ -25,6 +28,7 @@ export class TeamsComponent implements OnInit {
     this.helperService.setLoading(true);
 
     this.teams$ = this.teamsService.listTeams().pipe(
+      takeUntilDestroyed(this.destroyRef),
       map((res: TeamsList) => res.items || []),
       catchError((error: Error) => {
         console.error('Error loading teams:', error.message);
