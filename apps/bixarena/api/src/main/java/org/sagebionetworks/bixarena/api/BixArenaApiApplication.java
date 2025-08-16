3393781ea1b1d96ceee44b9cec11ed07f98b836f
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-08-16 21:48:59 +0000

    chore: remove unused imports in bixarena-api

--- apps/bixarena/api/src/main/java/org/sagebionetworks/bixarena/api/BixArenaApiApplication.java
+++ apps/bixarena/api/src/main/java/org/sagebionetworks/bixarena/api/BixArenaApiApplication.java
@@ -5,9 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
 
 @SpringBootApplication(nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class)
