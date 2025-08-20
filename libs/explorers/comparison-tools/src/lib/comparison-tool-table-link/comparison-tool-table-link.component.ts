67c92294b308c92ce0462bcacd0c0336fa25ba37
sagely1 <114952739+sagely1@users.noreply.github.com>
2025-08-20 12:29:44 -0700

    feat(model-ad): add table links (MG-355) (#3386)

--- libs/explorers/comparison-tools/src/lib/comparison-tool-table-link/comparison-tool-table-link.component.ts
+++ libs/explorers/comparison-tools/src/lib/comparison-tool-table-link/comparison-tool-table-link.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { isExternalLink } from '@sagebionetworks/shared/util';
 
@@ -12,7 +12,31 @@ export class ComparisonToolTableLinkComponent {
   linkText = input<string>('');
   linkUrl = input<string>('');
 
-  isExternalLink() {
+  private parsedUrl = computed(() => {
+    const url = this.linkUrl().trim();
+    const urlParts = url.split('?');
+
+    let path = urlParts[0];
+    // ensure path starts with '/'
+    path = path.startsWith('/') ? path : `/${path}`;
+
+    const queryParams: { [key: string]: string | boolean } = {};
+
+    // if url has parameters, parse them
+    if (urlParts.length > 1) {
+      const params = new URLSearchParams(urlParts[1] || '');
+      params.forEach((value, key) => {
+        queryParams[key] = value;
+      });
+    }
+
+    return { path, queryParams };
+  });
+
+  internalLinkPath = computed(() => this.parsedUrl().path);
+  internalLinkQueryParams = computed(() => this.parsedUrl().queryParams);
+
+  isExternalUrl(): boolean {
     return isExternalLink(this.linkUrl());
   }
 }
