0000ff6445c75083281e68f4da5c2f2680334867
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-08-21 03:54:40 +0000

    fix: make the app bind to 0.0.0.0 instead of 127.0.0.1

--- apps/bixarena/app/docker-entrypoint.sh
+++ apps/bixarena/app/docker-entrypoint.sh
@@ -5,6 +5,7 @@ if [ "$1" = 'python' ]; then
     cd ${APP_DIR}
     gosu "$APP_USERNAME" envsubst < model_config.template.json > model_config.json
     exec gosu "$APP_USERNAME" python -m main \
+        --host 0.0.0.0 \
         --port "${APP_PORT}" \
         --register-api-endpoint-file model_config.json
 fi
