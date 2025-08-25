1fec61894a70e80afb7fa0b629bd7d2311a9c812
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2025-08-25 20:57:00 +0000

    AG-1860: specify shared loading icon colors for agora, remove unnecessary explorers styles import

--- libs/agora/config/src/lib/constants.ts
+++ libs/agora/config/src/lib/constants.ts
@@ -1,3 +1,5 @@
+import { LoadingIconColors } from '@sagebionetworks/explorers/models';
+
 export const HELP_URL = 'https://help.adknowledgeportal.org/apd/Agora-Resources.2646671361.html';
 
 export const SUPPORT_EMAIL = 'agora@sagebionetworks.org';
@@ -13,3 +15,10 @@ export const ROUTE_PATHS = {
   NOT_FOUND: 'not-found',
   ERROR: 'error',
 } as const;
+
+// Use color-secondary to match agora-loading-icon
+export const AGORA_LOADING_ICON_COLORS: LoadingIconColors = {
+  colorInnermost: '#8b8ad1',
+  colorCentral: '#8b8ad1',
+  colorOutermost: '#8b8ad1',
+};
