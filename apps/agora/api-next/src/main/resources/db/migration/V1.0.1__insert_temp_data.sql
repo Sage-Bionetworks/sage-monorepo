88563187fb4b5acafd7c74f91a4f59565067fd0c
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-08-25 16:43:41 +0000

    chore: generate the api server stub

--- apps/agora/api-next/src/main/resources/db/migration/V1.0.1__insert_temp_data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO dataset (id, name, description) VALUES
-  (1, 'Mock Dataset 1', 'Clinical data from patients with cardiovascular conditions'),
-  (2, 'Mock Dataset 2', 'Longitudinal dataset of individuals with early signs of neurodegeneration'),
-  (3, 'Mock Dataset 3', 'Registry of patients enrolled in various oncology clinical trials');
-
--- Update the sequence to continue from the highest ID
-SELECT setval('dataset_id_seq', (SELECT MAX(id) FROM dataset));
