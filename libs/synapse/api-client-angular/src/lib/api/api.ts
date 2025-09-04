49bb37f7a7954602506b3dde0aaf1ba565e1f758
hallieswan <26949006+hallieswan@users.noreply.github.com>
2025-09-04 12:00:46 -0700

    chore(synapse): generate Angular API client files in kebab case (SMR-415) (#3441)

--- libs/synapse/api-client-angular/src/lib/api/api.ts
+++ libs/synapse/api-client-angular/src/lib/api/api.ts
@@ -1,89 +1,95 @@
-export * from './accessApprovalServices.service';
-import { AccessApprovalServicesService } from './accessApprovalServices.service';
-export * from './accessRequirementServices.service';
-import { AccessRequirementServicesService } from './accessRequirementServices.service';
-export * from './activityServices.service';
-import { ActivityServicesService } from './activityServices.service';
-export * from './agentChatServices.service';
-import { AgentChatServicesService } from './agentChatServices.service';
-export * from './asynchronousJobServices.service';
-import { AsynchronousJobServicesService } from './asynchronousJobServices.service';
-export * from './authenticationServices.service';
-import { AuthenticationServicesService } from './authenticationServices.service';
-export * from './authenticationServices2FA.service';
-import { AuthenticationServices2FAService } from './authenticationServices2FA.service';
-export * from './certifiedUserServices.service';
-import { CertifiedUserServicesService } from './certifiedUserServices.service';
-export * from './challengeServices.service';
-import { ChallengeServicesService } from './challengeServices.service';
-export * from './dOIServices.service';
-import { DOIServicesService } from './dOIServices.service';
-export * from './dataAccessServices.service';
-import { DataAccessServicesService } from './dataAccessServices.service';
-export * from './discussionServices.service';
-import { DiscussionServicesService } from './discussionServices.service';
-export * from './dockerAuthorizationServices.service';
-import { DockerAuthorizationServicesService } from './dockerAuthorizationServices.service';
-export * from './dockerCommitServices.service';
-import { DockerCommitServicesService } from './dockerCommitServices.service';
-export * from './dockerRegistryEventServices.service';
-import { DockerRegistryEventServicesService } from './dockerRegistryEventServices.service';
-export * from './downloadListServices.service';
-import { DownloadListServicesService } from './downloadListServices.service';
-export * from './drsServices.service';
-import { DrsServicesService } from './drsServices.service';
-export * from './entityBundleServicesV2.service';
-import { EntityBundleServicesV2Service } from './entityBundleServicesV2.service';
-export * from './entityServices.service';
-import { EntityServicesService } from './entityServices.service';
-export * from './evaluationServices.service';
-import { EvaluationServicesService } from './evaluationServices.service';
-export * from './fileServices.service';
-import { FileServicesService } from './fileServices.service';
-export * from './formServices.service';
-import { FormServicesService } from './formServices.service';
-export * from './jSONSchemaServices.service';
-import { JSONSchemaServicesService } from './jSONSchemaServices.service';
-export * from './logService.service';
-import { LogServiceService } from './logService.service';
-export * from './membershipInvitationServices.service';
-import { MembershipInvitationServicesService } from './membershipInvitationServices.service';
-export * from './membershipRequestServices.service';
-import { MembershipRequestServicesService } from './membershipRequestServices.service';
-export * from './messageServices.service';
-import { MessageServicesService } from './messageServices.service';
-export * from './openIDConnectServices.service';
-import { OpenIDConnectServicesService } from './openIDConnectServices.service';
-export * from './principalServices.service';
-import { PrincipalServicesService } from './principalServices.service';
-export * from './projectSettingsServices.service';
-import { ProjectSettingsServicesService } from './projectSettingsServices.service';
-export * from './recycleBinServices.service';
-import { RecycleBinServicesService } from './recycleBinServices.service';
-export * from './searchServices.service';
-import { SearchServicesService } from './searchServices.service';
-export * from './statisticsServices.service';
-import { StatisticsServicesService } from './statisticsServices.service';
-export * from './storageReportServices.service';
-import { StorageReportServicesService } from './storageReportServices.service';
-export * from './subscriptionServices.service';
-import { SubscriptionServicesService } from './subscriptionServices.service';
-export * from './tableServices.service';
-import { TableServicesService } from './tableServices.service';
-export * from './teamServices.service';
-import { TeamServicesService } from './teamServices.service';
-export * from './userGroupServices.service';
-import { UserGroupServicesService } from './userGroupServices.service';
-export * from './userProfileServices.service';
-import { UserProfileServicesService } from './userProfileServices.service';
-export * from './verificationServices.service';
-import { VerificationServicesService } from './verificationServices.service';
-export * from './webhookServices.service';
-import { WebhookServicesService } from './webhookServices.service';
-export * from './wikiPageServices.service';
-import { WikiPageServicesService } from './wikiPageServices.service';
-export * from './wikiPageServices2.service';
-import { WikiPageServices2Service } from './wikiPageServices2.service';
+export * from './access-approval-services.service';
+import { AccessApprovalServicesService } from './access-approval-services.service';
+export * from './access-requirement-services.service';
+import { AccessRequirementServicesService } from './access-requirement-services.service';
+export * from './activity-services.service';
+import { ActivityServicesService } from './activity-services.service';
+export * from './agent-chat-services.service';
+import { AgentChatServicesService } from './agent-chat-services.service';
+export * from './asynchronous-job-services.service';
+import { AsynchronousJobServicesService } from './asynchronous-job-services.service';
+export * from './authentication-services.service';
+import { AuthenticationServicesService } from './authentication-services.service';
+export * from './authentication-services2-fa.service';
+import { AuthenticationServices2FAService } from './authentication-services2-fa.service';
+export * from './certified-user-services.service';
+import { CertifiedUserServicesService } from './certified-user-services.service';
+export * from './challenge-services.service';
+import { ChallengeServicesService } from './challenge-services.service';
+export * from './doi-services.service';
+import { DOIServicesService } from './doi-services.service';
+export * from './data-access-services.service';
+import { DataAccessServicesService } from './data-access-services.service';
+export * from './discussion-services.service';
+import { DiscussionServicesService } from './discussion-services.service';
+export * from './docker-authorization-services.service';
+import { DockerAuthorizationServicesService } from './docker-authorization-services.service';
+export * from './docker-commit-services.service';
+import { DockerCommitServicesService } from './docker-commit-services.service';
+export * from './docker-registry-event-services.service';
+import { DockerRegistryEventServicesService } from './docker-registry-event-services.service';
+export * from './download-list-services.service';
+import { DownloadListServicesService } from './download-list-services.service';
+export * from './drs-services.service';
+import { DrsServicesService } from './drs-services.service';
+export * from './entity-bundle-services-v2.service';
+import { EntityBundleServicesV2Service } from './entity-bundle-services-v2.service';
+export * from './entity-services.service';
+import { EntityServicesService } from './entity-services.service';
+export * from './evaluation-services.service';
+import { EvaluationServicesService } from './evaluation-services.service';
+export * from './file-services.service';
+import { FileServicesService } from './file-services.service';
+export * from './form-services.service';
+import { FormServicesService } from './form-services.service';
+export * from './grid-services.service';
+import { GridServicesService } from './grid-services.service';
+export * from './json-schema-services.service';
+import { JSONSchemaServicesService } from './json-schema-services.service';
+export * from './log-service.service';
+import { LogServiceService } from './log-service.service';
+export * from './membership-invitation-services.service';
+import { MembershipInvitationServicesService } from './membership-invitation-services.service';
+export * from './membership-request-services.service';
+import { MembershipRequestServicesService } from './membership-request-services.service';
+export * from './message-services.service';
+import { MessageServicesService } from './message-services.service';
+export * from './open-id-connect-services.service';
+import { OpenIDConnectServicesService } from './open-id-connect-services.service';
+export * from './portals-services.service';
+import { PortalsServicesService } from './portals-services.service';
+export * from './principal-services.service';
+import { PrincipalServicesService } from './principal-services.service';
+export * from './project-settings-services.service';
+import { ProjectSettingsServicesService } from './project-settings-services.service';
+export * from './project-storage-services.service';
+import { ProjectStorageServicesService } from './project-storage-services.service';
+export * from './recycle-bin-services.service';
+import { RecycleBinServicesService } from './recycle-bin-services.service';
+export * from './search-services.service';
+import { SearchServicesService } from './search-services.service';
+export * from './statistics-services.service';
+import { StatisticsServicesService } from './statistics-services.service';
+export * from './storage-report-services.service';
+import { StorageReportServicesService } from './storage-report-services.service';
+export * from './subscription-services.service';
+import { SubscriptionServicesService } from './subscription-services.service';
+export * from './table-services.service';
+import { TableServicesService } from './table-services.service';
+export * from './team-services.service';
+import { TeamServicesService } from './team-services.service';
+export * from './user-group-services.service';
+import { UserGroupServicesService } from './user-group-services.service';
+export * from './user-profile-services.service';
+import { UserProfileServicesService } from './user-profile-services.service';
+export * from './verification-services.service';
+import { VerificationServicesService } from './verification-services.service';
+export * from './webhook-services.service';
+import { WebhookServicesService } from './webhook-services.service';
+export * from './wiki-page-services.service';
+import { WikiPageServicesService } from './wiki-page-services.service';
+export * from './wiki-page-services2.service';
+import { WikiPageServices2Service } from './wiki-page-services2.service';
 export const APIS = [
   AccessApprovalServicesService,
   AccessRequirementServicesService,
@@ -107,14 +113,17 @@ export const APIS = [
   EvaluationServicesService,
   FileServicesService,
   FormServicesService,
+  GridServicesService,
   JSONSchemaServicesService,
   LogServiceService,
   MembershipInvitationServicesService,
   MembershipRequestServicesService,
   MessageServicesService,
   OpenIDConnectServicesService,
+  PortalsServicesService,
   PrincipalServicesService,
   ProjectSettingsServicesService,
+  ProjectStorageServicesService,
   RecycleBinServicesService,
   SearchServicesService,
   StatisticsServicesService,
