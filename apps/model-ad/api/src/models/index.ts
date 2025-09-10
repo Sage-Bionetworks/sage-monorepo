d42ed31a5aa6b0f2afe29ec3973ab20a76b7c1f8
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2025-09-10 22:40:47 +0000

    MG-237: use kebab case for api file names

--- apps/model-ad/api/src/models/index.ts
+++ apps/model-ad/api/src/models/index.ts
@@ -1,5 +1,5 @@
 export * from './comparison-tool-config';
 export * from './data-version';
-export * from './diseaseCorrelationComparisonTool';
+export * from './disease-correlation-comparison-tool';
 export * from './model';
 export * from './model-overview-comparison-tool';
