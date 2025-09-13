746fe3fcbdfd28771c447ffc2c56c2d383265873
tschaffter <thomas.schaffter@gmail.com>
2025-09-13 18:46:03 +0000

    chore: cleanup

--- apps/openchallenges/auth-service/src/main/java/org/sagebionetworks/openchallenges/auth/service/AuthServiceApplication.java
+++ apps/openchallenges/auth-service/src/main/java/org/sagebionetworks/openchallenges/auth/service/AuthServiceApplication.java
@@ -4,18 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
 
-@SpringBootApplication(nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class)
+@SpringBootApplication
 @EnableConfigurationProperties
-@ComponentScan(
-  basePackages = {
-    "org.sagebionetworks.openchallenges.auth.service",
-    "org.sagebionetworks.openchallenges.auth.service.api",
-    "org.sagebionetworks.openchallenges.auth.service.configuration",
-  },
-  nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class
-)
+@ComponentScan(basePackages = { "org.sagebionetworks.openchallenges" })
 public class AuthServiceApplication {
 
   public static void main(String[] args) {
