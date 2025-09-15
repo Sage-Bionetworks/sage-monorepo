fc00b9dec62b0054f9a53dd38d1ff679e9866f12
tschaffter <thomas.schaffter@gmail.com>
2025-09-15 22:10:09 +0000

    chore: create Redocly plugin to update security in public openapi files

--- libs/openchallenges/api-description/plugins/plugin.mjs
+++ libs/openchallenges/api-description/plugins/plugin.mjs
@@ -1,10 +1,12 @@
 // Source: https://redocly.com/docs/cli/guides/replace-servers-url
 import ReplaceServersURL from './decorators/replace-servers-url.mjs';
+import InjectApiKeySecurity from './decorators/inject-api-key-security.mjs';
 
 /** @type {import('@redocly/cli').DecoratorsConfig} */
 const decorators = {
   oas3: {
-    'replace-servers-url': ReplaceServersURL,
+  'replace-servers-url': ReplaceServersURL,
+  'inject-api-key-security': InjectApiKeySecurity,
   },
 };
 
