1ea37b72acc06db605abe528b9c4722db33fb810
tschaffter <thomas.schaffter@gmail.com>
2025-09-16 18:09:09 +0000

    chore: remove deprecated shiny-base Docker project

--- apps/sage/shiny-base/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-# Pass environment variable from ECS to Shiny
-echo "" >> .Renviron
-echo $SECRETS_MANAGER_SECRETS | jq -r 'to_entries[] | [.key,.value] | join("=")' >> .Renviron
-
-# Now, start up Shiny
-/usr/bin/shiny-server
