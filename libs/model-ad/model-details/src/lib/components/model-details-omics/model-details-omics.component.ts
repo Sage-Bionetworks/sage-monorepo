c46a071beb3f4376500c8f40661a9d619532c0ab
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2025-09-16 19:09:32 +0000

    MG-278: conditionally display omics cards

--- libs/model-ad/model-details/src/lib/components/model-details-omics/model-details-omics.component.ts
+++ libs/model-ad/model-details/src/lib/components/model-details-omics/model-details-omics.component.ts
@@ -1,5 +1,6 @@
 import { Component, computed, input } from '@angular/core';
 import { ResourceCardsComponent } from '@sagebionetworks/explorers/ui';
+import { Model } from '@sagebionetworks/model-ad/api-client-angular';
 
 @Component({
   selector: 'model-ad-model-details-omics',
@@ -8,20 +9,26 @@ import { ResourceCardsComponent } from '@sagebionetworks/explorers/ui';
   styleUrls: ['./model-details-omics.component.scss'],
 })
 export class ModelDetailsOmicsComponent {
-  modelName = input.required<string>();
+  model = input.required<Model>();
 
-  cards = computed(() => [
-    {
-      imagePath: '/model-ad-assets/images/gene-expression.svg',
-      description: 'View Gene Expression results for this model in the comparison tool.',
-      title: 'Gene Expression',
-      link: `/comparison/expression?model=${this.modelName()}`,
-    },
-    {
-      imagePath: '/model-ad-assets/images/disease-correlation.svg',
-      description: 'View Disease Correlation results for this model in the comparison tool.',
-      title: 'Disease Correlation',
-      link: `/comparison/correlation?model=${this.modelName()}`,
-    },
-  ]);
+  cards = computed(() => {
+    const cards = [];
+    if (this.model().gene_expression) {
+      cards.push({
+        imagePath: '/model-ad-assets/images/gene-expression.svg',
+        description: 'View Gene Expression results for this model in the comparison tool.',
+        title: 'Gene Expression',
+        link: `/${this.model().gene_expression}`,
+      });
+    }
+    if (this.model().disease_correlation) {
+      cards.push({
+        imagePath: '/model-ad-assets/images/disease-correlation.svg',
+        description: 'View Disease Correlation results for this model in the comparison tool.',
+        title: 'Disease Correlation',
+        link: `/${this.model().disease_correlation}`,
+      });
+    }
+    return cards;
+  });
 }
