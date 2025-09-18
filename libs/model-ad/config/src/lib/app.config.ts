657e14381bebae40ac0e38c4a1ead662b57a02cf
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2025-09-18 22:52:06 +0000

    chore(model-ad): remove unused environment variables (MG-373)

--- libs/model-ad/config/src/lib/app.config.ts
+++ libs/model-ad/config/src/lib/app.config.ts
@@ -1,20 +1,8 @@
 import { InjectionToken } from '@angular/core';
 
-/* eslint-disable no-unused-vars */
-export enum Environment {
-  Production = 'prod',
-  Staging = 'staging',
-  Test = 'test',
-  Development = 'dev',
-  Local = 'local',
-}
-/* eslint-enable no-unused-vars */
-
 export interface AppConfig {
   appVersion: string;
   csrApiUrl: string;
-  dataUpdatedOn: string;
-  environment: Environment;
   googleTagManagerId: string;
   isPlatformServer: boolean;
   privacyPolicyUrl: string;
