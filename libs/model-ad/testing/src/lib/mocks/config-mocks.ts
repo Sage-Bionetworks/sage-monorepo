533f65b9f6e523b0a5d11f94587292f6e03f5222
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2025-09-18 22:52:06 +0000

    chore(model-ad): remove unused environment variables (MG-373)

--- libs/model-ad/testing/src/lib/mocks/config-mocks.ts
+++ libs/model-ad/testing/src/lib/mocks/config-mocks.ts
@@ -1,11 +1,9 @@
-import { AppConfig, Environment } from '@sagebionetworks/model-ad/config';
+import { AppConfig } from '@sagebionetworks/model-ad/config';
 
 export const configMock: AppConfig = {
   apiDocsUrl: 'http://localhost:8000/api-docs',
   appVersion: '1.0.0-beta',
   csrApiUrl: 'http://localhost:4200/v1',
-  dataUpdatedOn: 'yyyy-mm-dd',
-  environment: Environment.Development,
   googleTagManagerId: '',
   ssrApiUrl: 'http://model-ad-api:3333/v1',
   isPlatformServer: false,
