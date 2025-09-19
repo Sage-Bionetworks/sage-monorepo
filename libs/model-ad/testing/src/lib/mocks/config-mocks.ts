142d1d7c8ccf102fd9efd82b5e39a65e0d80f1a4
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2025-09-19 20:42:59 +0000

    MG-413: update config mock

--- libs/model-ad/testing/src/lib/mocks/config-mocks.ts
+++ libs/model-ad/testing/src/lib/mocks/config-mocks.ts
@@ -2,11 +2,12 @@ import { AppConfig } from '@sagebionetworks/model-ad/config';
 
 export const configMock: AppConfig = {
   apiDocsUrl: 'http://localhost:8000/api-docs',
-  appVersion: '1.0.0-beta',
+  appVersion: 'edge',
   csrApiUrl: 'http://localhost:4200/v1',
   googleTagManagerId: '',
   ssrApiUrl: 'http://model-ad-api:3333/v1',
   isPlatformServer: false,
   privacyPolicyUrl: 'http://localhost:4200/privacy-policy',
   termsOfUseUrl: 'http://localhost:4200/terms-of-use',
+  tagName: 'edge',
 };
