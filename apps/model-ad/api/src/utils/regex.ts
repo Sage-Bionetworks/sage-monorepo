0981ede5daeb86ed3e8b100bc91166f1946d6b7a
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2025-09-25 20:44:37 +0000

    AG-1876: move escapeRegexChars to shared lib so it can be reused by agora

--- apps/model-ad/api/src/utils/regex.ts
+++
@@ -1,4 +0,0 @@
-export function escapeRegexChars(input: string): string {
-  // Escape all regex special characters to prevent injection
-  return input.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
-}
