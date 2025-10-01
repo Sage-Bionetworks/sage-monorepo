c6c24a55ff79ebf27c5dc8fa8c2c48a83d1b8aa6
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2025-10-01 20:18:35 +0000

    AG-1880: remove old gene search component

--- libs/agora/genes/src/index.ts
+++ libs/agora/genes/src/index.ts
@@ -10,13 +10,12 @@ export * from './lib/components/gene-network/gene-network.component';
 export * from './lib/components/gene-nominations/gene-nominations.component';
 export * from './lib/components/gene-protein-selector/gene-protein-selector.component';
 export * from './lib/components/gene-resources/gene-resources.component';
-export * from './lib/components/gene-search/gene-search.component';
 export * from './lib/components/gene-similar/gene-similar.routes';
 export * from './lib/components/gene-soe-charts/gene-soe-charts.component';
 export * from './lib/components/gene-soe-list/gene-soe-list.component';
 export * from './lib/components/gene-soe/gene-soe.component';
-export * from './lib/components/gene-table/gene-table.component';
 export * from './lib/components/gene-table-column-selector/gene-table-column-selector.component';
+export * from './lib/components/gene-table/gene-table.component';
 
 export * from './lib/components/popover-link/popover-link.component';
 
