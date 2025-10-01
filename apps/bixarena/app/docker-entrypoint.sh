09fa9865494ce172bba3dfb3c369905c61d5b988
tschaffter <thomas.schaffter@gmail.com>
2025-10-01 03:04:59 +0000

    chore: app starts but cannot found page modules

--- apps/bixarena/app/docker-entrypoint.sh
+++ apps/bixarena/app/docker-entrypoint.sh
@@ -4,7 +4,9 @@ set -e
 if [ "$1" = 'python' ]; then
     cd ${APP_DIR}
     gosu "$APP_USERNAME" envsubst < model_config.template.json > model_config.json
-    exec gosu "$APP_USERNAME" python -m main \
+    : "${APP_PORT:=8100}"
+    # Use installed console script (provided by pyproject [project.scripts])
+    exec gosu "$APP_USERNAME" bixarena-app \
         --host 0.0.0.0 \
         --port "${APP_PORT}" \
         --register-api-endpoint-file model_config.json
