e435aa5f4b3238b5ca1ec64c6cf0dc4ee87e035f
Thomas Schaffter <thomas.schaffter@gmail.com>
2025-10-02 10:03:26 -0700

    feat(bixarena): explore the creation of a FastAPI service with OpenAPI Generator (SMR-503) (#3544)

---
+++ apps/bixarena/ai-service/tests/test_health_check_api.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+from fastapi.testclient import TestClient
+
+
+from bixarena_ai_service.models.basic_error import BasicError  # noqa: F401
+from bixarena_ai_service.models.health_check import HealthCheck  # noqa: F401
+
+
+def test_get_health_check(client: TestClient):
+    """Test case for get_health_check
+
+    Get health check information
+    """
+
+    headers = {}
+    # uncomment below to make a request
+    # response = client.request(
+    #    "GET",
+    #    "/health-check",
+    #    headers=headers,
+    # )
+
+    # uncomment below to assert the status code of the HTTP response
+    # assert response.status_code == 200
