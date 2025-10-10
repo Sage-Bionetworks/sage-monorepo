73675295f17541db2721ce2a724cdc51016b7554
sagely1 <114952739+sagely1@users.noreply.github.com>
2025-10-10 17:10:57 +0000

    add filter panel component

---
+++ libs/explorers/comparison-tools/src/lib/filter-panel/filter-panel.component.ts
@@ -0,0 +1,41 @@
+import { Component, inject, output } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { CheckboxModule } from 'primeng/checkbox';
+import { FormsModule } from '@angular/forms';
+import { TooltipModule } from 'primeng/tooltip';
+import { SvgIconComponent } from '@sagebionetworks/explorers/util';
+import { ComparisonToolFilterOption } from '@sagebionetworks/explorers/models';
+import { FilterPanelService } from './filter-panel.service';
+
+@Component({
+  selector: 'explorers-filter-panel',
+  imports: [CommonModule, FormsModule, TooltipModule, CheckboxModule, SvgIconComponent],
+  templateUrl: './filter-panel.component.html',
+  styleUrls: ['./filter-panel.component.scss'],
+})
+export class FilterPanelComponent {
+  changeEvent = output();
+
+  filterPanelService = inject(FilterPanelService);
+
+  handleChange(option: ComparisonToolFilterOption) {
+    console.log('handling change', option);
+    console.log(this.filterPanelService.filters());
+    this.filterPanelService.handleFilterChange(option);
+    console.log(this.filterPanelService.getSelectedFilters());
+    this.changeEvent.emit();
+  }
+
+  openPane(index: number) {
+    this.filterPanelService.setActivePane(index);
+  }
+
+  close() {
+    this.filterPanelService.close();
+  }
+
+  toggle() {
+    this.filterPanelService.toggle();
+  }
+}
---
+++ libs/explorers/comparison-tools/src/lib/filter-panel/filter-panel.service.ts
@@ -0,0 +1,86 @@
+import { inject, Injectable, signal } from '@angular/core';
+import {
+  ComparisonToolFilter,
+  ComparisonToolFilterOption,
+} from '@sagebionetworks/explorers/models';
+import { FilterService } from 'primeng/api';
+// import { intersectFilterCallback, urlLinkCallback } from './filter-helpers';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FilterPanelService {
+  private filterService = inject(FilterService);
+
+  private isOpenSignal = signal(false);
+  private filtersSignal = signal<ComparisonToolFilter[]>([]);
+  private activePaneSignal = signal(-1);
+
+  readonly isOpen = this.isOpenSignal.asReadonly();
+  readonly filters = this.filtersSignal.asReadonly();
+  readonly activePane = this.activePaneSignal.asReadonly();
+
+  setFilters(filters: ComparisonToolFilter[]) {
+    // this.filtersSignal.set([...filters]);
+    this.filtersSignal.set(filters);
+  }
+
+  toggle() {
+    this.isOpenSignal.update((current) => !current);
+
+    // Reset active pane when closing
+    if (!this.isOpen()) {
+      this.activePaneSignal.set(-1);
+    }
+  }
+
+  close() {
+    this.isOpenSignal.set(false);
+    this.activePaneSignal.set(-1);
+  }
+
+  open() {
+    this.isOpenSignal.set(true);
+  }
+
+  setActivePane(index: number) {
+    this.activePaneSignal.set(index);
+  }
+
+  handleFilterChange(option: ComparisonToolFilterOption) {
+    // update filters to reflect changed option
+    const currentFilters = this.filtersSignal();
+    const updatedFilters = currentFilters.map((filter) => ({
+      ...filter,
+      options: filter.options.map((o) =>
+        // o.value === option.value ? { ...o, selected: !o.selected } : o
+
+        // set selected based on label match
+        o.label === option.label ? { ...o, selected: option.selected } : o,
+      ),
+    }));
+    console.log('updating filters');
+    this.filtersSignal.set(updatedFilters);
+    console.log('updated filters', updatedFilters);
+  }
+
+  getSelectedFilters() {
+    const values: { [key: string]: string | number | string[] | number[] } = {};
+
+    for (const filter of this.filters()) {
+      const selectedOptions = filter.options
+        .filter((option) => option.selected)
+        .map((option) => option.label);
+      console.log('getselectedfilters:selectedOptions', selectedOptions);
+      if (selectedOptions.length) {
+        values[filter.name] = selectedOptions;
+      }
+    }
+
+    return values;
+  }
+
+  updateSearchFilters(filters: { [key: string]: any }) {
+    throw new Error('Method not implemented.');
+  }
+}
