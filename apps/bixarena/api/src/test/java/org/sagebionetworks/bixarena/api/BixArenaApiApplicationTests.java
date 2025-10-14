8089a0ab1b994e035b974a71d23f5499bd348cc2
tschaffter <thomas.schaffter@gmail.com>
2025-10-14 22:11:21 +0000

    chore: fix test error

--- apps/bixarena/api/src/test/java/org/sagebionetworks/bixarena/api/BixArenaApiApplicationTests.java
+++ apps/bixarena/api/src/test/java/org/sagebionetworks/bixarena/api/BixArenaApiApplicationTests.java
@@ -5,8 +5,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BixArenaApiApplicationTests {
 
   @Autowired
