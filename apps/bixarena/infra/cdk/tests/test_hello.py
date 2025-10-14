7d4dfc67666c3d470ae00bb75d3a4005d5f36989
tschaffter <thomas.schaffter@gmail.com>
2025-10-14 15:41:30 +0000

    chore: remove hello-world

--- apps/bixarena/infra/cdk/tests/test_hello.py
+++
@@ -1,8 +0,0 @@
-"""Hello unit test module."""
-
-from bixarena_infra_cdk.hello import hello
-
-
-def test_hello():
-    """Test the hello function."""
-    assert hello() == "Hello bixarena-infra-cdk"
