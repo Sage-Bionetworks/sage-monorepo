c048a339617f4902d21a9d878d56b86bec7e678f
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2025-10-15 16:50:41 +0000

    feat(explorers): incorporate selectors into CT controls (MG-425)

--- libs/explorers/comparison-tools/src/lib/comparison-tool-controls/comparison-tool-controls.component.ts
+++ libs/explorers/comparison-tools/src/lib/comparison-tool-controls/comparison-tool-controls.component.ts
@@ -1,12 +1,22 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { DisplayedResultsComponent } from '../displayed-results/displayed-results.component';
+import { Component, input, model, ViewEncapsulation } from '@angular/core';
+import { ComparisonToolConfig, SynapseWikiParams } from '@sagebionetworks/explorers/models';
 import { ComparisonToolSearchInputComponent } from '../comparison-tool-search-input/comparison-tool-search-input.component';
+import { DisplayedResultsComponent } from '../displayed-results/displayed-results.component';
+import { ComparisonToolSelectorsComponent } from './comparison-tool-selectors/comparison-tool-selectors.component';
 
 @Component({
   selector: 'explorers-comparison-tool-controls',
-  imports: [DisplayedResultsComponent, ComparisonToolSearchInputComponent],
+  imports: [
+    DisplayedResultsComponent,
+    ComparisonToolSelectorsComponent,
+    ComparisonToolSearchInputComponent,
+  ],
   templateUrl: './comparison-tool-controls.component.html',
   styleUrls: ['./comparison-tool-controls.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class ComparisonToolControlsComponent {}
+export class ComparisonToolControlsComponent {
+  pageConfigs = input<ComparisonToolConfig[]>([]);
+  popoverWikis = input<{ [key: string]: SynapseWikiParams }>({});
+  selection = model<string[]>([]);
+}
