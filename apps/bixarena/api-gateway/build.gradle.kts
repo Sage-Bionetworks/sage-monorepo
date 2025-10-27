011ebd22b2335813fe8aa46731baac80f58cf896
tschaffter <thomas.schaffter@gmail.com>
2025-10-27 16:39:22 +0000

    chore: use caffeine from the Gradle version catalog

--- apps/bixarena/api-gateway/build.gradle.kts
+++ apps/bixarena/api-gateway/build.gradle.kts
@@ -5,17 +5,14 @@ plugins {
 }
 
 dependencies {
+  implementation(libs.caffeine)
   implementation(libs.jackson.databind)
   implementation(libs.jackson.dataformat.yaml)
   implementation(libs.spring.boot.starter.actuator)
-  // implementation(libs.spring.boot.starter.oauth2.resource.server)
   implementation(libs.spring.boot.starter.security)
   implementation(libs.spring.boot.starter.webflux)
   implementation(libs.spring.cloud.starter.gateway.server.webflux)
   implementation(libs.spring.security.oauth2.jose)
-
-  // Caffeine cache for session and JWT caching
-  implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
 
   runtimeOnly(libs.spring.boot.devtools)
   testImplementation(libs.spring.boot.starter.test)
