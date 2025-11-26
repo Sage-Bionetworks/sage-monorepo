2e1d87e88851e77605c64d48cc2cd55c8ca6ba0a
mdsage1 <maria.diaz@sagebase.org>
2025-11-26 18:47:31 -0500

    update table schema for organizations to include new columns

--- apps/openchallenges/challenge-service/src/main/resources/db/migration/V1.0.0__create_tables.sql
+++ apps/openchallenges/challenge-service/src/main/resources/db/migration/V1.0.0__create_tables.sql
@@ -33,6 +33,9 @@ CREATE TABLE challenge (
   operation_id BIGINT,
   created_at TIMESTAMP WITH TIME ZONE NOT NULL,
   updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  total_competitors BIGINT,
+  total_teams BIGINT,
+  total_submissions BIGINT,
   CONSTRAINT fk_platform FOREIGN KEY (platform_id) REFERENCES challenge_platform(id),
   CONSTRAINT fk_operation FOREIGN KEY (operation_id) REFERENCES edam_concept(id),
   CONSTRAINT slug_check CHECK (
