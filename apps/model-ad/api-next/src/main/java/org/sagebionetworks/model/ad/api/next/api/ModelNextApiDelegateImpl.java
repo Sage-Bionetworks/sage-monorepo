ee59bcd334a5089b05acfff771fc022eb721ac9c
tschaffter <thomas.schaffter@gmail.com>
2025-12-10 19:21:13 +0000

    chore: implement the new endpoint

---
+++ apps/model-ad/api-next/src/main/java/org/sagebionetworks/model/ad/api/next/api/ModelNextApiDelegateImpl.java
@@ -0,0 +1,30 @@
+package org.sagebionetworks.model.ad.api.next.api;
+
+import lombok.RequiredArgsConstructor;
+import org.sagebionetworks.model.ad.api.next.model.dto.ModelDto;
+import org.sagebionetworks.model.ad.api.next.service.ModelService;
+import org.sagebionetworks.model.ad.api.next.util.ApiHelper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ModelNextApiDelegateImpl implements ModelNextApiDelegate {
+
+  private final ModelService modelService;
+
+  @Override
+  public ResponseEntity<ModelDto> getModelByName(String name) {
+    ModelDto model = modelService.getModelByName(name);
+
+    if (model == null) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    return ResponseEntity.ok()
+      .headers(ApiHelper.createNoCacheHeaders(MediaType.APPLICATION_JSON))
+      .body(model);
+  }
+}
