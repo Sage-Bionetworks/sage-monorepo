2eb228f834f579170816f5a3394d9a0477264123
tschaffter <thomas.schaffter@gmail.com>
2025-12-17 16:40:26 +0000

    chore: add log message when fetching model by name

--- apps/model-ad/api-next/src/main/java/org/sagebionetworks/model/ad/api/next/api/ModelApiDelegateImpl.java
+++ apps/model-ad/api-next/src/main/java/org/sagebionetworks/model/ad/api/next/api/ModelApiDelegateImpl.java
@@ -1,6 +1,7 @@
 package org.sagebionetworks.model.ad.api.next.api;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.sagebionetworks.model.ad.api.next.model.dto.ModelDto;
 import org.sagebionetworks.model.ad.api.next.service.ModelService;
 import org.sagebionetworks.model.ad.api.next.util.ApiHelper;
@@ -11,18 +12,22 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class ModelApiDelegateImpl implements ModelApiDelegate {
 
   private final ModelService modelService;
 
   @Override
   public ResponseEntity<ModelDto> getModelByName(String name) {
+    log.debug("Fetching model by name: {}", name);
     ModelDto model = modelService.getModelByName(name);
 
     if (model == null) {
+      log.debug("Model not found: {}", name);
       return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
+    log.debug("Successfully retrieved model: {}", name);
     return ResponseEntity.ok()
       .headers(ApiHelper.createNoCacheHeaders(MediaType.APPLICATION_JSON))
       .body(model);
