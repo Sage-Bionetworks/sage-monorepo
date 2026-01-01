4b1535eb9bde9ee74803b4c83483a7424e2478a7
rrchai <73901500+rrchai@users.noreply.github.com>
2026-01-01 22:38:03 +0000

    Revert "feat(model-ad): add request/response logging to data-version endpoint"
    This reverts commit 466d54f7d72a2579da4ef71a1287bd170faca605.

--- apps/model-ad/api-next/src/main/java/org/sagebionetworks/model/ad/api/next/api/DataVersionApiDelegateImpl.java
+++ apps/model-ad/api-next/src/main/java/org/sagebionetworks/model/ad/api/next/api/DataVersionApiDelegateImpl.java
@@ -1,34 +1,22 @@
 package org.sagebionetworks.model.ad.api.next.api;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.sagebionetworks.model.ad.api.next.model.dto.DataVersionDto;
 import org.sagebionetworks.model.ad.api.next.service.DataVersionService;
 import org.sagebionetworks.model.ad.api.next.util.ApiHelper;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Component
 @RequiredArgsConstructor
-@Slf4j
 public class DataVersionApiDelegateImpl implements DataVersionApiDelegate {
 
   private final DataVersionService dataVersionService;
 
   @Override
   public ResponseEntity<DataVersionDto> getDataVersion() {
-    ServletRequestAttributes attrs = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
     DataVersionDto dataVersion = dataVersionService.loadDataVersion();
-
-    if (attrs != null) {
-      HttpServletRequest req = attrs.getRequest();
-      log.info("{} {} status={}", req.getMethod(), req.getRequestURI(), HttpStatus.OK.value());
-    }
 
     return ResponseEntity.ok()
       .headers(ApiHelper.createNoCacheHeaders(MediaType.APPLICATION_JSON))
