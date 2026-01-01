92f5fb88610fe4ed9d3bc26c411f8698acf5f205
rrchai <73901500+rrchai@users.noreply.github.com>
2026-01-01 22:55:05 +0000

    feat(model-ad): add request/response logging to data-version endpoint

--- apps/model-ad/api-next/src/main/java/org/sagebionetworks/model/ad/api/next/api/DataVersionApiDelegateImpl.java
+++ apps/model-ad/api-next/src/main/java/org/sagebionetworks/model/ad/api/next/api/DataVersionApiDelegateImpl.java
@@ -1,22 +1,29 @@
 package org.sagebionetworks.model.ad.api.next.api;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.sagebionetworks.model.ad.api.next.model.dto.DataVersionDto;
 import org.sagebionetworks.model.ad.api.next.service.DataVersionService;
 import org.sagebionetworks.model.ad.api.next.util.ApiHelper;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class DataVersionApiDelegateImpl implements DataVersionApiDelegate {
 
   private final DataVersionService dataVersionService;
 
   @Override
   public ResponseEntity<DataVersionDto> getDataVersion() {
+    log.debug("Fetching data version");
+
     DataVersionDto dataVersion = dataVersionService.loadDataVersion();
+
+    ApiHelper.logHttpResponse(HttpStatus.OK.value());
 
     return ResponseEntity.ok()
       .headers(ApiHelper.createNoCacheHeaders(MediaType.APPLICATION_JSON))
