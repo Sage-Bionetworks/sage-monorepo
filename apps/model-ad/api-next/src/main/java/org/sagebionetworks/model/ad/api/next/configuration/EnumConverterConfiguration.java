0625f334cc76e3cc3a6d031027ab639f1ff2bb9f
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2026-01-06 17:03:18 +0000

    refactor(model-ad): move comparison-tool-config route to api-next and generate server stubs (MG-621)

--- apps/model-ad/api-next/src/main/java/org/sagebionetworks/model/ad/api/next/configuration/EnumConverterConfiguration.java
+++ apps/model-ad/api-next/src/main/java/org/sagebionetworks/model/ad/api/next/configuration/EnumConverterConfiguration.java
@@ -1,5 +1,6 @@
 package org.sagebionetworks.model.ad.api.next.configuration;
 
+import org.sagebionetworks.model.ad.api.next.model.dto.ComparisonToolPageDto;
 import org.sagebionetworks.model.ad.api.next.model.dto.ItemFilterTypeQueryDto;
 import org.sagebionetworks.model.ad.api.next.model.dto.ModelIdentifierTypeDto;
 import org.sagebionetworks.model.ad.api.next.model.dto.SexCohortDto;
@@ -12,6 +13,15 @@ import org.springframework.core.convert.converter.Converter;
 @Configuration(value = "org.sagebionetworks.model.ad.api.next.configuration.enumConverterConfiguration")
 public class EnumConverterConfiguration {
 
+    @Bean(name = "org.sagebionetworks.model.ad.api.next.configuration.EnumConverterConfiguration.comparisonToolPageConverter")
+    Converter<String, ComparisonToolPageDto> comparisonToolPageConverter() {
+        return new Converter<String, ComparisonToolPageDto>() {
+            @Override
+            public ComparisonToolPageDto convert(String source) {
+                return ComparisonToolPageDto.fromValue(source);
+            }
+        };
+    }
     @Bean(name = "org.sagebionetworks.model.ad.api.next.configuration.EnumConverterConfiguration.itemFilterTypeQueryConverter")
     Converter<String, ItemFilterTypeQueryDto> itemFilterTypeQueryConverter() {
         return new Converter<String, ItemFilterTypeQueryDto>() {
