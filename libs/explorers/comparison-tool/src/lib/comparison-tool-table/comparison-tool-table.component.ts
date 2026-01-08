adb97c3c57e28528c97c65183514a9d4066b2096
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2026-01-08 18:34:30 +0000

    fix: move panel outside of relatively positioned parent, so popover is positioned correctly

--- libs/explorers/comparison-tool/src/lib/comparison-tool-table/comparison-tool-table.component.ts
+++ libs/explorers/comparison-tool/src/lib/comparison-tool-table/comparison-tool-table.component.ts
@@ -20,7 +20,6 @@ import { SvgIconComponent } from '@sagebionetworks/explorers/util';
 import { TooltipModule } from 'primeng/tooltip';
 import { BaseTableComponent } from './base-table/base-table.component';
 import { ComparisonToolColumnsComponent } from './comparison-tool-columns/comparison-tool-columns.component';
-import { HeatmapDetailsPanelComponent } from './heatmap-details-panel/heatmap-details-panel.component';
 
 @Component({
   selector: 'explorers-comparison-tool-table',
@@ -30,7 +29,6 @@ import { HeatmapDetailsPanelComponent } from './heatmap-details-panel/heatmap-de
     SvgIconComponent,
     BaseTableComponent,
     DownloadDomImageComponent,
-    HeatmapDetailsPanelComponent,
   ],
   templateUrl: './comparison-tool-table.component.html',
   styleUrls: ['./comparison-tool-table.component.scss'],
@@ -44,7 +42,6 @@ export class ComparisonToolTableComponent implements AfterViewInit {
   platformService = inject(PlatformService);
 
   tableElement = viewChild<ElementRef>('table');
-  heatmapDetailsPanel = viewChild(HeatmapDetailsPanelComponent);
 
   pinnedResultsCount = this.comparisonToolService.pinnedResultsCount;
   maxPinnedItems = this.comparisonToolService.maxPinnedItems;
@@ -65,15 +62,6 @@ export class ComparisonToolTableComponent implements AfterViewInit {
   primaryColumnWidth = 300;
 
   constructor() {
-    effect(() => {
-      const panelData = this.comparisonToolService.heatmapDetailsPanelData();
-      if (panelData) {
-        this.heatmapDetailsPanel()?.show(panelData.event, panelData.data);
-      } else {
-        this.heatmapDetailsPanel()?.hide();
-      }
-    });
-
     if (this.platformService.isBrowser) {
       this.primaryColumnWidth = this.helperService.getNumberFromCSSValue(
         getComputedStyle(document.documentElement).getPropertyValue(
