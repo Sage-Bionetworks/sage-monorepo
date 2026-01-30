1cf8053da3a783c4be822f665e27e484915f360f
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2026-01-30 18:35:58 +0000

    feat(storybook): initialize shared storybook project

---
+++ libs/storybook/.storybook/main.ts
@@ -0,0 +1,16 @@
+import type { StorybookConfig } from '@storybook/angular';
+
+const config: StorybookConfig = {
+  stories: ['../**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
+  addons: [],
+  framework: {
+    name: '@storybook/angular',
+    options: {},
+  },
+};
+
+export default config;
+
+// To customize your webpack configuration you can use the webpackFinal field.
+// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
+// and https://nx.dev/recipes/storybook/custom-builder-configs
---
+++ libs/storybook/src/index.ts
@@ -0,0 +1,2 @@
+// This library serves as a host for aggregating Storybook stories from all projects.
+// It does not export any components itself.
