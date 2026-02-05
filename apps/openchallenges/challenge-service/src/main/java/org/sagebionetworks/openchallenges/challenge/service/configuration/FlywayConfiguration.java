8485d8d4a041839c9b2c62787b96ab771e5f7540
tschaffter <thomas.schaffter@gmail.com>
2026-02-05 19:53:04 +0000

    chore: align database config in services on BioArena approach

--- apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/configuration/FlywayConfiguration.java
+++ apps/openchallenges/challenge-service/src/main/java/org/sagebionetworks/openchallenges/challenge/service/configuration/FlywayConfiguration.java
@@ -3,12 +3,13 @@ package org.sagebionetworks.openchallenges.challenge.service.configuration;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class FlywayConfiguration {
 
-  // TODO: Only clean the DB in development mode, never in production.
-  // @Profile("dev")
+  // Only clean the DB in development mode, never in production.
+  @Profile("dev")
   @Bean
   public FlywayMigrationStrategy cleanMigrationStrategy() {
     return flyway -> {
