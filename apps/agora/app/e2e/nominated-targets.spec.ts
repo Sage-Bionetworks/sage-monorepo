6e69eb177d07fe282b9296df6163752c6e8f44d3
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2026-02-19 17:52:32 +0000

    fix(agora): update nominated-targets e2e tests (AG-1968)

--- apps/agora/app/e2e/nominated-targets.spec.ts
+++ apps/agora/app/e2e/nominated-targets.spec.ts
@@ -5,11 +5,11 @@ test.describe('specific viewport block', () => {
   test.use({ viewport: { width: 1600, height: 1200 } });
 
   test('has title', async ({ page }) => {
-    await page.goto('/genes/nominated-targets');
+    await page.goto('/comparison/nominated-targets');
 
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(
-      'Nominated Targets | Candidate genes for AD treatment or prevention',
+      "Nominated Targets | Candidate genes for Alzheimer's Disease treatment or prevention",
     );
   });
 });
@@ -17,6 +17,6 @@ test.describe('specific viewport block', () => {
 test.describe('legacy url redirects', () => {
   test('redirects to nominated targets', async ({ page }) => {
     await page.goto('/genes/(genes-router:genes-list)');
-    await expect(page).toHaveURL(`${baseURL}/genes/nominated-targets`);
+    await expect(page).toHaveURL(`${baseURL}/comparison/nominated-targets`);
   });
 });
