1701ab1f81c2a183c2516a3fe29b800d7f0b7440
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2026-02-24 23:16:54 +0000

    refactor(agora): replace agora's SvgIconService with shared explorers service, remove duplicated icons in agora-assets in favor of assets in explorers-assets (AG-1999)

--- libs/agora/services/src/index.ts
+++ libs/agora/services/src/index.ts
@@ -1,3 +1,2 @@
 export * from './lib/error.service';
 export * from './lib/helper.service';
-export * from './lib/svg-icon.service';
--- libs/agora/testing/src/lib/mocks/index.ts
+++ libs/agora/testing/src/lib/mocks/index.ts
@@ -14,4 +14,3 @@ export * from './team-service-mock';
 export * from './activated-route-stub';
 export * from './api-service-stub';
 export * from './genes-service-stub';
-export * from './svg-icon-service-stub';
