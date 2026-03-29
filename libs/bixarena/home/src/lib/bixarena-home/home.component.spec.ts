805b53b96b936723b516171abbbd53be081fe277
rrchai <73901500+rrchai@users.noreply.github.com>
2026-03-29 04:13:48 +0000

    feat(bixarena): use async pipe on home page for SSR-compatible stats fetch

--- libs/bixarena/home/src/lib/bixarena-home/home.component.spec.ts
+++ libs/bixarena/home/src/lib/bixarena-home/home.component.spec.ts
@@ -1,3 +1,4 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
 
@@ -8,6 +9,7 @@ describe('HomeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HomeComponent],
+      providers: [provideHttpClient()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
--- libs/bixarena/home/src/lib/bixarena-home/home.component.ts
+++ libs/bixarena/home/src/lib/bixarena-home/home.component.ts
@@ -1,21 +1,13 @@
-import { isPlatformServer } from '@angular/common';
-import { Component, inject, PLATFORM_ID, signal } from '@angular/core';
-import { PublicStats, StatsService } from '@sagebionetworks/bixarena/api-client';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { StatsService } from '@sagebionetworks/bixarena/api-client';
 
 @Component({
   selector: 'bixarena-home',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  readonly isPlatformServer = isPlatformServer(inject(PLATFORM_ID));
-  readonly stats = signal<PublicStats | null>(null);
-
-  constructor() {
-    if (this.isPlatformServer) return;
-    inject(StatsService)
-      .getPublicStats()
-      .subscribe((s) => this.stats.set(s));
-  }
+  readonly stats$ = inject(StatsService).getPublicStats();
 }
