80dcb5591f0dd6eba02580d1d3a47b7681b23f39
rrchai <73901500+rrchai@users.noreply.github.com>
2026-03-29 03:38:17 +0000

    feat(bixarena): generate feature lib stubs and wire routes

---
+++ libs/bixarena/home/src/lib/bixarena-home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bixarena-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {}
---
+++ libs/bixarena/leaderboard/angular/src/lib/leaderboard.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { LeaderboardComponent } from './bixarena-leaderboard-angular/leaderboard.component';
+
+export const routes: Routes = [{ path: '', component: LeaderboardComponent }];
