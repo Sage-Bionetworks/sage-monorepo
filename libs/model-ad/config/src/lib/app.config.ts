c926b64ebebe9f160d02ac40c60005a6b52f8ff0
rrchai <73901500+rrchai@users.noreply.github.com>
2026-03-30 18:29:47 +0000

    feat(model-ad,agora): remove unused APP_PORT token

--- libs/model-ad/config/src/lib/app.config.ts
+++ libs/model-ad/config/src/lib/app.config.ts
@@ -1,11 +1,6 @@
-import { InjectionToken } from '@angular/core';
-
 export {
   AppConfig,
   RuntimeAppConfig,
   RuntimeClientConfig,
   ServerConfig,
 } from '@sagebionetworks/explorers/config';
-
-// TODO: remove when app.config.server.ts is updated to use CONFIG_BASE_PATH
-export const APP_PORT = new InjectionToken<string>('APP_PORT');
