c40829911987ccf34898a396e65d8790e3e70bd3
Rongrong Chai <73901500+rrchai@users.noreply.github.com>
2026-04-07 12:37:51 -0400

    feat(model-ad,agora): migrate to the new Angular app config library (MG-644) (#3959)

--- apps/model-ad/app/src/main.ts
+++ apps/model-ad/app/src/main.ts
@@ -1,21 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppConfig } from '@sagebionetworks/model-ad/config';
-import { initSentry } from '@sagebionetworks/explorers/sentry';
-import { prefetchConfig } from '@sagebionetworks/explorers/util';
-
+import { getSentryEnvironment } from '@sagebionetworks/explorers/sentry';
+import * as Sentry from '@sentry/angular';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-prefetchConfig<AppConfig>().then((config) => {
-  initSentry({
-    dsn: 'https://bbf0d51ab53013fe73d95348a6bffe61@o4510881207418880.ingest.us.sentry.io/4510896864559104',
-    hostEnvironmentMap: {
-      'dev.modeladexplorer.org': 'dev',
-      'stage.modeladexplorer.org': 'stage',
-      'modeladexplorer.org': 'prod',
-    },
-    release: config?.sentryRelease,
-  });
-
-  bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+Sentry.init({
+  dsn: 'https://bbf0d51ab53013fe73d95348a6bffe61@o4510881207418880.ingest.us.sentry.io/4510896864559104',
+  environment: getSentryEnvironment({
+    'dev.modeladexplorer.org': 'dev',
+    'stage.modeladexplorer.org': 'stage',
+    'modeladexplorer.org': 'prod',
+  }),
 });
+
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
