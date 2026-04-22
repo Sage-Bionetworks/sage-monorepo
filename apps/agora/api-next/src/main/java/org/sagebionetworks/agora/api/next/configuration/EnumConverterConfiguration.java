f2a80e3a45b9cd9931b911bc8d04b50736983954
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2026-04-22 20:24:59 +0000

    fix(agora): regenerate all clients (AG-1972)

--- apps/agora/api-next/src/main/java/org/sagebionetworks/agora/api/next/configuration/EnumConverterConfiguration.java
+++ apps/agora/api-next/src/main/java/org/sagebionetworks/agora/api/next/configuration/EnumConverterConfiguration.java
@@ -2,36 +2,41 @@ package org.sagebionetworks.agora.api.next.configuration;
 
 import org.sagebionetworks.agora.api.next.model.dto.ComparisonToolConfigPageDto;
 import org.sagebionetworks.agora.api.next.model.dto.ItemFilterTypeQueryDto;
+import org.sagebionetworks.agora.api.next.model.dto.ModalityDto;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 
-@Configuration(
-  value = "org.sagebionetworks.agora.api.next.configuration.enumConverterConfiguration"
-)
+@Configuration(value = "org.sagebionetworks.agora.api.next.configuration.enumConverterConfiguration")
 public class EnumConverterConfiguration {
 
-  @Bean(
-    name = "org.sagebionetworks.agora.api.next.configuration.EnumConverterConfiguration.comparisonToolConfigPageConverter"
-  )
-  Converter<String, ComparisonToolConfigPageDto> comparisonToolConfigPageConverter() {
-    return new Converter<String, ComparisonToolConfigPageDto>() {
-      @Override
-      public ComparisonToolConfigPageDto convert(String source) {
-        return ComparisonToolConfigPageDto.fromValue(source);
-      }
-    };
-  }
+    @Bean(name = "org.sagebionetworks.agora.api.next.configuration.EnumConverterConfiguration.comparisonToolConfigPageConverter")
+    Converter<String, ComparisonToolConfigPageDto> comparisonToolConfigPageConverter() {
+        return new Converter<String, ComparisonToolConfigPageDto>() {
+            @Override
+            public ComparisonToolConfigPageDto convert(String source) {
+                return ComparisonToolConfigPageDto.fromValue(source);
+            }
+        };
+    }
+    @Bean(name = "org.sagebionetworks.agora.api.next.configuration.EnumConverterConfiguration.itemFilterTypeQueryConverter")
+    Converter<String, ItemFilterTypeQueryDto> itemFilterTypeQueryConverter() {
+        return new Converter<String, ItemFilterTypeQueryDto>() {
+            @Override
+            public ItemFilterTypeQueryDto convert(String source) {
+                return ItemFilterTypeQueryDto.fromValue(source);
+            }
+        };
+    }
+    @Bean(name = "org.sagebionetworks.agora.api.next.configuration.EnumConverterConfiguration.modalityConverter")
+    Converter<String, ModalityDto> modalityConverter() {
+        return new Converter<String, ModalityDto>() {
+            @Override
+            public ModalityDto convert(String source) {
+                return ModalityDto.fromValue(source);
+            }
+        };
+    }
 
-  @Bean(
-    name = "org.sagebionetworks.agora.api.next.configuration.EnumConverterConfiguration.itemFilterTypeQueryConverter"
-  )
-  Converter<String, ItemFilterTypeQueryDto> itemFilterTypeQueryConverter() {
-    return new Converter<String, ItemFilterTypeQueryDto>() {
-      @Override
-      public ItemFilterTypeQueryDto convert(String source) {
-        return ItemFilterTypeQueryDto.fromValue(source);
-      }
-    };
-  }
 }
