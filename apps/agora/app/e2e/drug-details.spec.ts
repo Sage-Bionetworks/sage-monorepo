9796305c1cf0639e23d62d43405e05e820094fcb
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2026-04-23 16:43:54 +0000

    feat(agora): update drug details e2e tests and add placeholder content for all tabs (AG-2049)

--- apps/agora/app/e2e/drug-details.spec.ts
+++ apps/agora/app/e2e/drug-details.spec.ts
@@ -1,16 +1,20 @@
 import { expect, test } from '@playwright/test';
 
+const chemblId = 'CHEMBL2105759';
+const drugBankId = 'DB11817';
+const drugName = 'Baricitinib';
+
 test.describe('drug details', () => {
-  const drugPath = '/drugs/CHEMBL2105759';
+  const drugPath = `/drugs/${chemblId}`;
 
   test('valid drug displays drug name', async ({ page }) => {
     await page.goto(drugPath);
-    await expect(page.getByRole('heading', { level: 1, name: 'Baricitinib' })).toBeVisible();
+    await expect(page.getByRole('heading', { level: 1, name: drugName })).toBeVisible();
   });
 
-  test('default tab is resources', async ({ page }) => {
+  test('default tab is summary', async ({ page }) => {
     await page.goto(drugPath);
-    await expect(page.getByRole('heading', { level: 2, name: 'Related Resources' })).toBeVisible();
+    await expect(page.getByRole('heading', { level: 2, name: 'Summary' })).toBeVisible();
   });
 
   test('correct tab is loaded from url', async ({ page }) => {
@@ -21,54 +25,72 @@ test.describe('drug details', () => {
   test('invalid tab in url defaults to first available tab', async ({ page }) => {
     await page.goto(`${drugPath}/does-not-exist`);
     await page.waitForURL(drugPath);
-    await expect(page.getByRole('heading', { level: 2, name: 'Related Resources' })).toBeVisible();
+    await expect(page.getByRole('heading', { level: 2, name: 'Summary' })).toBeVisible();
   });
 });
 
 test.describe('drug details - resources', () => {
-  const resourcesUrl = '/drugs/CHEMBL2105759/resources';
+  const resourcesUrl = `/drugs/${chemblId}/resources`;
+
+  test('AlzGPS card links to correct page', async ({ page }) => {
+    await page.goto(resourcesUrl);
+    const card = page.getByRole('link', {
+      name: /alzgps/i,
+    });
+    await expect(card).toHaveAttribute(
+      'href',
+      `https://alzgps.lerner.ccf.org/e/?action=true&type=DRUG&key=${drugBankId}`,
+    );
+  });
+
+  test('AlzPED card links to correct page', async ({ page }) => {
+    await page.goto(resourcesUrl);
+    const card = page.getByRole('link', {
+      name: /ad therapeutics/i,
+    });
+    await expect(card).toHaveAttribute('href', 'https://alzped.nia.nih.gov/');
+  });
+
+  test('FDA card links to correct page', async ({ page }) => {
+    await page.goto(resourcesUrl);
+    const card = page.getByRole('link', {
+      name: /fda/i,
+    });
+    await expect(card).toHaveAttribute('href', 'https://www.fda.gov/');
+  });
 
   test('Open Targets card links to correct page', async ({ page }) => {
     await page.goto(resourcesUrl);
     const card = page.getByRole('link', {
-      name: /open targets|TODO/i,
+      name: /open targets/i,
     });
-    await expect(card).toHaveAttribute(
-      'href',
-      'https://platform.opentargets.org/drug/CHEMBL2105759',
-    );
+    await expect(card).toHaveAttribute('href', `https://platform.opentargets.org/drug/${chemblId}`);
+  });
+
+  test('PubMed card links to correct page', async ({ page }) => {
+    await page.goto(resourcesUrl);
+    const card = page.getByRole('link', {
+      name: /pubmed/i,
+    });
+    await expect(card).toHaveAttribute('href', `https://pubmed.ncbi.nlm.nih.gov/?term=${drugName}`);
   });
 
   test('Pharos card links to correct page', async ({ page }) => {
     await page.goto(resourcesUrl);
-    const cards = page.getByRole('link');
-    const pharosCard = cards.filter({
-      has: page.locator('[href*="pharos.nih.gov"]'),
+    const card = page.getByRole('link', {
+      name: /druggable genome/i,
     });
-    await expect(pharosCard).toHaveAttribute('href', 'https://pharos.nih.gov/ligands/Baricitinib');
-  });
-
-  test('AlzGPS card links to correct page', async ({ page }) => {
-    await page.goto(resourcesUrl);
-    const cards = page.getByRole('link');
-    const alzgpsCard = cards.filter({
-      has: page.locator('[href*="alzgps.lerner.ccf.org"]'),
-    });
-    await expect(alzgpsCard).toHaveAttribute(
-      'href',
-      'https://alzgps.lerner.ccf.org/e/?action=true&type=DRUG&key=DB11817',
-    );
+    await expect(card).toHaveAttribute('href', `https://pharos.nih.gov/ligands/${drugName}`);
   });
 
   test('TACA card links to correct page', async ({ page }) => {
     await page.goto(resourcesUrl);
-    const cards = page.getByRole('link');
-    const tacaCard = cards.filter({
-      has: page.locator('[href*="taca.lerner.ccf.org"]'),
+    const card = page.getByRole('link', {
+      name: /taca/i,
     });
-    await expect(tacaCard).toHaveAttribute(
+    await expect(card).toHaveAttribute(
       'href',
-      'https://taca.lerner.ccf.org/e/?action=true&type=DRUG&key=DB11817',
+      `https://taca.lerner.ccf.org/e/?action=true&type=DRUG&key=${drugBankId}`,
     );
   });
 });
