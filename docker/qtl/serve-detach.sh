d383a2be67b0ed7b7614d3f04f80ee770ecd91c4
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2026-04-28 22:39:32 +0000

    feat(qtl): add docker stack (QTL-56)

---
+++ docker/qtl/serve-detach.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+product_name="qtl"
+
+args=(
+  # List of services in alphanumeric order
+  --file docker/"$product_name"/services/apex.yml
+  --file docker/"$product_name"/services/api-docs.yml
+  --file docker/"$product_name"/services/api.yml
+  --file docker/"$product_name"/services/app.yml
+  --file docker/"$product_name"/services/data.yml
+  --file docker/"$product_name"/services/mongo.yml
+
+  --file docker/"$product_name"/networks.yml
+  --file docker/"$product_name"/volumes.yml
+
+  --project-name "$product_name"
+
+  up $1 --detach
+)
+
+docker compose "${args[@]}"
