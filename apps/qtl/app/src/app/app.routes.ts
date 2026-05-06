19dc2477148225238cc1a1a0a67fe315ed127f74
hallieswan <26949006+hallieswan@users.noreply.github.com>
2026-05-06 09:28:41 -0700

    feat(qtl): add static pages and footer (QTL-21, QTL-22, QTL-23, QTL-24, QTL-25, QTL-27, QTL-28, QTL-29) (#4045)

--- apps/qtl/app/src/app/app.routes.ts
+++ apps/qtl/app/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Route } from '@angular/router';
-import { ROUTE_PATHS } from '@sagebionetworks/qtl/config';
+import { SynapseWikiParams } from '@sagebionetworks/explorers/models';
+import { ROUTE_PATHS, SUPPORT_EMAIL } from '@sagebionetworks/qtl/config';
 
 export const routes: Route[] = [
   {
@@ -9,5 +10,70 @@ export const routes: Route[] = [
       title: 'xQTL Explorer',
       description: 'home page',
     },
+  },
+  {
+    path: ROUTE_PATHS.ABOUT,
+    loadChildren: () =>
+      import('@sagebionetworks/explorers/shared').then((routes) => routes.wikiHeroRoute),
+    data: {
+      title: 'About the xQTL Explorer',
+      description:
+        'The xQTL Explorer is funded by the National Institute on Aging, and is developed and maintained by Sage Bionetworks.',
+      heroTitle: 'About',
+      wikiParams: {
+        wikiId: '640479',
+        ownerId: 'syn74592415',
+      } as SynapseWikiParams,
+    },
+  },
+  {
+    path: ROUTE_PATHS.NEWS,
+    loadChildren: () =>
+      import('@sagebionetworks/explorers/shared').then((routes) => routes.wikiHeroRoute),
+    data: {
+      title: 'News | xQTL Explorer Releases',
+      description:
+        "See what's new in the xQTL Explorer, from new features to our latest data updates.",
+      heroTitle: 'News',
+      wikiParams: {
+        wikiId: '640478',
+        ownerId: 'syn74592415',
+      } as SynapseWikiParams,
+    },
+  },
+  {
+    path: ROUTE_PATHS.TERMS_OF_SERVICE,
+    loadChildren: () =>
+      import('@sagebionetworks/explorers/shared').then((routes) => routes.termsOfServiceRoute),
+    data: {
+      title: 'xQTL Explorer | Terms of Service',
+      description:
+        'The xQTL Explorer is powered by Synapse, a platform for supporting scientific collaborations centered around shared biomedical data sets. Our goal is to make biomedical research more transparent, more reproducible, and more accessible to a broader audience of scientists.',
+    },
+  },
+  {
+    path: ROUTE_PATHS.NOT_FOUND,
+    loadChildren: () =>
+      import('@sagebionetworks/explorers/shared').then((routes) => routes.notFoundRoute),
+    data: {
+      title: 'xQTL Explorer | Page Not Found',
+      description: 'The link you followed may be broken, or the page may have been removed.',
+      robots: 'noindex, follow',
+      supportEmail: SUPPORT_EMAIL,
+    },
+  },
+  {
+    path: ROUTE_PATHS.ERROR,
+    loadChildren: () =>
+      import('@sagebionetworks/explorers/shared').then((routes) => routes.errorPageRoute),
+    data: {
+      title: 'xQTL Explorer | Error',
+      description: 'Error Page',
+      supportEmail: SUPPORT_EMAIL,
+    },
+  },
+  {
+    path: '**',
+    redirectTo: `/${ROUTE_PATHS.NOT_FOUND}`,
   },
 ];
