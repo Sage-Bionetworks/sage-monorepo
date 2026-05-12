1c00c334d9e886612487379cd726b8af68aaed9f
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2026-05-12 19:24:02 +0000

    feat(qtl): add impact tab legend (QTL-41)

---
+++ libs/qtl/eqtl-comparison-tool/src/lib/components/impact/legend/impact-legend.component.ts
@@ -0,0 +1,35 @@
+import { Component, inject, model } from '@angular/core';
+import { SimpleTableCell, SimpleTableColumn } from '@sagebionetworks/explorers/models';
+import { PlatformService } from '@sagebionetworks/explorers/services';
+import { SimpleTableComponent } from '@sagebionetworks/explorers/ui';
+import { CELL_CLASSES } from '@sagebionetworks/qtl/config';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+
+@Component({
+  selector: 'qtl-eqtl-comparison-tool-impact-legend',
+  imports: [ButtonModule, DialogModule, SimpleTableComponent],
+  templateUrl: './impact-legend.component.html',
+  styleUrls: ['./impact-legend.component.scss'],
+})
+export class ImpactLegendComponent {
+  platformService = inject(PlatformService);
+
+  visible = model<boolean>(false);
+
+  protected readonly columns: SimpleTableColumn[] = [
+    { name: 'Abbreviation', width: '30%' },
+    { name: 'Description', width: '70%' },
+  ];
+
+  protected readonly rows: SimpleTableCell[][] = Object.entries(CELL_CLASSES).map(
+    ([longName, { color, short }]) => [
+      { type: 'swatch' as const, color, text: short },
+      { type: 'text' as const, value: longName, italic: true },
+    ],
+  );
+
+  protected openInNewTab(): void {
+    // TODO: navigate to the standalone legend page once it exists (QTL-42)
+  }
+}
