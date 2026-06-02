8b8e29404588dd66bd73ec178bd3a9ffd592948b
sagely1 <114952739+sagely1@users.noreply.github.com>
2026-06-02 00:57:48 +0000

    add header to home page

--- libs/qtl/home/src/lib/home.component.ts
+++ libs/qtl/home/src/lib/home.component.ts
@@ -1,13 +1,31 @@
 import { Component, input } from '@angular/core';
-import { StatCardData } from '@sagebionetworks/explorers/models';
-import { LinkBarComponent, StatCardsComponent } from '@sagebionetworks/explorers/ui';
+import { Chiclet, StatCardData } from '@sagebionetworks/explorers/models';
+import {
+  ChicletCardComponent,
+  LinkBarComponent,
+  StatCardsComponent,
+} from '@sagebionetworks/explorers/ui';
 
 @Component({
   selector: 'qtl-home',
-  imports: [LinkBarComponent, StatCardsComponent],
+  imports: [LinkBarComponent, ChicletCardComponent, StatCardsComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  chiclets: Chiclet[] = [
+    { text: 'PAK1', backgroundColor: '#9D67D6' },
+    { text: 'chr1:109.8Mb', backgroundColor: '#3f51b5' },
+    { text: 'rs1801133', backgroundColor: '#1AA582' },
+    { text: 'rs6265', backgroundColor: '#1AA582' },
+    { text: 'APOE', backgroundColor: '#9D67D6' },
+    { text: 'chrX:73.5Mb', backgroundColor: '#1AA582' },
+    { text: 'GATA3, FADS1, SLC30A8', backgroundColor: '#9D67D6' },
+    { text: 'chr2:164.3Mb', backgroundColor: '#3f51b5' },
+    { text: 'chr2:164.3Mb', backgroundColor: '#3f51b5' },
+    { text: 'rs38090111, rs48102218', backgroundColor: '#1AA582' },
+    { text: 'DRD2', backgroundColor: '#9D67D6' },
+  ];
+
   statCards = input<StatCardData[]>([]);
 }
