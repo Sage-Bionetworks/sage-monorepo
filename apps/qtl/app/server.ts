6d6a71a2164ac880e5e41547e92fe2335ec0f36c
rrchai <73901500+rrchai@users.noreply.github.com>
2026-06-02 20:01:35 +0000

    fix(qtl): add deployed domains to allowedHosts (AG-2113)

--- apps/qtl/app/server.ts
+++ apps/qtl/app/server.ts
@@ -13,7 +13,7 @@ export function app(): express.Express {
   const indexHtml = join(serverDistFolder, 'index.server.html');
 
   const commonEngine = new CommonEngine({
-    allowedHosts: ['localhost'],
+    allowedHosts: ['localhost', '*.adknowledgeportal.org'],
   });
 
   server.set('view engine', 'html');
