5145a5252173b9c5f76bc065c3949caff70aea35
Ubuntu <26949006+hallieswan@users.noreply.github.com>
2026-06-02 20:32:43 +0000

    feat(agora): redesign teams page with card-based layout (AG-2111)

--- libs/agora/teams/src/lib/team-list/team-list.routes.ts
+++
@@ -1,4 +0,0 @@
-import { Routes } from '@angular/router';
-import { TeamListComponent } from './team-list.component';
-
-export const teamListRoutes: Routes = [{ path: '', component: TeamListComponent }];
