0a89613efea83f85815e920f7065bb4d71d69e1c
rrchai <73901500+rrchai@users.noreply.github.com>
2026-08-21 06:19:39 +0000

    fix(model-ad): point SSR API URL to api-next

--- libs/model-ad/testing/src/lib/mocks/config-mocks.ts
+++ libs/model-ad/testing/src/lib/mocks/config-mocks.ts
@@ -4,7 +4,7 @@ export const configMock: RuntimeServerConfig = {
   appVersion: 'local',
   commitSha: '',
   csrApiUrl: 'http://localhost:4200/v1',
-  ssrApiUrl: 'http://model-ad-api:3333/v1',
+  ssrApiUrl: 'http://model-ad-api-next:3334/v1',
   apiDocsUrl: 'http://localhost:8000/api-docs',
   googleTagManagerEnabled: false,
   googleTagManagerId: '',
