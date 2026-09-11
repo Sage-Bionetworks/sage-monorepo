aab1d21cdd2505d548760aa0377140909f37ee16
sagely1 <114952739+sagely1@users.noreply.github.com>
2026-09-11 17:27:40 +0000

    MG-1098: add p-value const

--- libs/explorers/comparison-tool/src/lib/comparison-tool.variables.ts
+++ libs/explorers/comparison-tool/src/lib/comparison-tool.variables.ts
@@ -2,3 +2,5 @@ export const knownColorMetricToDisplayName = [
   { field: 'log2_fc', displayName: 'Log2 Fold Change' },
   { field: 'correlation', displayName: 'Correlation' },
 ];
+
+export const ADJUSTED_P_VALUE_LABEL = 'Adjusted p-value';
